2479ad4f1b6d44cb1b9ed7c267f8d5d1ffb3803b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:34 +0000

    Add package jz-asf-demo/0.1/jz_asf_demo-0.1.tar.gz

---
+++ packages/jz-asf-demo/jz_asf_demo-0.1.tar.gz/jz_asf_demo-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name='jz_asf_demo',
+	version='0.1',
+	description='A sample Python package',
+	author='Jiang Zhu',
+	author_email='jzhu4@alaska.edu.com',
+	packages=['jz_asf_demo']
+)
+
