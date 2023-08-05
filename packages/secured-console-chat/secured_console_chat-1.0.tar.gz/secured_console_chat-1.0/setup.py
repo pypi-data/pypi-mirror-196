33e24ea0c6c95d15fb0fea37b6e975e10b681bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:41 +0000

    Add package secured-console-chat/1.0/secured_console_chat-1.0.tar.gz

---
+++ packages/secured-console-chat/secured_console_chat-1.0.tar.gz/secured_console_chat-1.0/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+  
+with open("README.md", "r", encoding="utf-8") as fh:
+    description = fh.read()
+  
+setuptools.setup(
+    name="secured_console_chat",
+    version="1.0",
+    author="dinosaurtirex",
+    author_email="sneakybeaky18@gmail.com",
+    packages=[
+        "cmd_chat", 
+        "cmd_chat/client",
+        "cmd_chat/client/core",
+        "cmd_chat/client/core/abs",
+        "cmd_chat/server",
+    ],
+    description="Secured console chat with RSA & Fernet",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dinosaurtirex/cmd-chat",
+    license='MIT',
+    python_requires='>=3.10',
+    install_requires=[
+        "sanic",
+        "requests",
+        "rsa",
+        "cryptography",
+        "colorama",
+        "pydantic",
+        "websocket-client"
+    ]
+)
