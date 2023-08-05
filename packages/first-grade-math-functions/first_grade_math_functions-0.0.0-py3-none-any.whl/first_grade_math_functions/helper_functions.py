ec6cd439d499adb0a09423a1518f408acf2e4327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:22 +0000

    Add package first-grade-math-functions/0.0.0/first_grade_math_functions-0.0.0-py3-none-any.whl

---
+++ packages/first-grade-math-functions/first_grade_math_functions-0.0.0-py3-none-any.whl/first_grade_math_functions/helper_functions.py
@@ -0,0 +1,14 @@
+def sum_two_integers(a, b):
+    return a + b
+
+
+def subtract_two_integers(a, b):
+    return a - b
+
+
+def multiply_two_integers(a, b):
+    return a * b
+
+
+def divide_two_integers(a, b):
+    return a / b
