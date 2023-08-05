c82eb9a9299f715d456d9b37f09be4c2e321811a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:42 +0000

    Add package ha-mqtt-discoverable/0.7.0/ha_mqtt_discoverable-0.7.0-py3-none-any.whl

---
+++ packages/ha-mqtt-discoverable/ha_mqtt_discoverable-0.7.0-py3-none-any.whl/ha_mqtt_discoverable/utils.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+#
+# Copyright 2022 Joe Block <jpb@unixorn.net>
+# License: Apache 2.0
+
+import re
+
+import yaml
+from ha_mqtt_discoverable import CONFIGURATION_KEY_NAMES
+
+
+def clean_string(raw: str) -> str:
+    """
+    MQTT Discovery protocol only allows [a-zA-Z0-9_-]
+    """
+    result = re.sub(r"[^A-Za-z0-9_-]", "-", raw)
+    return result
+
+
+def read_yaml_file(path: str = None) -> dict:
+    """
+    Return the data structure contained in a yaml file
+
+    Args:
+        path (str): Path to read from
+
+    Returns:
+        Data decoded from YAML file content
+    """
+    with open(path) as yamlFile:
+        data = yaml.safe_load(yamlFile)
+        return data
+
+
+def valid_configuration_key(name: str) -> bool:
+    """
+    Confirm that a configuration key is in the allowed list
+    """
+    return name in CONFIGURATION_KEY_NAMES
