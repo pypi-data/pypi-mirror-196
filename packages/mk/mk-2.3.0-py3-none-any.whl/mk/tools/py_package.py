c98623a2fe5939000cf829a2e518f87d57413d92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:38 +0000

    Add package mk/2.3.0/mk-2.3.0-py3-none-any.whl

---
+++ packages/mk/mk-2.3.0-py3-none-any.whl/mk/tools/py_package.py
@@ -0,0 +1,62 @@
+import os
+import sys
+from typing import List, Optional
+
+from mk.exec import run_or_fail
+from mk.tools import Action, Tool
+
+
+class PyPackageTool(Tool):
+    """Expose build, install and uninstall commands for python packages."""
+
+    name = "pip"
+
+    def __init__(self) -> None:
+        super().__init__(self)
+
+    def run(self, action: Optional[Action] = None) -> None:
+        if not action:
+            return
+        if action.name == "build":
+            cmd = [
+                sys.executable,
+                "-m",
+                "build",
+                "--sdist",
+                "--wheel",
+                "--outdir",
+                "dist",
+            ]
+            run_or_fail(cmd, tee=True)
+            run_or_fail(f"{sys.executable} -m twine check dist/*", tee=True)
+        elif action.name == "install":
+            cmd = [sys.executable, "-m", "pip", action.name, "-e", "."]
+            run_or_fail(cmd, tee=True)
+        elif action.name == "uninstall":
+            pkg_name = run_or_fail(
+                [sys.executable, "setup.py", "--name"], tee=False
+            ).stdout
+            cmd = [sys.executable, "-m", "pip", action.name, "-y", pkg_name]
+            run_or_fail(cmd, tee=True)
+
+    def is_present(self, path: str) -> bool:
+        for name in ("setup.cfg", "setup.py", "pyproject.toml"):
+            if os.path.isfile(name):
+                return True
+        return False
+
+    def actions(self) -> List[Action]:
+        actions = []
+
+        for name in ("install", "uninstall"):
+            description = f"Use pip to {name} current package"
+            actions.append(Action(name=name, tool=self, description=description))
+        actions.append(
+            Action(
+                name="build",
+                tool=self,
+                description="Use python build to produce sdist and wheel, followed by twine check",
+            )
+        )
+
+        return actions
---
+++ packages/mk/mk-2.3.0-py3-none-any.whl/mk/tools/shell.py
@@ -0,0 +1,42 @@
+import glob
+import os
+from typing import List, Optional
+
+from mk.exec import run_or_fail
+from mk.tools import Action, Tool
+
+
+class ShellTool(Tool):
+    name = "shell"
+
+    def run(self, action: Optional[Action] = None) -> None:
+        if action and action.filename:
+            run_or_fail(action.filename, tee=True)
+
+    def is_present(self, path: str) -> bool:
+        return True
+
+    def actions(self) -> List[Action]:
+        actions: List[Action] = []
+        exclude_list = ["setup.py"]
+        for filename in [
+            *glob.glob("*"),
+            *glob.glob("tools/*.*"),
+            *glob.glob("bin/*.*"),
+            *glob.glob("scripts/*.*"),
+        ]:
+            if (
+                os.path.isfile(filename)
+                and os.access(filename, os.X_OK)
+                and filename not in exclude_list
+            ):
+                name = os.path.splitext(os.path.basename(filename))[0]
+                actions.append(
+                    Action(
+                        name=name,
+                        description=f"[dim]./{filename}[/dim]",
+                        tool=self,
+                        filename=filename,
+                    )
+                )
+        return actions
