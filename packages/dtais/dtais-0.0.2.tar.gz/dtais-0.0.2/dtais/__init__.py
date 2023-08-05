f670bb54c59bfd3bb6c7a6967aa4a1f6848f362b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:11 +0000

    Add package dtais/0.0.2/dtais-0.0.2.tar.gz

---
+++ packages/dtais/dtais-0.0.2.tar.gz/dtais-0.0.2/dtais/__init__.py
@@ -0,0 +1,7 @@
+import os
+
+if not os.path.exists("C://dtais"):
+    os.mkdir("C://dtais")
+
+if not os.path.exists("C://dtais/data"):
+    os.mkdir("C://dtais/data")
---
+++ packages/dtais/dtais-0.0.2.tar.gz/dtais-0.0.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="dtais",
+    version="0.0.2",
+    author="Lazarrt",
+    author_email="",
+    description="a test version of dtais",
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
