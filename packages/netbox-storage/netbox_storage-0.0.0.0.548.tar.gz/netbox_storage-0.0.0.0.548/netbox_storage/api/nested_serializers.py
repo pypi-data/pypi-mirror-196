b0e2172060ea7e17fc8b7b4968ae5e2873c7b5b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:22 +0000

    Add package netbox-storage/0.0.0.0.548/netbox_storage-0.0.0.0.548.tar.gz

---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.548.tar.gz/netbox_storage-0.0.0.0.548/netbox_storage/api/nested_serializers.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers
+
+from netbox.api.serializers import WritableNestedSerializer
+from netbox_storage.models import Filesystem, Drive, Partition, MountedVolume
+
+
+#
+# Filesystem
+#
+
+class NestedFilesystemSerializer(WritableNestedSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name="plugins-api:netbox_storage-api:filesystem-detail"
+    )
+
+    class Meta:
+        model = Filesystem
+        fields = ["id", "url", "display", "filesystem"]
+
+
+#
+# Drive
+#
+class NestedDriveSerializer(WritableNestedSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name="plugins-api:netbox_storage-api:drive-detail"
+    )
+
+    class Meta:
+        model = Drive
+        fields = ["id", "url", "display", "size", "identifier"]
+
+
+#
+# Partition
+#
+class NestedPartitionSerializer(WritableNestedSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name="plugins-api:netbox_storage-api:partition-detail"
+    )
+
+    class Meta:
+        model = Partition
+        fields = ["id", "url", "display", "size", "device", "type"]
+
+
+#
+# Linux Volume
+#
+class NestedMountedVolumeSerializer(WritableNestedSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name="plugins-api:netbox_storage-api:mountedvolume-detail"
+    )
+
+    class Meta:
+        model = MountedVolume
+        fields = ["id", "url", "display", "size", "path"]
---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.548.tar.gz/netbox_storage-0.0.0.0.548/netbox_storage/template_content.py
@@ -0,0 +1,65 @@
+import logging
+
+from extras.plugins import PluginTemplateExtension
+from netbox_storage.models import PhysicalVolume, LogicalVolume, MountedVolume, StorageConfigurationLinuxVolume, \
+    TemplateConfigurationLinuxVolume
+
+
+class RelatedDrives(PluginTemplateExtension):
+    model = "virtualization.virtualmachine"
+
+    def left_page(self):
+        obj = self.context.get("object")
+
+        lv = LogicalVolume.objects.all()
+
+        storage_configuration = StorageConfigurationLinuxVolume.objects.filter(virtual_machine=obj)
+
+        platform = obj.platform
+        if platform is not None:
+            if platform.name.lower().__contains__('windows'):
+                return self.render(
+                    "netbox_storage/inc/windowsvolume_box.html",
+                    extra_context={
+                        # "volumes": volumes,
+                        # "unmapped_drives": unmapped_drives,
+                        "type": type(obj.platform),
+                    },
+                )
+            elif platform.name.lower().__contains__('linux'):
+                return self.render(
+                    "netbox_storage/inc/linuxvolume_box.html"
+                )
+        else:
+            return self.render(
+                "netbox_storage/inc/unknown_os_box.html",
+                extra_context={
+                    "lv": lv,
+                    "storage_configuration": storage_configuration
+                }
+            )
+
+
+class TemplateVolumeConfig(PluginTemplateExtension):
+    model = "dcim.platform"
+
+    def right_page(self):
+        obj = self.context.get("object")
+
+        lv = LogicalVolume.objects.all()
+        template_linux_volumes = TemplateConfigurationLinuxVolume.objects.filter(platform=obj)
+        for tlv in template_linux_volumes:
+            tlv.linux_volume.device.drive
+
+
+        # storage_configuration = StorageConfigurationLinuxVolume.objects.filter(virtual_machine=obj)
+
+        return self.render(
+            "netbox_storage/inc/template_box.html",
+            extra_context={
+                "template_linux_volumes": template_linux_volumes,
+            }
+        )
+
+
+template_extensions = [RelatedDrives, TemplateVolumeConfig]
