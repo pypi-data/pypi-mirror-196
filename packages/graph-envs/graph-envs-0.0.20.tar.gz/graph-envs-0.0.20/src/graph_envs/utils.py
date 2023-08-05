7fac5d1cf5a78dd87bddd1105af3ca242d073dbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:05 +0000

    Add package graph-envs/0.0.20/graph-envs-0.0.20.tar.gz

---
+++ packages/graph-envs/graph-envs-0.0.20.tar.gz/graph-envs-0.0.20/src/graph_envs/utils.py
@@ -0,0 +1,31 @@
+import numpy as np 
+import gymnasium as gym 
+import torch 
+
+
+
+def devectorize_graph(vector, env_id, **kwargs):
+    bs = vector.shape[0]
+    
+    if env_id == "ShortestPath-v0":
+        x = vector[:, :2*kwargs['n_nodes']].reshape(bs, kwargs['n_nodes'], 2)
+        edge_features = vector[:, 2*kwargs['n_nodes']:2*kwargs['n_nodes'] + 2*kwargs['n_edges']].reshape(bs, 2*kwargs['n_edges'], 1)
+        edge_index = vector[:, 2*kwargs['n_nodes'] + 2*kwargs['n_edges']:].reshape(bs, 2*kwargs['n_edges'], 2).long()
+    elif env_id == "SteinerTree-v0":
+        x = vector[:, :2*kwargs['n_nodes']].reshape(bs, kwargs['n_nodes'], 2)
+        edge_features = vector[:, 2*kwargs['n_nodes']: 2*kwargs['n_nodes']+2*kwargs['n_edges']*2].reshape(bs, 2*kwargs['n_edges'], 2)
+        edge_index = vector[:, 2*kwargs['n_nodes'] + 2*kwargs['n_edges']*2:].reshape(bs, 2*kwargs['n_edges'], 2).long()
+    elif env_id == "MaxIndependentSet-v0":
+        x = vector[:, :2*kwargs['n_nodes']].reshape(bs, kwargs['n_nodes'], 2)
+        edge_features = vector[:, 2*kwargs['n_nodes']: 2*kwargs['n_nodes']+2*kwargs['n_edges']].reshape(bs, 2*kwargs['n_edges'], 1)
+        edge_index = vector[:, 2*kwargs['n_nodes'] + 2*kwargs['n_edges']:].reshape(bs, 2*kwargs['n_edges'], 2).long()
+        
+    return x, edge_features, edge_index
+    
+
+def to_pyg_graph(x, edge_features, edge_index):
+    import torch_geometric as pyg
+    graphs = [pyg.data.Data(x=x[i,:,:], edge_attr=edge_features[i,:,:], edge_index=edge_index[i,:,:].T) for i in range(x.shape[0])]
+    batch = pyg.data.Batch.from_data_list(graphs).to(x.device)
+    return batch
+
