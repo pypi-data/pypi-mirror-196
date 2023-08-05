0307ed562f1a20c3d94580ed7fa5de21ef628fdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:46 +0000

    Add package pyhanja/0.0.3/pyhanja-0.0.3.tar.gz

---
+++ packages/pyhanja/pyhanja-0.0.3.tar.gz/pyhanja-0.0.3/build_wheel.sh
@@ -0,0 +1,17 @@
+# sudo docker run -it --rm -v $(pwd):/src quay.io/pypa/manylinux2014_x86_64 /bin/bash
+
+# CentOS 7 (glibc 2.17) or later
+TARGET=manylinux2014_x86_64
+SRCDIR=/src
+cd $SRCDIR
+rm -rf $SRCDIR/dist
+
+for dir in /opt/python/*/; do
+    rm -rf $SRCDIR/_skbuild $SRCDIR/build $SRCDIR/pyhanja.egg-info
+    $dir/bin/python -m pip install setuptools pybind11 cmake scikit-build setuptools_scm[toml]
+    $dir/bin/python $SRCDIR/setup.py bdist_wheel
+done
+
+for file in $SRCDIR/dist/*; do
+    /usr/local/bin/auditwheel repair --wheel-dir $SRCDIR/dist/manylinux --plat $TARGET $file
+done
