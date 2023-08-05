13caa4f43a1b071a1d45897eb5b5bcdb8c699ac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:56 +0000

    Add package openbk/1.0.1/openbk-1.0.1-py3-none-any.whl

---
+++ packages/openbk/openbk-1.0.1-py3-none-any.whl/openbk/utils/categories.py
@@ -0,0 +1,32 @@
+cih = {
+    "ecom_inter": "PAIEMENT INTERNET INTERNATIONAL",
+    "ecom_national": "PAIEMENT INTERNET NATIONAL",
+    "wire_in_same_bank": "VIREMENT RECU DE",
+    "wire_in_conf": "VIRT RECU",
+    "wire_out": "VIREMENT EMIS EN FAVEUR DE",
+    "withdrawal_gab": "RETRAIT CARTE",
+    "invoice_pay": " PAIEMENT FACTURE",
+    "card_pay": "PAIEMENT PAR CARTE",
+    "wire_fee": "COMMISSION VIREMENT EMIS",
+    "refund_card": "REGLEMENT CREDIT CARTE",
+    "ext_gab_fee": "FRAIS RETRAIT GAB CONFRERE",
+    "allow_ecom_fee": "FRAIS ANNUELS DOTATIONS"
+}
+
+awb = {
+    "wire_in_same_bank": "VIR.WEB RECU DE",
+    "wire_in_conf": "VIREMENT RECU DE",
+    "wire_out": "VIR WEB VERS",
+    "withdrawal_gab": "AWBGAB",
+    "invoice_pay": "PAIEMENT ",
+    "card_pay": "PAIMENT/CB",
+    "ext_gab_fee": "FRAIS/RETRAIT ESP GAB",
+    "ecom_national": "",
+    "ecom_inter": "",
+    "card_pay": ""
+}
+
+banks = {
+    'CIH': cih,
+    'AWB': awb
+}
---
+++ packages/openbk/openbk-1.0.1-py3-none-any.whl/openbk/utils/exceptions.py
@@ -0,0 +1,14 @@
+class FileReadException(Exception):
+    pass
+
+class DataExtractionException(Exception):
+    pass
+
+class NoTransactionsException(Exception):
+    pass
+
+class NoBankDetected(Exception):
+    pass
+
+class MethodNotSupported(Exception):
+    pass
