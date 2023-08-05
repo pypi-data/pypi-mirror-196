21874a27453bdef144a0b249ec060f313958776d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:29 +0000

    Add package ipywebgl/0.1.0/ipywebgl-0.1.0.tar.gz

---
+++ packages/ipywebgl/ipywebgl-0.1.0.tar.gz/ipywebgl-0.1.0/ipywebgl/_version.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Jerome Eippers.
+# Distributed under the terms of the Modified BSD License.
+
+__version__ = "0.1.0"
+version_info = [int(i) for i in __version__.split('.')]
