63cdf25b266cd24901caecf4ef994ac0cf1645a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:48 +0000

    Add package yolov8-detect/1.0/yolov8-detect-1.0.tar.gz

---
+++ packages/yolov8-detect/yolov8-detect-1.0.tar.gz/yolov8-detect-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yolov8-detect',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy'
+    ],
+    author='xingyue',
+    author_email='ywb10.8@qq.com',
+    description='offer the datasets can tran the new model,offer the model and image can infer the object in the image'
+)
