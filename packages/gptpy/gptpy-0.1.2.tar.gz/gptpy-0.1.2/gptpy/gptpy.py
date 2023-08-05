4721832854ed67fe92c04b94debfb12e886725a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:07 +0000

    Add package gptpy/0.1.2/gptpy-0.1.2.tar.gz

---
+++ packages/gptpy/gptpy-0.1.2.tar.gz/gptpy-0.1.2/gptpy/gptpy.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import argparse
+import subprocess
+import traceback
+
+import requests
+
+
+def run_file(file_path, *args):
+    # execute the specified file
+    try:
+        f = open(file_path, "r")
+        txt = f.read()
+        # exec(txt)
+        subprocess.check_call(["python", file_path] + list(args))
+    except Exception as e:
+        exception_log = traceback.format_exc()
+        r = requests.post(
+            "https://api.gptpy.net/api/gptpy/free",
+            json={"python_interpreter_error": exception_log, "user_code": txt},
+        )
+        print("-" * 50)
+        print("[GPTPy] Your code has an error!")
+        print("[GPTPy] Error Reason: ")
+        print(r.json()["explanation"])
+        print("[GPTPy] Here's fixed code:")
+        print(r.json()["suggested_code"])
+        print("-" * 50)
+        # you can also log or handle the exception here
+
+
+def main():  # define command-line arguments
+    parser = argparse.ArgumentParser(
+        description="Run a Python file with a custom wrapper"
+    )
+    parser.add_argument("file", type=str, help="Path to the Python file to run")
+    parser.add_argument(
+        "args", nargs=argparse.REMAINDER, help="Arguments to pass to the Python script"
+    )
+
+    # parse the arguments
+    args = parser.parse_args()
+
+    # run the file with the wrapper
+    run_file(args.file, *args.args)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/gptpy/gptpy-0.1.2.tar.gz/gptpy-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gptpy",
+    version="0.1.2",
+    description="GPTPy: Your kind Python guide, powered by AI to fix errors and explain code",
+    author="Junbum Lee",
+    author_email="junbumlee@icloud.com",
+    url="https://github.com/beomi/gptpy",
+    packages=find_packages(),
+    entry_points={"console_scripts": ["gptpy = gptpy.gptpy:main"]},
+    install_requires=["requests"],
+)
