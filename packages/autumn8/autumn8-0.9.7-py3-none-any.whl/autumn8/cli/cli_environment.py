7974107365838d43f0fa6774000fa0808f140584
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:10 +0000

    Add package autumn8/0.9.7/autumn8-0.9.7-py3-none-any.whl

---
+++ packages/autumn8/autumn8-0.9.7-py3-none-any.whl/autumn8/cli/cli_environment.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+import enum
+from typing import Optional
+
+
+@dataclass
+class EnvironmentProperties:
+    app_host: str
+    s3_host: str
+    s3_bucket_name: str
+    s3_bucket_root_folder: Optional[str]
+
+
+# TODO remove - official CLI builds should only point toward production
+
+
+class CliEnvironment(enum.Enum):
+    value: EnvironmentProperties
+    LOCALHOST = EnvironmentProperties(
+        app_host="http://localhost",
+        s3_host="http://localhost:4566",
+        s3_bucket_name="predictor-bucket",
+        s3_bucket_root_folder=None,
+    )
+    STAGING = EnvironmentProperties(
+        app_host="http://54.83.236.20",
+        s3_host="https://s3-accelerate.amazonaws.com",
+        s3_bucket_name="autodl-staging",
+        s3_bucket_root_folder="autodl-staging",
+    )
+    PRODUCTION = EnvironmentProperties(
+        app_host="https://autodl.autumn8.ai",
+        s3_host="https://s3-accelerate.amazonaws.com",
+        s3_bucket_name="autodl-staging",
+        s3_bucket_root_folder="autodl-production",
+    )
---
+++ packages/autumn8/autumn8-0.9.7-py3-none-any.whl/autumn8/examples/model.py
@@ -0,0 +1,24 @@
+import autumn8
+import numpy as np
+import torch
+from convblock import ConvBlock
+from torch import nn
+
+
+class TestModel(nn.Module):
+    def __init__(self):
+        super(TestModel, self).__init__()
+        self.conv = torch.nn.Conv2d(3, 32, 3, 1)
+        self.block1 = ConvBlock()
+        self.block2 = ConvBlock()
+
+    def forward(self, x):
+        return self.block2(self.block1(self.conv(x)))
+
+
+dummy_input = torch.randn(1, 3, 28, 28)
+# just for testing prediction on server
+np.save("inp.npy", dummy_input.numpy())
+model = TestModel()
+
+autumn8.lib.attach_model(model, dummy_input)
