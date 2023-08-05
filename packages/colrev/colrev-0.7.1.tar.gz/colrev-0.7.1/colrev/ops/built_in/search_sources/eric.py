60a03c8a9673518227b6818adef7e51fc8f3f311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:15 +0000

    Add package colrev/0.7.1/colrev-0.7.1.tar.gz

---
+++ packages/colrev/colrev-0.7.1.tar.gz/colrev-0.7.1/colrev/ops/built_in/search_sources/eric.py
@@ -0,0 +1,103 @@
+#! /usr/bin/env python
+"""SearchSource: ERIC"""
+from __future__ import annotations
+
+import typing
+from dataclasses import dataclass
+from pathlib import Path
+
+import zope.interface
+from dacite import from_dict
+from dataclasses_jsonschema import JsonSchemaMixin
+
+import colrev.env.package_manager
+import colrev.exceptions as colrev_exceptions
+import colrev.ops.search
+import colrev.record
+
+# pylint: disable=unused-argument
+# pylint: disable=duplicate-code
+
+
+@zope.interface.implementer(
+    colrev.env.package_manager.SearchSourcePackageEndpointInterface
+)
+@dataclass
+class ERICSearchSource(JsonSchemaMixin):
+    """SearchSource for ERIC"""
+
+    settings_class = colrev.env.package_manager.DefaultSourceSettings
+    source_identifier = "url"
+    search_type = colrev.settings.SearchType.DB
+    heuristic_status = colrev.env.package_manager.SearchSourceHeuristicStatus.oni
+    short_name = "ERIC"
+    link = "https://eric.ed.gov/"
+
+    def __init__(
+        self, *, source_operation: colrev.operation.CheckOperation, settings: dict
+    ) -> None:
+        self.search_source = from_dict(data_class=self.settings_class, data=settings)
+
+    def validate_source(
+        self,
+        search_operation: colrev.ops.search.Search,
+        source: colrev.settings.SearchSource,
+    ) -> None:
+        """Validate the SearchSource (parameters etc.)"""
+
+        search_operation.review_manager.logger.debug(
+            f"Validate SearchSource {source.filename}"
+        )
+
+        if "query_file" not in source.search_parameters:
+            raise colrev_exceptions.InvalidQueryException(
+                f"Source missing query_file search_parameter ({source.filename})"
+            )
+
+        if not Path(source.search_parameters["query_file"]).is_file():
+            raise colrev_exceptions.InvalidQueryException(
+                f"File does not exist: query_file {source.search_parameters['query_file']} "
+                f"for ({source.filename})"
+            )
+
+        search_operation.review_manager.logger.debug(
+            f"SearchSource {source.filename} validated"
+        )
+
+    @classmethod
+    def heuristic(cls, filename: Path, data: str) -> dict:
+        """Source heuristic for ERIC"""
+
+        result = {"confidence": 0.1}
+
+        # Note : no features in bib file for identification
+
+        return result
+
+    @classmethod
+    def add_endpoint(
+        cls, search_operation: colrev.ops.search.Search, query: str
+    ) -> typing.Optional[colrev.settings.SearchSource]:
+        """Add SearchSource as an endpoint (based on query provided to colrev search -a )"""
+        return None
+
+    def load_fixes(
+        self,
+        load_operation: colrev.ops.load.Load,
+        source: colrev.settings.SearchSource,
+        records: typing.Dict,
+    ) -> dict:
+        """Load fixes for ERIC"""
+
+        return records
+
+    def prepare(
+        self, record: colrev.record.Record, source: colrev.settings.SearchSource
+    ) -> colrev.record.Record:
+        """Source-specific preparation for ERIC"""
+
+        return record
+
+
+if __name__ == "__main__":
+    pass
---
+++ packages/colrev/colrev-0.7.1.tar.gz/colrev-0.7.1/colrev/ops/pdf_get_man.py
@@ -0,0 +1,148 @@
+#! /usr/bin/env python
+"""CoLRev pdf_get_man operation: Get PDF documents manually."""
+from __future__ import annotations
+
+import csv
+import typing
+from pathlib import Path
+
+import pandas as pd
+
+import colrev.operation
+import colrev.record
+
+
+class PDFGetMan(colrev.operation.Operation):
+    """Get PDFs manually"""
+
+    def __init__(
+        self,
+        *,
+        review_manager: colrev.review_manager.ReviewManager,
+        notify_state_transition_operation: bool = True,
+    ) -> None:
+        super().__init__(
+            review_manager=review_manager,
+            operations_type=colrev.operation.OperationsType.pdf_get_man,
+            notify_state_transition_operation=notify_state_transition_operation,
+        )
+
+        self.verbose = True
+
+        package_manager = self.review_manager.get_package_manager()
+        self.pdf_get_man_package_endpoints: dict[
+            str, typing.Any
+        ] = package_manager.load_packages(
+            package_type=colrev.env.package_manager.PackageEndpointType.pdf_get_man,
+            selected_packages=review_manager.settings.pdf_get.pdf_get_man_package_endpoints,
+            operation=self,
+        )
+
+    def get_pdf_get_man(self, *, records: dict) -> list:
+        """Get the records that are missing a PDF"""
+        missing_records = []
+        for record in records.values():
+            if (
+                record["colrev_status"]
+                == colrev.record.RecordState.pdf_needs_manual_retrieval
+            ):
+                missing_records.append(record)
+        return missing_records
+
+    def export_retrieval_table(self, *, records: dict) -> None:
+        """Export a table for manual PDF retrieval"""
+
+        missing_records = self.get_pdf_get_man(records=records)
+        missing_pdf_files_csv = Path("missing_pdf_files.csv")
+
+        if len(missing_records) > 0:
+            missing_records_df = pd.DataFrame.from_records(missing_records)
+            # pylint: disable=duplicate-code
+            col_order = [
+                "ID",
+                "author",
+                "title",
+                "journal",
+                "booktitle",
+                "year",
+                "volume",
+                "number",
+                "pages",
+                "doi",
+            ]
+            missing_records_df = missing_records_df.reindex(col_order, axis=1)
+            missing_records_df.to_csv(
+                missing_pdf_files_csv, index=False, quoting=csv.QUOTE_ALL
+            )
+
+            self.review_manager.logger.info(
+                "Created missing_pdf_files.csv with paper details"
+            )
+
+    def discard(self) -> None:
+        """Discard missing PDFs (set to pdf_not_available)"""
+
+        records = self.review_manager.dataset.load_records_dict()
+        for record in records.values():
+            if (
+                record["colrev_status"]
+                == colrev.record.RecordState.pdf_needs_manual_retrieval
+            ):
+                record["colrev_status"] = colrev.record.RecordState.pdf_not_available
+        self.review_manager.dataset.save_records_dict(records=records)
+        self.review_manager.dataset.add_record_changes()
+        self.review_manager.create_commit(
+            msg="Discard missing PDFs", manual_author=True
+        )
+
+    def get_data(self) -> dict:
+        """Get the data for pdf-get-man"""
+        # pylint: disable=duplicate-code
+
+        self.review_manager.pdf_dir.mkdir(exist_ok=True)
+
+        records_headers = self.review_manager.dataset.load_records_dict(
+            header_only=True
+        )
+        record_header_list = list(records_headers.values())
+        nr_tasks = len(
+            [
+                x
+                for x in record_header_list
+                if colrev.record.RecordState.pdf_needs_manual_retrieval
+                == x["colrev_status"]
+            ]
+        )
+        pad = min((max(len(x["ID"]) for x in record_header_list) + 2), 40)
+        items = self.review_manager.dataset.read_next_record(
+            conditions=[
+                {"colrev_status": colrev.record.RecordState.pdf_needs_manual_retrieval}
+            ]
+        )
+        pdf_get_man_data = {"nr_tasks": nr_tasks, "PAD": pad, "items": items}
+        self.review_manager.logger.debug(
+            self.review_manager.p_printer.pformat(pdf_get_man_data)
+        )
+        return pdf_get_man_data
+
+    def pdfs_retrieved_manually(self) -> bool:
+        """Check whether PDFs were retrieved manually"""
+        return self.review_manager.dataset.has_changes()
+
+    def main(self) -> None:
+        """Get PDFs manually (main entrypoint)"""
+
+        records = self.review_manager.dataset.load_records_dict()
+
+        for (
+            pdf_get_man_package_endpoint
+        ) in self.review_manager.settings.pdf_get.pdf_get_man_package_endpoints:
+            endpoint = self.pdf_get_man_package_endpoints[
+                pdf_get_man_package_endpoint["endpoint"]
+            ]
+
+            records = endpoint.pdf_get_man(self, records)
+
+
+if __name__ == "__main__":
+    pass
