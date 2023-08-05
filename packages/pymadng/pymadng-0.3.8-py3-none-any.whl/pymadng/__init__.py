ffb3e5554c387e69890b4427d5419a134a08737f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:47 +0000

    Add package pymadng/0.3.8/pymadng-0.3.8-py3-none-any.whl

---
+++ packages/pymadng/pymadng-0.3.8-py3-none-any.whl/pymadng/__init__.py
@@ -0,0 +1,13 @@
+from .mad_object import MAD
+
+__title__ = "pymadng"
+__version__ = "0.3.8"
+
+__summary__ = "Python interface to MAD-NG running as subprocess"
+__uri__ = "https://github.com/MethodicalAcceleratorDesign/MADpy"
+
+__credits__ = """
+Creator: Joshua Gray <joshua.mark.gray at cern.ch>
+"""
+
+__all__ = ["MAD"]
