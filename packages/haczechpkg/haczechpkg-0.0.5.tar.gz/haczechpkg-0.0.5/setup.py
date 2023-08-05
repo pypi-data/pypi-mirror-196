5605ed3879e1eb0bf6f5d590c91361ae7d13452b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:24 +0000

    Add package haczechpkg/0.0.5/haczechpkg-0.0.5.tar.gz

---
+++ packages/haczechpkg/haczechpkg-0.0.5.tar.gz/haczechpkg-0.0.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.5'
+DESCRIPTION = 'test pkg'
+LONG_DESCRIPTION = 'my first test pkg'
+
+setup(
+    name='haczechpkg',
+    version=VERSION,
+    author='me',
+    author_email='me@me.me',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+
+    keywords=['python', 'test_pkg'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+    ]
+)
