b7a5eecc20f6a84d643e1365b641be5502f9612c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:43 +0000

    Add package vulkpy/0.0.4/vulkpy-0.0.4.tar.gz

---
+++ packages/vulkpy/vulkpy-0.0.4.tar.gz/vulkpy-0.0.4/vulkpy/__init__.py
@@ -0,0 +1,29 @@
+"""
+vulkpy: GPGPU array on Vulkan
+=============================
+
+vulkpy provides GPU computations.
+
+See Also
+--------
+vulkpy.vkarray : Core Module
+vulkpy.random : Random Module
+vulkpy.nn : Neural Network Module
+vulkpy.util : Utility Module
+
+
+Examples
+--------
+>>> import vulkpy as vk
+
+>>> gpu = vk.GPU()
+>>> a = vk.Array(gpu, data=[1, 2, 3])
+>>> b = vk.Array(gpu, data=[3, 3, 3])
+
+>>> c = a + b
+>>> print(c)
+[4., 5., 6.]
+"""
+from .vkarray import GPU, U32Array, Shape, Array
+from . import random
+from . import nn
