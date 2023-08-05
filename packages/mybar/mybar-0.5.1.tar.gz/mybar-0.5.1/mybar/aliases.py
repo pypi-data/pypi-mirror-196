2ce9672d495c3392fd176900bcd75545f72d2a0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:19 +0000

    Add package mybar/0.5.1/mybar-0.5.1.tar.gz

---
+++ packages/mybar/mybar-0.5.1.tar.gz/mybar-0.5.1/mybar/aliases.py
@@ -0,0 +1,20 @@
+T = TypeVar('T')
+B = TypeVar('B')
+Bar_T = TypeVar('Bar')
+
+Args: TypeAlias = list
+ConsoleControlCode: TypeAlias = str
+FieldName: TypeAlias = str
+FieldParamSpec: TypeAlias = dict[str]
+FormatStr: TypeAlias = str
+Icon: TypeAlias = str
+JSONText: TypeAlias = str
+Kwargs: TypeAlias = dict
+PTY_Icon: TypeAlias = str
+PTY_Separator: TypeAlias = str
+Pattern: TypeAlias = str
+Separator: TypeAlias = str
+TTY_Icon: TypeAlias = str
+TTY_Separator: TypeAlias = str
+TemplateSpec: TypeAlias = dict[str]
+
