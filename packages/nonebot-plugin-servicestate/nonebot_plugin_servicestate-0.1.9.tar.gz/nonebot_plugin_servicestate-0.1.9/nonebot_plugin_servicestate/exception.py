a147f4916de088a2eda20ad9664526399f7b6942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:34 +0000

    Add package nonebot-plugin-servicestate/0.1.9/nonebot_plugin_servicestate-0.1.9.tar.gz

---
+++ packages/nonebot-plugin-servicestate/nonebot_plugin_servicestate-0.1.9.tar.gz/nonebot_plugin_servicestate-0.1.9/nonebot_plugin_servicestate/exception.py
@@ -0,0 +1,28 @@
+class ProtocolUnsopportError(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
+class NameConflictError(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
+class NameNotFoundError(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
+class ParamInvalidError(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
+class ParamCountInvalidError(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
+class NameEscapeCharacterCountError(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
