6f7df8f05d7c77c36faccc961ad2cec662ccdd1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:31 +0000

    Add package pytest-gitlabci-parallelized/0.1.3/pytest-gitlabci-parallelized-0.1.3.tar.gz

---
+++ packages/pytest-gitlabci-parallelized/pytest-gitlabci-parallelized-0.1.3.tar.gz/pytest-gitlabci-parallelized-0.1.3/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import os
+
+
+def read(file_name):
+    file_path = os.path.join(os.path.dirname(__file__), file_name)
+    with open(file_path, "r") as f:
+        return f.read()
+
+
+setuptools.setup(
+    name="pytest-gitlabci-parallelized",
+    version="0.1.3",
+    author="Michał Leśniewski, Ryan Wilson-Perkin",
+    author_email="mlesniew@gmail.com",
+    license="MIT",
+    url="https://github.com/mlesniew/pytest-gitlabci-parallelized",
+    description="Parallelize pytest across GitLab CI workers.",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    py_modules=["pytest_gitlabci_parallelized"],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    install_requires=["pytest"],
+    classifiers=[
+        "Framework :: Pytest",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Testing",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ],
+    entry_points={"pytest11": ["gitlabci-parallelized = pytest_gitlabci_parallelized"]},
+)
