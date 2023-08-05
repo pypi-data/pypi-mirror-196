3b248ef616b844634f0d3d496f2440c0851fe846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:16 +0000

    Add package trex-lib/0.2.36.dev20230308/trex-lib-0.2.36.dev20230308.tar.gz

---
+++ packages/trex-lib/trex-lib-0.2.36.dev20230308.tar.gz/trex-lib-0.2.36.dev20230308/trexlib/utils/google/gcloud_util.py
@@ -0,0 +1,45 @@
+'''
+Created on 24 Dec 2020
+
+@author: jacklok
+'''
+import logging, json
+from google.oauth2 import service_account
+from google.cloud import storage
+from google.cloud import tasks_v2
+from trexlib.utils.security_util import create_basic_authentication, verfiy_basic_authentication
+from trexlib import conf as lib_conf
+from trexlib.utils.string_util import random_string
+from datetime import datetime, timedelta
+from google.protobuf import timestamp_pb2
+
+logger = logging.getLogger('debug')
+
+
+def create_cloud_storage_client(info=None, credential_filepath=None):
+    if info:
+        cred = service_account.Credentials.from_service_account_info(info)
+        
+    else:
+        if credential_filepath:
+            cred = service_account.Credentials.from_service_account_file(credential_filepath)
+        else:
+            cred = service_account.Credentials.from_service_account_file(
+                                                            lib_conf.STORAGE_CREDENTIAL_PATH)
+    
+    
+    logger.debug('admin_conf.STORAGE_CREDENTIAL_PATH=%s', lib_conf.STORAGE_CREDENTIAL_PATH)
+    logger.debug('cred._project_id=%s', cred._project_id)
+    logger.debug('cred.service_account_email=%s', cred.service_account_email)
+    
+    
+    client = storage.Client(project=lib_conf.CLOUD_STORAGE_PROJECT_ID, credentials=cred)
+    
+    return client
+
+def connect_to_bucket(info=None, credential_filepath=None):    
+    client = create_cloud_storage_client(info=info, credential_filepath=credential_filepath)
+    # Get the bucket that the file will be uploaded to.
+    bucket = client.get_bucket(lib_conf.CLOUD_STORAGE_BUCKET)
+    
+    return bucket
