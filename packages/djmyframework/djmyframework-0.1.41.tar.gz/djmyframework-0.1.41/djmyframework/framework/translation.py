85593f5955c61f6b9aca6dc34669606a54cc56eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:19 +0000

    Add package djmyframework/0.1.41/djmyframework-0.1.41.tar.gz

---
+++ packages/djmyframework/djmyframework-0.1.41.tar.gz/djmyframework-0.1.41/djmyframework/framework/translation.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020-06-05 09:26
+# @Author : xzr
+# @File : translation.py
+# @Software: PyCharm
+# @Contact : xzregg@gmail.com
+# @Desc :
+
+from django.utils.translation import gettext_lazy
+
+_ = gettext_lazy
