d1bc709133c5a3e9aabdb9d45d00274817d7b696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:04 +0000

    Add package codalab/1.6.1/codalab-1.6.1-py3-none-any.whl

---
+++ packages/codalab/codalab-1.6.1-py3-none-any.whl/codalab/bundles/make_bundle.py
@@ -0,0 +1,26 @@
+'''
+MakeBundle is a Bundle type that symlinks a number of targets in from other
+bundles to produce a new, packaged bundle.
+'''
+from typing import List
+
+from codalab.bundles.derived_bundle import DerivedBundle
+from codalab.worker.bundle_state import State
+from codalab.objects.metadata_spec import MetadataSpec
+
+
+class MakeBundle(DerivedBundle):
+    BUNDLE_TYPE = 'make'
+    METADATA_SPECS = list(DerivedBundle.METADATA_SPECS)  # type: List
+    METADATA_SPECS.append(
+        MetadataSpec(
+            'staged_status',
+            str,
+            'Information about the status of the staged bundle (staged_status).',
+            generated=True,
+        )
+    )
+
+    @classmethod
+    def construct(cls, targets, command, metadata, owner_id, uuid=None, state=State.CREATED):
+        return super(MakeBundle, cls).construct(targets, command, metadata, owner_id, uuid, state)
