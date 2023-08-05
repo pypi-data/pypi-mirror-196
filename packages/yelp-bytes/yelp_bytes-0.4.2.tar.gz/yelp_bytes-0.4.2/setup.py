8773d980ede226f35e5d77c399b99c3fe0bd863e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:26 +0000

    Add package yelp-bytes/0.4.2/yelp_bytes-0.4.2.tar.gz

---
+++ packages/yelp-bytes/yelp_bytes-0.4.2.tar.gz/yelp_bytes-0.4.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='yelp_bytes',
+    description='Utilities for dealing with byte strings, invented and maintained by Yelp.',
+    url='https://github.com/Yelp/yelp_bytes',
+    version='0.4.2',
+
+    author='Buck Evan',
+    author_email='buck@yelp.com',
+
+    platforms='all',
+    classifiers=[
+        'License :: Public Domain',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'License :: OSI Approved :: The Unlicense (Unlicense)',
+    ],
+    python_requires='>=3.6',
+    package_data={
+        'yelp_bytes': ['py.typed'],
+    },
+    install_requires=['yelp_encodings'],
+
+    py_modules=['yelp_bytes'],
+)
