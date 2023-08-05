5171f8ffe3a7828fe155469d4b1013142e683cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:22 +0000

    Add package Bubot-WebServer/0.1.10/Bubot_WebServer-0.1.10-py3-none-any.whl

---
+++ packages/Bubot-WebServer/Bubot_WebServer-0.1.10-py3-none-any.whl/BubotObj/OcfDevice/subtype/WebServer/ApiHelper.py
@@ -0,0 +1,35 @@
+from Bubot.Helpers.Helper import get_tzinfo
+from aiohttp import web
+from bson.json_util import dumps, JSONOptions
+
+json_options = JSONOptions(
+    tz_aware=False,
+    # tzinfo=get_tzinfo()
+)
+
+
+class DeviceApi:
+    def __init__(self, response, **kwargs):
+        self.response = response
+        self.db = kwargs.get('db')
+        self.filter_fields = {}
+        self.list_limit = 1000
+
+
+class WebResponse:
+
+    @staticmethod
+    def json_response(data, *, status: int = 200, headers=None, content_type: str = 'application/json',
+                      dumps=dumps) -> web.Response:
+        text = dumps(data, ensure_ascii=False, json_options=json_options)
+        return web.Response(text=text, status=status, headers=headers, content_type=content_type)
+
+    @staticmethod
+    def text_response(text='', *, status: int = 200, headers=None) -> web.Response:
+        return web.Response(text=text, status=status, headers=headers)
+
+
+class WsResponse:
+    @staticmethod
+    def json_response(data):
+        return data
