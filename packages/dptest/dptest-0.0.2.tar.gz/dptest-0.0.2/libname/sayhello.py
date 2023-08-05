6dfe4d503d0c7272a24b5abaec7e7f0b75f93279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:20 +0000

    Add package dptest/0.0.2/dptest-0.0.2.tar.gz

---
+++ packages/dptest/dptest-0.0.2.tar.gz/dptest-0.0.2/libname/sayhello.py
@@ -0,0 +1,10 @@
+import numpy
+class say:
+    def __init__(self) -> None:
+        pass
+
+    def hello(self):
+        print("Hello World")
+
+    def random(self):
+        print(numpy.random.randint(100))
---
+++ packages/dptest/dptest-0.0.2.tar.gz/dptest-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Basic hello'
+# Setting up
+setup(
+    name="dptest",
+    version=VERSION,
+    author="My Name",
+    author_email="<pth.a15.33.tran.thien@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=["numpy"],
+    keywords=['python', 'test'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    ]
+)
