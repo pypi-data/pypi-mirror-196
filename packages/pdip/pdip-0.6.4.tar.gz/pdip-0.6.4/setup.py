b5337e3499551ad3563068303a1f6eaa2ed3dfee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:12 +0000

    Add package pdip/0.6.4/pdip-0.6.4.tar.gz

---
+++ packages/pdip/pdip-0.6.4.tar.gz/pdip-0.6.4/setup.py
@@ -0,0 +1,64 @@
+from os import getenv
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+here = Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+env_version = getenv('PYPI_PACKAGE_VERSION', default='0.6.4')
+version = env_version.replace('v', '')
+setup(
+    name='pdip',
+    version=f'{version}',
+    description='Python Data Integrator infrastructures package',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ahmetcagriakca/pdip',
+    download_url=f'https://github.com/ahmetcagriakca/pdip/archive/refs/tags/v{version}.tar.gz',
+    author='Ahmet Çağrı AKCA',
+    author_email='ahmetcagriakca@gmail.com',
+    license='MIT',
+    packages=find_packages(
+        exclude=["tests", "tests*", "test_*", '__pycache__', '*.__pycache__', '__pycache.*', '*.__pycache__.*']),
+    zip_safe=False,
+    keywords=['PDI', 'API', 'ETL', 'PROCESS', 'MULTIPROCESS', 'IO', 'CQRS', 'MSSQL', 'ORACLE', 'POSTGRES', 'MYSQL',
+              'CSV'],
+    python_requires='>=3.8',
+    install_requires=[
+        "PyYAML",
+        "injector",
+        "jsonpickle",
+        "Fernet",
+        "cryptography",
+        "SQLAlchemy",
+        "rpyc",
+        "Flask",
+        "Flask_Cors",
+        "Flask-Ext",
+        "Flask-Injector",
+        "flask-restx",
+        "Werkzeug",
+        "dataclasses",
+        "dataclasses-json",
+        "requests",
+        "kafka-python",
+        "pandas",
+        "coverage",
+        "func-timeout"
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/ahmetcagriakca/pdip/issues',
+        'Source': 'https://github.com/ahmetcagriakca/pdip',
+    },
+)
