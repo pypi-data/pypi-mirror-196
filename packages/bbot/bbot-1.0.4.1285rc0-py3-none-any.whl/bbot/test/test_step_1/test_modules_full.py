7174ba5874230ec9378b911c508dd5057a0fff6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:35 +0000

    Add package bbot/1.0.4.1285rc0/bbot-1.0.4.1285rc0-py3-none-any.whl

---
+++ packages/bbot/bbot-1.0.4.1285rc0-py3-none-any.whl/bbot/test/test_step_1/test_modules_full.py
@@ -0,0 +1,102 @@
+import logging
+
+from ..bbot_fixtures import *  # noqa: F401
+from ..modules_test_classes import *
+
+log = logging.getLogger(f"bbot.test")
+
+
+def test_gowitness(bbot_config, bbot_scanner, bbot_httpserver):
+    x = Gowitness(bbot_config, bbot_scanner, bbot_httpserver)
+    x.run()
+
+
+def test_httpx(bbot_config, bbot_scanner, bbot_httpserver):
+    x = Httpx(bbot_config, bbot_scanner, bbot_httpserver)
+    x.run()
+
+
+def test_excavate(bbot_config, bbot_scanner, bbot_httpserver):
+    x = Excavate(bbot_config, bbot_scanner, bbot_httpserver)
+    x.run()
+
+
+def test_subdomain_hijack(bbot_config, bbot_scanner, bbot_httpserver):
+    x = Subdomain_Hijack(bbot_config, bbot_scanner, bbot_httpserver)
+    x.run()
+
+
+def test_fingerprintx(bbot_config, bbot_scanner, bbot_httpserver):
+    x = Fingerprintx(bbot_config, bbot_scanner, bbot_httpserver)
+    x.run()
+
+
+def test_otx(bbot_config, bbot_scanner, bbot_httpserver):
+    x = Otx(bbot_config, bbot_scanner, bbot_httpserver)
+    x.run()
+
+
+def test_anubisdb(bbot_config, bbot_scanner, bbot_httpserver):
+    x = Anubisdb(bbot_config, bbot_scanner, bbot_httpserver)
+    x.run()
+
+
+def test_getparam_brute(bbot_config, bbot_scanner, bbot_httpserver):
+    x = Getparam_brute(bbot_config, bbot_scanner, bbot_httpserver)
+    x.run()
+
+
+def test_telerik(bbot_config, bbot_scanner, bbot_httpserver):
+    x = Telerik(bbot_config, bbot_scanner, bbot_httpserver)
+    x.run()
+
+
+def test_leakix(bbot_config, bbot_scanner, bbot_httpserver):
+    x = LeakIX(bbot_config, bbot_scanner, bbot_httpserver)
+    x.run()
+
+
+def test_massdns(bbot_config, bbot_scanner, bbot_httpserver):
+    x = Massdns(bbot_config, bbot_scanner, bbot_httpserver)
+    x.run()
+
+
+# This is disabled because github's EDR is configured to delete the masscan binary
+# def test_masscan(bbot_config, bbot_scanner, bbot_httpserver):
+#     x = Masscan(bbot_config, bbot_scanner, bbot_httpserver)
+#     x.run()
+
+
+def test_badsecrets(bbot_config, bbot_scanner, bbot_httpserver):
+    x = Badsecrets(bbot_config, bbot_scanner, bbot_httpserver)
+    x.run()
+
+
+def test_robots(bbot_config, bbot_scanner, bbot_httpserver):
+    x = Robots(bbot_config, bbot_scanner, bbot_httpserver)
+    x.run()
+
+
+def test_asn(bbot_config, bbot_scanner, bbot_httpserver):
+    x = ASN(bbot_config, bbot_scanner, bbot_httpserver)
+    x.run()
+
+
+def test_wafw00f(bbot_config, bbot_scanner, bbot_httpserver):
+    x = Wafw00f(bbot_config, bbot_scanner, bbot_httpserver)
+    x.run()
+
+
+def test_ffuf(bbot_config, bbot_scanner, bbot_httpserver):
+    x = Ffuf(bbot_config, bbot_scanner, bbot_httpserver)
+    x.run()
+
+
+def test_ffuf_shortnames(bbot_config, bbot_scanner, bbot_httpserver):
+    x = Ffuf_shortnames(bbot_config, bbot_scanner, bbot_httpserver)
+    x.run()
+
+
+def test_iis_shortnames(bbot_config, bbot_scanner, bbot_httpserver):
+    x = Iis_shortnames(bbot_config, bbot_scanner, bbot_httpserver)
+    x.run()
