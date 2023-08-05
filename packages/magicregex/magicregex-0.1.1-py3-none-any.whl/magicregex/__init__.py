9265a61f033e3b52c4ea1bf8433caf8790c3f9ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:40 +0000

    Add package magicregex/0.1.1/magicregex-0.1.1-py3-none-any.whl

---
+++ packages/magicregex/magicregex-0.1.1-py3-none-any.whl/magicregex/__init__.py
@@ -0,0 +1,55 @@
+from magicregex.core import createRegEx
+from magicregex.core import (
+    CreateInput,
+    charIn,
+    charNotIn,
+    anyOf,
+    mayBe,
+    exactly,
+    oneOrMore,
+)
+from magicregex.core import char, word, wordChar, wordBoundary, digit, whitespace
+from magicregex.core import letter, lowercase, uppercase, tab, linefeed, carriageReturn
+from magicregex.core import notWordChar, notWordBoundary, notDigit, notWhitespace
+from magicregex.core import (
+    notLetter,
+    notLowercase,
+    notUppercase,
+    notTab,
+    notLinefeed,
+    notCarriageReturn,
+)
+
+
+__all__ = [
+    "createRegEx",
+    "CreateInput",
+    "charIn",
+    "charNotIn",
+    "anyOf",
+    "mayBe",
+    "exactly",
+    "oneOrMore",
+    "char",
+    "word",
+    "wordChar",
+    "wordBoundary",
+    "digit",
+    "whitespace",
+    "letter",
+    "lowercase",
+    "uppercase",
+    "tab",
+    "linefeed",
+    "carriageReturn",
+    "notWordChar",
+    "notWordBoundary",
+    "notDigit",
+    "notWhitespace",
+    "notLetter",
+    "notLowercase",
+    "notUppercase",
+    "notTab",
+    "notLinefeed",
+    "notCarriageReturn",
+]
