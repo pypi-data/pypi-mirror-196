ffa7ecf655cafd0d3de38ef67652bfa623e72447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:26 +0000

    Add package ytdl-sub/2023.3.7/ytdl_sub-2023.3.7-py3-none-any.whl

---
+++ packages/ytdl-sub/ytdl_sub-2023.3.7-py3-none-any.whl/ytdl_sub/cli/main.py
@@ -0,0 +1,163 @@
+import argparse
+import gc
+import sys
+from typing import List
+from typing import Tuple
+
+from ytdl_sub.cli.download_args_parser import DownloadArgsParser
+from ytdl_sub.cli.main_args_parser import parser
+from ytdl_sub.config.config_file import ConfigFile
+from ytdl_sub.subscriptions.subscription import Subscription
+from ytdl_sub.utils.file_handler import FileHandlerTransactionLog
+from ytdl_sub.utils.file_lock import working_directory_lock
+from ytdl_sub.utils.logger import Logger
+
+logger = Logger.get()
+
+# View is a command to run a simple dry-run on a URL using the `_view` preset.
+# Use ytdl-sub dl arguments to use the preset
+_VIEW_EXTRA_ARGS_FORMATTER = "--preset _view --overrides.url {}"
+
+
+def _download_subscriptions_from_yaml_files(
+    config: ConfigFile, args: argparse.Namespace
+) -> List[Tuple[Subscription, FileHandlerTransactionLog]]:
+    """
+    Downloads all subscriptions from one or many subscription yaml files.
+
+    Parameters
+    ----------
+    config
+        Configuration file
+    args
+        Arguments from argparse
+
+    Returns
+    -------
+    List of (subscription, transaction_log)
+
+    Raises
+    ------
+    Validation exception if main arg is specified as a subscription path
+    """
+    subscription_paths: List[str] = args.subscription_paths
+    subscriptions: List[Subscription] = []
+    output: List[Tuple[Subscription, FileHandlerTransactionLog]] = []
+
+    # Load all the subscriptions first to perform all validation before downloading
+    for path in subscription_paths:
+        subscriptions += Subscription.from_file_path(config=config, subscription_path=path)
+
+    for subscription in subscriptions:
+        logger.info(
+            "Beginning subscription %s for %s",
+            ("dry run" if args.dry_run else "download"),
+            subscription.name,
+        )
+        logger.debug("Subscription full yaml:\n%s", subscription.as_yaml())
+        transaction_log = subscription.download(dry_run=args.dry_run)
+
+        output.append((subscription, transaction_log))
+        gc.collect()  # Garbage collect after each subscription download
+        Logger.cleanup()  # Cleanup logger after each successful subscription download
+
+    return output
+
+
+def _download_subscription_from_cli(
+    config: ConfigFile, dry_run: bool, extra_args: List[str]
+) -> Tuple[Subscription, FileHandlerTransactionLog]:
+    """
+    Downloads a one-off subscription using the CLI
+
+    Parameters
+    ----------
+    config
+        Configuration file
+    dry_run
+        Whether this is a dry-run
+    extra_args
+        Extra arguments from argparse that contain dynamic subscription options
+
+    Returns
+    -------
+    Subscription and its download transaction log
+    """
+    dl_args_parser = DownloadArgsParser(
+        extra_arguments=extra_args, config_options=config.config_options
+    )
+    subscription_args_dict = dl_args_parser.to_subscription_dict()
+    subscription_name = f"cli-dl-{dl_args_parser.get_args_hash()}"
+
+    subscription = Subscription.from_dict(
+        config=config, preset_name=subscription_name, preset_dict=subscription_args_dict
+    )
+
+    logger.info("Beginning CLI %s", ("dry run" if dry_run else "download"))
+    return subscription, subscription.download(dry_run=dry_run)
+
+
+def _view_url_from_cli(
+    config: ConfigFile, url: str, split_chapters: bool
+) -> Tuple[Subscription, FileHandlerTransactionLog]:
+    """
+    `ytdl-sub view` dry-runs a URL to print its source variables. Use the pre-built `_view` preset,
+    inject the URL argument, and dry-run.
+    """
+    preset = "_view_split_chapters" if split_chapters else "_view"
+    subscription = Subscription.from_dict(
+        config=config,
+        preset_name="ytdl-sub-view",
+        preset_dict={"preset": preset, "overrides": {"url": url}},
+    )
+
+    logger.info(
+        "Viewing source variables for URL '%s'%s",
+        url,
+        " with split chapters" if split_chapters else "",
+    )
+    return subscription, subscription.download(dry_run=True)
+
+
+def main() -> List[Tuple[Subscription, FileHandlerTransactionLog]]:
+    """
+    Entrypoint for ytdl-sub, without the error handling
+    """
+    # If no args are provided, print help and exit
+    if len(sys.argv) < 2:
+        parser.print_help()
+        return []
+
+    args, extra_args = parser.parse_known_args()
+
+    # Load the config
+    config: ConfigFile = ConfigFile.from_file_path(args.config)
+    transaction_logs: List[Tuple[Subscription, FileHandlerTransactionLog]] = []
+
+    with working_directory_lock(config=config):
+        if args.subparser == "sub":
+            transaction_logs = _download_subscriptions_from_yaml_files(config=config, args=args)
+
+        # One-off download
+        elif args.subparser == "dl":
+            transaction_logs.append(
+                _download_subscription_from_cli(
+                    config=config, dry_run=args.dry_run, extra_args=extra_args
+                )
+            )
+        elif args.subparser == "view":
+            transaction_logs.append(
+                _view_url_from_cli(config=config, url=args.url, split_chapters=args.split_chapters)
+            )
+
+    for subscription, transaction_log in transaction_logs:
+        if transaction_log.is_empty:
+            logger.info("No files changed for %s", subscription.name)
+        else:
+            logger.info(
+                "Downloads for %s:\n%s\n",
+                subscription.name,
+                transaction_log.to_output_message(subscription.output_directory),
+            )
+
+    return transaction_logs
---
+++ packages/ytdl-sub/ytdl_sub-2023.3.7-py3-none-any.whl/ytdl_sub/main.py
@@ -0,0 +1,49 @@
+import sys
+
+from ytdl_sub import __local_version__
+from ytdl_sub.cli.main_args_parser import parser
+from ytdl_sub.utils.exceptions import ValidationException
+from ytdl_sub.utils.logger import Logger
+
+
+def _main():
+    # Set log level before any other ytdl-sub files are imported. That way, when loggers
+    # get initialized, they will see the set log level
+    args, _ = parser.parse_known_args()
+    Logger.set_log_level(log_level_name=args.ytdl_sub_log_level)
+
+    # pylint: disable=import-outside-toplevel
+    import ytdl_sub.cli.main
+
+    # pylint: enable=import-outside-toplevel
+
+    ytdl_sub.cli.main.main()
+
+
+def main():
+    """
+    Entrypoint for ytdl-sub
+    """
+    logger = Logger.get()
+    try:
+        _main()
+        Logger.cleanup()  # Ran successfully, so we can delete the debug file
+    except ValidationException as validation_exception:
+        logger.error(validation_exception)
+        sys.exit(1)
+    except Exception:  # pylint: disable=broad-except
+        logger.exception("An uncaught error occurred:")
+        logger.error(
+            "Version %s\nPlease upload the error log file '%s' and make a Github "
+            "issue at https://github.com/jmbannon/ytdl-sub/issues with your config and "
+            "command/subscription yaml file to reproduce. Thanks for trying ytdl-sub!",
+            __local_version__,
+            Logger.debug_log_filename(),
+        )
+        sys.exit(1)
+
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
