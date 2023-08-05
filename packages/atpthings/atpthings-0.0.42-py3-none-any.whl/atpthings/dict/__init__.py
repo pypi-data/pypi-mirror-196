85110647e610a81d3f573fca3c7ccfab7f478fcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:05 +0000

    Add package atpthings/0.0.42/atpthings-0.0.42-py3-none-any.whl

---
+++ packages/atpthings/atpthings-0.0.42-py3-none-any.whl/atpthings/dict/__init__.py
@@ -0,0 +1,10 @@
+"""
+atpthings.dict
+--------------
+Utilities for manipulating with dict (dictionary)
+
+
+"""
+from .get import get_keys, get_values, get_keys_as_list, get_values_from_dict_list
+from .io import load, save
+from .convert import list_to_dict
