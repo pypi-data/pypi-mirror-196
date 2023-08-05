3be0ae3259cbd8229c4882f7ee9aad6faee6b694
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:36 +0000

    Add package lprp-detr/0.1.1/lprp_detr-0.1.1-py3-none-any.whl

---
+++ packages/lprp-detr/lprp_detr-0.1.1-py3-none-any.whl/lprp_detr/d2/detr/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+from lprp_detr.d2.detr.config import add_detr_config
+from lprp_detr.d2.detr.detr import Detr
+from lprp_detr.d2.detr.dataset_mapper import DetrDatasetMapper
