1a3116df67171bcff907a3ff2bdfaa63d1bde915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:09 +0000

    Add package thcli/1.4.0/thcli-1.4.0.tar.gz

---
+++ packages/thcli/thcli-1.4.0.tar.gz/thcli-1.4.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='thcli',
+    version='1.4.0',
+    py_modules=['main'],
+    install_requires=requirements,
+    author='Taehoon Kang',
+    entry_points={
+        'console_scripts': ['thcli=main:main'],
+    },
+    author_email='kangthink@gmail.com',
+    description='Greeting for fun.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
