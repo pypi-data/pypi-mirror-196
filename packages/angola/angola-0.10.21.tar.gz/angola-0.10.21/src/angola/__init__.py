100c8f7e7ade7fe133287caebcc93ee20a31ec85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:09 +0000

    Add package angola/0.10.21/angola-0.10.21.tar.gz

---
+++ packages/angola/angola-0.10.21.tar.gz/angola-0.10.21/src/angola/__init__.py
@@ -0,0 +1,6 @@
+# ------------------------------------------------------------------------------
+# -- Angola --
+# ------------------------------------------------------------------------------
+
+from .database import Database as db, CollectionItem, CollectionItemClass
+from arango.exceptions import ArangoError
