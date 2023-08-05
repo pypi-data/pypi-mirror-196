6827b60e3ac43b758b6a663fb6a37366b5d942d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:02 +0000

    Add package geocontour/1.2.1/geocontour-1.2.1-py3-none-any.whl

---
+++ packages/geocontour/geocontour-1.2.1-py3-none-any.whl/geocontour/geocontour.py
@@ -0,0 +1,88 @@
+import sys
+import numpy as np
+import geocontour.check as gcc
+import geocontour.grid as gcg
+
+def build(contour,latitudes,longitudes,connecttype='cell',simplify=False):
+    """
+    Returns a geocontour from a contour input
+
+    Inputs (Required):
+        contour - A 2-d Nx2 numpy array of ordered latitude/longitude points (degrees) describing the edge of a mask
+        latitudes - An evenly spaced numpy array of latitude points (degrees)
+        longitudes - An evenly spaced numpy array of longitude points (degrees)
+
+    Inputs (Optional):
+        connecttype ('cell'/'center') - method of linking contour cells, default='cell'
+            'cell' implies drawing a single connection through a cell from the preceeding to the following cell, resulting in a geocontour the same length as the input contour
+            'center' implies drawing two lines through a cell, intersecting the center of the cell, resulting in a geocontour double the length of the input contour
+            Both methods functionally provide the same output when using the geocontour with data
+        simplify (True/False) - remove cells for which outward vectors sum to 0 and merge cells containing multiple contour segments, default=False
+
+    Outputs:
+        geocontour - A 3-d Nx2x5 numpy array defining a list of N contour cells and their edge points, lengths, and outward unit vectors
+    """
+    gcc.ccontour(contour,latitudes,longitudes)
+    contourdiff=np.diff(contour,axis=0)
+    if connecttype=='center':
+        geocontour=np.full((2*(contour.shape[0]-1),2,5),np.nan)
+        geocontour[:,:,0]=np.stack((contour[:-1],contour[1:]),axis=1).reshape(-1,2)
+        geocontour[:,:,1]=np.stack((contour[:-1],contour[:-1]+contourdiff/2),axis=1).reshape(-1,2)
+        geocontour[:,:,2]=np.stack((contour[1:]-contourdiff/2,contour[1:]),axis=1).reshape(-1,2)
+        if simplify:
+            deleteindices=np.array([],dtype='int')
+            geocontour_unique,countindex=np.unique(geocontour[:,:,0],axis=0,return_counts=True)
+            for k in geocontour_unique[countindex>1]:
+                indices=np.nonzero(((geocontour[:,:,0]-k)==0).all(axis=1))[0]
+                starts=geocontour[indices,:,1]
+                ends=geocontour[indices,:,2]
+                uniquestart=starts[(starts-ends[:,None]).any(axis=2).all(axis=0)]
+                uniqueend=ends[(ends-starts[:,None]).any(axis=2).all(axis=0)]
+                if uniquestart.size==0 and uniqueend.size==0:
+                    deleteindices=np.append(deleteindices,indices)
+                else:
+                    startkeep=np.argwhere((geocontour[:,:,1]==uniquestart[0]).all(axis=1))[0]==indices
+                    endkeep=np.argwhere((geocontour[:,:,2]==uniqueend[0]).all(axis=1))[0]==indices
+                    deleteindices=np.append(deleteindices,indices[~(startkeep+endkeep)])
+            geocontour=np.delete(geocontour,deleteindices,axis=0)
+    elif connecttype=='cell':
+        geocontour=np.full((contour.shape[0]-1,2,5),np.nan)
+        geocontour[:,:,0]=contour[:-1]
+        geocontour[:,:,1]=geocontour[:,:,0]-np.roll(contourdiff/2,1,axis=0)
+        geocontour[:,:,2]=geocontour[:,:,0]+contourdiff/2
+        if simplify:
+            deleteindices=np.array([],dtype='int')
+            geocontour_unique,countindex=np.unique(geocontour[:,:,0],axis=0,return_counts=True)
+            for k in geocontour_unique[countindex>1]:
+                indices=np.nonzero(((geocontour[:,:,0]-k)==0).all(axis=1))[0]
+                starts=geocontour[indices,:,1]
+                ends=geocontour[indices,:,2]
+                uniquestart=starts[(starts-ends[:,None]).any(axis=2).all(axis=0)]
+                uniqueend=ends[(ends-starts[:,None]).any(axis=2).all(axis=0)]
+                if uniquestart.size==0 and uniqueend.size==0:
+                    deleteindices=np.append(deleteindices,indices)
+                elif uniquestart.size==2 and uniqueend.size==2:
+                    geocontour[indices[0],:,1]=uniquestart[0]
+                    geocontour[indices[0],:,2]=uniqueend[0]
+                    deleteindices=np.append(deleteindices,indices[1:])
+            deleteindices=np.append(deleteindices,np.nonzero(((geocontour[:,:,1]-geocontour[:,:,2])==0).all(axis=1))[0])
+            geocontour=np.delete(geocontour,deleteindices,axis=0)
+    else:
+        sys.exit('ERROR - unrecognized connecttype input '+connecttype+', valid options are \'cell\'/\'center\'')
+    gridnorm=np.sqrt(np.sum((geocontour[:,:,2]-geocontour[:,:,1])**2,axis=1))
+    geocontour[:,0,3]=gridnorm
+    lonlengths=gcg.lonlen(geocontour[:,0,0])
+    latlengths=gcg.latlen(geocontour[:,0,0])
+    lengths=np.stack((latlengths,lonlengths),axis=1)
+    geocontour[:,1,3]=np.sqrt(np.sum(((geocontour[:,:,2]-geocontour[:,:,1])*lengths)**2,axis=1))
+    winding=np.sum((contour[:-1,0]+contour[1:,0])*np.diff(contour[:,1]))
+    if winding<0:
+        rotation=np.array([-1,1])
+    elif winding>0:
+        rotation=np.array([1,-1])
+    else:
+        sys.exit('ERROR - Can\'t determine orientation of contour')
+    gridnorm[gridnorm==0]=1
+    geocontour[:,:,4]=(geocontour[:,:,2]-geocontour[:,:,1])[:,::-1]*rotation/gridnorm[:,None]
+    return geocontour
+
---
+++ packages/geocontour/geocontour-1.2.1-py3-none-any.whl/geocontour/grid.py
@@ -0,0 +1,213 @@
+import sys
+import numpy as np
+import geocontour.check as gcc
+
+def spacing(dimension):
+    """
+    Returns the grid spacing for a given input dimension
+
+    Inputs (Required):
+        dimension - An evenly spaced numpy array of longitude or latitude points (degrees)
+
+    Outputs:
+        spacing - A positive float indicating the spacing of the input dimension (degrees)
+    """
+    gcc.cdim(dimension)
+    spacing=abs(np.diff(dimension)[0])
+    return spacing
+
+def lonlens(latitudes,lonspacing=1):
+    """
+    Returns the lengths of a degree (default) of longitude over a range of latitudes
+
+    Inputs (Required):
+        latitudes - An evenly spaced numpy array of latitude points (degrees)
+
+    Inputs (Optional):
+        lonspacing - A float/int specifying longitude spacing
+            e.g. for lengths of a half degree of longitude at the given latitudes, lonspacing=0.5
+
+    Outputs:
+        lonlens - A numpy array of longitude lengths (m)
+    """
+    gcc.cdim(latitudes)
+    a=6378137
+    b=6356752.3142
+    e=np.sqrt((a**2-b**2)/a**2)
+    latrads=latitudes*np.pi/180
+    lonlengths=lonspacing*np.pi*a*np.cos(latrads)/(180*np.sqrt(1-(e*np.sin(latrads))**2))
+    return lonlengths
+
+def latlens(latitudes):
+    """
+    Returns the grid lengths of a defined range of latitudes
+
+    Inputs (Required):
+        latitudes - An evenly spaced numpy array of latitude points (degrees)
+
+    Outputs:
+        latlens - A numpy array of latitude lengths (m)
+    """
+    gcc.cdim(latitudes)
+    a=6378137
+    b=6356752.3142
+    e=np.sqrt((a**2-b**2)/a**2)
+    latrads=latitudes*np.pi/180
+    latspacing=spacing(latitudes)
+    latlengths=latspacing*np.pi*a*(1-e**2)/(180*(1-(e*np.sin(latrads))**2)**(3/2))
+    return latlengths
+
+def lonlen(latitude):
+    """
+    Returns the length of a degree of longitude at the input latitude
+
+    Inputs (Required):
+        latitude - A latitude point or array of points (degrees)
+
+    Outputs:
+        lonlen - The length of a degree of longitude at the input latitude(s) (m)
+    """
+    a=6378137
+    b=6356752.3142
+    e=np.sqrt((a**2-b**2)/a**2)
+    latrad=latitude*np.pi/180
+    lonlength=np.pi*a*np.cos(latrad)/(180*np.sqrt(1-(e*np.sin(latrad))**2))
+    return lonlength
+
+def latlen(latitude):
+    """
+    Returns the length of a degree of latitude at the input latitude
+
+    Inputs (Required):
+        latitude - A latitude point or array of points (degrees)
+
+    Outputs:
+        latlen - The length of a degree of latitude at the input latitude(s) (m)
+    """
+    a=6378137
+    b=6356752.3142
+    e=np.sqrt((a**2-b**2)/a**2)
+    latrad=latitude*np.pi/180
+    latlength=np.pi*a*(1-e**2)/(180*(1-(e*np.sin(latrad))**2)**(3/2))
+    return latlength
+
+def areas(latitudes,longitudes,units=1):
+    """
+    Returns the cell areas of a grid defined by a range of latitudes and longitudes
+
+    Inputs (Required):
+        latitudes - An evenly spaced numpy array of latitude points (degrees)
+        longitudes - An evenly spaced numpy array of longitude points (degrees)
+
+    Inputs (Optional):
+        units - unit multiplier for outputs, default=1 (m^2)
+            e.g. for km^2, mult=1000m x 1000m=1e6
+
+    Outputs:
+        areas - A numpy array of areas, with dimension 0: latitude and dimension 1: longitude (default in m^2)
+    """
+    gcc.cdim(latitudes)
+    gcc.cdim(longitudes)
+    lonspacing=spacing(longitudes)
+    latlengths=latlens(latitudes)
+    lonlengths=lonlens(latitudes,lonspacing=lonspacing)
+    grdareas=np.repeat((latlengths*lonlengths)[:,np.newaxis],len(longitudes),axis=1)/units
+    return grdareas
+
+def clonrng(longitudes):
+    """
+    Returns a descriptor for the range of a set of longitude points
+        negative (-180 to 180), positive (0 to 360), or indeterminate (0 to 180) range
+
+    Inputs (Required):
+        longitudes - A numpy array of longitude points (degrees)
+
+    Outputs:
+        longituderange ('neg'/'pos'/'ind') - A descriptor for the longitude range of the input
+    """
+    if len(longitudes.shape)!=1:
+        sys.exit('ERROR - Longitude input is not 1-dimensional')
+    if (longitudes < 0).any() and (longitudes > 180).any():
+        sys.exit('ERROR - Longitude input has values less than 0 and greater than 180, range should either be -180 to 180 or 0 to 360')
+    elif (longitudes < 0).any():
+        longituderange='neg'
+    elif (longitudes > 180).any():
+        longituderange='pos'
+    else:
+        longituderange='ind'
+    return longituderange
+
+def clatdir(latitudes):
+    """
+    Returns a descriptor for the direction of a set of latitude points
+        increasing or decreasing
+
+    Inputs (Required):
+        latitudes - A numpy array of latitude points (degrees)
+
+    Outputs:
+        latitudedirection ('inc'/'dec') - A descriptor for the direction of the input
+    """
+    gcc.cdim(latitudes)
+    indlatmin=latitudes.argmin()
+    indlatmax=latitudes.argmax()
+    if indlatmin==0 and indlatmax==len(latitudes)-1:
+        latitudedirection='inc'
+    elif indlatmin==len(latitudes)-1 and indlatmax==0:
+        latitudedirection='dec'
+    else:
+        sys.exit('ERROR - Inconsistency in input latitude array, not ordered small to large or large to small')
+    return latitudedirection
+
+def switchlon(longitudes,outrange,print_output='n'):
+    """
+    Returns a set of longitude points switched in place between negative (-180 to 180) and positive (0 to 360)
+
+    Inputs (Required):
+        longitudes - A numpy array of longitude points (degrees)
+        outrange - ('pos'/'neg') Selection of range for longitude outputs: negative (-180 to 180) or positive (0 to 360)
+
+    Inputs(Optional):
+        print_output ('y'/'n') - flag to print out whether input was altered, default='n'
+
+    Outputs:
+        switchlon - A numpy array of longitudes, altered if not consistent with selected output range
+    """
+    if outrange!='pos' and outrange!='neg':
+        sys.exit('ERROR - Unrecognized longitude output range. Valid inputs are \'pos\' or \'neg\'')
+    inprange=clonrng(longitudes)
+    if inprange==outrange or inprange=='ind':
+        switchlon=longitudes
+        if print_output=='y':
+            print('Input already has values consistent with selected output range')
+    elif inprange=='neg' and outrange=='pos':
+        switchlon=longitudes+360*(longitudes<0)
+        if print_output=='y':
+            print('Switched negative input to positive output')
+    elif inprange=='pos' and outrange=='neg':
+        switchlon=longitudes-360*(longitudes>180)
+        if print_output=='y':
+            print('Switched positive input to negative output')
+    else:
+        sys.exit('ERROR - Function should not reach this point, check source')
+    return switchlon
+
+def switchind(longitudes):
+    """
+    Returns the index where a longitude array either crosses 0 or 180 degrees
+
+    Inputs (Required):
+        longitudes - An evenly spaced numpy array of longitude points (degrees)
+
+    Outputs:
+        switchind - An index for where array crosses 0 or 180 degrees, or 0 if no such point exists
+    """
+    gcc.cdim(longitudes)
+    inprange=clonrng(longitudes)
+    if inprange=='ind':
+        switchind=0
+    elif inprange=='neg':
+        switchind=np.nonzero(longitudes>=0)[0][0]
+    elif inprange=='pos':
+        switchind=np.nonzero(longitudes>180)[0][0]
+    return switchind
