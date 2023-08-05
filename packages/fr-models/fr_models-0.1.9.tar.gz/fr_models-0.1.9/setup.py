cb30863239c86a0541f0e8ee1b64e1eb1cef4d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:04 +0000

    Add package fr-models/0.1.9/fr_models-0.1.9.tar.gz

---
+++ packages/fr-models/fr_models-0.1.9.tar.gz/fr_models-0.1.9/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fr_models',
+    version='0.1.9',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'scipy',
+        'torch',
+        'torchdiffeq',
+        # 'torch_interpolations @ git+https://www.github.com/sbarratt/torch_interpolations',
+        'hyc-utils>=0.5.3',
+    ],
+    extras_require={
+        'test': ['pytest', 'pytest-benchmark'],
+    }
+)
