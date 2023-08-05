5a438d584a5f512972419b6e497f110a444b2e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:35 +0000

    Add package console-gpt/0.3/console_gpt-0.3.tar.gz

---
+++ packages/console-gpt/console_gpt-0.3.tar.gz/console_gpt-0.3/README.md
@@ -0,0 +1,5 @@
+### Package helps to interact with Chatgpt 
+
+### Changes : v2 = "Calling the bot"
+
+### Changes : v3 = "Package problems"
---
+++ packages/console-gpt/console_gpt-0.3.tar.gz/console_gpt-0.3/setup.py
@@ -0,0 +1,22 @@
+# __Author__ = "Pranav Chandran"
+# __Date__ = 08-03-2023
+# __Time__ = 11:33
+# __FileName__ = setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='console_gpt',
+    version='0.3', #updated version
+    author='Pranav Chandran',
+    author_email='pranav.chandran@gmail.com',
+    description='A package to interact with ChatbotGPT',
+    long_description=open('README.md').read(),
+    packages=['console_gpt'],
+    install_requires=['openai'],
+    url='',
+    classifiers=[
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
