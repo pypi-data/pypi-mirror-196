8c9bf9ffb6efba274d38fb679e7614091f9ac781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:36 +0000

    Add package hal9/1.0.1/hal9-1.0.1-py3-none-any.whl

---
+++ packages/hal9/hal9-1.0.1-py3-none-any.whl/hal9/core.py
@@ -0,0 +1,36 @@
+import requests
+import time
+import tempfile
+import sys
+import runpy
+
+def get_app(prompt :str) -> str:
+    """Generate an app using that does what the prompt says
+
+    Parameters
+    ----------
+    prompt : str 
+            The prompt that you want the app to achieve.
+    """
+
+    response = requests.post('https://api.hal9.com/api/generator', json = {
+        'prompt': prompt
+    })
+    while not response.ok:
+        response = requests.post('https://api.hal9.com/api/generator', json = {
+        'prompt': prompt
+    })
+    task_id = response.json()['taskid']
+    r = requests.get('https://api.hal9.com/api/generator/'+ str(task_id))
+    print('Loading your app..')
+    if r.ok:
+        while not r.json()['ready']:
+            time.sleep(3)
+            r = requests.get('https://api.hal9.com/api/generator/'+ str(task_id))
+    apps =r.json()['apps']
+    code = apps[0]['code']
+    with tempfile.NamedTemporaryFile(mode = "w+", prefix = 'st_app', suffix='.py') as tmp:
+        tmp.write(code)
+        tmp.seek(0)
+        sys.argv = ["streamlit", "run", tmp.name ]
+        runpy.run_module("streamlit", run_name="__main__")
