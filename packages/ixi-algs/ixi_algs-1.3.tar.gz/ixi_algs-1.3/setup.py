f0bfd83854bb64a4676aef5121809e5a464da0f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:37 +0000

    Add package ixi-algs/1.3/ixi_algs-1.3.tar.gz

---
+++ packages/ixi-algs/ixi_algs-1.3.tar.gz/ixi_algs-1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(name='ixi_algs',
+      version='1.3',
+      description='ixi_algs',
+      packages=['ixi_algs'],
+      author_email='zxcbbtihs37docxh@yandex.ru',
+      zip_safe=False,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/ixslea/ixi_algs",
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+      ],
+    
+      python_requires='>=3.6')
