54e98a6c8ee00d0427e51bb3e9ce0c6336ec9989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:49 +0000

    Add package textwatermark/0.1.9/textwatermark-0.1.9-py3-none-any.whl

---
+++ packages/textwatermark/textwatermark-0.1.9-py3-none-any.whl/textwatermark/templates/homograph_punctuations.py
@@ -0,0 +1,34 @@
+"""Homographs in Punctuations"""
+from textwatermark.defines import WMMethod
+
+# 同形异义-标点
+CONFUSABLES_CHARS = {
+    "!": "ǃ！",
+    '"': "ʺ＂",
+    "%": "٪⁒",
+    "'": "ꞌ᾽",
+    "*": "⁎∗",
+    ",": "٫‚",
+    "-": "‐‑",
+    ".": "ꓸ․",
+    "/": "Ⳇ∕",
+    ":": "˸∶",
+    "?": "ʔॽ",
+    ";": ";；",
+    "\\": "∖⧵",
+    "~": "⁓∼",
+    "‧": "··",
+    "¯": "ˉ‾",
+    # '“': '‟‶',
+    # '”': 'ˮ″',
+    # '‘': 'ʻ‵',
+    # '’': 'ʼ′',
+    # '(': '❨﴾',
+    # ')': '❩﴿',
+    # '<': '˂❮',
+    # '>': '˃❯',
+}
+
+for ikey, ival in CONFUSABLES_CHARS.items():
+    CONFUSABLES_CHARS[ikey] = ikey + ival
+method = WMMethod.FIND_AND_REPLACE
