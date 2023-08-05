0f697dc9beac7127dc54690bcbc35963dfebc619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:39 +0000

    Add package tinto-prueba/0.0.2/tinto_prueba-0.0.2.tar.gz

---
+++ packages/tinto-prueba/tinto_prueba-0.0.2.tar.gz/tinto_prueba-0.0.2/tinto_prueba/probar.py
@@ -0,0 +1,9 @@
+from methods import tinto
+experimento="5"
+src_data = "C:\\Users\\borja\\PycharmProjects\\TINTORERA\\Datasets\\iris.csv"
+folder = "C:\\Users\\borja\\PycharmProjects\\TINTORERA\\imgs\\"+experimento
+tintonera=tinto()
+#tintonera.saveHyperparameters()
+
+tintonera.loadHyperparameters()
+tintonera.generateImages()
