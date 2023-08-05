38bd61e10da9548a74b489cb714850ced35c6385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:37 +0000

    Add package queues-master-new/1.0.2/queues_master_new-1.0.2.tar.gz

---
+++ packages/queues-master-new/queues_master_new-1.0.2.tar.gz/queues_master_new-1.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='queues_master_new',
+    version='1.0.2',
+    author='itskekoff',
+    packages=find_packages(),
+    install_requires=['requests'],
+)
