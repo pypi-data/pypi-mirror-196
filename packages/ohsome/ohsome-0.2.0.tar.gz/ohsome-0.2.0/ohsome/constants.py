ff113e4fee2089187a721e0fb9ad318142a9427d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:13 +0000

    Add package ohsome/0.2.0/ohsome-0.2.0.tar.gz

---
+++ packages/ohsome/ohsome-0.2.0.tar.gz/ohsome-0.2.0/ohsome/constants.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Constants and default values"""
+
+OHSOME_BASE_API_URL = "https://api.ohsome.org/v1/"
+DEFAULT_LOG = True
+DEFAULT_LOG_DIR = "./ohsome_log"
+# update version in pyproject.toml as well
+OHSOME_VERSION = "0.2.0"
