3122b4c24652db857dfba5080f883a0912ef5316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:26 +0000

    Add package yjm-test-pip/0.2/yjm-test-pip-0.2.tar.gz

---
+++ packages/yjm-test-pip/yjm-test-pip-0.2.tar.gz/yjm-test-pip-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='yjm-test-pip',
+    version='0.2',
+    author='yujiaming',
+    author_email='yujiaminger@gmail.com',
+    description='My test Python Package',
+    packages=["yjm-test"],
+    python_requires=">=3.6"
+)
---
+++ packages/yjm-test-pip/yjm-test-pip-0.2.tar.gz/yjm-test-pip-0.2/yjm-test/__init__.py
@@ -0,0 +1,1 @@
+from yjm import message_print
