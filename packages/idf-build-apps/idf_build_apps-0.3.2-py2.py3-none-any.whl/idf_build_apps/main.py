9535a5699ac2e5be1af0f73f19e70ec75d125ad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:54 +0000

    Add package idf-build-apps/0.3.2/idf_build_apps-0.3.2-py2.py3-none-any.whl

---
+++ packages/idf-build-apps/idf_build_apps-0.3.2-py2.py3-none-any.whl/idf_build_apps/main.py
@@ -0,0 +1,191 @@
+# SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: Apache-2.0
+import os
+import re
+import shutil
+from pathlib import Path
+
+from . import LOGGER
+from .app import App
+from .constants import ALL_TARGETS
+from .finder import _find_apps
+from .manifest.manifest import FolderRule, Manifest
+from .utils import BuildError, get_parallel_start_stop
+
+try:
+    import typing as t
+except ImportError:
+    pass
+
+
+def find_apps(
+    paths,  # type: (list[str] | str)
+    target,  # type: str
+    build_system='cmake',  # type: str
+    recursive=False,  # type: bool
+    exclude_list=None,  # type: list[str] | None
+    work_dir=None,  # type: str | None
+    build_dir='build',  # type: str
+    config_rules_str=None,  # type: list[str] | None
+    build_log_path=None,  # type: str | None
+    size_json_path=None,  # type: str | None
+    check_warnings=False,  # type: bool
+    preserve=True,  # type: bool
+    manifest_files=None,  # type: list[str] | str | None
+    default_build_targets=None,  # type: list[str] | str | None
+    depends_on_components=None,  # type: list[str] | str | None
+    manifest_rootpath=None,  # type: str | None
+):  # type: (...) -> list[App]
+    if default_build_targets:
+        if isinstance(default_build_targets, str):
+            default_build_targets = [default_build_targets]
+
+        LOGGER.info('Overriding DEFAULT_BUILD_TARGETS to %s', default_build_targets)
+        FolderRule.DEFAULT_BUILD_TARGETS = default_build_targets
+
+    # always set the manifest rootpath at the very beginning of find_apps
+    Manifest.ROOTPATH = Path(manifest_rootpath or os.curdir).resolve()
+
+    if manifest_files:
+        if isinstance(manifest_files, str):
+            manifest_files = [manifest_files]
+
+        rules = set()
+        for _manifest_file in manifest_files:
+            LOGGER.debug('Loading manifest file: %s', _manifest_file)
+            rules.update(Manifest.from_file(_manifest_file).rules)
+        manifest = Manifest(rules)
+        App.MANIFEST = manifest
+
+    apps = []
+    if isinstance(paths, str):
+        paths = [paths]
+
+    if target == 'all':
+        targets = ALL_TARGETS
+    else:
+        targets = [target]
+
+    if isinstance(depends_on_components, str):
+        depends_on_components = [depends_on_components]
+
+    for target in targets:
+        for path in paths:
+            apps.extend(
+                _find_apps(
+                    path,
+                    target,
+                    build_system=build_system,
+                    recursive=recursive,
+                    exclude_list=exclude_list or [],
+                    work_dir=work_dir,
+                    build_dir=build_dir or 'build',
+                    config_rules_str=config_rules_str,
+                    build_log_path=build_log_path,
+                    size_json_path=size_json_path,
+                    check_warnings=check_warnings,
+                    preserve=preserve,
+                    depends_on_components=depends_on_components,
+                )
+            )
+    apps.sort()
+
+    LOGGER.info('Found %d apps in total', len(apps))
+    return apps
+
+
+def build_apps(
+    apps,  # type: list[App]
+    build_verbose=False,  # type: bool
+    parallel_count=1,  # type: int
+    parallel_index=1,  # type: int
+    dry_run=False,  # type: bool
+    keep_going=False,  # type: bool
+    collect_size_info=None,  # type: t.TextIO | None
+    collect_app_info=None,  # type: t.TextIO | None
+    ignore_warning_strs=None,  # type: list[str] | None
+    ignore_warning_file=None,  # type: t.TextIO | None
+    copy_sdkconfig=False,  # type: bool
+    depends_on_components=None,  # type: list[str] | str | None
+):  # type: (...) -> t.Tuple[int, list[App]] | int
+    ignore_warnings_regexes = []
+    if ignore_warning_strs:
+        for s in ignore_warning_strs:
+            ignore_warnings_regexes.append(re.compile(s))
+    if ignore_warning_file:
+        for s in ignore_warning_file:
+            ignore_warnings_regexes.append(re.compile(s.strip()))
+    App.IGNORE_WARNS_REGEXES = ignore_warnings_regexes
+
+    start, stop = get_parallel_start_stop(len(apps), parallel_count, parallel_index)
+    LOGGER.info('Total %s apps. running build for app %s-%s', len(apps), start, stop)
+
+    failed_apps = []
+    exit_code = 0
+
+    LOGGER.info('Building the following apps:')
+    if apps[start - 1 : stop]:
+        for app in apps[start - 1 : stop]:
+            LOGGER.info('  %s (preserve: %s)', app, app.preserve)
+    else:
+        LOGGER.info('  parallel count is too large. build nothing...')
+
+    actual_built_apps = []
+    for i, app in enumerate(apps):
+        index = i + 1  # we use 1-based
+        if index < start or index > stop:
+            continue
+
+        # attrs
+        app.dry_run = dry_run
+        app.index = index
+        app.verbose = build_verbose
+
+        LOGGER.info('=> Building app %s: %s', index, repr(app))
+        is_built = False
+        try:
+            is_built = app.build(depends_on_components)
+        except BuildError as e:
+            LOGGER.error(str(e))
+            if keep_going:
+                failed_apps.append(app)
+                exit_code = 1
+            else:
+                if depends_on_components:
+                    return 1, actual_built_apps
+                else:
+                    return 1
+        finally:
+            if is_built:
+                actual_built_apps.append(app)
+
+            if collect_app_info:
+                collect_app_info.write(app.to_json() + '\n')
+
+            if collect_size_info:
+                try:
+                    # this may not work if the build is failed
+                    app.collect_size_info(collect_size_info)
+                except Exception as e:
+                    LOGGER.debug(e)
+                    pass
+
+            if copy_sdkconfig:
+                try:
+                    shutil.copy(
+                        os.path.join(app.work_dir, 'sdkconfig'),
+                        os.path.join(app.build_path, 'sdkconfig'),
+                    )
+                except Exception as e:
+                    LOGGER.debug(e)
+                    pass
+
+    if failed_apps:
+        LOGGER.error('Build failed for the following apps:')
+        for app in failed_apps:
+            LOGGER.error('  %s', app)
+
+    if depends_on_components:
+        return exit_code, actual_built_apps
+    else:
+        return exit_code
---
+++ packages/idf-build-apps/idf_build_apps-0.3.2-py2.py3-none-any.whl/idf_build_apps/utils.py
@@ -0,0 +1,202 @@
+# SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: Apache-2.0
+
+import fnmatch
+import logging
+import os
+import re
+import shutil
+import subprocess
+import sys
+
+from . import LOGGER
+
+try:
+    import typing as t
+except ImportError:
+    pass
+
+
+_SDKCONFIG_KV_RE = re.compile(r'^([^#=]+)=(.+)$')
+
+
+def dict_from_sdkconfig(path):  # type: (str) -> dict[str, str]
+    """
+    Parse the sdkconfig file at 'path', return name:value pairs as a dict
+    """
+    result = {}
+    with open(path) as f:
+        for line in f:
+            m = _SDKCONFIG_KV_RE.match(line)
+            if m:
+                val = m.group(2)
+                if val.startswith('"') and val.endswith('"'):
+                    val = val[1:-1]
+                result[m.group(1)] = val
+    return result
+
+
+class ConfigRule:
+    def __init__(self, file_name, config_name):  # type: (str, str) -> None
+        """
+        ConfigRule represents the sdkconfig file and the config name.
+
+        For example:
+            - filename='', config_name='default' — represents the default app configuration, and gives it a name
+                'default'
+            - filename='sdkconfig.*', config_name=None - represents the set of configurations, names match the wildcard
+                value
+
+        :param file_name: name of the sdkconfig file fragment, optionally with a single wildcard ('*' character).
+            can also be empty to indicate that the default configuration of the app should be used
+        :param config_name: name of the corresponding build configuration, or None if the value of wildcard is to be
+            used
+        """
+
+        self.file_name = file_name
+        self.config_name = config_name
+
+
+def config_rules_from_str(rule_strings):  # type: (list[str]) -> list[ConfigRule]
+    """
+    Helper function to convert strings like 'file_name=config_name' into ConfigRule objects
+
+    :param rule_strings: list of rules as strings
+    :return: list of ConfigRules
+    """
+    if not rule_strings:
+        return []
+
+    rules = []
+    for rule_str in rule_strings:
+        items = rule_str.split('=', 2)
+        rules.append(ConfigRule(items[0], items[1] if len(items) == 2 else None))
+    # '' is the default config, sort this one to the front
+    return sorted(rules, key=lambda x: x.file_name)
+
+
+def setup_logging(verbose=0, log_file=None):  # type: (int, str | None) -> None
+    if not verbose:
+        level = logging.WARNING
+    elif verbose == 1:
+        level = logging.INFO
+    else:
+        level = logging.DEBUG
+
+    LOGGER.setLevel(level)
+    if log_file:
+        stream = open(log_file, 'w')
+    else:
+        stream = sys.stderr
+    handler = logging.StreamHandler(stream)
+    handler.setLevel(level)
+    handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S'))
+
+    LOGGER.handlers = [handler]
+    LOGGER.propagate = False
+
+
+def get_parallel_start_stop(total, parallel_count, parallel_index):  # type: (int, int, int) -> (int, int)
+    """
+    Calculate the start and stop indices for a parallel task (1-based).
+
+    :param total: total number of tasks
+    :type total: int
+    :param parallel_count: number of parallel tasks to run
+    :type parallel_count: int
+    :param parallel_index: index of the parallel task to run
+    :type parallel_index: int
+    :return: start and stop indices, [start, stop]
+    :rtype: (int, int)
+    """
+    if parallel_count == 1:
+        return 1, total
+
+    num_builds_per_job = (total + parallel_count - 1) // parallel_count
+
+    _min = num_builds_per_job * (parallel_index - 1) + 1
+    _max = min(num_builds_per_job * parallel_index, total)
+
+    return _min, _max
+
+
+class BuildError(RuntimeError):
+    pass
+
+
+class InvalidCommand(SystemExit):
+    def __init__(self, msg):
+        super().__init__('Invalid Command: ' + msg.strip())
+
+
+def rmdir(path, exclude_file_patterns=None):
+    if not exclude_file_patterns:
+        shutil.rmtree(path, ignore_errors=True)
+        return
+
+    if isinstance(exclude_file_patterns, str):
+        exclude_file_patterns = [exclude_file_patterns]
+
+    for root, dirs, files in os.walk(path, topdown=False):
+        for f in files:
+            fp = os.path.join(root, f)
+            remove = True
+            for pattern in exclude_file_patterns:
+                if pattern and fnmatch.fnmatch(f, pattern):
+                    remove = False
+                    break
+            if remove:
+                os.remove(fp)
+        for d in dirs:
+            try:
+                os.rmdir(os.path.join(root, d))
+            except OSError:
+                pass
+
+
+def find_first_match(pattern, path):
+    for root, _, files in os.walk(path):
+        res = fnmatch.filter(files, pattern)
+        if res:
+            return os.path.join(root, res[0])
+    return None
+
+
+def subprocess_run(
+    cmd,  # type: list[str]
+    log_terminal=True,  # type: bool
+    log_fs=None,  # type: t.TextIO | None
+    check=False,  # type: bool
+):  # type: (...) -> int
+    """
+    Subprocess.run for older python versions
+
+    :param cmd: cmd
+    :type cmd: list[str]
+    :param log_terminal: print to `sys.stdout` if set to `True`
+    :type log_terminal: bool
+    :param log_fs: write to this file stream if not `None`
+    :type log_fs: TextIO
+    :param check: raise `subprocess.CalledProcessError` when return code is non-zero
+    :type check: bool
+    :return: return code
+    :rtype: int
+    """
+    LOGGER.info('Running %s', ' '.join(cmd))
+
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    for line in p.stdout:
+        if isinstance(line, bytes):
+            line = line.decode('utf-8')
+
+        if log_terminal:
+            sys.stdout.write(line)
+
+        if log_fs:
+            log_fs.write(line)
+
+    returncode = p.wait()
+    if check and returncode != 0:
+        raise subprocess.CalledProcessError(returncode, cmd)
+
+    return returncode
