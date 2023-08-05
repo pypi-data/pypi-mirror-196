b2891bb6e4630abcffa11ac7f84115c8e5c97bc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:04 +0000

    Add package market-gpt/0.3.2/market-gpt-0.3.2.tar.gz

---
+++ packages/market-gpt/market-gpt-0.3.2.tar.gz/market-gpt-0.3.2/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "market-gpt",
+    version = "0.3.2",
+    author = "Nero Chen",
+    author_email = "nerocube.tw@gmail.com",
+    description = "Get market sentiment score with the assistance of OpenAI GPT-3.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/NeroCube/market-gpt",
+    project_urls = {
+        "Bug Tracker": "https://github.com/NeroCube/market-gpt/issues",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "market_gpt"},
+    packages = setuptools.find_packages(where="market_gpt"),
+    python_requires = ">=3.7.1",
+    install_requires=['openai >= 0.26.1'],
+    entry_points={
+        'console_scripts': [
+            'market-gpt=market_gpt:__main__',
+        ]
+    }
+)
