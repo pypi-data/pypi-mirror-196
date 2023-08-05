8584368125fb814f50ed2b4a350320d88dd3e7af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:09 +0000

    Add package serpyco-rs/0.7.0/serpyco_rs-0.7.0-cp311-none-win_amd64.whl

---
+++ packages/serpyco-rs/serpyco_rs-0.7.0-cp311-none-win_amd64.whl/serpyco_rs/metadata.py
@@ -0,0 +1,62 @@
+from dataclasses import dataclass
+from decimal import Decimal
+from enum import Enum
+from typing import Union
+
+
+@dataclass(frozen=True)
+class Min:
+    value: Union[int, float, Decimal]
+
+
+@dataclass(frozen=True)
+class Max:
+    value: Union[int, float, Decimal]
+
+
+@dataclass(frozen=True)
+class MinLength:
+    value: int
+
+
+@dataclass(frozen=True)
+class MaxLength:
+    value: int
+
+
+@dataclass(frozen=True)
+class Places:
+    value: int
+
+
+@dataclass(frozen=True)
+class Alias:
+    value: str
+
+
+class Format(Enum):
+    no_format = "no_format"
+    camel_case = "camel_case"
+
+
+@dataclass(frozen=True)
+class FiledFormat:
+    format: Format
+
+
+CamelCase: FiledFormat = FiledFormat(Format.camel_case)
+NoFormat: FiledFormat = FiledFormat(Format.no_format)
+
+
+@dataclass(frozen=True)
+class NoneFormat:
+    omit: bool
+
+
+@dataclass(frozen=True)
+class Discriminator:
+    name: str
+
+
+KeepNone: NoneFormat = NoneFormat(False)
+OmitNone: NoneFormat = NoneFormat(True)
