a6174b6a04af19f5ff56b62195acf90f8c484eba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:45 +0000

    Add package django-to-galaxy/0.6.1/django_to_galaxy-0.6.1-py3-none-any.whl

---
+++ packages/django-to-galaxy/django_to_galaxy-0.6.1-py3-none-any.whl/django_to_galaxy/models/__init__.py
@@ -0,0 +1,6 @@
+from .galaxy_instance import GalaxyInstance  # noqa
+from .galaxy_user import GalaxyUser  # noqa
+from .galaxy_output_file import GalaxyOutputFile  # noqa
+from .history import History  # noqa
+from .invocation import Invocation  # noqa
+from .workflow import Workflow  # noqa
