75bd48d51ff1df8fdb8c0cc7d4f3dcf962ca1706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:33 +0000

    Add package filter-stations/0.2.1/filter_stations-0.2.1.tar.gz

---
+++ packages/filter-stations/filter_stations-0.2.1.tar.gz/filter_stations-0.2.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='filter_stations',
+    version='0.2.1',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Making it easier to navigate and clean station data',
+    author='Austin Kaburia',
+    author_email='kaburiaaustin1@gmail.com',
+    url='https://github.com/kaburia/Packaging/tree/main/filter_stations',
+    install_requires=[
+        'pandas',
+        'requests',
+        'python-dateutil',
+        'argparse',
+        'haversine',
+        'matplotlib',
+        'numpy',
+        'IPython',
+        'json',
+        'folium',
+        'base64',
+        'io'
+    ],
+    entry_points={
+        'console_scripts': [
+            'my-script=filter_stations.my_script:main'
+        ]
+    }
+)
