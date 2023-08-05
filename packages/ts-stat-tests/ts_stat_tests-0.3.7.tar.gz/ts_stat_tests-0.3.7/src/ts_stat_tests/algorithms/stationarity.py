e2cbaa07665775f66f2923115dbdc6a2257ccbf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:08 +0000

    Add package ts-stat-tests/0.3.7/ts_stat_tests-0.3.7.tar.gz

---
+++ packages/ts-stat-tests/ts_stat_tests-0.3.7.tar.gz/ts_stat_tests-0.3.7/src/ts_stat_tests/algorithms/stationarity.py
@@ -0,0 +1,38 @@
+# from pmdarima.arima.stationarity import PPTest, ADFTest, KPSSTest
+# from statsmodels.tsa.api import adfuller, kpss
+# """
+# For a really good article on ADF & KPSS tests, check: [When A Time Series Only Quacks Like A Duck: Testing for Stationarity Before Running Forecast Models. With Python. And A Duckling Picture.](https://towardsdatascience.com/when-a-time-series-only-quacks-like-a-duck-10de9e165e)
+# """
+
+
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+#    Setup                                                                  ####
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+
+# ---------------------------------------------------------------------------- #
+# Imports                                                                   ####
+# ---------------------------------------------------------------------------- #
+
+
+# Python ----
+
+
+# Packages ----
+
+
+# Locals ----
+
+
+# ---------------------------------------------------------------------------- #
+# Exports                                                                   ####
+# ---------------------------------------------------------------------------- #
+
+
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+#    Algorithms                                                             ####
+#                                                                              #
+# ---------------------------------------------------------------------------- #
