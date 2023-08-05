f50553fda4a20b994d71a540ce852eaf74ff75a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:55 +0000

    Add package xclientai/0.0.2/xclientai-0.0.2-py3-none-any.whl

---
+++ packages/xclientai/xclientai-0.0.2-py3-none-any.whl/xclientai/__init__.py
@@ -0,0 +1,15 @@
+__version__ = "0.0.2"
+
+from xclientai.datatypes import (
+    Credentials,
+    Signature,
+    BenchmarkConfig,
+    Accelerator,
+    BenchmarkResults,
+    BenchmarkInfo,
+    AccelerationInfo,
+    Notification,
+    Job
+)
+
+from xclientai.jobs import JobsClient
