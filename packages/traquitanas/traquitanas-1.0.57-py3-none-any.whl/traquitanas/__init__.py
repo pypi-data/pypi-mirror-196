6848ed948a7dcb1ce92422d61d2b4c483f23a155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:28 +0000

    Add package traquitanas/1.0.57/traquitanas-1.0.57-py3-none-any.whl

---
+++ packages/traquitanas/traquitanas-1.0.57-py3-none-any.whl/traquitanas/__init__.py
@@ -0,0 +1,16 @@
+"""
+Used when import all
+Example: from traquitanas import *
+
+https://stackoverflow.com/questions/4902974/python-import-subpackage-from-a-package-not-working
+
+"""
+
+from traquitanas.net import downloads
+from traquitanas.data import convert_to_sql
+from traquitanas.scrapping import adds, gecko, scrapping
+
+# from traquitanas import scrapping
+
+# Net
+__all__ = ['downloads', 'scrapping', 'gecko', 'adds', 'convert_to_sql']
