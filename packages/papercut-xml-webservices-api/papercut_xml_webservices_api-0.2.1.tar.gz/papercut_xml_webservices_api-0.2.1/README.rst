f8874e47ea90f694bde98cc779bea9746407b95d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:48 +0000

    Add package papercut-xml-webservices-api/0.2.1/papercut_xml_webservices_api-0.2.1.tar.gz

---
+++ packages/papercut-xml-webservices-api/papercut_xml_webservices_api-0.2.1.tar.gz/papercut_xml_webservices_api-0.2.1/README.rst
@@ -0,0 +1,15 @@
+============================
+PaperCut XML WebServices API
+============================
+
+.. image:: https://img.shields.io/pypi/v/papercut_xml_webservices_api.svg
+    :target: https://pypi.python.org/pypi/papercut_xml_webservices_api
+
+-  Free software: MIT license
+-   `Documentation <https://github.com/safehome-jdev/PaperCutSoftware/blob/main/api/papercut_xml_webservices_api/README.md>`__
+-  `PaperCut API Docs <https://www.papercut.com/help/manuals/ng-mf/common/tools-web-services/>`__
+
+Credits
+-------
+
+This package was created with `Cookiecutter <https://github.com/audreyr/cookiecutter>`___ and the `audreyr/cookiecutter-pypackage <https://github.com/audreyr/cookiecutter-pypackage>`__ project template.
---
+++ packages/papercut-xml-webservices-api/papercut_xml_webservices_api-0.2.1.tar.gz/papercut_xml_webservices_api-0.2.1/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import find_packages, setup
+
+with open("./README.rst") as readme_file:
+    readme = readme_file.read()
+
+with open("./HISTORY.rst") as history_file:
+    history = history_file.read()
+
+setup(
+    author="Jordan Reyes",
+    author_email="jreyes@safehomedev.com",
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.10",
+    ],
+    description="PaperCut NG/MF server's API uses XML-RPC. This library utilizes Python's [stable XMLRPC library",
+    license="MIT license",
+    long_description=readme + "\n\n" + history,
+    include_package_data=True,
+    keywords=["papercut", "xml", "api", "NG", "MF"],
+    name="papercut_xml_webservices_api",
+    packages=find_packages(
+        include=["papercut_xml_webservices_api", "papercut_xml_webservices_api.*"]
+    ),
+    url="https://github.com/safehome_jdev/PaperCutSoftware",
+    version="0.2.1",
+    zip_safe=False,
+)
