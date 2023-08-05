1b46f6695216bb46d1524148da22ea4d90edfdfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:58 +0000

    Add package inigo-py/0.22.2/inigo_py-0.22.2-py3-none-any.whl

---
+++ packages/inigo-py/inigo_py-0.22.2-py3-none-any.whl/inigo_py/__init__.py
@@ -0,0 +1,13 @@
+import pathlib
+
+from .ffi import get_version
+from .middleware import DjangoMiddleware, FlaskMiddleware, InigoContext
+
+__all__ = [
+    'get_version',
+    'DjangoMiddleware',
+    'FlaskMiddleware',
+    'InigoContext'
+]
+
+__version__ = (pathlib.Path(__file__).parent.resolve() / "VERSION").read_text(encoding="utf-8").strip()
