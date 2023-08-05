78a296ec7d7368a1af83b30839b473a71724d32c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:21 +0000

    Add package pydelling/1.10.6/pydelling-1.10.6-py3-none-any.whl

---
+++ packages/pydelling/pydelling-1.10.6-py3-none-any.whl/pydelling/managers/BaseManager.py
@@ -0,0 +1,151 @@
+from .BaseStudy import BaseStudy
+from abc import ABC, abstractmethod
+from typing import Dict, List, Union
+import logging
+from alive_progress import alive_bar
+from tqdm import tqdm
+from pydelling.utils import create_results_folder
+from docker import DockerClient
+import subprocess
+
+
+logger = logging.getLogger(__name__)
+
+
+class BaseManager(ABC):
+    """This class is the base class for all simulation managers. A simulation manager should be able to:
+    - Manage study [BaseStudy] objects
+    - Run simulations on a given software
+    - Read the output status of the simulation
+    """
+    def __init__(self, name: str = None):
+        self.results_folder = None
+        self.studies: Dict[str, BaseStudy] = {}
+        self.manager_name = name if name is not None else self.__class__.__name__
+        self.is_dummy = False
+
+    def add_study(self, study: BaseStudy):
+        """This method adds a study to the manager.
+        """
+        assert isinstance(study, BaseStudy), f"Study must be a object from a class inherited from BaseStudy, not {type(study)}"
+        self.studies[study.name] = study
+
+    def run(self,
+            studies_folder: str = './studies',
+            n_cores: int = 1,
+            docker_image: str = None,
+            dummy: bool = False,
+            start_from: int = None,
+            petsc_dir: str = '/opt/pflotran-dev/petsc',
+            petsc_arch: str = 'arch-linux-c-opt',
+            pre_commands: List[str] = None,
+            **kwargs,
+            ):
+        """This method runs all the studies.
+        """
+        self.is_dummy = dummy
+        # Initialize callbacks
+        for study in self.studies.values():
+            study.initialize_callbacks(self)
+
+        self.results_folder = create_results_folder(studies_folder)
+        # self.generate_run_files(studies_folder=studies_folder)
+        # Run the precommands if any
+        if pre_commands is not None:
+            for command in pre_commands:
+                logger.info(f"Running precommand: {command}")
+                subprocess.run(command, shell=True)
+        for study in tqdm(self.studies.values(), desc="Running studies", colour="white"):
+            study: BaseStudy
+            if start_from is not None:
+                if study.idx < start_from:
+                    logger.info(
+                        f"Skipping study {study.name} (idx: {study.idx}) because start_from is set to {start_from}")
+                    self.run_study(study,
+                                   docker_image=docker_image,
+                                   n_cores=n_cores,
+                                   dummy=True,
+                                   petsc_dir=petsc_dir,
+                                   petsc_arch=petsc_arch,
+                                   **kwargs)
+                    continue
+            self.run_study(study,
+                           docker_image=docker_image,
+                           n_cores=n_cores,
+                           dummy=dummy,
+                           petsc_dir=petsc_dir,
+                           petsc_arch=petsc_arch,
+                           **kwargs)
+
+    def generate_run_files(self, studies_folder: str = './studies'):
+        """This method generates the run files for all the studies.
+        """
+        for study in self.studies.values():
+            study: BaseStudy
+            study.to_file(self.results_folder / study.name)
+
+    @abstractmethod
+    def _run_study(self, study: BaseStudy, n_cores: int = 1, **kwargs):
+        """This method runs a study.
+        """
+        logger.info(f"Running study {study.name}")
+        pass
+
+    @abstractmethod
+    def _run_study_docker(self,
+                          study: BaseStudy,
+                          docker_image: str,
+                          n_cores: int = 1,
+                          **kwargs,
+                          ):
+        """This method runs a study using docker.
+        """
+        logger.info(f"Running study {study.name} using docker image {docker_image}")
+
+    def run_study(self,
+                  study: BaseStudy,
+                  n_cores: int = 1,
+                  docker_image: str = None,
+                  dummy: bool = False,
+                  **kwargs,
+                  ):
+        """This method runs a study.
+        """
+        logger.info(f"Running study {study.name}")
+        # Create the study files
+        study.output_folder = self.results_folder / study.name
+        study.pre_run()
+        for callback in study.callbacks:
+            if callback.kind == 'pre':
+                callback.run()
+        study.to_file(self.results_folder / study.name)
+        # Run the study
+        if docker_image is None:
+            if dummy:
+                logger.info("Dummy run, not running the study")
+            else:
+                self._run_study(study, n_cores=n_cores, **kwargs)
+        else:
+            self._run_study_docker(study, docker_image, n_cores=n_cores, **kwargs)
+
+        for callback in study.callbacks:
+            if callback.kind == 'post':
+                callback.run()
+        study.post_run()
+
+
+    @property
+    def n_studies(self):
+        """This method returns the number of studies.
+        """
+        return len(self.studies)
+
+    def merge_studies(self):
+        """This methods merges the studies generated by the manager.
+        """
+        pass
+
+
+
+
+
---
+++ packages/pydelling/pydelling-1.10.6-py3-none-any.whl/pydelling/managers/PflotranManager.py
@@ -0,0 +1,144 @@
+from .BaseManager import BaseManager
+from . import PflotranStudy
+import subprocess
+import docker
+from docker.models.containers import Container
+from io import BytesIO
+from pathlib import Path
+from pydelling.utils import create_results_folder
+from pydelling.managers.PflotranPostprocessing import PflotranPostprocessing
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+class PflotranManager(BaseManager):
+    def _get_study_status(self, study_id: int):
+        """This method returns the status of a study.
+        """
+        pass
+
+    def _run_study(self,
+                   study: PflotranStudy,
+                   n_cores: int = 1,
+                   petsc_dir: str = '/opt/pflotran-dev/petsc',
+                   petsc_arch: str = 'arch-linux-c-opt',
+                   **kwargs,
+                   ):
+        """This method runs a study.
+        """
+        if n_cores == 1:
+            # Run the study in serial
+            subprocess.run(['pflotran', '-pflotranin', study.input_file_name], cwd=study.output_folder.absolute())
+        else:
+            # Run the study in parallel
+            os.environ['PETSC_DIR'] = petsc_dir
+            os.environ['PETSC_ARCH'] = petsc_arch
+            subprocess.call([f'$PETSC_DIR/$PETSC_ARCH/bin/mpirun -np {n_cores} pflotran -pflotranin {study.input_file_name}'],
+                           cwd=study.output_folder.absolute(),
+                           shell=True
+                           )
+
+    def _run_study_docker(self,
+                          study: PflotranStudy,
+                          docker_image: str,
+                          n_cores: int = 1,
+                          **kwargs,
+                          ):
+        """This method runs a study using docker.
+        """
+        docker_client = docker.from_env()
+            # Run the study in serial
+
+        try:
+            docker_client.api.start(self.manager_name)
+            container = docker_client.containers.get(self.manager_name)
+        except:
+            docker_client.api.create_container(
+                name=self.manager_name,
+                image=docker_image,
+                command='/bin/sh',
+                detach=True,
+                tty=True,
+                volumes=[f'/home/pflotran/'],
+                host_config=docker_client.api.create_host_config(
+                    binds={
+                        Path().cwd() / f'studies/{study.name}': {
+                            'bind': f'/home/pflotran/{study.name}',
+                            'mode': 'rw',
+                        }
+                    })
+            )
+            docker_client.api.start(container=self.manager_name)
+            container = docker_client.containers.get(self.manager_name)
+
+        # Compress study folder to a tar file
+        bytes_io = BytesIO()
+        import tarfile
+        pw_tar = tarfile.TarFile(fileobj=bytes_io, mode='w')
+        pw_tar.add(study.output_folder, arcname=study.output_folder.name)
+        pw_tar.close()
+        bytes_io.seek(0)
+        # volume = docker_client.volumes.create(name=study.output_folder.name, driver='local', driver_opts={'type': 'none', 'device': study.output_folder, 'o': 'bind'})
+        # Copy the tar file to the container
+        container.exec_run(cmd="bash -c 'cd /home/pflotran'")
+        container.put_archive('/home/pflotran', bytes_io)
+        container.exec_run(cmd="bash -c 'ls'")
+        if n_cores == 1:
+            container.exec_run(cmd=f"sudo bash -c 'export PATH=$PATH:/home/pflotran/pflotran/src/pflotran &&"
+                                   f" cd /home/pflotran/{study.output_folder.name} && "
+                                   f"pflotran -pflotranin {study.input_file_name}'")
+        else:
+            result = container.exec_run(cmd=f"sudo bash -c 'export PATH=$PATH:/home/pflotran/pflotran/src/pflotran && "
+                                   f"export PETSC_DIR=/home/pflotran/petsc && "
+                                   f"export PETSC_ARCH=docker-petsc && "
+                                   f"cd /home/pflotran/{study.output_folder.name} && "
+                                   f"$PETSC_DIR/$PETSC_ARCH/bin/mpiexec -np {n_cores} pflotran -pflotranin {study.input_file_name}'")
+        container.stop()
+        container.remove()
+
+    def merge_results(self, move=False, postprocess=True):
+        """This method merges the results of all the studies.
+        """
+        self.run(dummy=True)
+        import shutil
+        # results_folder = create_results_folder(list(self.studies.values())[0].output_folder)
+        self.merged_folder = create_results_folder(self.results_folder / 'merged_results')
+        for study in self.studies.values():
+            hdf5_files = list(study.output_folder.glob('*.h5'))
+            for file in hdf5_files:
+                if 'restart' not in file.name:
+                    if not move:
+                        shutil.copy(file, self.results_folder / 'merged_results')
+                    else:
+                        shutil.move(file, self.results_folder / 'merged_results')
+
+        # Copy -domain.h5 file
+
+
+        if postprocess:
+            domain_folder = list(self.studies.values())[0].output_folder / 'input_files'
+            domain_file = list(domain_folder.glob('*-domain.h5'))[0]
+            shutil.copy(domain_file, self.results_folder / 'merged_results')
+            logger.info('Postprocessing results')
+            pflotran_postprocesser = PflotranPostprocessing()
+            # Change the working directory to the results folder
+            import os
+            os.chdir(self.results_folder / 'merged_results')
+            pflotran_postprocesser.run()
+            # Return to the original working directory
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
