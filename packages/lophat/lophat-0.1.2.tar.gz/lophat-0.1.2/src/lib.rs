76015189b221de7e93738565d1158a4e9a38ad1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:46 +0000

    Add package lophat/0.1.2/lophat-0.1.2.tar.gz

---
+++ packages/lophat/lophat-0.1.2.tar.gz/lophat-0.1.2/src/lib.rs
@@ -0,0 +1,43 @@
+use column::VecColumn;
+use pyo3::prelude::*;
+use pyo3::types::PyIterator;
+
+mod column;
+mod decomposition;
+mod lock_free;
+
+pub use column::Column;
+pub use decomposition::{rv_decompose, PersistenceDiagram, RVDecomposition};
+pub use lock_free::rv_decompose_lock_free;
+
+#[pyfunction]
+fn compute_pairings_serial(matrix: &PyIterator) -> PersistenceDiagram {
+    rv_decompose(matrix.map(|col| {
+        VecColumn {
+            internal: col
+                .and_then(PyAny::extract::<Vec<usize>>)
+                .expect("Column is a list of unsigned integers"),
+        }
+    }))
+    .diagram()
+}
+
+#[pyfunction]
+fn compute_pairings(matrix: &PyIterator) -> PersistenceDiagram {
+    rv_decompose_lock_free(matrix.map(|col| {
+        VecColumn {
+            internal: col
+                .and_then(PyAny::extract::<Vec<usize>>)
+                .expect("Column is a list of unsigned integers"),
+        }
+    }))
+    .diagram()
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn lophat(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(compute_pairings, m)?)?;
+    m.add_function(wrap_pyfunction!(compute_pairings_serial, m)?)?;
+    Ok(())
+}
