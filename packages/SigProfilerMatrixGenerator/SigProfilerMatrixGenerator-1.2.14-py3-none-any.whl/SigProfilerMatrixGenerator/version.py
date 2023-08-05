b810feb708cde3786aa5bd9c25cc5f162093a178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:31 +0000

    Add package SigProfilerMatrixGenerator/1.2.14/SigProfilerMatrixGenerator-1.2.14-py3-none-any.whl

---
+++ packages/SigProfilerMatrixGenerator/SigProfilerMatrixGenerator-1.2.14-py3-none-any.whl/SigProfilerMatrixGenerator/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEMATRIXGENERATOR SETUP.PY
+short_version = '1.2.14'
+version = '1.2.14'
+Update = 'v1.2.14: Resolve issues with rn6 and yeast. Add basic SBS tests.'
+	
+	
