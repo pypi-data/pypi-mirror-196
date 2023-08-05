49f87b2b4e3fc7a3c63723d32b23803dfb7bba4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:10 +0000

    Add package console-gpt/0.2/console_gpt-0.2.tar.gz

---
+++ packages/console-gpt/console_gpt-0.2.tar.gz/console_gpt-0.2/README.md
@@ -0,0 +1,2 @@
+### Package helps to interact with Chatgpt
+### Changes : v2 = "Calling the bot"
---
+++ packages/console-gpt/console_gpt-0.2.tar.gz/console_gpt-0.2/console_gpt/console_gpt.py
@@ -0,0 +1,45 @@
+__Author__ = "Pranav Chandran"
+__Date__ = "08/03/2023"
+__FileName__ = "main.py"
+
+"""
+This is the code for connecting to ChatbotGPT and getting the response
+Set up your OpenAI API credentials: To use the ChatGPT API, you will need 
+to set up your OpenAI API credentials.
+"""
+import openai
+
+
+class ChatbotGPT:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        openai.api_key = self.api_key
+
+    @staticmethod
+    def get_response(prompt):
+        response = openai.Completion.create(
+            engine="davinci",
+            prompt=prompt,
+            max_tokens=60,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
+        return response.choices[0].text.strip()
+
+    def ask(self):
+        #  ask the user to give input
+        prompt = input("You: ")
+        return self.get_response(prompt)
+
+
+if __name__ == "__main__":
+    # Set up your OpenAI API credentials
+    key = "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"
+    chatbot = ChatbotGPT(key)
+    while True:
+        print("Chatbot: ", chatbot.ask())
+
+
+
+
