63d23855c038c1837e924d3a4112ae80c8ddf153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:16 +0000

    Add package pyseek/0.2.1/pyseek-0.2.1-py3-none-any.whl

---
+++ packages/pyseek/pyseek-0.2.1-py3-none-any.whl/pyseek/config.py
@@ -0,0 +1,66 @@
+import configparser
+from typing import List
+from pyseek import SUCCESS, DIR_ERROR, FILE_ERROR, CONFIG_ERROR
+from pathlib import Path
+from pyseek import setup
+
+
+def create_file(
+    configuration_directory: str = setup.CONFIGURATION_DIRECTORY,
+    filename: str = "config.ini",
+) -> int:
+    """Take a configuration directory and create a file in it.
+
+    Args:
+        configuration_directory (str): the configuration directory
+        filename (str, optional): the name of the file to create. Defaults to "config.ini".
+
+    Returns:
+        int: the result code of the operation
+    """
+    try:
+        configuration_directory = Path(configuration_directory)
+        configuration_directory.mkdir(exist_ok=True)
+    except OSError:
+        return DIR_ERROR
+    try:
+        configuration_file = configuration_directory / filename
+        configuration_file.touch(exist_ok=True)
+    except OSError:
+        return FILE_ERROR
+    return SUCCESS
+
+
+def init_config(user_agent: str):
+    """Called from the pyseek init command
+    creates a configuration directory specified at config_file_dir
+    and a configuration file in that directory called config.ini.
+    The configuration file is a .ini file with a section called API
+    """
+    f = "config.ini"
+    result = create_file(filename=f)
+    if result == SUCCESS:
+        config = configparser.ConfigParser()
+        config["API"] = {"User-Agent": user_agent}
+        config["TickerUpdateFrequency"] = {"Frequency": "never"}
+        try:
+            with open(Path(setup.CONFIGURATION_DIRECTORY) / f, "w") as configfile:
+                config.write(configfile)
+        except OSError:
+            return CONFIG_ERROR
+        return SUCCESS
+    else:
+        return result
+
+
+def get_api_settings() -> configparser.SectionProxy:
+    """Get the API settings from the config file"""
+    settings_file = Path(setup.CONFIGURATION_DIRECTORY) / "config.ini"
+    config = configparser.ConfigParser()
+    config.read(settings_file)
+    return config["API"]
+
+
+if __name__ == "__main__":
+    setting = get_api_settings()
+    print(setting)
---
+++ packages/pyseek/pyseek-0.2.1-py3-none-any.whl/pyseek/utils.py
@@ -0,0 +1,102 @@
+import json
+from typing import TypeVar
+
+import requests
+from pathlib import Path
+from pyseek import models
+from pyseek import setup
+from pyseek import config
+
+
+centralIndexKey = TypeVar("centralIndexKey", str, int, models.CIK)
+
+
+def set_headers() -> dict:
+    """Set the headers for the requests call"""
+    settings = config.get_api_settings()
+    return {"User-Agent": settings["User-Agent"]}
+
+
+def make_request(url: str, requestTimeout: int = 5) -> dict:
+    """Handles all the requests calls for the package
+
+    Args:
+        url (str): url to request
+
+    Returns:
+        dict: the json returned
+    """
+    try:
+        r = requests.get(url, headers=set_headers(), timeout=requestTimeout)
+        r.raise_for_status()
+        return r.json()
+    except requests.ConnectionError:
+        print("there was a connection error")
+    except requests.JSONDecodeError:
+        print(f"There was a JSON decode error for url: {url}")
+        print(f"Check the url for errors. If the url is correct, try again later.")
+
+
+def company_from_ticker(ticker: str) -> int:
+    """Use the company_tickers.json file downloaded in `pyseek init` to get the CIK number for the company
+
+    Args:
+        ticker (str): ticker to look up
+
+    Returns:
+        int: company information for a given ticker
+    """
+    ticker = ticker.upper()
+    with open(Path(setup.CONFIGURATION_DIRECTORY) / "company_tickers.json", "r") as fp:
+        data = json.load(fp)
+    return [company for company in data.values() if company["ticker"] == ticker]
+
+
+def company_from_cik(cik: int) -> str:
+    """Use the company_tickers.json file downloaded in `pyseek init` to get the ticker for the company
+
+    Args:
+        cik (int): cik number to look up
+
+    Returns:
+        str: company information for given cik number
+    """
+    with open(Path(setup.CONFIGURATION_DIRECTORY) / "company_tickers.json", "r") as fp:
+        data = json.load(fp)
+    return [company for company in data.values() if company["cik_str"] == cik]
+
+
+def download_document(
+    cik: centralIndexKey, accession_number: str, primaryDocument: str
+):
+    """Download a company submission
+
+    Args:
+        cik (central_index_key): CIK, str, int
+        accession_number (str): Accession number of the submission
+        primaryDocument (str): Name of the primary document
+
+    Returns:
+        str: The submission as a string
+    """
+    try:
+        response = requests.get(
+            f"https://www.sec.gov/Archives/edgar/data/{cik}/{accession_number.replace('-', '')}/{primaryDocument}",
+            headers=set_headers(),
+        )
+        response.raise_for_status()
+    except requests.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        print(f"Other error occurred: {err}")
+    else:
+        print("Success!")
+    return response.text
+
+
+def write_file(
+    obj: dict,
+    filename: str,
+):
+    with open(filename, "w") as fp:
+        json.dump(obj, fp, indent=4)
