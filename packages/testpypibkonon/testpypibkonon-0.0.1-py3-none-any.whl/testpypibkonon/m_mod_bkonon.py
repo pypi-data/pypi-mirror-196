104f5140e2eaba38f9b74b94b95d6c5297afa12b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:47 +0000

    Add package testpypibkonon/0.0.1/testpypibkonon-0.0.1-py3-none-any.whl

---
+++ packages/testpypibkonon/testpypibkonon-0.0.1-py3-none-any.whl/testpypibkonon/m_mod_bkonon.py
@@ -0,0 +1,17 @@
+import numpy
+
+def name(first, last):
+    """Returns full data of parameteres"""
+    full = first + " " + last
+    return full.title()
+
+def get_numpy_arr(lst):
+    """Returns array of numpy package"""
+
+    return numpy.array(lst)
+
+if __name__=='__main__':
+    print('Adrian', 'Zapała')
+    print(get_numpy_arr([1,2,3,4,5]))
+
+
