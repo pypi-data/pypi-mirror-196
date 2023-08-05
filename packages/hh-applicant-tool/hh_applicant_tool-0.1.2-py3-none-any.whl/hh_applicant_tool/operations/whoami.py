2354960423826d3057592002387c8aaf3df3cf3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:46 +0000

    Add package hh-applicant-tool/0.1.2/hh_applicant_tool-0.1.2-py3-none-any.whl

---
+++ packages/hh-applicant-tool/hh_applicant_tool-0.1.2-py3-none-any.whl/hh_applicant_tool/operations/whoami.py
@@ -0,0 +1,29 @@
+# Этот модуль можно использовать как образец для других
+import argparse
+import logging
+
+from ..api import ApiClient
+from ..main import BaseOperation
+from ..main import Namespace as BaseNamespace
+from ..utils import dumps
+
+logger = logging.getLogger(__package__)
+
+
+class Namespace(BaseNamespace):
+    pass
+
+
+class Operation(BaseOperation):
+    """Выведет текущего пользователя"""
+
+    def setup_parser(self, parser: argparse.ArgumentParser) -> None:
+        pass
+
+    def run(self, args: Namespace) -> None:
+        assert args.config["token"]
+        api = ApiClient(
+            access_token=args.config["token"]["access_token"],
+        )
+        result = api.get("/me")
+        print(dumps(result))
