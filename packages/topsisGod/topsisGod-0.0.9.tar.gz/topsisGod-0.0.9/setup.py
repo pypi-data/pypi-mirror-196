ac8ab1539d6d298defb1348d49368967c0736bc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:59 +0000

    Add package topsisGod/0.0.9/topsisGod-0.0.9.tar.gz

---
+++ packages/topsisGod/topsisGod-0.0.9.tar.gz/topsisGod-0.0.9/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = 'topsisGod',
+    version = '0.0.9',
+    description = 'Calculation of Topsis Score',
+    py_modules = ['topsisGod'],
+    package_dir = {'':'src'},
+    classifiers=[
+        "Operating System :: OS Independent",
+    ],
+    long_description=long_description,
+    long_description_content_type = "text/markdown",
+    author="Sahil Chhabra",
+    author_email="sahil.chh718@gmail.com",
+)
