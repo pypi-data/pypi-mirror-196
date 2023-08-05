2b4d0a455bab5753ea22cff3aed2665ccbd92c1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:29 +0000

    Add package flet-django/0.2.6/flet_django-0.2.6-py3-none-any.whl

---
+++ packages/flet-django/flet_django-0.2.6-py3-none-any.whl/flet_django/middlewares.py
@@ -0,0 +1,64 @@
+from abc import ABC
+from dataclasses import dataclass
+from typing import Callable
+
+from django.urls import resolve, Resolver404
+
+import flet as ft
+from flet_core import Control
+
+from .routes import FtViewRoute
+from .types import PAGE_CLASS
+from .views import ft_view
+
+
+@dataclass
+class GenericMiddleware(ABC):
+    page: PAGE_CLASS
+    route: str
+
+    def parse_route(self):
+        return None
+
+    def parse_view(self, view):
+        return view
+
+
+def simple_view_middleware(
+    controls: list[Control] = None,
+    text: str = None,
+    **kwargs
+):
+    if controls is None:
+        controls = [ft.Text(text)]
+
+    class SimpeViewMiddleware(GenericMiddleware):
+
+        def parse_route(self):
+            def __get_view(page):
+                return ft_view(
+                    page=page,
+                    controls=controls,
+                    **kwargs
+                )
+            return __get_view
+
+    return SimpeViewMiddleware
+
+
+def urls_middleware(urls: tuple):
+    class UrlsMiddleware(GenericMiddleware):
+        def parse_route(self):
+            try:
+                result = resolve(self.route, urls)
+                return FtViewRoute(
+                    control=result.func,
+                    name=result.url_name,
+                    route=self.route,
+                    args=result.args,
+                    kwargs=result.kwargs
+                )
+            except Resolver404:
+                return None
+
+    return UrlsMiddleware
---
+++ packages/flet-django/flet_django-0.2.6-py3-none-any.whl/flet_django/types.py
@@ -0,0 +1,4 @@
+from typing import TypeVar
+
+DESTINATION_CLASS = TypeVar('DESTINATION_CLASS', bound='Fatum')
+PAGE_CLASS = TypeVar('PAGE_CLASS', bound='GenericPage')
