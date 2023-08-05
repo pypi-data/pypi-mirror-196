e687936ff98d9d9c7fd52002ac070b32d427b21c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:09 +0000

    Add package joern2sarif/1.1.12/joern2sarif-1.1.12.tar.gz

---
+++ packages/joern2sarif/joern2sarif-1.1.12.tar.gz/joern2sarif-1.1.12/joern2sarif/lib/config.py
@@ -0,0 +1,12 @@
+default_driver_name = "Joern"
+
+tool_drivers = {
+    "joern": "Joern",
+    "ocular": "ShiftLeft Ocular",
+    "ng-sast": "ShiftLeft NextGen Analysis",
+    "ngsast": "ShiftLeft NextGen Analysis",
+    "core": "ShiftLeft CORE Analysis",
+}
+
+# URL for viewing reports online
+hosted_viewer_uri = "https://sarifviewer.azurewebsites.net"
