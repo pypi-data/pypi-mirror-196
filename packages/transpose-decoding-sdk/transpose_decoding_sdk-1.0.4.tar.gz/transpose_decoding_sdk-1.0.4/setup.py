4393898218ef8ee37781c3c53a6c730d56fc468e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:23 +0000

    Add package transpose-decoding-sdk/1.0.4/transpose_decoding_sdk-1.0.4.tar.gz

---
+++ packages/transpose-decoding-sdk/transpose_decoding_sdk-1.0.4.tar.gz/transpose_decoding_sdk-1.0.4/setup.py
@@ -0,0 +1,44 @@
+from setuptools import find_packages, setup
+with open('README.md', 'r') as f: long_description = f.read()
+    
+setup(
+    name='transpose_decoding_sdk',
+    version='1.0.4',
+    
+    # meta
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    description='The Transpose Decoding SDK is a Python package that makes decoding contract activity on EVM blockchains as simple as possible. Simply specify a contract address and ABI to start streaming historical or live activity across decoded logs, transactions, and traces.',
+    keywords=['web3', 'defi', 'ethereum', 'transpose', 'polygon', 'goerli', 'abi', 'decode', 'event', 'log', 'traces', 'rpc', 'api'],
+    license='MIT',
+    
+    # classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Topic :: Database',
+        'Topic :: Utilities'
+    ],
+
+    # homepage
+    url='https://github.com/TransposeData/transpose-decoding-sdk',
+    
+    # author
+    author='Alex Langshur (alangshur)',
+    author_email='alex@transpose.io',
+    
+    # packages
+    packages=find_packages(exclude=['demo', 'tests']),
+
+    # dependencies
+    install_requires=[
+        'eth-event',
+        'pip-chill',
+        'dateutils',
+        'web3'
+    ]
+)
