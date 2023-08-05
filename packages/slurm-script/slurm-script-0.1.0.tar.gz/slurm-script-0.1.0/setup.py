da9fbb706565ab78477778801edd75abba39e5e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:31 +0000

    Add package slurm-script/0.1.0/slurm-script-0.1.0.tar.gz

---
+++ packages/slurm-script/slurm-script-0.1.0.tar.gz/slurm-script-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['slurm_script']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['pomodoro = slurm_sciprt.main:main']}
+
+setup_kwargs = {
+    'name': 'slurm-script',
+    'version': '0.1.0',
+    'description': 'Python Slurm script interface',
+    'long_description': '# `slurm_script` Python Slurm command generator\n\nThis package is designed to generate slurm script to submit `mpi` jobs to a cluster.\n',
+    'author': 'Kyoungseoun Chung',
+    'author_email': 'kyoungseoun.chung@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
