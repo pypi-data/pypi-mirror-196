401943dd0e9fa094a27bb6578bf05b3462138cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:31 +0000

    Add package streamlit-searchbox/0.1.0/streamlit_searchbox-0.1.0-py3-none-any.whl

---
+++ packages/streamlit-searchbox/streamlit_searchbox-0.1.0-py3-none-any.whl/streamlit_searchbox/frontend/build/precache-manifest.2c50c76c9a41e203102090d4cedaa34b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f97844e92582184c7f0602a613b41dfe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "896ea15d566adc5aaeb9",
+    "url": "./static/js/2.1e75748d.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.1e75748d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "797df9b589a1f9c75e43",
+    "url": "./static/js/main.3cbbd599.chunk.js"
+  },
+  {
+    "revision": "6a4557bc8e92ab112127",
+    "url": "./static/js/runtime-main.1401e97d.js"
+  },
+  {
+    "revision": "986104ad1bef72ea38e3f318ac48a3b9",
+    "url": "./static/media/st-arrow.986104ad.svg"
+  },
+  {
+    "revision": "c801c2937becbac9f123a134fec6155c",
+    "url": "./static/media/st-clear.c801c293.svg"
+  }
+]);
