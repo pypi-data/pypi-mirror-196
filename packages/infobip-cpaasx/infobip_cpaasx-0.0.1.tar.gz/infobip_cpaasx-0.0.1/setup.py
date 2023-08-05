16153c2c8a73bad02e38cdc3d578db7767e22c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:10 +0000

    Add package infobip-cpaasx/0.0.1/infobip_cpaasx-0.0.1.tar.gz

---
+++ packages/infobip-cpaasx/infobip_cpaasx-0.0.1.tar.gz/infobip_cpaasx-0.0.1/setup.py
@@ -0,0 +1,54 @@
+# coding: utf-8
+
+"""
+    This class is auto generated from the Infobip OpenAPI specification
+    through the OpenAPI Specification Client API libraries (Re)Generator (OSCAR),
+    powered by the OpenAPI Generator (https://openapi-generator.tech).
+"""
+
+
+from setuptools import setup, find_packages  # noqa: H301
+
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+NAME = "infobip_cpaasx"
+VERSION = "0.0.1"
+PYTHON_REQUIRES = ">=3.7"
+REQUIRES = ["urllib3 >= 1.25.3", "python-dateutil", "pydantic", "aenum"]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="Infobip CPAASX Python Client Library",
+    author="Infobip Ltd.",
+    author_email="support@infobip.com",
+    url="https://pypi.org/project/infobip-cpaasx-python-client",
+    keywords=[
+        "sms",
+        "mms",
+        "numbers",
+        "cpaas",
+        "infobip"
+    ],
+    install_requires=REQUIRES,
+    packages=find_packages(exclude=["test", "tests"]),
+    include_package_data=True,
+    license="MIT",
+    long_description_content_type="text/markdown",
+    long_description="""\
+    Infobip CPAASX Python Client Library.  # noqa: E501
+    """,
+    classifiers=[
+        "Intended Audience :: Telecommunications Industry",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Libraries",
+    ],
+)
