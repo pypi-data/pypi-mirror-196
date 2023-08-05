eee4582e805b4e80ca19ff12781ee43ca8a9a2b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:14 +0000

    Add package dm-env-wrappers/0.0.9/dm_env_wrappers-0.0.9.tar.gz

---
+++ packages/dm-env-wrappers/dm_env_wrappers-0.0.9.tar.gz/dm_env_wrappers-0.0.9/README.md
@@ -0,0 +1,23 @@
+# `dm_env_wrappers`
+
+[![PyPI Python Version][pypi-versions-badge]][pypi]
+[![PyPI version][pypi-badge]][pypi]
+[![dm_env_wrappers-tests][tests-badge]][tests]
+
+[pypi-versions-badge]: https://img.shields.io/pypi/pyversions/dm_env_wrappers
+[pypi-badge]: https://badge.fury.io/py/dm_env_wrappers.svg
+[pypi]: https://pypi.org/project/dm_env_wrappers/
+[tests-badge]: https://github.com/kevinzakka/dm_env_wrappers/actions/workflows/build.yml/badge.svg
+[tests]: https://github.com/kevinzakka/dm_env_wrappers/actions/workflows/build.yml
+
+`dm_env_wrappers` is a collection of frequently-used wrappers for environments that respect the [dm_env](https://github.com/deepmind/dm_env) interface (e.g., dm_control suite/composer envs).
+
+## Installation (Python 3.8+)
+
+```bash
+pip install --upgrade dm_env_wrappers
+```
+
+## Contributing
+
+If you find a bug or would like to add a new wrapper, please feel free to open an [issue](https://github.com/kevinzakka/dm_env_wrappers/issues) or submit a [PR](https://github.com/kevinzakka/dm_env_wrappers/pulls)!
---
+++ packages/dm-env-wrappers/dm_env_wrappers-0.0.9.tar.gz/dm_env_wrappers-0.0.9/dm_env_wrappers/__init__.py
@@ -0,0 +1,51 @@
+"""dm_env_wrappers: A collection of wrappers for dm_env environments."""
+
+from dm_env_wrappers._src.action_repeat import ActionRepeatWrapper
+from dm_env_wrappers._src.base import EnvironmentWrapper, wrap_all
+from dm_env_wrappers._src.canonical_spec import CanonicalSpecWrapper
+from dm_env_wrappers._src.concatenate_observations import ConcatObservationWrapper
+from dm_env_wrappers._src.episode_statistics import EpisodeStatisticsWrapper
+from dm_env_wrappers._src.expand_scalar_observation_shapes import (
+    ExpandScalarObservationShapesWrapper,
+)
+from dm_env_wrappers._src.frame_stacking import FrameStackingWrapper
+from dm_env_wrappers._src.mujoco.dm_control_video import DmControlVideoWrapper
+from dm_env_wrappers._src.mujoco.dm_control import DmControlWrapper
+from dm_env_wrappers._src.observation_action_reward import (
+    ObservationActionRewardWrapper,
+)
+from dm_env_wrappers._src.single_precision import SinglePrecisionWrapper
+from dm_env_wrappers._src.step_limit import StepLimitWrapper
+from dm_env_wrappers._src.validate_spec import ValidateActionSpecWrapper
+from dm_env_wrappers._src.video import VideoWrapper
+
+__version__ = "0.0.9"
+
+__all__ = (
+    "ActionRepeatWrapper",
+    "CanonicalSpecWrapper",
+    "ConcatObservationWrapper",
+    "DmControlWrapper",
+    "DmControlVideoWrapper",
+    "EnvironmentWrapper",
+    "EpisodeStatisticsWrapper",
+    "ExpandScalarObservationShapesWrapper",
+    "FrameStackingWrapper",
+    "ObservationActionRewardWrapper",
+    "SinglePrecisionWrapper",
+    "StepLimitWrapper",
+    "ValidateActionSpecWrapper",
+    "VideoWrapper",
+    "wrap_all",
+)
+
+#  _________________________________________
+# / Please don't use symbols in `_src` they \
+# \ are not part of the public API.         /
+#  -----------------------------------------
+#         \   ^__^
+#          \  (oo)\_______
+#             (__)\       )\/\
+#                 ||----w |
+#                 ||     ||
+#
