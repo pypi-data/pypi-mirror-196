79a4df64e93a2656cc2485489b357594ee1657c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:58 +0000

    Add package cloudspot-license-api/1.3.0/cloudspot-license-api-1.3.0.tar.gz

---
+++ packages/cloudspot-license-api/cloudspot-license-api-1.3.0.tar.gz/cloudspot-license-api-1.3.0/cloudspotlicense/models/auth.py
@@ -0,0 +1,67 @@
+from .base import BaseModel, ObjectListModel
+
+class ImpersonateResponse(BaseModel):
+    
+    def __init__(self,
+        token=None,
+        user=None,
+        company=None
+    ):
+        super().__init__()
+        
+        self.token = token
+        self.user = user
+        self.company = company
+
+class AuthPermission(BaseModel):
+    
+    def __init__(self,
+        company_id=None,
+        company_name=None,
+        permissions=None           
+    ):
+        super().__init__()
+        
+        self.company_id = company_id
+        self.company_name = company_name
+        self.permissions = permissions
+    
+class AuthPermissions(ObjectListModel):
+
+    def __init__(self):
+        super().__init__(list=[], listObject=AuthPermission)
+
+class AuthResponse(BaseModel):
+    
+    def __init__(self,
+        token=None,
+        permissions=None
+    ):
+
+        super().__init__()
+
+        self.token = token
+        self.permissions = permissions if permissions else AuthPermissions()
+
+class User(BaseModel):
+    
+    def __init__(self,
+        first_name=None,
+        last_name=None,
+        email=None
+    ):
+        
+        super().__init__()
+        
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+
+class CompanyPermissionsResponse(BaseModel):
+    def __init__(self,
+        permissions=None
+    ):
+
+        super().__init__()
+        
+        self.permissions = permissions
