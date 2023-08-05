3de1525f191cbfe7932522fbbe7d720bb68f9685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:42 +0000

    Add package isar/1.12.5/isar-1.12.5-py3-none-any.whl

---
+++ packages/isar/isar-1.12.5-py3-none-any.whl/isar/models/mission_metadata/mission_metadata.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from datetime import date, datetime
+from typing import Optional, Union
+from uuid import UUID
+
+from isar.config.settings import settings
+
+additional_meta: dict = {}
+
+
+@dataclass
+class MissionMetadata:
+    mission_id: Union[UUID, int, str, None]
+    coordinate_reference_system: str = settings.COORDINATE_REFERENCE_SYSTEM
+    vertical_reference_system: str = settings.VERTICAL_REFERENCE_SYSTEM
+    data_classification: str = settings.DATA_CLASSIFICATION
+    source_url: Optional[str] = None
+    plant_code: str = settings.PLANT_CODE
+    plant_name: str = settings.PLANT_NAME
+    plant_short_name: str = settings.PLANT_SHORT_NAME
+    media_orientation_reference_system: str = (
+        settings.MEDIA_ORIENTATION_REFERENCE_SYSTEM
+    )
+    robot_id: str = settings.ROBOT_ID
+    robot_model: str = settings.ROBOT_TYPE
+    mission_date: date = datetime.utcnow().date()
