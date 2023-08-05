9cd5cf8f91755ecfdc81f81bf4155092d41d2b6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:31 +0000

    Add package CxAdmin/1.0.56.2a1/CxAdmin-1.0.56.2a1.tar.gz

---
+++ packages/CxAdmin/CxAdmin-1.0.56.2a1.tar.gz/CxAdmin-1.0.56.2a1/src/CxAdmin/api/cxStatistics.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from typing import Any
+from CxAdmin.api.cxItem import CxItem
+
+
+class CxStatistics(CxItem[dict[str, Any]]):
+    def getInteractions(
+        self, between: tuple[datetime, datetime]
+    ) -> list[dict[str, Any]]:
+        params: str = f"?start={between[0]}&end={between[1]}&limit=1000"
+        responsesJsons: list[dict[str, Any]] = []
+        response = self._httpClient.get(f"{self._path}/interactions{params}")
+        responseJson = response.json()
+        responseResults = responseJson["results"]
+        responsesJsons.extend(responseResults)
+        while len(responsesJsons) < responseJson["total"]:
+            params = f"?start={between[0]}&end={between[1]}&limit=1000&offset={len(responsesJsons)}"
+            response = self._httpClient.get(f"{self._path}/interactions{params}")
+            responseJson = response.json()
+            responseResults = responseJson["results"]
+            responsesJsons.extend(responseResults)
+
+        return responsesJsons
+
+    def get(self) -> list[dict[str, Any]]:
+        statsJson: list[dict[str, Any]] = self._httpClient.get(
+            f"{self._path}/statistics"
+        ).json()["result"]
+        return statsJson
