41994eca9617c92ddda3b98d78811b518bd9e112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:01 +0000

    Add package inigo-py/0.22.2/inigo_py-0.22.2.tar.gz

---
+++ packages/inigo-py/inigo_py-0.22.2.tar.gz/inigo_py-0.22.2/setup.py
@@ -0,0 +1,47 @@
+import setuptools
+import pathlib
+
+# read README.md
+root = pathlib.Path(__file__).parent.resolve()
+long_description = (root / "README.md").read_text(encoding="utf-8")
+version = (root / "inigo_py" / "VERSION").read_text(encoding="utf-8").strip()
+
+setuptools.setup(
+    name='inigo_py',
+    version=version,
+    author='Nikolai Kaploniuk',
+    author_email='nkaploniuk@inigo.io',
+    description='Inigo Middleware',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/inigolabs/inigo-py',
+    project_urls={
+        "Bug Tracker": "https://github.com/inigolabs/inigo-py/issues",
+        "Documentation": "https://docs.inigo.io",
+        "Support": "https://slack.inigo.io",
+    },
+    license='MIT',
+    packages=['inigo_py'],
+    install_requires=['pyjwt', 'django'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Django',
+        'Framework :: Flask',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11'
+    ],
+    python_requires='>= 3.8',
+    keywords="api graphql inigo django flask graphene",
+    package_data={
+        'inigo_py': [
+            'lib/*',
+            'VERSION'
+        ]
+    },
+)
