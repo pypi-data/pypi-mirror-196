6a2de2eeb47db239f37804f5f2e20a68e760c0fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:50 +0000

    Add package talbots/0.0.4/talbots-0.0.4-py3-none-any.whl

---
+++ packages/talbots/talbots-0.0.4-py3-none-any.whl/talbots/__init__.py
@@ -0,0 +1,7 @@
+from .paths import Paths
+from .bibtext import Bibtex
+from .download import Download
+from .extraction import Extraction
+from .filters import Filters
+
+__all__ = ["Bibtex", "Paths", "Download", "Extraction", "Filters"]
