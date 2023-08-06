a043a6c82f9c00adc2fb7fa54c484f3cf8a73366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:45 +0000

    Add package genai/0.12.0a0/genai-0.12.0a0-py3-none-any.whl

---
+++ packages/genai/genai-0.12.0a0-py3-none-any.whl/genai/generate.py
@@ -0,0 +1,93 @@
+import openai
+
+NOTEBOOK_CREATE_NEXT_CELL_PROCLAMATION = f"""
+As a coding assistant, your task is to help users write code in Python within Jupyter Notebooks. Provide comments and code for the user to read and edit, ensuring it can be run successfully. The user will be able to run the code in the cell and see the output.
+""".strip()
+
+
+NOTEBOOK_ERROR_DIAGNOSER_PROCLAMATION = f"""
+As a coding assistant, you'll diagnose errors in Python code written in a Jupyter Notebook. Use %pip instead of !pip and format your response using GitHub flavored markdown. Provide concise code examples in your response which will be rendered in Markdown in the notebook.
+""".strip()
+
+
+def content(completion):
+    return completion["choices"][0]["message"]["content"]
+
+
+def deltas(completion):
+    for chunk in completion:
+        delta = chunk["choices"][0]["delta"]
+        if "content" in delta:
+            yield delta["content"]
+
+
+def generate_next_cell(
+    context,  # List[Dict[str, str]]
+    text,
+    stream=False,
+):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            # Establish the context in which GPT will respond
+            {
+                "role": "system",
+                "content": NOTEBOOK_CREATE_NEXT_CELL_PROCLAMATION,
+            },
+            # In, Out
+            *context,
+            # The user code/text
+            {
+                "role": "user",
+                "content": text,
+            },
+        ],
+        stream=stream,
+    )
+
+    if stream:
+        yield from deltas(response)
+    else:
+        yield content(response)
+
+
+def generate_exception_suggestion(
+    # The user's code
+    code,
+    # The exception with traceback
+    etype,
+    evalue,
+    plaintext_traceback,
+    stream=False,
+):
+    # Cap our error report at ~1024 characters
+    error_report = f"{etype.__name__}: {evalue}\n{plaintext_traceback}"
+
+    if len(error_report) > 1024:
+        error_report = error_report[:1024] + "\n..."
+
+    messages = [
+        # Establish the context in which GPT will respond with role: assistant
+        {
+            "role": "system",
+            "content": NOTEBOOK_ERROR_DIAGNOSER_PROCLAMATION,
+        },
+        # The user sent code
+        {"role": "user", "content": code},
+        # The system literally wrote back with the error
+        {
+            "role": "system",
+            "content": error_report,
+        },
+    ]
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        stream=stream,
+    )
+
+    if stream:
+        yield from deltas(response)
+    else:
+        yield content(response)
---
+++ packages/genai/genai-0.12.0a0-py3-none-any.whl/genai/suggestions.py
@@ -0,0 +1,89 @@
+"""
+This module creates a custom exception handler that will send the error to OpenAI's
+ChatGPT in order to help debug the code. It will also display the error in the
+notebook as usual.
+"""
+
+
+from IPython.display import Pretty
+from IPython.core.display_functions import display
+from IPython import get_ipython
+
+from traceback import TracebackException
+
+from genai.generate import generate_exception_suggestion
+
+
+def can_handle_display_updates():
+    """Determine (roughly) if the client can handle display updates."""
+    try:
+        from IPython import get_ipython
+
+        ipython = get_ipython()
+        if ipython is None:
+            return False
+
+        name = ipython.__class__.__name__
+
+        if name == "ZMQInteractiveShell":
+            return True
+        elif name == "TerminalInteractiveShell":
+            return False
+        else:
+            # Just assume they can otherwise
+            return True
+    except ImportError:
+        # No IPython, so no display updates whatsoever
+        return False
+
+
+# this function will be called on exceptions in any cell
+def custom_exc(shell, etype, evalue, tb, tb_offset=None):
+    # still show the error within the notebook, don't just swallow it
+    shell.showtraceback((etype, evalue, tb), tb_offset=tb_offset)
+
+    try:
+        # Get the current code
+        code = shell.user_ns["In"][-1]
+
+        heading = display(
+            Pretty(("Let's see how we can fix this... 🔧")), display_id=True
+        )
+
+        # Highly colorized tracebacks do not help GPT as much as a clean plaintext traceback.
+        formatted = TracebackException(etype, evalue, tb, limit=20).format(chain=True)
+        plaintext_traceback = "\n".join(formatted)
+
+        stream = can_handle_display_updates()
+
+        suggestion = generate_exception_suggestion(
+            code=code,
+            etype=etype,
+            evalue=evalue,
+            plaintext_traceback=plaintext_traceback,
+            stream=stream,
+        )
+
+        content = ""
+
+        for delta in suggestion:
+            content = content + delta
+
+            heading.update(
+                {
+                    "text/markdown": f"## 💡 Suggestion\n\n{content}",
+                    "text/plain": f"## 💡 Suggestion\n\n{content}",
+                },
+                raw=True,
+            )
+
+    except Exception as e:
+        print("Error while trying to provide a suggestion: ", e)
+
+
+def register(ipython=None):
+    """Register the exception handler with the given IPython instance."""
+    if ipython is None:
+        ipython = get_ipython()
+
+    ipython.set_custom_exc((Exception,), custom_exc)
