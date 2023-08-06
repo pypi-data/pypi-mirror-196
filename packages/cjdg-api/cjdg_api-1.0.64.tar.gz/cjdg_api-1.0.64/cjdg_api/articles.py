c846229dfd43a57fee68c6ab253783a4aa4c9bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:10 +0000

    Add package cjdg-api/1.0.64/cjdg_api-1.0.64.tar.gz

---
+++ packages/cjdg-api/cjdg_api-1.0.64.tar.gz/cjdg_api-1.0.64/cjdg_api/articles.py
@@ -0,0 +1,44 @@
+from .base import base
+
+
+class articles(base):
+
+    def __init__(self, token, domain=True, safe=False, app_secret=None):
+        super().__init__(token, domain, safe, app_secret)
+
+    def getById(self, articleId):
+        api_name = "contmgn/getArticleById"
+        data = {}
+        data["articleId"] = articleId
+        return self.request(api_name)
+
+    def addComment(self, articleId, content, stren=0):
+        api_name = "contmgn/addComment"
+        data = {}
+        data["articleId"] = articleId
+        data["content"] = content
+        data["stren"] = stren
+        return self.request(api_name, data)
+
+    def getCommentListByArticleId(self, articleId,  page=1, row=20):
+        api_name = "contmgn/getCommentListByArticleId"
+        data = {}
+        data["articleId"] = articleId
+        data["page"] = page
+        data["rows"] = row
+        return self.request(api_name, data)
+
+    def addPraiseNum(self, articleId):
+        api_name = "contmgn/addPraiseNum"
+        data = {}
+        data["articleId"] = articleId
+        return self.request(api_name, data)
+
+    def addReplyPraise(self, targetId, articleId):
+        api_name = "contmgn/addReplyPraise"
+        data = {}
+        data["favourToType"] = 3
+        data["favourType"] = 2
+        data["targetId"] = targetId
+        data["targetParentId"] = articleId
+        return self.request(api_name, data)
---
+++ packages/cjdg-api/cjdg_api-1.0.64.tar.gz/cjdg_api-1.0.64/cjdg_api/pk.py
@@ -0,0 +1,70 @@
+# 排行榜PK
+
+from .base import base
+
+
+class pk(base):
+
+    def __init__(self, token, domain=True, safe=False, app_secret=None):
+        super().__init__(token, domain, safe, app_secret)
+
+
+    # 排行榜PK列表
+    def query(self,data):
+        api_name = 'om-web/storemanagement/pk/pk/query'
+        return self.request(api_name,data)
+
+
+    # 排行榜新增/编辑
+    # 配置必填项和主指标
+    def save(self):
+        api_name = 'om-web/stoermanagement/pk/v2/pk/save'
+        return self.request(api_name)
+
+
+    # 配置其他副指标，非必填项
+    def pkIndex(self):
+        api_name = 'om-web/storemanagement/pk/pk/saveOrUpdatePkIndex'
+        return self.request(api_name)
+
+
+    # 选择组织/战队
+    def player(self):
+        api_name = 'om-web/storemanagement/pk/v2/pk/savePlayer'
+        self.query
+        return self.request(api_name)
+
+
+
+    # 发布
+    def public(self):
+        api_name = 'om-web/storemanagement/pk/v2/pk/public'
+        return self.request(api_name)
+
+
+
+    # 查看PK基础信息
+    def queryDetail(self):
+        api_name = 'om-web/storemanagement/pk/pk/querydetailselectmenber'
+        return self.request(api_name)
+
+
+    # 查看上报明细
+    def submitDetail(self):
+        api_name = 'om-web/storemanagement/pk/pk/querySubmitDetailList4Backend'
+        return self.request(api_name)
+
+
+    # 删除PK
+    def pkdel(self):
+        api_name = 'om-web/storemanagement/pk/pk/delete'
+        return self.request(api_name)
+
+
+def main():
+    token = ""
+    a = pk(token=token)
+    a.query(data=data)
+    
+if __name__ == '__main__':
+    main()
