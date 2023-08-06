bb889c05d946c6f0fb8c3bba368b392ed99eb08f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:22 +0000

    Add package sk-transformers/0.10.2/sk_transformers-0.10.2-py3-none-any.whl

---
+++ packages/sk-transformers/sk_transformers-0.10.2-py3-none-any.whl/sk_transformers/number_transformer.py
@@ -0,0 +1,131 @@
+import operator
+import warnings
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+import numpy as np
+import pandas as pd
+
+from sk_transformers.base_transformer import BaseTransformer
+from sk_transformers.utils import check_ready_to_transform
+
+
+class MathExpressionTransformer(BaseTransformer):
+    """Applies an function/operation to a column and a given value or column.
+    The operation can be a function from NumPy's mathematical functions or
+    operator package.
+
+    **Warning!** Some functions/operators may not work as expected. Especially, functions that don't
+    belong in [`numpy.ufunc`](https://numpy.org/doc/stable/reference/ufuncs.html) are not supported.
+    NumPy functions with return values that don't fit the size of the source column are also not supported.
+
+    Example:
+    ```python
+    import pandas as pd
+    from sk_transformers import MathExpressionTransformer
+
+    X = pd.DataFrame({"foo": [1, 2, 3], "bar": [4, 5, 6]})
+    transformer = MathExpressionTransformer([("foo", "np.add", "bar", None)])
+    transformer.fit_transform(X)
+    ```
+    ```
+       foo  bar  foo_add_bar
+    0    1    4            5
+    1    2    5            7
+    2    3    6            9
+    ```
+
+    Args:
+        features (List[str, str, Union[int, float]]): List of tuples containing the name of the column to apply the operation on,
+            a string representation of the operation (see list above) and the value to apply the operation on. The value can be
+            an number (int or float) or the name of another column in the dataframe. If the value is `None`, it it expected that
+            the operation only takes one argument. The fourth entry of the tuple is a dictionary passed as `kwargs` to the operation.
+    """
+
+    __slots__ = ("features",)
+
+    def __init__(
+        self,
+        features: List[
+            Tuple[str, str, Union[int, float, str, None], Optional[Dict[str, Any]]]
+        ],
+    ) -> None:
+        super().__init__()
+        self.features = features
+
+    def __verify_operation(self, operation: str) -> Tuple[bool, Any]:
+        if operation.startswith("np"):
+            if hasattr(np, operation[3:]):
+                op = getattr(np, operation[3:])
+                is_np_op = True
+                if not isinstance(op, np.ufunc):
+                    raise ValueError(
+                        f"The function `{operation}` is not a NumPy universal function. If you are using `np.sum` or `np.prod`, please use `np.add` or `np.multiply` instead."
+                    )
+            else:
+                raise AttributeError(f"Operation {operation[3:]} not found in NumPy!")
+
+        elif hasattr(operator, operation) and operation not in [
+            "attrgetter",
+            "itemgetter",
+            "methodcaller",
+        ]:
+            op = getattr(operator, operation)
+            is_np_op = False
+
+        else:
+            raise AttributeError(
+                f"Invalid operation! `{operation}` is not a valid operation! Please refer to the `numpy` and `operator` package."
+            )
+
+        return is_np_op, op
+
+    def __abbreviate_numpy_in_operation(self, operation: str) -> str:
+        """Replaces `numpy` at the start of a string with `np`.
+
+        Args:
+            operation (str): The operation as a string.
+
+        Returns:
+            str: The operation as a string with numpy replaced with np.
+        """
+        if operation.startswith("numpy"):
+            operation = "np" + operation[5:]
+        return operation
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """Applies the operation to the column and the value.
+
+        Args:
+            X (pandas.DataFrame): DataFrame containing the columns to apply the operation on.
+
+        Returns:
+            pandas.DataFrame: The original dataframe with the new columns. The new columns are named as follows:
+            '`column_name`_`operation`_`value`' or '`column_name`_`operation`' if `value` is `None`.
+        """
+        X = check_ready_to_transform(self, X, [feature[0] for feature in self.features])
+
+        for feature, operation, value, kwargs in self.features:
+            operation = self.__abbreviate_numpy_in_operation(operation)
+            is_np_op, op = self.__verify_operation(operation)
+
+            new_column = f"{feature}_{operation}".replace("np.", "")
+            new_column_with_value = f"{feature}_{operation}_{value}".replace("np.", "")
+
+            if is_np_op:
+                warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
+                if value is None:
+                    X[new_column] = op(X[feature], **kwargs or {})
+                elif isinstance(value, str):
+                    X[new_column_with_value] = op(X[feature], X[value], **kwargs or {})
+                else:
+                    X[new_column_with_value] = op(X[feature], value, **kwargs or {})
+            else:
+                if value is None:
+                    X[new_column] = op(X[feature])
+                elif isinstance(value, str):
+                    X[new_column_with_value] = op(X[feature], X[value])
+                else:
+                    X[new_column_with_value] = op(
+                        X[feature], value
+                    )  # This created a ragged array in will be deprecated in future.
+        return X
---
+++ packages/sk-transformers/sk_transformers-0.10.2-py3-none-any.whl/sk_transformers/string_transformer.py
@@ -0,0 +1,480 @@
+import functools
+import ipaddress
+import itertools
+import re
+import unicodedata
+from difflib import SequenceMatcher
+from typing import List, Optional, Tuple, Union
+
+import pandas as pd
+import phonenumbers
+import swifter  # pylint: disable=unused-import
+
+from sk_transformers.base_transformer import BaseTransformer
+from sk_transformers.utils import check_ready_to_transform
+
+
+class IPAddressEncoderTransformer(BaseTransformer):
+    """Encodes IPv4 and IPv6 strings addresses to a float representation. To
+    shrink the values to a reasonable size IPv4 addresses are divided by 2^10
+    and IPv6 addresses are divided by 2^48. Those values can be changed using
+    the `ip4_divisor` and `ip6_divisor` parameters.
+
+    Example:
+    ```python
+    import pandas as pd
+    from sk_transformers import IPAddressEncoderTransformer
+
+    X = pd.DataFrame({"foo": ["192.168.1.1", "2001:0db8:3c4d:0015:0000:0000:1a2f:1a2b"]})
+    transformer = IPAddressEncoderTransformer(["foo"])
+    transformer.fit_transform(X)
+    ```
+    ```
+                foo
+    0  3.232236e-01
+    1  4.254077e-11
+    ```
+
+    Args:
+        features (List[str]): List of features which should be transformed.
+        ip4_divisor (float): Divisor for IPv4 addresses.
+        ip6_divisor (float): Divisor for IPv6 addresses.
+        error_value (Union[int, float]): Value if parsing fails.
+    """
+
+    __slots__ = (
+        "features",
+        "ip4_divisor",
+        "ip6_divisor",
+        "error_value",
+    )
+
+    def __init__(
+        self,
+        features: List[str],
+        ip4_divisor: float = 1e10,
+        ip6_divisor: float = 1e48,
+        error_value: Union[int, float] = -999,
+    ) -> None:
+        super().__init__()
+        self.features = features
+        self.ip4_divisor = ip4_divisor
+        self.ip6_divisor = ip6_divisor
+        self.error_value = error_value
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """Transforms the column containing the IP addresses to float column.
+
+        Args:
+            X (pandas.DataFrame): DataFrame to transform.
+
+        Returns:
+            pandas.DataFrame: Transformed dataframe.
+        """
+        X = check_ready_to_transform(self, X, self.features)
+
+        function = functools.partial(
+            IPAddressEncoderTransformer.__to_float,
+            self.ip4_divisor,
+            self.ip6_divisor,
+            self.error_value,
+        )
+        for column in self.features:
+            X[column] = X[column].map(function)
+
+        return X
+
+    @staticmethod
+    def __to_float(
+        ip4_devisor: float,
+        ip6_devisor: float,
+        error_value: Union[int, float],
+        ip_address: str,
+    ) -> float:
+        try:
+            return int(ipaddress.IPv4Address(ip_address)) / int(ip4_devisor)
+        except:  # pylint: disable=bare-except
+            try:
+                return int(ipaddress.IPv6Address(ip_address)) / int(ip6_devisor)
+            except:  # pylint: disable=bare-except
+                return error_value
+
+
+class EmailTransformer(BaseTransformer):
+    """Transforms an email address into multiple features.
+
+    Example:
+    ```python
+    import pandas as pd
+    from sk_transformers import EmailTransformer
+
+    X = pd.DataFrame({"foo": ["person-123@test.com"]})
+    transformer = EmailTransformer(["foo"])
+    transformer.fit_transform(X)
+    ```
+    ```
+              foo foo_domain  foo_num_of_digits  foo_num_of_letters  \
+    0  person-123       test                  3                   6
+
+    foo_num_of_special_chars  foo_num_of_repeated_chars  foo_num_of_words
+    0                         1                          1                 2
+    ```
+
+    Args:
+        features (List[str]): List of features which should be transformed.
+    """
+
+    __slots__ = ("features",)
+
+    def __init__(self, features: List[str]) -> None:
+        super().__init__()
+        self.features = features
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """Transforms the one column from X, containing the email addresses,
+        into multiple columns.
+
+        Args:
+            X (pandas.DataFrame): DataFrame to transform.
+
+        Returns:
+            pandas.DataFrame: Transformed dataframe containing the extra columns.
+        """
+        X = check_ready_to_transform(self, X, self.features)
+
+        for column in self.features:
+            X[f"{column}_domain"] = (
+                X[column].str.split("@").str[1].str.split(".").str[0]
+            )
+
+            X[column] = X[column].str.split("@").str[0]
+
+            X[f"{column}_num_of_digits"] = X[column].map(
+                EmailTransformer.__num_of_digits
+            )
+            X[f"{column}_num_of_letters"] = X[column].map(
+                EmailTransformer.__num_of_letters
+            )
+            X[f"{column}_num_of_special_chars"] = X[column].map(
+                EmailTransformer.__num_of_special_characters
+            )
+            X[f"{column}_num_of_repeated_chars"] = X[column].map(
+                EmailTransformer.__num_of_repeated_characters
+            )
+            X[f"{column}_num_of_words"] = X[column].map(EmailTransformer.__num_of_words)
+        return X
+
+    @staticmethod
+    def __num_of_digits(string: str) -> int:
+        return sum(map(str.isdigit, string))
+
+    @staticmethod
+    def __num_of_letters(string: str) -> int:
+        return sum(map(str.isalpha, string))
+
+    @staticmethod
+    def __num_of_special_characters(string: str) -> int:
+        return len(re.findall(r"[^A-Za-z0-9]", string))
+
+    @staticmethod
+    def __num_of_repeated_characters(string: str) -> int:
+        return max(len("".join(g)) for _, g in itertools.groupby(string))
+
+    @staticmethod
+    def __num_of_words(string: str) -> int:
+        return len(re.findall(r"[.\-_]", string)) + 1
+
+
+class StringSimilarityTransformer(BaseTransformer):
+    """Calculates the similarity between two strings using the `gestalt pattern
+    matching` algorithm from the `SequenceMatcher` class.
+
+    Example:
+    ```python
+    import pandas as pd
+    from sk_transformers import StringSimilarityTransformer
+
+    X = pd.DataFrame(
+        {
+            "foo": ["abcdefgh", "ijklmnop", "qrstuvwx"],
+            "bar": ["ghabcdef", "ijklmnop", "qr000000"],
+        }
+    )
+    transformer = StringSimilarityTransformer(("foo", "bar"))
+    transformer.fit_transform(X)
+    ```
+    ```
+            foo       bar  foo_bar_similarity
+    0  abcdefgh  ghabcdef                0.75
+    1  ijklmnop  ijklmnop                1.00
+    2  qrstuvwx  qr000000                0.25
+    ```
+
+    Args:
+        features (Tuple[str, str]): The two columns that contain the strings for which the similarity should be calculated.
+    """
+
+    __slots__ = ("features",)
+
+    def __init__(self, features: Tuple[str, str]) -> None:
+        super().__init__()
+        self.features = features
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """Calculates the similarity of two strings provided in `features`.
+
+        Args:
+            X (pandas.DataFrame): DataFrame to transform.
+
+        Returns:
+            pandas.DataFrame: Original dataframe containing the extra column with the calculated similarity.
+        """
+        X = check_ready_to_transform(self, X, list(self.features))
+
+        X[f"{self.features[0]}_{self.features[1]}_similarity"] = X[
+            [self.features[0], self.features[1]]
+        ].swifter.apply(
+            lambda x: StringSimilarityTransformer.__similar(
+                StringSimilarityTransformer.__normalize_string(x[self.features[0]]),
+                StringSimilarityTransformer.__normalize_string(x[self.features[1]]),
+            ),
+            axis=1,
+        )
+        return X
+
+    @staticmethod
+    def __similar(a: str, b: str) -> float:
+        return SequenceMatcher(None, a, b).ratio()
+
+    @staticmethod
+    def __normalize_string(string: str) -> str:
+        string = str(string).strip().lower()
+        return (
+            unicodedata.normalize("NFKD", string)
+            .encode("utf8", "strict")
+            .decode("utf8")
+        )
+
+
+class PhoneTransformer(BaseTransformer):
+    """Transforms a phone number into multiple features.
+
+    Example:
+    ```python
+    import pandas as pd
+    from sk_transformers import PhoneTransformer
+
+    X = pd.DataFrame({"foo": ["+49123456789", "0044987654321", "3167891234"]})
+    transformer = PhoneTransformer(["foo"])
+    transformer.fit_transform(X)
+    ```
+    ```
+                 foo  foo_national_number  foo_country_code
+    0   +49123456789             0.123457              0.49
+    1  0044987654321             0.987654              0.44
+    2     3167891234          -999.000000           -999.00
+    ```
+
+    Args:
+        features (List[str]): List of features which should be transformed.
+        national_number_divisor (float): Divider `national_number`.
+        country_code_divisor (flat): Divider for `country_code`.
+        error_value (str): Value to use if the phone number is invalid or the parsing fails.
+    """
+
+    __slots__ = (
+        "features",
+        "national_number_divisor",
+        "country_code_divisor",
+        "error_value",
+    )
+
+    def __init__(
+        self,
+        features: List[str],
+        national_number_divisor: float = 1e9,
+        country_code_divisor: float = 1e2,
+        error_value: str = "-999",
+    ) -> None:
+        super().__init__()
+        self.features = features
+        self.national_number_divisor = national_number_divisor
+        self.country_code_divisor = country_code_divisor
+        self.error_value = error_value
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """Calculates the similarity of two strings provided in `features`.
+
+        Args:
+            X (pandas.DataFrame): DataFrame to transform.
+
+        Returns:
+            pandas.DataFrame: Original dataframe containing the extra column with the calculated similarity.
+        """
+        X = check_ready_to_transform(self, X, self.features)
+
+        for column in self.features:
+            X[f"{column}_national_number"] = X[column].swifter.apply(
+                lambda x: PhoneTransformer.__phone_to_float(
+                    "national_number",
+                    x,
+                    int(self.national_number_divisor),
+                    self.error_value,
+                )
+            )
+            X[f"{column}_country_code"] = X[column].swifter.apply(
+                lambda x: PhoneTransformer.__phone_to_float(
+                    "country_code", x, int(self.country_code_divisor), self.error_value
+                )
+            )
+
+        return X
+
+    @staticmethod
+    def __phone_to_float(
+        attribute: str, phone: str, divisor: int, error_value: str
+    ) -> float:
+        phone = phone.replace(" ", "")
+        phone = re.sub(r"[^0-9+-]", "", phone)
+        phone = re.sub(r"^00", "+", phone)
+        try:
+            return float(getattr(phonenumbers.parse(phone, None), attribute)) / divisor
+        except:  # pylint: disable=W0702
+            try:
+                return float(re.sub(r"(?<!^)[^0-9]", "", error_value))
+            except:  # pylint: disable=W0702
+                return float(error_value)
+
+
+class StringSlicerTransformer(BaseTransformer):
+    """Slices all entries of specified string features using the `slice()`
+    function.
+
+    Note: The arguments for the `slice()` function are passed as a tuple. This shares
+    the python quirk of writing a tuple with a single argument with the trailing comma.
+
+    Example:
+    ```python
+    import pandas as pd
+    from sk_transformers import StringSlicerTransformer
+
+    X = pd.DataFrame({"foo": ["abc", "def", "ghi"], "bar": ["jkl", "mno", "pqr"]})
+    transformer = StringSlicerTransformer([("foo", (0, 3, 2)), ("bar", (2,))])
+    transformer.fit_transform(X)
+    ```
+    ```
+       foo  bar foo_slice bar_slice
+    0  abc  jkl        ac        jk
+    1  def  mno        df        mn
+    2  ghi  pqr        gi        pq
+    ```
+
+    Args:
+        features (List[Tuple[str, Union[Tuple[int], Tuple[int, int], Tuple[int, int, int]], Optional[str]]]): The arguments to the `slice` function, for each feature.
+    """
+
+    __slots__ = ("features",)
+
+    def __init__(
+        self,
+        features: List[
+            Tuple[
+                str,
+                Union[Tuple[int], Tuple[int, int], Tuple[int, int, int]],
+                Optional[str],
+            ]
+        ],
+    ) -> None:
+        super().__init__()
+        self.features = features
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """Slices the strings of specified features in the dataframe.
+
+        Args:
+            X (pandas.DataFrame): DataFrame to transform.
+
+        Returns:
+            pandas.DataFrame: Original dataframe with sliced strings in specified features.
+        """
+        X = check_ready_to_transform(self, X, [feature[0] for feature in self.features])
+
+        for slice_tuple in self.features:
+            column = slice_tuple[0]
+            slice_args = slice_tuple[1]
+            slice_column = slice_tuple[2] if len(slice_tuple) == 3 else column
+            X[slice_column] = [x[slice(*slice_args)] for x in X[column]]
+
+        return X
+
+
+class StringSplitterTransformer(BaseTransformer):
+    """Uses the pandas `str.split` method to split a column of strings into
+    multiple columns.
+
+    Example:
+    ```python
+    import pandas as pd
+    from sk_transformers import StringSplitterTransformer
+
+    X = pd.DataFrame({"foo": ["a_b", "c_d", "e_f"], "bar": ["g*h*i", "j*k*l", "m*n*o"]})
+    transformer = StringSplitterTransformer([("foo", "_", 2), ("bar", "*", 3)])
+    transformer.fit_transform(X)
+    ```
+    ```
+       foo    bar foo_part_1 foo_part_2 bar_part_1 bar_part_2 bar_part_3
+    0  a_b  g*h*i          a          b          g          h          i
+    1  c_d  j*k*l          c          d          j          k          l
+    2  e_f  m*n*o          e          f          m          n          o
+    ```
+
+    Args:
+        features (List[Tuple[str, str, Optional[int]]]): A list of tuples where
+            the first element is the name of the feature,
+            the second element is the string separator,
+            and a third optional element is the desired number of splits.
+            If the third element is not provided or is equal to 0 or -1, maximum number of splits are made.
+    """
+
+    __slots__ = ("features",)
+
+    def __init__(
+        self,
+        features: List[
+            Tuple[
+                str,
+                str,
+                Optional[int],
+            ]
+        ],
+    ) -> None:
+        super().__init__()
+        self.features = features
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """Splits the strings based on a separator character.
+
+        Args:
+            X (pandas.DataFrame): DataFrame to transform.
+
+        Returns:
+            pandas.DataFrame: Dataframe containing additional columns containing
+                each split part of the string.
+        """
+        X = check_ready_to_transform(self, X, [feature[0] for feature in self.features])
+
+        for split_tuple in self.features:
+            column = split_tuple[0]
+            separator = split_tuple[1]
+
+            max_possible_splits = X[column].str.count(separator).max()
+            maxsplit = split_tuple[2] if len(split_tuple) == 3 else max_possible_splits
+            if maxsplit in [0, -1] or maxsplit > max_possible_splits:
+                maxsplit = max_possible_splits
+
+            split_column_names = [f"{column}_part_{i+1}" for i in range(maxsplit + 1)]
+            X[split_column_names] = X[column].str.split(
+                separator, n=maxsplit, expand=True
+            )
+
+        return X
