916c8bee1e532ee6bc10f002f68dbf5d922fac40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:22 +0000

    Add package klinecenter/0.1.0/klinecenter-0.1.0.tar.gz

---
+++ packages/klinecenter/klinecenter-0.1.0.tar.gz/klinecenter-0.1.0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="klinecenter",
+    version="0.1.0",
+    author="klinecenter",
+    author_email="kline8@foxmail.com",
+    description="",
+    long_description="",
+    # long_description_content_type="text",
+    url="https://kline8.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    install_requires=[
+        "pandas>=1.0.0",
+    ],
+)
