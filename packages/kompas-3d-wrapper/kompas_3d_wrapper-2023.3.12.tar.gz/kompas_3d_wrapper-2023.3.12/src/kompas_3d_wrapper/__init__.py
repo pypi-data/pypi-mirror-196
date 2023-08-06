bed339a37b7026bf087286146f06a4716e7e5d9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:54 +0000

    Add package kompas-3d-wrapper/2023.3.12/kompas_3d_wrapper-2023.3.12.tar.gz

---
+++ packages/kompas-3d-wrapper/kompas_3d_wrapper-2023.3.12.tar.gz/kompas_3d_wrapper-2023.3.12/src/kompas_3d_wrapper/__init__.py
@@ -0,0 +1,14 @@
+"""Kompas 3D Wrapper."""
+
+from kompas_3d_wrapper.__main__ import get_kompas_api5
+from kompas_3d_wrapper.__main__ import get_kompas_api7
+from kompas_3d_wrapper.__main__ import import_kompas_ldefin2d_mischelpers
+from kompas_3d_wrapper.__main__ import start_kompas
+
+
+__all__ = [
+    "import_kompas_ldefin2d_mischelpers",
+    "start_kompas",
+    "get_kompas_api5",
+    "get_kompas_api7",
+]
