1020f3e41138c1811e5895c90f7ebe9205a9c24c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:45 +0000

    Add package qosic-sdk/4.0.3/qosic_sdk-4.0.3-py3-none-any.whl

---
+++ packages/qosic-sdk/qosic_sdk-4.0.3-py3-none-any.whl/qosic/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for qosic-sdk."""
+from .api import Client  # noqa
+from .providers import MTN, MOOV  # noqa
+from .utils import Result, Payer  # noqa
+
+__author__ = """Tobi DEGNON"""
+__email__ = "degnonfrancis@gmail.com"
+__version__ = "3.0.2"
