f5079e085d2733967c15579707def6674c2489e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:36 +0000

    Add package psvis/1.1.0/psvis-1.1.0.tar.gz

---
+++ packages/psvis/psvis-1.1.0.tar.gz/psvis-1.1.0/psvis/models/__init__.py
@@ -0,0 +1,11 @@
+from .backbones import *  # noqa: F401,F403
+from .builder import (BACKBONES, DETECTORS, HEADS, LOSSES, NECKS,
+                      ROI_EXTRACTORS, SHARED_HEADS, build_backbone,
+                      build_detector, build_head, build_loss, build_neck,
+                      build_roi_extractor, build_shared_head)
+
+__all__ = [
+    'BACKBONES', 'NECKS', 'ROI_EXTRACTORS', 'SHARED_HEADS', 'HEADS', 'LOSSES',
+    'DETECTORS', 'build_backbone', 'build_neck', 'build_roi_extractor',
+    'build_shared_head', 'build_head', 'build_loss', 'build_detector'
+]
---
+++ packages/psvis/psvis-1.1.0.tar.gz/psvis-1.1.0/psvis/models/backbones/__init__.py
@@ -0,0 +1,8 @@
+
+from .resnet import ResNet, ResNetV1d
+from .shufflenet_v1 import ShuffleNetV1
+
+
+__all__ = [ 'ResNet', 'ResNetV1d',
+    'ShuffleNetV1'
+]
