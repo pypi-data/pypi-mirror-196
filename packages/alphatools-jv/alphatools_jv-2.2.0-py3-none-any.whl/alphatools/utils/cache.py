6624906d4ea57a77e16f9106c08ddd5ac62c8810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:11 +0000

    Add package alphatools-jv/2.2.0/alphatools_jv-2.2.0-py3-none-any.whl

---
+++ packages/alphatools-jv/alphatools_jv-2.2.0-py3-none-any.whl/alphatools/utils/cache.py
@@ -0,0 +1,34 @@
+import json
+import logging
+
+logging.basicConfig(
+    format='[%(asctime)s] %(levelname)-8s {%(pathname)s:%(lineno)d} : %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S', level=logging.WARNING
+)
+
+def cache_to_file(dir_name, _hash):
+    def decorator(original_func):
+        logger = logging.getLogger(__name__)
+
+        def new_func(self, params):
+            hash_result = _hash(params)
+            file_name = dir_name + "/{}.csv".format(hash_result)
+            try:
+                f = open(file_name, 'r')
+                cache = json.load(f)
+                f.close()
+            except (IOError, ValueError):
+                cache = {}
+
+            if hash_result not in cache or not cache[hash_result]:
+                logger.debug("Could not find results in cache. Calling the function to get results")
+                cache[hash_result] = original_func(self, params)
+                f = open(file_name, 'w')
+                json.dump(cache, f)
+                f.close()
+            logger.debug("Results already exist. Fetching from cache")
+            return cache[hash_result]
+
+        return new_func
+
+    return decorator
