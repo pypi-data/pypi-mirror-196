27448cefc84ae92dcd4422f038a1ca454bc8bb24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:25 +0000

    Add package lusid-workflow-sdk-preview/0.1.161/lusid-workflow-sdk-preview-0.1.161.tar.gz

---
+++ packages/lusid-workflow-sdk-preview/lusid-workflow-sdk-preview-0.1.161.tar.gz/lusid-workflow-sdk-preview-0.1.161/lusid_workflow/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from lusid_workflow.api.task_definitions_api import TaskDefinitionsApi
+from lusid_workflow.api.task_instances_api import TaskInstancesApi
