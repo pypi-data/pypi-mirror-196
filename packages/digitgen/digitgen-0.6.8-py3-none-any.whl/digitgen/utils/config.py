ee5a8fc3b77d3ed5a16da8645580716c0deadd81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:44 +0000

    Add package digitgen/0.6.8/digitgen-0.6.8-py3-none-any.whl

---
+++ packages/digitgen/digitgen-0.6.8-py3-none-any.whl/digitgen/utils/config.py
@@ -0,0 +1,228 @@
+CHUNK_SIZE = 32768
+
+CONFIGURATION = {
+    "digit_config": {
+        "0": {
+            "fontscale": 30,
+            "size": [
+                17,
+                30
+            ],
+            "start_point": [
+                0,
+                0
+            ],
+            "bbox": [
+                0,
+                4,
+                16,
+                26
+            ]
+        },
+        "1": {
+            "fontscale": 30,
+            "size": [
+                11,
+                30
+            ],
+            "start_point": [
+                0,
+                0
+            ],
+            "bbox": [
+                0,
+                4,
+                8,
+                26
+            ]
+        },
+        "2": {
+            "fontscale": 30,
+            "size": [
+                16,
+                30
+            ],
+            "start_point": [
+                0,
+                0
+            ],
+            "bbox": [
+                0,
+                4,
+                15,
+                26
+            ]
+        },
+        "3": {
+            "fontscale": 30,
+            "size": [
+                16,
+                30
+            ],
+            "start_point": [
+                0,
+                0
+            ],
+            "bbox": [
+                0,
+                4,
+                15,
+                26
+            ]
+        },
+        "4": {
+            "fontscale": 30,
+            "size": [
+                16,
+                30
+            ],
+            "start_point": [
+                0,
+                0
+            ],
+            "bbox": [
+                0,
+                4,
+                15,
+                26
+            ]
+        },
+        "5": {
+            "fontscale": 30,
+            "size": [
+                16,
+                30
+            ],
+            "start_point": [
+                0,
+                0
+            ],
+            "bbox": [
+                0,
+                4,
+                15,
+                26
+            ]
+        },
+        "6": {
+            "fontscale": 30,
+            "size": [
+                16,
+                30
+            ],
+            "start_point": [
+                0,
+                0
+            ],
+            "bbox": [
+                0,
+                4,
+                15,
+                26
+            ]
+        },
+        "7": {
+            "fontscale": 30,
+            "size": [
+                16,
+                30
+            ],
+            "start_point": [
+                0,
+                0
+            ],
+            "bbox": [
+                0,
+                4,
+                15,
+                26
+            ]
+        },
+        "8": {
+            "fontscale": 30,
+            "size": [
+                17,
+                30
+            ],
+            "start_point": [
+                0,
+                0
+            ],
+            "bbox": [
+                0,
+                4,
+                14,
+                26
+            ]
+        },
+        "9": {
+            "fontscale": 30,
+            "size": [
+                17,
+                30
+            ],
+            "start_point": [
+                0,
+                0
+            ],
+            "bbox": [
+                0,
+                4,
+                16,
+                26
+            ]
+        },
+        " ": {
+            "fontscale": 30,
+            "size": [
+                10,
+                30
+            ],
+            "start_point": [
+                0,
+                0
+            ],
+            "bbox": [
+                0,
+                4,
+                9,
+                26
+            ]
+        },
+        "default": {
+            "fontscale": 30,
+            "size": [
+                20,
+                30
+            ],
+            "start_point": [
+                3,
+                0
+            ],
+            "bbox": [
+                1,
+                1,
+                19,
+                29
+            ]
+        }
+    },
+    "common_configs": {
+        "background_color": (
+            255,
+            255,
+            255
+        ),
+        "digit_color": (
+            0,
+            0,
+            0
+        ),
+        "bbox_color": [
+            255,
+            82,
+            82
+        ],
+        "bbox_width": 1
+    }
+}
