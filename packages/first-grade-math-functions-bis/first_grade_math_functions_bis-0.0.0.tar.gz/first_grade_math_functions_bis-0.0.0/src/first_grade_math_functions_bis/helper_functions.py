5b451ad8e8f4473cf59c077a1205504d14b445ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:07 +0000

    Add package first-grade-math-functions-bis/0.0.0/first_grade_math_functions_bis-0.0.0.tar.gz

---
+++ packages/first-grade-math-functions-bis/first_grade_math_functions_bis-0.0.0.tar.gz/first_grade_math_functions_bis-0.0.0/src/first_grade_math_functions_bis/helper_functions.py
@@ -0,0 +1,18 @@
+def sum_two_integers(a, b):
+    return a + b
+
+
+def subtract_two_integers(a, b):
+    return a - b
+
+
+def multiply_two_integers(a, b):
+    return a * b
+
+
+def divide_two_integers(a, b):
+    return a / b
+
+
+def who_do_i_love():
+    print("I love Eline")
