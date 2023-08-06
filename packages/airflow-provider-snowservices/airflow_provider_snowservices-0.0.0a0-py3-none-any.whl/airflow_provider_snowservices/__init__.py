709502f9a68e096f0e3c036e7f7cdf7790091ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:28 +0000

    Add package airflow-provider-snowservices/0.0.0a0/airflow_provider_snowservices-0.0.0a0-py3-none-any.whl

---
+++ packages/airflow-provider-snowservices/airflow_provider_snowservices-0.0.0a0-py3-none-any.whl/airflow_provider_snowservices/__init__.py
@@ -0,0 +1,10 @@
+
+__version__ = "0.0.0a"
+
+def get_provider_info():
+    return {
+        "package-name": "airflow-provider-snowservices",  # Required
+        "name": "airflow-provider-snowservices",  # Required
+        "description": "A short description of the package",  # Required
+        "versions": [__version__],  # Required
+    }
