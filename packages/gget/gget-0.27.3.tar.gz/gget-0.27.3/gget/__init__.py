d788d225069e39d086f8d6cf2bad4b56f9b1e9ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:04 +0000

    Add package gget/0.27.3/gget-0.27.3.tar.gz

---
+++ packages/gget/gget-0.27.3.tar.gz/gget-0.27.3/gget/__init__.py
@@ -0,0 +1,25 @@
+from .gget_ref import ref
+from .gget_search import search
+from .gget_info import info
+from .gget_seq import seq
+from .gget_muscle import muscle
+from .gget_blast import blast
+from .gget_blat import blat
+from .gget_enrichr import enrichr
+from .gget_archs4 import archs4
+from .gget_alphafold import alphafold
+from .gget_setup import setup
+from .gget_pdb import pdb
+
+import logging
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s %(message)s",
+    level=logging.INFO,
+    datefmt="%c",
+)
+# Mute numexpr threads info
+logging.getLogger("numexpr").setLevel(logging.WARNING)
+
+__version__ = "0.27.3"
+__author__ = "Laura Luebbert"
+__email__ = "lauraluebbert@caltech.edu"
