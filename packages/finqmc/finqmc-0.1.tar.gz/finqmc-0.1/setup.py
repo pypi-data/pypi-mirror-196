24116d0895d2c8d447dd454973b58a415bc20627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:02 +0000

    Add package finqmc/0.1/finqmc-0.1.tar.gz

---
+++ packages/finqmc/finqmc-0.1.tar.gz/finqmc-0.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="finqmc",
+    version="0.1",
+    author="Bruce",
+    author_email="siyuan.jin@connect.ust.hk",
+    description="Quantum Monte Carlo framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/siyuan-bruce/QuantumMonteCarlo",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'qiskit'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
