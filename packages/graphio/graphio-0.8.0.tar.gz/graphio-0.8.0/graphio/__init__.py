f7cbbad420782055543aad60f603c00e651edea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:07 +0000

    Add package graphio/0.8.0/graphio-0.8.0.tar.gz

---
+++ packages/graphio/graphio-0.8.0.tar.gz/graphio-0.8.0/graphio/__init__.py
@@ -0,0 +1,5 @@
+from graphio.objects.nodeset import NodeSet
+from graphio.objects.relationshipset import RelationshipSet
+from graphio.objects.datacontainer import Container
+from graphio.model import ModelNode, ModelRelationship, MergeKey, Label, NodeDescriptor
+from graphio.objects.properties import ArrayProperty
