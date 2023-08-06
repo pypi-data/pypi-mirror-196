7ba7615ef7fdfb1a67801a13694dafe3ebc069f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:03 +0000

    Add package moldrug/3.4.0/moldrug-3.4.0.tar.gz

---
+++ packages/moldrug/moldrug-3.4.0.tar.gz/moldrug-3.4.0/moldrug/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+For information of MolDrug:
+    Docs: https://moldrug.readthedocs.io/en/latest/
+    Source Code: https://github.com/ale94mleon/moldrug
+"""
+from moldrug._version import __version__, __version_tuple__
+
+__author__ = "Alejandro Martínez León"
+__email__ = "ale94mleon@gmail.com"
