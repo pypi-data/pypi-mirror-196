10d20f53829280c6d33512b939eceb561ce0dcbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:18 +0000

    Add package linearlogic/0.3.4/linearlogic-0.3.4.tar.gz

---
+++ packages/linearlogic/linearlogic-0.3.4.tar.gz/linearlogic-0.3.4/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='linearlogic',
+    version='0.3.4',
+    author="Linear Logic Inc.",
+    author_email="support@linearlogic.ai",
+    description="Linear Logic Python SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://linearlogic.ai",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "awesome_progress_bar>=1.7.2",
+        "requests>=2.28.1",
+        "colored==1.4.3"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+        'console_scripts': ['linlog=linlog.cli:main'],
+    }
+)
