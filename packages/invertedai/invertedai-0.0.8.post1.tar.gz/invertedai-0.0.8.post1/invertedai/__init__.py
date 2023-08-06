75efc698fbf7a542365007758c84d13475feac18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:28 +0000

    Add package invertedai/0.0.8.post1/invertedai-0.0.8.post1.tar.gz

---
+++ packages/invertedai/invertedai-0.0.8.post1.tar.gz/invertedai-0.0.8.post1/invertedai/__init__.py
@@ -0,0 +1,48 @@
+import os
+from distutils.util import strtobool
+
+from invertedai.api.light import light
+from invertedai.api.location import location_info
+from invertedai.api.initialize import initialize, async_initialize
+from invertedai.api.drive import drive, async_drive
+from invertedai.cosimulation import BasicCosimulation
+from invertedai.utils import Jupyter_Render, IAILogger, Session
+
+dev = strtobool(os.environ.get("IAI_DEV", "false"))
+if dev:
+    dev_url = os.environ.get("IAI_DEV_URL", "http://localhost:8000")
+log_level = os.environ.get("IAI_LOG_LEVEL", "WARNING")
+log_console = strtobool(os.environ.get("IAI_LOG_CONSOLE", "true"))
+log_file = strtobool(os.environ.get("IAI_LOG_FILE", "false"))
+api_key = os.environ.get("IAI_API_KEY", "")
+
+logger = IAILogger(level=log_level, consoel=bool(log_console), log_file=bool(log_file))
+
+session = Session(api_key)
+add_apikey = session.add_apikey
+use_mock_api = session.use_mock_api
+
+if strtobool(os.environ.get("IAI_MOCK_API", "false")):
+    use_mock_api()
+
+model_resources = {
+    "initialize": ("post", "/initialize"),
+    "drive": ("post", "/drive"),
+    "location_info": ("get", "/location_info"),
+    "light": ("get", "/light"),
+    "test": ("get", "/test"),
+}
+__all__ = [
+    "BasicCosimulation",
+    "Jupyter_Render",
+    "logger",
+    "session",
+    "add_apikey",
+    "use_mock_api",
+    "drive",
+    "initialize",
+    "location_info",
+    "light",
+    "async_initialize",
+    "async_drive",
+]
