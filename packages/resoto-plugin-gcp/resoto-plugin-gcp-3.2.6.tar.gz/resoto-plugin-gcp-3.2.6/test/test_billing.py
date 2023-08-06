ffb6b0602bdab18d65dc55be458cf2fe95dc7405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:17 +0000

    Add package resoto-plugin-gcp/3.2.6/resoto-plugin-gcp-3.2.6.tar.gz

---
+++ packages/resoto-plugin-gcp/resoto-plugin-gcp-3.2.6.tar.gz/resoto-plugin-gcp-3.2.6/test/test_billing.py
@@ -0,0 +1,15 @@
+from .random_client import roundtrip, json_roundtrip
+from resoto_plugin_gcp.resources.billing import *
+from resoto_plugin_gcp.resources.base import GraphBuilder
+
+
+def test_gcp_billing_account(random_builder: GraphBuilder) -> None:
+    roundtrip(GcpBillingAccount, random_builder)
+    json_roundtrip(GcpProjectBillingInfo, random_builder)
+    assert len(random_builder.edges_of(GcpBillingAccount, GcpProjectBillingInfo)) > 0
+
+
+def test_gcp_service(random_builder: GraphBuilder) -> None:
+    roundtrip(GcpService, random_builder)
+    json_roundtrip(GcpSku, random_builder)
+    assert len(random_builder.edges_of(GcpService, GcpSku)) > 0
