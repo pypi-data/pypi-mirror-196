af102c699ed7d8713b95c4860f72c063ffe27d3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:36 +0000

    Add package nse-ab-quantlib/0.8.0/nse_ab_quantlib-0.8.0.tar.gz

---
+++ packages/nse-ab-quantlib/nse_ab_quantlib-0.8.0.tar.gz/nse_ab_quantlib-0.8.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+import glob
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    install_requires = fh.read()
+
+
+__version__ = "0.8.0"
+
+setuptools.setup(
+    name="nse_ab_quantlib",
+    version=__version__,
+    author="Jaskirat Singh",
+    author_email="jaskiratsingh1208@gmail.com",
+    description="Way of Testing option selling strategies in NSE",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jaskirat1208/backtest-platform",
+    scripts=glob.glob('quantlib/quantlib/main/*.py'),
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
