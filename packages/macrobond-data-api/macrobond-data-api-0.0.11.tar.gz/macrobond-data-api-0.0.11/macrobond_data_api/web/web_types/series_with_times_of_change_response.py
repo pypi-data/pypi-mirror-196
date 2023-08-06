282ab27ad113f3102a46c016e9fc02574a4366e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:40 +0000

    Add package macrobond-data-api/0.0.11/macrobond-data-api-0.0.11.tar.gz

---
+++ packages/macrobond-data-api/macrobond-data-api-0.0.11.tar.gz/macrobond-data-api-0.0.11/macrobond_data_api/web/web_types/series_with_times_of_change_response.py
@@ -0,0 +1,12 @@
+# pylint: disable = missing-module-docstring
+
+from typing import Optional, List
+
+from .series_response import SeriesResponse
+
+
+class SeriesWithTimesOfChangeResponse(SeriesResponse):
+    """A time series with times of change"""
+
+    timesOfChange: Optional[List[Optional[str]]]
+    """The time each value was last modified"""
