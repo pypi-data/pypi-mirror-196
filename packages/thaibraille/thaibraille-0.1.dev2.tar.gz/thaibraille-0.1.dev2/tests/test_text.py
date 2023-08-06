fec70296d31a643dbe86029944a439122a048fa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:45 +0000

    Add package thaibraille/0.1.dev2/thaibraille-0.1.dev2.tar.gz

---
+++ packages/thaibraille/thaibraille-0.1.dev2.tar.gz/thaibraille-0.1.dev2/tests/test_text.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import unittest
+from thaibraille.text import *
+
+
+class TestTextPackage(unittest.TestCase):
+    def test_thai_word_braille(self):
+        self.assertEqual(thai_word_braille("ลิ้น"), '⠇⠃⠲⠝')
+        self.assertEqual(thai_word_braille("ว่าง"), '⠺⠔⠡⠻')
+        self.assertEqual(thai_word_braille("แก้ม"), '⠣⠛⠲⠍')
+
+    def test_thai_text_braille(self):
+        self.assertEqual(thai_text_braille("แมวกิน   ปลา"), ['⠣⠍⠺', '⠛⠃⠝', '   ', '⠯⠇⠡'])
