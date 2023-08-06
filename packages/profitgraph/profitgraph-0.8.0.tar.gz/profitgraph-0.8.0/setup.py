2c7ad0951bd6a003472c91d5db97075f5522dfca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:46 +0000

    Add package profitgraph/0.8.0/profitgraph-0.8.0.tar.gz

---
+++ packages/profitgraph/profitgraph-0.8.0.tar.gz/profitgraph-0.8.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def main():
+    import profitgraph as app
+
+    setup(
+        name=app.__project__,
+        version=app.__version__,
+        description=app.__doc__,
+        long_description=read('README.md'),
+        classifiers=app.__classifiers__,
+        author=app.__author__,
+        author_email=app.__author_email__,
+        url=app.__url__,
+        license=[
+            c.rsplit('::', 1)[1].strip()
+            for c in app.__classifiers__
+            if c.startswith('License ::')
+        ][0],
+        keywords=app.__keywords__,
+        packages=find_packages(),
+        include_package_data=True,
+        platforms=app.__platforms__,
+        entry_points=app.__entry_points__,
+        install_requires=app.__requires__,
+    )
+
+if __name__ == '__main__':
+    main()
