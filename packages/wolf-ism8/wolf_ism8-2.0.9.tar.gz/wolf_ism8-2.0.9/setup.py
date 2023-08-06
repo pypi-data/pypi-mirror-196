38d6286146b4c8d7a055d0e945e2641c718de2d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:19 +0000

    Add package wolf-ism8/2.0.9/wolf_ism8-2.0.9.tar.gz

---
+++ packages/wolf-ism8/wolf_ism8-2.0.9.tar.gz/wolf_ism8-2.0.9/setup.py
@@ -0,0 +1,13 @@
+""" setup for creating pypi package """
+import setuptools
+
+
+setuptools.setup(
+    name="wolf_ism8",
+    version="2.0.4",
+    author="marcschmiedchen",
+    author_email="marc.schmiedchen@protonmail.com",
+    description="Get data from wolf heating system via ISM8",
+    long_description="test",
+    packages=["wolf_ism8"],
+)
