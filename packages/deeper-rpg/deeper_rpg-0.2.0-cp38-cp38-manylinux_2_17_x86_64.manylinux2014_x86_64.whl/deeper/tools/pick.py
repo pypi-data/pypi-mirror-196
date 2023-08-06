418f3ca428834ace733c6256276f5a994836a91d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:46:29 +0000

    Add package deeper-rpg/0.2.0/deeper_rpg-0.2.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/deeper-rpg/deeper_rpg-0.2.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/deeper/tools/pick.py
@@ -0,0 +1,74 @@
+from loguru import logger
+
+from pyglet import clock
+import pyglet.window.mouse as mouse
+import arcade
+from arcade import key
+
+from .scene_tool import SceneEditTool
+
+
+class Hovered:
+    def __init__(self, block, position):
+        self.block = block
+        self.position = position
+
+class Selected:
+    def __init__(self, block):
+        self.block = block
+
+class PickTool(SceneEditTool):
+    def __init__(self, view, edit_state) -> None:
+        super().__init__(view, edit_state, 'Pick')
+        self.hovered = None
+        self.selected = None
+
+    def on_mouse_motion(self, x: int, y: int, dx: int, dy: int):
+        #print("mouse: ", x, y)
+        ray = self.camera.mouse_to_ray(x, y)
+        result = self.scene.cast_ray(ray)
+        #print(result)
+        if result:
+            block, contact = result
+            #print("contact: ", contact)
+            self.hovered = Hovered(block, contact)
+        else:
+            self.hovered = None
+
+    def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
+        super().on_mouse_press(x, y, button, modifiers)
+        #logger.debug(f"{self.view.title}:{self.title}:on_mouse_press")
+        if button != mouse.LEFT or not self.hovered:
+            return
+        
+        self.selected = Selected(self.hovered.block)
+        if self._click_count == 2:
+            #self.push_entity_editor()
+            clock.schedule_once(lambda dt, *args, **kwargs : self.push_entity_editor(), 0)
+            return
+
+
+    def push_entity_editor(self):
+        from deeper.state import EntityEditState
+        from deeper.views.entity_editor import EntityEditor
+        self.window.push_view(EntityEditor(self.window, EntityEditState(self.world, self.selected.block)))
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        if symbol == key.DELETE and self.selected:
+            self.world.delete_entity(self.selected.block)
+            if self.hovered and self.hovered.block == self.selected.block:
+                self.hovered = None
+            self.selected = None
+        elif symbol == key.ESCAPE:
+            self.selected = None
+
+    def draw(self):
+        if self.hovered:
+            pos = self.camera.project(self.hovered.position)
+            #print("self.hovered.position: ", self.hovered.position)
+            #print("pos: ", pos)
+            #arcade.draw_circle_outline(*pos.xy, 18, arcade.color.RED, 3)
+            self.scene.draw_aabb(self.hovered.block.aabb)
+
+        if self.selected:
+            self.scene.draw_aabb(self.selected.block.aabb, color=arcade.color.RED)
---
+++ packages/deeper-rpg/deeper_rpg-0.2.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/deeper/widgets/entity_window.py
@@ -0,0 +1,26 @@
+import pyglet
+import imgui
+
+from deeper.dimgui import Widget, Window
+from deeper.kits.component_widget_kit import ComponentWidgetKit
+
+class EntityPanel(Widget):
+    def __init__(self, children=...):
+        super().__init__(children)
+
+    def draw(self):
+        for child in self.children:
+            expanded, child.visible = imgui.collapsing_header(child.name, child.visible)
+            if expanded:
+                child.draw()
+        return True
+
+class EntityWindow(Window):
+    def __init__(self, world, entity):
+        self.world = world
+        self.entity = entity
+        components = world.components_for_entity(entity)
+        children = []
+        for component in components:
+            children.append(ComponentWidgetKit.instance.build(component))
+        super().__init__("Entity", children=[EntityPanel(children)])
