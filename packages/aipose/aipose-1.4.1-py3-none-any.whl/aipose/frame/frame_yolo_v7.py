0762006567b50180f0c86d2e70059da33e5a4c67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:22 +0000

    Add package aipose/1.4.1/aipose-1.4.1-py3-none-any.whl

---
+++ packages/aipose/aipose-1.4.1-py3-none-any.whl/aipose/frame/frame_yolo_v7.py
@@ -0,0 +1,31 @@
+from typing import List
+
+import torch
+from numpy import ndarray
+
+from aipose.frame import FrameManagerBase
+from aipose.models.yolov7 import YoloV7Pose, YoloV7PoseKeypoints
+
+
+class FrameYoloV7(FrameManagerBase):
+    def __init__(self):
+        self.model = YoloV7Pose()
+
+    def before_read_frame(self):
+        torch.cuda.empty_cache()
+
+    def on_frame(self, frame: ndarray) -> ndarray:
+        prediction, image_tensor = self.model(frame)
+        processed_frame = self.on_predict(frame, prediction, image_tensor)
+        if processed_frame is None:
+            return frame
+        else:
+            return processed_frame
+
+    def on_predict(
+        self,
+        frame: ndarray,
+        prediction: List[YoloV7PoseKeypoints],
+        image_tensor: ndarray,
+    ) -> None | ndarray:
+        raise NotImplementedError
