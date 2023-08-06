dceb518cf75eef42d067ed3f0fac579816653b8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:09 +0000

    Add package dwork/0.0.11/dwork-0.0.11.tar.gz

---
+++ packages/dwork/dwork-0.0.11.tar.gz/dwork-0.0.11/dwork/language/expression.py
@@ -0,0 +1,106 @@
+import abc
+from typing import Any
+from .types import Type
+
+
+class Expression(abc.ABC):
+    def __add__(self, right: Any) -> "Expression":
+        from .operators import Add
+
+        return binary_op(Add, self, right)
+
+    def __sub__(self, right: Any) -> "Expression":
+        from .operators import Sub
+
+        return binary_op(Sub, self, right)
+
+    def __mul__(self, right: Any) -> "Expression":
+        from .operators import Mul
+
+        return binary_op(Mul, self, right)
+
+    def __truediv__(self, right: "Expression") -> "Expression":
+        from .operators import TrueDiv
+
+        return binary_op(TrueDiv, self, right)
+
+    def __floordiv__(self, right: "Expression") -> "Expression":
+        from .operators import FloorDiv
+
+        return binary_op(FloorDiv, self, right)
+
+    @abc.abstractproperty
+    def type(self) -> Type:
+        raise NotImplementedError
+
+    def is_dp(self) -> bool:
+        """
+        Returns `True` if the value of this expression already fulfills
+        the DP property.
+        """
+        return False
+
+    def sum(self) -> "Expression":
+        """
+        Returns the sum of an expression. Assumes that the expression is indexable.
+        """
+        from .functions import Sum
+
+        return Sum(self)
+
+    @abc.abstractmethod
+    def true(self) -> Any:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def sensitivity(self) -> Any:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def dp(self, epsilon: float) -> Any:
+        raise NotImplementedError
+
+
+def to_expression(value: Any) -> Expression:
+    return Constant(value)
+
+
+def binary_op(op, left, right):
+    if not isinstance(left, Expression):
+        left = to_expression(left)
+    if not isinstance(right, Expression):
+        right = to_expression(right)
+    return op(left, right)
+
+
+class Constant(Expression):
+    def __init__(self, value):
+        self.value = value
+        # we call this to check whether we have a type for this constant
+        self.type
+
+    @property
+    def type(self) -> Type:
+        if isinstance(self.value, int):
+            from .types import Integer
+
+            return Integer(min=self.value, max=self.value)
+        elif isinstance(self.value, float):
+            from .types import Float
+
+            return Float(min=self.value, max=self.value)
+        else:
+            raise ValueError("unsupported constant type")
+
+    def sensitivity(self) -> Any:
+        return 0
+
+    def true(self) -> Any:
+        return self.value
+
+    def dp(self, epsilon: float) -> Any:
+        return self.true()
+
+
+class ConditionalExpression(Expression):
+    pass
---
+++ packages/dwork/dwork-0.0.11.tar.gz/dwork-0.0.11/dwork/language/types.py
@@ -0,0 +1,289 @@
+from ..mechanisms import laplace_noise, geometric_noise
+from typing import Optional, Union, Any
+import math
+import abc
+
+maxint = int(2e31 - 1)
+
+
+class Type:
+    @abc.abstractmethod
+    def dp(self, value: Any, sensitivity: Any, epsilon: float) -> Any:
+        raise NotImplementedError
+
+
+class Numeric(Type):
+    @abc.abstractmethod
+    def __add__(self, other: "Numeric") -> "Numeric":
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def __mul__(self, other: "Numeric") -> "Numeric":
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def __sub__(self, other: "Numeric") -> "Numeric":
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def __truediv__(self, other: "Numeric") -> "Numeric":
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def __floordiv__(self, other: "Numeric") -> "Numeric":
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def sum(self) -> "Numeric":
+        raise NotImplementedError
+
+    @property
+    def range(self) -> Any:
+        return self.max - self.min
+
+    @property
+    def absmin(self) -> Any:
+        return min(abs(self.min), abs(self.max))
+
+    @property
+    def absmax(self) -> Any:
+        return max(abs(self.min), abs(self.max))
+
+    @abc.abstractproperty
+    def min(self) -> Any:
+        raise NotImplementedError
+
+    @abc.abstractproperty
+    def max(self) -> Any:
+        raise NotImplementedError
+
+
+class Array(Numeric):
+    def __init__(self, type: Type):
+        if not isinstance(type, Numeric):
+            raise ValueError("array requires a numeric type")
+        self.type = type
+
+    @property
+    def itemtype(self) -> Type:
+        return self.type
+
+    @property
+    def min(self):
+        return self.type.min
+
+    @property
+    def max(self):
+        return self.type.max
+
+    def __add__(self, other: Numeric) -> Numeric:
+        if isinstance(other, Array):
+            other_type: Union[Numeric, Numeric] = other.type
+        else:
+            other_type = other
+        if not isinstance(other_type, Numeric):
+            raise ValueError("cannot add these types")
+        return Array(self.type + other_type)
+
+    def __sub__(self, other: Numeric) -> Numeric:
+        if isinstance(other, Array):
+            other_type: Union[Numeric, Numeric] = other.type
+        else:
+            other_type = other
+        if not isinstance(other_type, Numeric):
+            raise ValueError("cannot add these types")
+        return Array(self.type - other_type)
+
+    def __truediv__(self, other: Numeric) -> Numeric:
+        if isinstance(other, Array):
+            other_type: Union[Numeric, Numeric] = other.type
+        else:
+            other_type = other
+        if not isinstance(other_type, Numeric):
+            raise ValueError("cannot divide these types")
+        return Array(self.type / other_type)
+
+    def __floordiv__(self, other: Numeric) -> Numeric:
+        if isinstance(other, Array):
+            other_type: Union[Numeric, Numeric] = other.type
+        else:
+            other_type = other
+        if not isinstance(other_type, Numeric):
+            raise ValueError("cannot divide these types")
+        return Array(self.type // other_type)
+
+    def __mul__(self, other: Numeric) -> Numeric:
+        if isinstance(other, Array):
+            other_type: Union[Numeric, Numeric] = other.type
+        else:
+            other_type = other
+        if not isinstance(other_type, Numeric):
+            raise ValueError("cannot multiply these types")
+        return Array(self.type * other_type)
+
+    def dp(self, value: Any, sensitivity: Any, epsilon: float) -> Any:
+        raise NotImplementedError
+
+    def sum(self) -> Numeric:
+        if not isinstance(self.type, Numeric):
+            raise ValueError("underlying type is not Numeric")
+        return self.type.sum()
+
+
+class Integer(Numeric):
+    """
+    Represents integer data
+    """
+
+    def __sub__(self, other: Numeric) -> Numeric:
+        """
+        Returns the type of a - b
+        """
+        if isinstance(other, Array):
+            return Array(self - other.type)
+        return Integer()
+
+    def __mul__(self, other: Numeric) -> Numeric:
+        """
+        Returns the type of a * b
+        """
+        if isinstance(other, Array):
+            return Array(self * other.type)
+        return Integer()
+
+    def __truediv__(self, other: Numeric) -> Numeric:
+        """
+        Returns the type of a / b
+        """
+        if isinstance(other, Array):
+            return Array(self / other.type)
+        return Integer()
+
+    def __floordiv__(self, other: Numeric) -> Numeric:
+        """
+        Returns the type of a // b
+        """
+        if isinstance(other, Array):
+            return Array(self // other.type)
+        return Integer()
+
+    def __init__(self, min: Optional[int] = -maxint, max: Optional[int] = maxint):
+        self._min = min
+        self._max = max
+
+    @property
+    def min(self) -> Any:
+        return self._min
+
+    @property
+    def max(self) -> Any:
+        return self._max
+
+    def __add__(self, other: Numeric) -> Numeric:
+        if not isinstance(other, Numeric):
+            raise ValueError("can only add numeric types")
+        if isinstance(other, Array):
+            return Array(other.type + self)
+        return Integer(
+            int(math.floor(self.min + other.min)), int(math.ceil(self.max + other.max))
+        )
+
+    def sum(self) -> "Integer":
+        return Integer()
+
+    def dp(self, value: Any, sensitivity: Any, epsilon: float) -> Any:
+        return min(
+            max(
+                value + geometric_noise(epsilon, symmetric=True) * sensitivity, self.min
+            ),
+            self.max,
+        )
+
+    type = int
+
+
+class Float(Numeric):
+    """
+    Represents floating point data
+    """
+
+    def __mul__(self, other: Numeric) -> Numeric:
+        """
+        Returns the type of a / b
+        """
+        if isinstance(other, Array):
+            return Array(self * other.type)
+        return Float()
+
+    def __sub__(self, other: Numeric) -> Numeric:
+        """
+        Returns the type of a / b
+        """
+        if isinstance(other, Array):
+            return Array(other.type - self)
+        return Float(self.min - other.max, self.max - other.min)
+
+    def __truediv__(self, other: Numeric) -> Numeric:
+        """
+        Returns the type of a / b
+        """
+        if isinstance(other, Array):
+            return Array(self / other.type)
+        return Float()
+
+    def __floordiv__(self, other: Numeric) -> Numeric:
+        """
+        Returns the type of a / b
+        """
+        if isinstance(other, Array):
+            return Array(self // other.type)
+        return Float()
+
+    def __init__(
+        self, min: Optional[float] = float("-inf"), max: Optional[float] = float("inf")
+    ):
+        self._min = min
+        self._max = max
+
+    @property
+    def min(self) -> Any:
+        return self._min
+
+    @property
+    def max(self) -> Any:
+        return self._max
+
+    def sum(self) -> Numeric:
+        return Float()
+
+    def __add__(self, other: Numeric) -> Numeric:
+        if not isinstance(other, Numeric):
+            raise ValueError("can only add numeric types")
+        if isinstance(other, Array):
+            return Array(self + other.type)
+        return Float(self.min + other.min, self.max + other.max)
+
+    def dp(self, value: Any, sensitivity: Any, epsilon: float) -> Any:
+        return min(
+            max(value + laplace_noise(epsilon) * sensitivity, self.min), self.max
+        )
+
+    type = float
+
+
+class Categorical(Type):
+    """
+    Represents categorical data
+    """
+
+
+class Boolean(Type):
+    """
+    Represents boolean data
+    """
+
+    def dp(self, value: Any, sensitivity: Any, epsilon: float) -> Any:
+        """
+        To do: implement randomized response-
+        """
+        raise NotImplementedError
