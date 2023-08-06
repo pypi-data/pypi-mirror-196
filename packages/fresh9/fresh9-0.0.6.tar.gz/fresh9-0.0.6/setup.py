b77fa0dae8182f64da758d71c7074e83ddc83806
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:13 +0000

    Add package fresh9/0.0.6/fresh9-0.0.6.tar.gz

---
+++ packages/fresh9/fresh9-0.0.6.tar.gz/fresh9-0.0.6/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from io import open
+from setuptools import setup
+
+
+version = '0.0.6'
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='fresh9',
+    version=version,
+
+    author='Dmitrii',
+    author_email='refresher228@mail.ru',
+
+    description=(
+        u'Machinelearning hypr'
+
+    ),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    
+    packages=['fresh9'],
+    
+)
