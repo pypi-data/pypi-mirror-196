f26c0ca2979925acffe33351b1aa55da9375efaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:46:58 +0000

    Add package deeper-rpg/0.2.0/deeper_rpg-0.2.0-cp39-none-win_amd64.whl

---
+++ packages/deeper-rpg/deeper_rpg-0.2.0-cp39-none-win_amd64.whl/deeper/kits/entity_kit.py
@@ -0,0 +1,20 @@
+from loguru import logger
+
+import deeper.entities
+from deeper.constants import *
+from .kit import Kit
+
+
+class EntityKit(Kit):
+    builders_path = deeper.entities
+
+    def find(self, blueprint):
+        if blueprint.name in self.builders:
+            return self.builders[blueprint.name]
+        if hasattr(blueprint, 'extends'):
+            return self.find(blueprint.catalog.find(blueprint.extends))
+
+    def build(self, blueprint, world, layer, position=DEFAULT_VEC3):
+        # logger.debug(blueprint.__dict__)
+        builder = self.find(blueprint)
+        return builder.build(blueprint, world, layer, position)
---
+++ packages/deeper-rpg/deeper_rpg-0.2.0-cp39-none-win_amd64.whl/deeper/widgets/blueprint/sprite_vu_bp_widget.py
@@ -0,0 +1,21 @@
+import glm
+import imgui
+
+from deeper.blueprints.component.sprite_vu_blueprint import SpriteVuBlueprint
+from .blueprint_widget import BlueprintWidget, BlueprintWidgetBuilder
+
+class SpriteVuBpWidget(BlueprintWidget):
+    def __init__(self, blueprint):
+        super().__init__(blueprint)
+        self.blueprint = blueprint
+
+    def draw(self):
+        changed, offset = imgui.drag_float2(
+            'Offset', *self.blueprint.offset, change_speed=0.1
+        )
+        if changed:
+            self.blueprint.offset = glm.vec2(*offset)
+
+class SpriteVuBpWidgetBuilder(BlueprintWidgetBuilder):
+    key = SpriteVuBlueprint
+    cls = SpriteVuBpWidget
