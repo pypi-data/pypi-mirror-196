53c411ed30240b1ce8d5fe7baedec6df96a20903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:46 +0000

    Add package zcbot-service-sdk/0.0.1/zcbot_service_sdk-0.0.1.tar.gz

---
+++ packages/zcbot-service-sdk/zcbot_service_sdk-0.0.1.tar.gz/zcbot_service_sdk-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
+setup(name='zcbot_service_sdk',
+    version='0.0.1',
+    description='zcbot service sdk for zsodata',
+    long_description=long_description,
+    author='yk',
+    author_email='815583442@qq.com',
+    url='http://gitee.com',
+    install_requires=['celery==5.2.7', 'redis==4.3.5', ],
+    python_requires=">=3.7",
+    license='BSD License',
+    packages=find_packages(),
+    platforms=['all'],
+
+
+)
