6b7ab2b1dcca7d4526953cd80803b02f70430a29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:38 +0000

    Add package ws2223-group7-hanabi-learning-environment-hgts/0.0.14/ws2223-group7-hanabi-learning-environment_hgts-0.0.14.tar.gz

---
+++ packages/ws2223-group7-hanabi-learning-environment-hgts/ws2223-group7-hanabi-learning-environment_hgts-0.0.14.tar.gz/ws2223-group7-hanabi-learning-environment_hgts-0.0.14/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages
+from skbuild import setup
+
+setup(
+    name='ws2223-group7-hanabi-learning-environment_hgts',
+    version='0.0.14',    
+    description='ws2223-group7 Learning environment for the game of hanabi hgts agent.',
+    long_description='ws2223-group7 Learning environment for the game of hanabi hgts agent.',
+    long_description_content_type="text/markdown",
+    author='ws2223-group7/hanabi-learning-environment',
+    packages=find_packages(),
+    install_requires=['cffi'],
+    python_requires=">=3.6",
+    classifiers = [
+    "Programming Language :: Python :: 3",
+    "Operating System :: POSIX :: Linux",
+    ],
+    entry_points={
+    'console_scripts': [
+        'ws2223-group7-hanabi-learning-environment-hgts-rcd = main.main_rec:main',
+        'ws2223-group7-hanabi-learning-environment-hgts-info = main.main_info:main',
+        'ws2223-group7-hanabi-learning-environment-hgts-own = main.main_own:main',
+    ],
+}
+)
