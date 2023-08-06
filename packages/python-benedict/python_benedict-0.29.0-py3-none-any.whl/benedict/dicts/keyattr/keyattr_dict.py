747a9a2edf8853ad9f3a982de01381af31304105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:25 +0000

    Add package python-benedict/0.29.0/python_benedict-0.29.0-py3-none-any.whl

---
+++ packages/python-benedict/python_benedict-0.29.0-py3-none-any.whl/benedict/dicts/keyattr/keyattr_dict.py
@@ -0,0 +1,46 @@
+from benedict.dicts.base import BaseDict
+
+
+class KeyattrDict(BaseDict):
+    _keyattr_enabled = None
+
+    def __init__(self, *args, **kwargs):
+        self._keyattr_enabled = kwargs.pop("keyattr_enabled", True)
+        super().__init__(*args, **kwargs)
+
+    @property
+    def keyattr_enabled(self):
+        return self._keyattr_enabled
+
+    @keyattr_enabled.setter
+    def keyattr_enabled(self, value):
+        self._keyattr_enabled = value
+
+    def __getattr__(self, attr):
+        if not self._keyattr_enabled:
+            raise AttributeError
+        # return super().__getattr__(attr)
+        try:
+            return self.__getitem__(attr)
+        except KeyError:
+            self.__setitem__(attr, {})
+            return self.__getitem__(attr)
+
+    def __setattr__(self, attr, value):
+        if attr in self:
+            # set existing key
+            if not self._keyattr_enabled:
+                raise AttributeError
+            self.__setitem__(attr, value)
+        elif hasattr(self.__class__, attr):
+            # set existing attr
+            super().__setattr__(attr, value)
+        else:
+            # set new key
+            if not self._keyattr_enabled:
+                raise AttributeError
+            self.__setitem__(attr, value)
+
+    def __setstate__(self, state):
+        super().__setstate__(state)
+        self._keyattr_enabled = state["_keyattr_enabled"]
