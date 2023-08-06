82231e35049a5802c4e6a88870efb7723525db3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:32 +0000

    Add package universal-pathlib/0.0.22/universal_pathlib-0.0.22-py3-none-any.whl

---
+++ packages/universal-pathlib/universal_pathlib-0.0.22-py3-none-any.whl/upath/implementations/http.py
@@ -0,0 +1,49 @@
+import upath.core
+
+
+class _HTTPAccessor(upath.core._FSSpecAccessor):
+    def __init__(self, parsed_url, *args, **kwargs):
+        super().__init__(parsed_url, *args, **kwargs)
+
+    def _format_path(self, path):
+        return str(path)
+
+
+class HTTPPath(upath.core.UPath):
+    _default_accessor = _HTTPAccessor
+
+    def is_dir(self):
+        try:
+            return self._path_type() == "directory"
+        except FileNotFoundError:
+            return False
+
+    def is_file(self):
+        try:
+            return self._path_type() == "file"
+        except FileNotFoundError:
+            return False
+
+    def _path_type(self):
+        try:
+            next(self.iterdir())
+        except (StopIteration, NotADirectoryError):
+            return "file"
+        else:
+            return "directory"
+
+    def _sub_path(self, name):
+        """
+        `fsspec` returns the full path as `scheme://netloc/<path>` with
+        `listdir` and `glob`. However, in `iterdir` and `glob` we only want the
+        relative path to `self`.
+        """
+        complete_address = self._format_parsed_parts(
+            None, None, [self.path], url=self._url, **self._kwargs
+        )
+
+        if name.startswith(complete_address):
+            name = name[len(complete_address) :]  # noqa: E203
+        name = name.strip("/")
+
+        return name
