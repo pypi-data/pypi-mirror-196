17bfbdf6adbb034bf4d2978ece9e4943c234b0fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:01 +0000

    Add package ssm-tool-test/2.1.0/ssm_tool_test-2.1.0.tar.gz

---
+++ packages/ssm-tool-test/ssm_tool_test-2.1.0.tar.gz/ssm_tool_test-2.1.0/src/ssm_tool_test/ssm.py
@@ -0,0 +1,24 @@
+# Definitions of methods to get parameter store from aws
+# Note: using aws credentials
+import boto3
+
+
+SSM_CLIENT = boto3.client('ssm')
+
+class ParameterStore():
+    """
+    Structure class of SSM parameter store
+
+    Method availables: get_parameter, describe_parameter
+    """
+
+    def get_parameter(name):
+        try:
+            ssm_parameter = SSM_CLIENT.get_parameter(
+                Name=name,
+                WithDecryption=True|False
+            )
+            return ssm_parameter['Parameter']
+        except SSM_CLIENT.exceptions.ParameterNotFound as message:
+            print(f"Error: {message}")
+    
