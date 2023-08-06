e6bceb781603b26c486c1a3ddecb8bf1f2d322fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:07 +0000

    Add package django-idom/3.0.0/django_idom-3.0.0.tar.gz

---
+++ packages/django-idom/django_idom-3.0.0.tar.gz/django_idom-3.0.0/src/django_idom/__init__.py
@@ -0,0 +1,13 @@
+from django_idom import components, decorators, hooks, types, utils
+from django_idom.websocket.paths import IDOM_WEBSOCKET_PATH
+
+
+__version__ = "3.0.0"
+__all__ = [
+    "IDOM_WEBSOCKET_PATH",
+    "hooks",
+    "components",
+    "decorators",
+    "types",
+    "utils",
+]
