b49615910ed82b1d111d964c241ce5a5cc182026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:47 +0000

    Add package py-parallel-processing/0.4.2/py_parallel_processing-0.4.2-py3-none-any.whl

---
+++ packages/py-parallel-processing/py_parallel_processing-0.4.2-py3-none-any.whl/py_parallel_processing/main.py
@@ -0,0 +1,28 @@
+
+
+def py_parallel_pools(some_func, params):
+    """
+    함수를 병렬처리하는 함수입니다.
+
+    Args:
+        some_func: 병렬처리할 함수
+        params: 함수의 인자로 사용될 리스트
+
+    Returns:
+        병렬처리된 함수의 반환값들을 리스트 형태로 반환합니다.
+    """
+    from pathos.pools import ProcessPool
+    from tqdm import tqdm
+
+    try:
+        with ProcessPool() as pool:
+            # 함수를 병렬처리하면서 진행상황 바(progress bar)를 표시합니다.
+            return_variables = list(
+                tqdm(pool.imap(some_func, params), total=len(params)))
+            # 반환값이 None인 경우는 리스트에서 제외합니다.
+            return_variables = [i for i in return_variables if i is not None]
+    finally:
+        pool.close()
+        pool.join()
+
+    return return_variables
