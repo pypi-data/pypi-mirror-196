1f20b4d1e9bc3994b40e416a16f74a232e6c1f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:55 +0000

    Add package chalkpy/1.15.4/chalkpy-1.15.4-py3-none-any.whl

---
+++ packages/chalkpy/chalkpy-1.15.4-py3-none-any.whl/chalk/__init__.py
@@ -0,0 +1,38 @@
+from chalk._version import __version__
+from chalk.features import Cron, Environments, Tags, description, is_primary, owner, tags
+from chalk.features.resolver import OfflineResolver, OnlineResolver, Resolver, offline, online
+from chalk.logging import chalk_logger
+from chalk.state import State
+from chalk.streams import Windowed, stream, windowed
+from chalk.utils import AnyDataclass
+from chalk.utils.duration import CronTab, Duration, ScheduleOptions
+
+batch = offline
+realtime = online
+
+__all__ = [
+    "AnyDataclass",
+    "Cron",
+    "CronTab",
+    "Duration",
+    "Environments",
+    "OfflineResolver",
+    "OnlineResolver",
+    "Resolver",
+    "ScheduleOptions",
+    "State",
+    "Tags",
+    "Windowed",
+    "__version__",
+    "batch",
+    "chalk_logger",
+    "description",
+    "is_primary",
+    "offline",
+    "online",
+    "owner",
+    "realtime",
+    "stream",
+    "tags",
+    "windowed",
+]
