06ad02244bb255c7811df258a881a116b9bfd06f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:03 +0000

    Add package subaligner/0.3.0/subaligner-0.3.0-py3.8.egg

---
+++ packages/subaligner/subaligner-0.3.0-py3.8.egg/subaligner/exception.py
@@ -0,0 +1,14 @@
+class UnsupportedFormatException(Exception):
+    """ An exception raised due to unsupported formats."""
+
+
+class TerminalException(Exception):
+    """ An exception raised due to unrecoverable failures."""
+
+
+class NoFrameRateException(Exception):
+    """ An exception raised due to frame rate not found."""
+
+
+class TranscriptionException(Exception):
+    """ An exception raised due to transcription failures."""
