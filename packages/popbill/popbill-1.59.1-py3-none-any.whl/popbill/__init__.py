06412cca4cdf23a94b5e42ef2d946372519a2bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:31 +0000

    Add package popbill/1.59.1/popbill-1.59.1-py3-none-any.whl

---
+++ packages/popbill/popbill-1.59.1-py3-none-any.whl/popbill/__init__.py
@@ -0,0 +1,26 @@
+__version__ = '1.59.1'
+Version = __version__  # for backward compatibility
+__all__ = ["PopbillException", "JoinForm", "ContactInfo", "CorpInfo",
+           "TaxinvoiceService", "Taxinvoice", "TaxinvoiceDetail", "Contact",
+           "FaxService", "FaxReceiver", "FileData",
+           "StatementService", "Statement", "StatementDetail",
+           "CashbillService", "Cashbill",
+           "MessageService", "MessageReceiver",
+           "HTTaxinvoiceService", "HTCashbillService",
+           "ClosedownService", "BizInfoCheckService",
+           "KakaoService", "KakaoReceiver", "KakaoButton",
+           "EasyFinBankService", "BankAccountInfo", "AccountCheckService", "Response","UseHistory","RefundHistoryResult","RefundHistory","RefundForm","PaymentHistoryResult","PaymentResponse", "PaymentForm", "PaymentHistory" ]
+
+from .base import PopbillException, JoinForm, ContactInfo, CorpInfo, Response,UseHistory,RefundHistoryResult,RefundHistory,RefundForm,PaymentHistoryResult,PaymentResponse, PaymentForm, PaymentHistory
+from .taxinvoiceService import *
+from .statementService import *
+from .faxService import *
+from .cashbillService import *
+from .messageService import *
+from .htTaxinvoiceService import *
+from .htCashbillService import *
+from .closedownService import *
+from .bizInfoCheckService import *
+from .kakaoService import *
+from .easyFinBankService import *
+from .accountCheckService import *
