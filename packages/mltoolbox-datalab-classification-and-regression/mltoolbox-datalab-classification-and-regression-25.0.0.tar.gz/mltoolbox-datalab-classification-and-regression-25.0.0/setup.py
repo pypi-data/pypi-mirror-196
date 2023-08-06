4924fd1e8de55c7f6510e8ecc06ac1b182d668c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:08 +0000

    Add package mltoolbox-datalab-classification-and-regression/25.0.0/mltoolbox-datalab-classification-and-regression-25.0.0.tar.gz

---
+++ packages/mltoolbox-datalab-classification-and-regression/mltoolbox-datalab-classification-and-regression-25.0.0.tar.gz/mltoolbox-datalab-classification-and-regression-25.0.0/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+import reporter
+
+setup(name='mltoolbox-datalab-classification-and-regression', version='25.0.0', description='Ethical research - dep confusion package. This is not a real package, do not install.')
