d01fcea4efd1b4e24b0c022345f8d3159d4c8b57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:45 +0000

    Add package typernexrad/0.1.1/typernexrad-0.1.1.tar.gz

---
+++ packages/typernexrad/typernexrad-0.1.1.tar.gz/typernexrad-0.1.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open ('mypackage_readme.txt', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='typernexrad',
+    version='0.1.1',
+    description= "CLI which helps to access several functionalities of NEXRAD",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Team 7',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "": [".env"],
+    },
+    py_modules=['team7_typer.typer_main'],
+    install_requires=[
+        'typer',
+        'boto3',
+        'python-dotenv',
+        'pandas',
+        'python-dotenv',
+        'bcrypt',
+        'python-jose',
+        'fastapi',
+        'requests',
+
+                ],
+entry_points={
+    'console_scripts': [
+        'typer_t7 =team7_typer.typer_main:app',
+    ],
+}
+)
