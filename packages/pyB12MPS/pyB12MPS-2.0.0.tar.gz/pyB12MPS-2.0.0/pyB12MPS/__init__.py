d177f52b677f7efc911964a89756b79ca085c301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:52 +0000

    Add package pyB12MPS/2.0.0/pyB12MPS-2.0.0.tar.gz

---
+++ packages/pyB12MPS/pyB12MPS-2.0.0.tar.gz/pyB12MPS-2.0.0/pyB12MPS/__init__.py
@@ -0,0 +1,2 @@
+from .mps import *
+from .version import __version__
---
+++ packages/pyB12MPS/pyB12MPS-2.0.0.tar.gz/pyB12MPS-2.0.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open('README.md','r') as f:
+    long_description = f.read()
+
+with open("pyB12MPS/version.py", "r") as f:
+    # Define __version__
+    exec(f.read())
+
+setuptools.setup(
+    name='pyB12MPS',
+    version=__version__,
+    author='Bridge12 Technologies, Inc',
+    author_email='tkeller@bridge12.com',
+    description='A Python Package for Interfacing with the Bridge12 MPS',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='http://www.bridge12.com/',
+    project_urls={
+        'Source Code':'https://github.com/Bridge12Technologies/pyB12MPS',
+        'Documentation':'http://pyb12mps.bridge12.com',
+        },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=['numpy','pyserial'],
+)
