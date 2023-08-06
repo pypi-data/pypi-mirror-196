75f18ba7e0873c1179652af688debcbb6ec2f59f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:32 +0000

    Add package printcleaner/0.1.1/printcleaner-0.1.1.tar.gz

---
+++ packages/printcleaner/printcleaner-0.1.1.tar.gz/printcleaner-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['printcleaner']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['libcst>=0.4.7,<0.5.0', 'typer>=0.6.1,<0.7.0']
+
+setup_kwargs = {
+    'name': 'printcleaner',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'A. L. Walker',
+    'author_email': 'walkernotwalker@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
