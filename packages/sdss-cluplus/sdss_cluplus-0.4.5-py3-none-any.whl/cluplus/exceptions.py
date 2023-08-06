759ad3676770bc5a29e530528d906398bb84a42a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:52 +0000

    Add package sdss-cluplus/0.4.5/sdss_cluplus-0.4.5-py3-none-any.whl

---
+++ packages/sdss-cluplus/sdss_cluplus-0.4.5-py3-none-any.whl/cluplus/exceptions.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# @Author: Florian Briegel (briegel@mpia.de)
+# @Date: 2021-08-18
+# @Filename: exceptions.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+
+class ProxyException(Exception):
+    """Base proxy exception"""
+    
+    pass
+
+
+class ProxyPartialInvokeException(ProxyException):
+    """One or more invocation failed"""
+
+    pass
+
+class ProxyActorIsNotReachableException(ProxyException):
+    """The actor is not reachable"""
+
+    pass
+
+class ProxyUnpackKeysNotAllFoundException(ProxyException):
+    """More than one provided unpack filter keys not found"""
+
+    pass
