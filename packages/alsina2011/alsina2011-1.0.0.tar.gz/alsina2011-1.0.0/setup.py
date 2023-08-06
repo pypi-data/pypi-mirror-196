020fb27e4ab31177a0d1daeaa51599b31ee77c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:25 +0000

    Add package alsina2011/1.0.0/alsina2011-1.0.0.tar.gz

---
+++ packages/alsina2011/alsina2011-1.0.0.tar.gz/alsina2011-1.0.0/setup.py
@@ -0,0 +1,71 @@
+# {# pkglts, pysetup.kwds
+# format setup arguments
+from pathlib import Path
+from setuptools import setup, find_packages
+
+short_descr = "Data and formalisms from Alsina et al. (2011)"
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read()
+
+# find packages
+pkgs = find_packages('src')
+
+src_dir = Path("src/alsina2011")
+
+data_files = []
+for pth in src_dir.rglob("*"):
+    if not pth.is_dir() and "__pycache__" not in pth.parts:
+        if pth.suffix in ['.json', '.ini', '.csv', '.rst', '.svg']:
+            data_files.append(str(pth.relative_to(src_dir)))
+
+pkg_data = {'alsina2011': data_files}
+
+setup_kwds = dict(
+    name='alsina2011',
+    version="1.0.0",
+    description=short_descr,
+    long_description=readme + '\n\n' + history,
+    author="revesansparole",
+    author_email="revesansparole@gmail.com",
+    url='https://gitlab.com/b326/alsina2011',
+    license='cc_by_nc',
+    zip_safe=False,
+
+    packages=pkgs,
+    
+    package_dir={'': 'src'},
+    
+    
+    package_data=pkg_data,
+    setup_requires=[
+        "pytest-runner",
+        ],
+    install_requires=[
+        ],
+    tests_require=[
+        "coverage",
+        "pytest",
+        "pytest-cov",
+        "pytest-mock",
+        ],
+    entry_points={},
+    keywords='',
+    
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.9",
+    ],
+    )
+# #}
+# change setup_kwds below before the next pkglts tag
+
+# do not change things below
+# {# pkglts, pysetup.call
+setup(**setup_kwds)
+# #}
---
+++ packages/alsina2011/alsina2011-1.0.0.tar.gz/alsina2011-1.0.0/test/test_packaging.py
@@ -0,0 +1,5 @@
+import alsina2011
+
+
+def test_package_exists():
+    assert alsina2011.__version__
