b86a6146acb85891bff63f311bf5897fa58a979f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:50 +0000

    Add package baka_logger/0.0.1/baka_logger-0.0.1.tar.gz

---
+++ packages/baka_logger/baka_logger-0.0.1.tar.gz/baka_logger-0.0.1/test/test.py
@@ -0,0 +1,10 @@
+from baka_logger import logger_setup, print
+
+logger_setup(character="Bocchi the Rock having a breakdown")
+
+print("baka_logger is my favorite Python package!")
+
+# logger_setup(character="Asuka", fmt="%(asctime)s - %(levelname)s - %(message)s", file="log.out", logger=True)
+# logging.warning("The program has crashed!")
+# logger_setup()
+# 1/0
