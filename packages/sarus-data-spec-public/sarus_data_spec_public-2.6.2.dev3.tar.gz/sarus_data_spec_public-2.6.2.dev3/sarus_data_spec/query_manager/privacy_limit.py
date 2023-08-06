cee152d28fce8596e33777b9bb1d50049b371bd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:58 +0000

    Add package sarus-data-spec-public/2.6.2.dev3/sarus_data_spec_public-2.6.2.dev3.tar.gz

---
+++ packages/sarus-data-spec-public/sarus_data_spec_public-2.6.2.dev3.tar.gz/sarus_data_spec_public-2.6.2.dev3/sarus_data_spec/query_manager/privacy_limit.py
@@ -0,0 +1,28 @@
+import typing as t
+
+import sarus_data_spec.typing as st
+
+
+class DeltaEpsilonLimit(st.PrivacyLimit):
+    """A Privacy limit defined by explicitly listing the points.
+
+    Note: that epsilon is decreasing as a function of delta.
+    """
+
+    def __init__(self, delta_epsilon_dict: t.Dict[float, float]):
+        """The delta epsilon dict holds delta as keys and epsilon as values.
+        {delta: epsilon}
+        """
+        assert min(delta_epsilon_dict.keys()) >= 0
+        assert max(delta_epsilon_dict.keys()) <= 1
+        assert min(delta_epsilon_dict.values()) >= 0
+        self.delta_epsilon = delta_epsilon_dict.copy()
+
+    def delta_epsilon_dict(self) -> t.Dict[float, float]:
+        return self.delta_epsilon
+
+
+if t.TYPE_CHECKING:
+    import sarus_differential_privacy.typing as sdpt
+
+    _: sdpt.PrivacyLimit = DeltaEpsilonLimit({0.0: 0.0})
