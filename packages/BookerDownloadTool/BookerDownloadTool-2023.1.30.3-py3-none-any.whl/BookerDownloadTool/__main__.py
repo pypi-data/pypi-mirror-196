e0d9e11d3446577dfa44fad4a828c270c3418aee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:43 +0000

    Add package BookerDownloadTool/2023.1.30.3/BookerDownloadTool-2023.1.30.3-py3-none-any.whl

---
+++ packages/BookerDownloadTool/BookerDownloadTool-2023.1.30.3-py3-none-any.whl/BookerDownloadTool/__main__.py
@@ -0,0 +1,73 @@
+import argparse
+import sys
+from . import __version__
+from .zhihu_ques_sele import *
+from .zhihu_ques import *
+from .lightnovel import *
+from .dl_gh_book import *
+from .bili import *
+
+def main():
+    parser = argparse.ArgumentParser(prog="BookerDownloadTool", formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("-v", "--version", action="version", version=f"PYBP version: {__version__}")
+    parser.set_defaults(func=lambda x: parser.print_help())
+    subparsers = parser.add_subparsers()
+    
+    gh_book_parser = subparsers.add_parser("gh-book", help="download books from github")
+    gh_book_parser.add_argument("url", help="SUMMARY.md url")
+    gh_book_parser.add_argument("-t", "--threads", type=int, default=5, help="num of threads")
+    gh_book_parser.add_argument("-p", "--proxy", help="proxy")
+    gh_book_parser.add_argument("-a", "--article", default='article', help="article selector")
+    gh_book_parser.set_defaults(func=dl_gh_book)
+    
+    bili_parser = subparsers.add_parser("bili", help="download bilibili video")
+    bili_parser.add_argument("id", help="av or bv")
+    bili_parser.add_argument("-a", "--audio", type=bool, default=False, help="whether to convert to audio")
+    bili_parser.set_defaults(func=download_bili)
+
+    bili_kw_parser = subparsers.add_parser("bili-kw", help="download bilibili video by kw")
+    bili_kw_parser.add_argument("kw", help="keyword")
+    bili_kw_parser.add_argument("-s", "--start", type=int, default=1, help="starting page")
+    bili_kw_parser.add_argument("-e", "--end", type=int, default=10000000, help="ending page")
+    bili_kw_parser.add_argument("-a", "--audio", type=bool, default=False, help="whether to convert to audio")
+    bili_kw_parser.set_defaults(func=batch_kw_bili)
+  
+    bili_home_parser = subparsers.add_parser("bili-home", help="download bilibili video by user")
+    bili_home_parser.add_argument("mid", help="user id")
+    bili_home_parser.add_argument("-s", "--start", type=int, default=1, help="starting page")
+    bili_home_parser.add_argument("-e", "--end", type=int, default=10000000, help="ending page")
+    bili_home_parser.add_argument("-a", "--audio", type=bool, default=False, help="whether to convert to audio")
+    bili_home_parser.set_defaults(func=batch_home_bili)
+    
+    ln_parser = subparsers.add_parser("lightnovel", help="download lightnovel")
+    ln_parser.add_argument("id", help="id")
+    ln_parser.add_argument("-s", "--save-path", default='out', help="path to save")
+    ln_parser.add_argument("-c", "--cookie", default=os.environ.get('WK8_COOKIE', ''), help="wenku8.net cookie")
+    ln_parser.set_defaults(func=download_ln)
+
+    ln_batch_parser = subparsers.add_parser("lightnovel-batch", help="download lightnovel in batch")
+    ln_batch_parser.add_argument("fname", help="file name of ids")
+    ln_batch_parser.add_argument("-s", "--save-path", default='out', help="path to save")
+    ln_batch_parser.add_argument("-c", "--cookie", default=os.environ.get('WK8_COOKIE', ''), help="wenku8.net cookie")
+    ln_batch_parser.set_defaults(func=batch_ln)
+
+    ln_fetch_parser = subparsers.add_parser("lightnovel-fetch", help="fetch lightnovel ids")
+    ln_fetch_parser.add_argument("fname", help="file fname")
+    ln_fetch_parser.add_argument("-c", "--cookie", default=os.environ.get('WK8_COOKIE', ''), help="wenku8.net cookie")
+    ln_fetch_parser.add_argument("-s", "--start", required=True, help="starting date (YYYYMMDD)")
+    ln_fetch_parser.add_argument("-e", "--end", required=True, help="ending date (YYYYMMDD)")
+    ln_fetch_parser.set_defaults(func=fetch_ln)
+    
+    zhihu_ques_parser = subparsers.add_parser("zhihu-ques", help="crawl zhihu question by requests")
+    zhihu_ques_parser.add_argument("qid", help="qid")
+    zhihu_ques_parser.add_argument("-o", "--opti-mode", default='thres', help="img optimazation mode, default 'thres'")
+    zhihu_ques_parser.set_defaults(func=zhihu_ques)
+
+    zhihu_ques_sele_parser = subparsers.add_parser("zhihu-ques-sele", help="crawl zhihu question by **selenium**")
+    zhihu_ques_sele_parser.add_argument("qid", help="qid")
+    zhihu_ques_sele_parser.set_defaults(func=zhihu_ques_sele)
+    
+    args = parser.parse_args()
+    args.func(args)
+    
+if __name__ == '__main__': main()
---
+++ packages/BookerDownloadTool/BookerDownloadTool-2023.1.30.3-py3-none-any.whl/BookerDownloadTool/util.py
@@ -0,0 +1,65 @@
+import requests
+import os
+import shutil
+from os import path
+import imgyaso
+
+bili_hdrs = {
+    'User-Agent': 'PostmanRuntime/7.26.8',
+    'Referer': 'https://www.bilibili.com/',
+}
+
+default_hdrs = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
+}
+
+UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36'
+
+DIR = path.dirname(path.abspath(__file__))
+
+def d(name):
+    return path.join(DIR, name)
+
+def opti_img(img, mode, colors):
+    if mode == 'quant':
+        return imgyaso.pngquant_bts(img, colors)
+    elif mode == 'grid':
+        return imgyaso.grid_bts(img)
+    elif mode == 'trunc':
+        return imgyaso.trunc_bts(img, colors)
+    elif mode == 'thres':
+        return imgyaso.adathres_bts(img)
+    else:
+        return img
+
+def fname_escape(name):
+    return name.replace('\\', '＼') \
+               .replace('/', '／') \
+               .replace(':', '：') \
+               .replace('*', '＊') \
+               .replace('?', '？') \
+               .replace('"', '＂') \
+               .replace('<', '＜') \
+               .replace('>', '＞') \
+               .replace('|', '｜')
+
+def request_retry(method, url, retry=10, check_status=False, **kw):
+    kw.setdefault('timeout', 10)
+    for i in range(retry):
+        try:
+            r = requests.request(method, url, **kw)
+            if check_status: r.raise_for_status()
+            return r
+        except KeyboardInterrupt as e:
+            raise e
+        except Exception as e:
+            print(f'{url} retry {i}')
+            if i == retry - 1: raise e
+
+def safe_mkdir(dir):
+    try: os.mkdir(dir)
+    except: pass
+    
+def safe_rmdir(dir):
+    try: shutil.rmtree(dir)
+    except: pass
