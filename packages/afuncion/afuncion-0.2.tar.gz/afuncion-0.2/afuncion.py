5ffb99e25f787ff1782d0d981a45d1305ce5e0d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:10 +0000

    Add package afuncion/0.2/afuncion-0.2.tar.gz

---
+++ packages/afuncion/afuncion-0.2.tar.gz/afuncion-0.2/afuncion.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 11 00:40:31 2023
+
+@author: mayte
+"""
+
+def hola_mundo(name="mundo"):
+    print("hola"+ name)
+    
---
+++ packages/afuncion/afuncion-0.2.tar.gz/afuncion-0.2/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 11 00:43:41 2023
+
+@author: mayte
+"""
+
+from setuptools import find_packages, setup
+setup(
+      name='afuncion',
+      version='0.2',
+      packages=find_packages(),
+      py_modules=['afuncion'])
