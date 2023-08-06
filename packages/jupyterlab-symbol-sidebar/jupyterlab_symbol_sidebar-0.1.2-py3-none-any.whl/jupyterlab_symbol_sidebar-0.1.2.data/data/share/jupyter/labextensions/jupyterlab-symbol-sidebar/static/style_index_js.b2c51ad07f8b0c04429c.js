13efd6b55bb5f70841a21371ffbefa992cbb1843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:03 +0000

    Add package jupyterlab-symbol-sidebar/0.1.2/jupyterlab_symbol_sidebar-0.1.2-py3-none-any.whl

---
+++ packages/jupyterlab-symbol-sidebar/jupyterlab_symbol_sidebar-0.1.2-py3-none-any.whl/jupyterlab_symbol_sidebar-0.1.2.data/data/share/jupyter/labextensions/jupyterlab-symbol-sidebar/static/style_index_js.b2c51ad07f8b0c04429c.js
@@ -0,0 +1,73 @@
+"use strict";
+(self["webpackChunkjupyterlab_symbol_sidebar"] = self["webpackChunkjupyterlab_symbol_sidebar"] || []).push([["style_index_js"],{
+
+/***/ "./node_modules/css-loader/dist/cjs.js!./style/base.css":
+/*!**************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js!./style/base.css ***!
+  \**************************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/cssWithMappingToString.js */ "./node_modules/css-loader/dist/runtime/cssWithMappingToString.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);
+// Imports
+
+
+var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default()));
+// Module
+___CSS_LOADER_EXPORT___.push([module.id, "/*\n    See the JupyterLab Developer Guide for useful CSS Patterns:\n\n    https://jupyterlab.readthedocs.io/en/stable/developer/css.html\n*/\n\n.block_container {\n    text-align:center;\n    display: grid;\n    grid-template-columns: auto auto auto auto;\n    gap: 7px;\n    background-color: var(--jp-layout-color1);\n    padding: 10px;\n    overflow-y: scroll;\n    max-height: 95%;\n    color: var(--jp-ui-font-color0);\n}\n\n.block_container > div {\n    word-wrap: break-word;\n    background-color: var(--jp-layout-color1);\n    box-shadow: var(--jp-elevation-z2);\n    text-align: center;\n    padding: 3px 0;\n    font-size: 20px;\n    border-radius: 1px;\n    margin: 0px 0px;\n}\n\n.block_container > div > div {\n    font-size: 8px;\n    padding: 0px 1px;\n    margin: 0px;\n}\n\n.sidebar-container {\n    height: 100%;\n    background-color: var(--jp-layout-color1);\n}\n\n.notice {\n    word-wrap: break-word;\n    text-align: center;\n    padding: 3px 0;\n    font-size: 10px;\n    border-radius: 1px;\n    margin: 0px 0px;\n    color: var(--jp-inverse-layout-color3);\n}\n\n\n\n\n", "",{"version":3,"sources":["webpack://./style/base.css"],"names":[],"mappings":"AAAA;;;;CAIC;;AAED;IACI,iBAAiB;IACjB,aAAa;IACb,0CAA0C;IAC1C,QAAQ;IACR,yCAAyC;IACzC,aAAa;IACb,kBAAkB;IAClB,eAAe;IACf,+BAA+B;AACnC;;AAEA;IACI,qBAAqB;IACrB,yCAAyC;IACzC,kCAAkC;IAClC,kBAAkB;IAClB,cAAc;IACd,eAAe;IACf,kBAAkB;IAClB,eAAe;AACnB;;AAEA;IACI,cAAc;IACd,gBAAgB;IAChB,WAAW;AACf;;AAEA;IACI,YAAY;IACZ,yCAAyC;AAC7C;;AAEA;IACI,qBAAqB;IACrB,kBAAkB;IAClB,cAAc;IACd,eAAe;IACf,kBAAkB;IAClB,eAAe;IACf,sCAAsC;AAC1C","sourcesContent":["/*\n    See the JupyterLab Developer Guide for useful CSS Patterns:\n\n    https://jupyterlab.readthedocs.io/en/stable/developer/css.html\n*/\n\n.block_container {\n    text-align:center;\n    display: grid;\n    grid-template-columns: auto auto auto auto;\n    gap: 7px;\n    background-color: var(--jp-layout-color1);\n    padding: 10px;\n    overflow-y: scroll;\n    max-height: 95%;\n    color: var(--jp-ui-font-color0);\n}\n\n.block_container > div {\n    word-wrap: break-word;\n    background-color: var(--jp-layout-color1);\n    box-shadow: var(--jp-elevation-z2);\n    text-align: center;\n    padding: 3px 0;\n    font-size: 20px;\n    border-radius: 1px;\n    margin: 0px 0px;\n}\n\n.block_container > div > div {\n    font-size: 8px;\n    padding: 0px 1px;\n    margin: 0px;\n}\n\n.sidebar-container {\n    height: 100%;\n    background-color: var(--jp-layout-color1);\n}\n\n.notice {\n    word-wrap: break-word;\n    text-align: center;\n    padding: 3px 0;\n    font-size: 10px;\n    border-radius: 1px;\n    margin: 0px 0px;\n    color: var(--jp-inverse-layout-color3);\n}\n\n\n\n\n"],"sourceRoot":""}]);
+// Exports
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
+
+
+/***/ }),
+
+/***/ "./style/index.js":
+/*!************************!*\
+  !*** ./style/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _base_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./base.css */ "./style/base.css");
+
+
+
+/***/ }),
+
+/***/ "./style/base.css":
+/*!************************!*\
+  !*** ./style/base.css ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! !../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js */ "./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js");
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !!../node_modules/css-loader/dist/cjs.js!./base.css */ "./node_modules/css-loader/dist/cjs.js!./style/base.css");
+
+            
+
+var options = {};
+
+options.insert = "head";
+options.singleton = false;
+
+var update = _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default()(_node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__["default"], options);
+
+
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__["default"].locals || {});
+
+/***/ })
+
+}]);
+//# sourceMappingURL=style_index_js.b2c51ad07f8b0c04429c.js.map
