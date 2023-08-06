0bdea1d99310a8f7ee883a7de4732faeb1b8b667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:48 +0000

    Add package pulp-cli-deb/0.0.4/pulp-cli-deb-0.0.4.tar.gz

---
+++ packages/pulp-cli-deb/pulp-cli-deb-0.0.4.tar.gz/pulp-cli-deb-0.0.4/pulpcore/cli/deb/__init__.py
@@ -0,0 +1,20 @@
+from pulpcore.cli.common import main
+from pulpcore.cli.common.context import PluginRequirement
+from pulpcore.cli.common.generic import PulpCLIContext, pass_pulp_context
+
+from pulpcore.cli.deb.distribution import distribution
+from pulpcore.cli.deb.publication import publication
+from pulpcore.cli.deb.remote import remote
+from pulpcore.cli.deb.repository import repository
+
+
+@main.group()
+@pass_pulp_context
+def deb(pulp_ctx: PulpCLIContext) -> None:
+    pulp_ctx.needs_plugin(PluginRequirement("deb"))
+
+
+deb.add_command(distribution)
+deb.add_command(publication)
+deb.add_command(remote)
+deb.add_command(repository)
