90935778c0a63f7fc17f6127cd1f33ab0024c5b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:10 +0000

    Add package orkgnlp/0.10.0/orkgnlp-0.10.0-py3-none-any.whl

---
+++ packages/orkgnlp/orkgnlp-0.10.0-py3-none-any.whl/orkgnlp/annotation/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+""" Annotation services. """
+
+from orkgnlp.annotation.agriner import AgriNer
+from orkgnlp.annotation.csner import CSNer
+from orkgnlp.annotation.tdm import TdmExtractor
+
+__all__ = ["AgriNer", "CSNer", "TdmExtractor"]
