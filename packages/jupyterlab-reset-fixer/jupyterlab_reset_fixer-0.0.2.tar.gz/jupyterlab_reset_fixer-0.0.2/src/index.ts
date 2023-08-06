77e62bf8ff4b28ca6fb2bce50643134c0e7ca6b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:30 +0000

    Add package jupyterlab-reset-fixer/0.0.2/jupyterlab_reset_fixer-0.0.2.tar.gz

---
+++ packages/jupyterlab-reset-fixer/jupyterlab_reset_fixer-0.0.2.tar.gz/jupyterlab_reset_fixer-0.0.2/src/index.ts
@@ -0,0 +1,25 @@
+import {
+  IRouter,
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+/**
+ * Initialization data for the jupyterlab-reset-fixer extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab-reset-fixer:plugin',
+  autoStart: true,
+  requires: [IRouter],
+  activate: (app: JupyterFrontEnd, router: IRouter) => {
+    console.log('JupyterLab extension jupyterlab-reset-fixer is activated!');
+
+    router.register({
+      command: 'apputils:reset-on-load',
+      pattern: /(\?reset|\&reset)=($|&)/,
+      rank: 20 // High priority: 20:100.
+    });
+  }
+};
+
+export default plugin;
