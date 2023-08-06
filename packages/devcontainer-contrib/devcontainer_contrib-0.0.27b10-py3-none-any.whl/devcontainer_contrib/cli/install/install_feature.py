acfd101e37ee7a8fa8c5dcc1c9d742c82cdc5bff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:05 +0000

    Add package devcontainer-contrib/0.0.27b10/devcontainer_contrib-0.0.27b10-py3-none-any.whl

---
+++ packages/devcontainer-contrib/devcontainer_contrib-0.0.27b10-py3-none-any.whl/devcontainer_contrib/cli/install/install_feature.py
@@ -0,0 +1,15 @@
+import logging
+from typing import Dict, Optional
+
+from devcontainer_contrib.utils.oci_feature import OCIFeature
+from devcontainer_contrib.utils.oci_feature_installer import OCIFeatureInstaller
+
+logger = logging.getLogger(__name__)
+
+
+def install_feature(
+    feature: str,
+    options: Optional[Dict[str, str]] = None,
+    remote_user: Optional[str] = None,
+) -> None:
+    OCIFeatureInstaller.install(OCIFeature(feature), options, remote_user)
