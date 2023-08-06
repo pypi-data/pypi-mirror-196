55a588ca75158d08ea16ba230ae829e1fb3afbae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:27 +0000

    Add package airbyte/0.0.0/airbyte-0.0.0-py3-none-any.whl

---
+++ packages/airbyte/airbyte-0.0.0-py3-none-any.whl/airbyte/models/operations/__init__.py
@@ -0,0 +1,9 @@
+from .canceljob import *
+from .createconnection import *
+from .createdestination import *
+from .createjob import *
+from .createsource import *
+from .getjob import *
+from .listjobs import *
+
+__all__ = ["CancelJobPathParams","CancelJobRequest","CancelJobResponse","CreateConnectionRequest","CreateConnectionResponse","CreateDestinationRequest","CreateDestinationResponse","CreateJobRequest","CreateJobResponse","CreateSourceRequest","CreateSourceResponse","GetJobPathParams","GetJobRequest","GetJobResponse","ListJobsQueryParams","ListJobsRequest","ListJobsResponse"]
---
+++ packages/airbyte/airbyte-0.0.0-py3-none-any.whl/airbyte/sdk.py
@@ -0,0 +1,91 @@
+import requests as requests_http
+from . import utils
+from .connections import Connections
+from .destinations import Destinations
+from .jobs import Jobs
+from .sources import Sources
+from airbyte.models import shared
+
+SERVERS = [
+	"https://api.airbyte.com/v1/",
+]
+
+class Airbyte:
+    connections: Connections
+    destinations: Destinations
+    jobs: Jobs
+    sources: Sources
+    
+    _client: requests_http.Session
+    _security_client: requests_http.Session
+    _security: shared.Security
+    _server_url: str = SERVERS[0]
+    _language: str = "python"
+    _sdk_version: str = ""
+    _gen_version: str = "1.8.7"
+
+    def __init__(self) -> None:
+        self._client = requests_http.Session()
+        self._security_client = requests_http.Session()
+        self._init_sdks()
+
+    def config_server_url(self, server_url: str, params: dict[str, str] = None):
+        if params is not None:
+            self._server_url = utils.template_url(server_url, params)
+        else:
+            self._server_url = server_url
+
+        self._init_sdks()
+    
+    
+
+    def config_client(self, client: requests_http.Session):
+        self._client = client
+        
+        if self._security is not None:
+            self._security_client = utils.configure_security_client(self._client, self._security)
+        self._init_sdks()
+    
+    def config_security(self, security: shared.Security):
+        self._security = security
+        self._security_client = utils.configure_security_client(self._client, security)
+        self._init_sdks()
+    
+    def _init_sdks(self):
+        self.connections = Connections(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+        self.destinations = Destinations(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+        self.jobs = Jobs(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+        self.sources = Sources(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+    
