05806559d50260e63086c9c6284a395870c29cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:36 +0000

    Add package kor/0.2.0/kor-0.2.0-py3-none-any.whl

---
+++ packages/kor/kor-0.2.0-py3-none-any.whl/kor/nodes.py
@@ -0,0 +1,204 @@
+"""Definitions of input elements."""
+import abc
+import dataclasses
+import re
+from typing import Sequence, Mapping, Any, Generic, TypeVar, Optional
+
+# For now, limit what's allowed for identifiers.
+# The main constraints
+# 1) Relying on HTML parser to parse output
+# 2) One of the type descriptors is TypeScript, so we want to produce valid TypeScript identifiers.
+# We can lift the constraints later if it becomes important, not worth the effort for a v0.
+VALID_IDENTIFIER_PATTERN = re.compile(r"^[a-z_][0-9a-z_]*$")
+
+T = TypeVar("T")
+
+
+class AbstractVisitor(Generic[T], abc.ABC):
+    """An abstract visitor. Define here to avoid cyclical imports for now."""
+
+    def visit_text(self, node: "Text") -> T:
+        """Visit text node."""
+        return self.visit_default(node)
+
+    def visit_number(self, node: "Number") -> T:
+        """Visit text node."""
+        return self.visit_default(node)
+
+    def visit_object(self, node: "Object") -> T:
+        """Visit object node."""
+        return self.visit_default(node)
+
+    def visit_selection(self, node: "Selection") -> T:
+        """Visit selection node."""
+        return self.visit_default(node)
+
+    def visit_option(self, node: "Option") -> T:
+        """Visit option node."""
+        return self.visit_default(node)
+
+    def visit_default(self, node: "AbstractInput") -> T:
+        """Default node implementation."""
+        raise NotImplementedError()
+
+
+@dataclasses.dataclass(frozen=True, kw_only=True)
+class AbstractInput(abc.ABC):
+    """Abstract input node.
+
+    Each input is expected to have a unique ID, and should
+    only use alphanumeric characters.
+
+    The ID should be unique across all inputs that belong
+    to a given form.
+
+    The description should describe what the input is about.
+    """
+
+    id: str  # Unique ID
+    description: str = ""
+    multiple: bool = True
+
+    def __post_init__(self) -> None:
+        """Post initialization hook."""
+        if not VALID_IDENTIFIER_PATTERN.match(self.id):
+            raise ValueError(
+                f"`{self.id}` is not a valid identifier. "
+                f"Please only use lower cased a-z, _ or the digits 0-9"
+            )
+
+        if not self.multiple:
+            raise ValueError(
+                "Reserved parameter. At the moment, multiple has to be True."
+            )
+
+    @abc.abstractmethod
+    def accept(self, visitor: AbstractVisitor) -> Any:
+        """Accept a visitor."""
+        raise NotImplementedError()
+
+    # Update return type to `Self` when bumping python version.
+    def replace(
+        self, id: Optional[str] = None, description: Optional[str] = None
+    ) -> "AbstractInput":
+        """Wrapper around data-classes replace."""
+        attributes = {}
+        if id:
+            attributes["id"] = id
+        if description:
+            attributes["description"] = description
+        return dataclasses.replace(self, **attributes)
+
+
+@dataclasses.dataclass(frozen=True, kw_only=True)
+class ExtractionInput(AbstractInput, abc.ABC):
+    """An abstract definition for inputs that involve extraction.
+
+    An extraction input can be associated with extraction examples.
+
+    An extraction example is a 2-tuple composed of a text segment and the expected
+    extraction.
+
+    For example:
+        [
+            ("I bought this cookie for $10", "$10"),
+            ("Eggs cost twelve dollars", "twelve dollars"),
+        ]
+    """
+
+    examples: Sequence[tuple[str, str | Sequence[str]]] = tuple()
+
+
+@dataclasses.dataclass(frozen=True, kw_only=True)
+class Number(ExtractionInput):
+    """Built-in number input."""
+
+    def accept(self, visitor: AbstractVisitor[T]) -> T:
+        """Accept a visitor."""
+        return visitor.visit_number(self)
+
+
+@dataclasses.dataclass(frozen=True, kw_only=True)
+class Text(ExtractionInput):
+    """Built-in text input."""
+
+    def accept(self, visitor: AbstractVisitor[T]) -> T:
+        """Accept a visitor."""
+        return visitor.visit_text(self)
+
+
+@dataclasses.dataclass(frozen=True, kw_only=True)
+class Option(AbstractInput):
+    """Built-in option input must be part of a selection input."""
+
+    examples: Sequence[str] = tuple()
+
+    def accept(self, visitor: AbstractVisitor[T]) -> T:
+        """Accept a visitor."""
+        return visitor.visit_option(self)
+
+
+@dataclasses.dataclass(frozen=True, kw_only=True)
+class Selection(AbstractInput):
+    """Built-in selection input.
+
+    A selection input is composed of one or more options.
+
+    ## Null examples
+
+    Null examples are segments of text for which nothing should be extracted.
+    Good null examples will likely be challenging, adversarial examples.
+
+    For example:
+        for an extraction input about company names nothing should be extracted
+        from the text: "I eat an apple every day.".
+    """
+
+    options: Sequence[Option]
+    null_examples: Sequence[str] = tuple()
+
+    def accept(self, visitor: AbstractVisitor[T]) -> T:
+        """Accept a visitor."""
+        return visitor.visit_selection(self)
+
+
+@dataclasses.dataclass(frozen=True, kw_only=True)
+class Object(AbstractInput):
+    """A definition for an object extraction.
+
+    An extraction input can be associated with 2 different types of examples:
+
+    1) extraction examples (called simply `examples`)
+    2) null examples (called `null_examples`)
+
+    ## Extraction examples
+
+    A standard extraction example is a 2-tuple composed of a text segment and the expected
+    extraction.
+
+    For example:
+        [
+            ("I bought this cookie for $10", "$10"),
+            ("Eggs cost twelve dollars", "twelve dollars"),
+        ]
+
+    ## Null examples
+
+    Null examples are segments of text for which nothing should be extracted.
+    Good null examples will likely be challenging, adversarial examples.
+
+    For example:
+        for an extraction input about company names nothing should be extracted
+        from the text: "I eat an apple every day.".
+    """
+
+    attributes: Sequence[ExtractionInput]
+    examples: Sequence[tuple[str, Mapping[str, str | Sequence[str]]]] = tuple()
+    # If false, will treat the inputs independent.
+    # Is there a better name for this?! I want it to be True by default
+    # which rules out as_input_bag
+    group_as_object: bool = True
+
+    def accept(self, visitor: AbstractVisitor[T]) -> T:
+        """Accept a visitor."""
+        return visitor.visit_object(self)
---
+++ packages/kor/kor-0.2.0-py3-none-any.whl/kor/prompts.py
@@ -0,0 +1,159 @@
+"""Code to dynamically generate appropriate LLM prompts."""
+import abc
+import dataclasses
+from typing import Union, Literal, Callable, List, Tuple
+
+from kor.examples import generate_examples
+from kor.nodes import AbstractInput
+from kor.type_descriptors import (
+    generate_typescript_description,
+    generate_bullet_point_description,
+)
+
+PROMPT_FORMAT = Union[Literal["openai-chat"], Literal["string"]]
+
+
+@dataclasses.dataclass(frozen=True, kw_only=True)
+class PromptGenerator(abc.ABC):
+    """Define abstract interface for a prompt."""
+
+    @abc.abstractmethod
+    def format_as_string(self, user_input: str, node: AbstractInput) -> str:
+        """Format as a prompt to a standard LLM."""
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def format_as_chat(
+        self, user_input: str, node: AbstractInput
+    ) -> list[dict[str, str]]:
+        """Format as a prompt to a chat model."""
+        raise NotImplementedError()
+
+
+@dataclasses.dataclass(frozen=True, kw_only=True)
+class ExtractionTemplate(PromptGenerator):
+    """Prompt generator for extraction purposes."""
+
+    prefix: str
+    type_descriptor: str
+    suffix: str
+    example_generator: Callable[
+        [AbstractInput], List[Tuple[str, str]]
+    ] = generate_examples
+
+    def __post_init__(self) -> None:
+        """Validate the template."""
+        if self.prefix.endswith("\n"):
+            raise ValueError("Please do not end the prefix with new lines.")
+
+        if self.suffix.endswith("\n"):
+            raise ValueError("Please do not end the suffix with new lines.")
+
+    def replace(self, **kwargs) -> "ExtractionTemplate":
+        """Bind to replace function for convenience."""
+        return dataclasses.replace(self, **kwargs)
+
+    def generate_instruction_segment(self, node: AbstractInput) -> str:
+        """Generate the instruction segment of the extraction."""
+        if self.type_descriptor == "TypeScript":
+            type_description = generate_typescript_description(node)
+        elif self.type_descriptor == "BulletPoint":
+            type_description = generate_bullet_point_description(node)
+        else:
+            raise NotImplementedError()
+        return f"{self.prefix}\n\n{type_description}\n\n{self.suffix}"
+
+    def format_as_string(self, user_input: str, node: AbstractInput) -> str:
+        """Format the template for a `standard` LLM model."""
+        instruction_segment = self.generate_instruction_segment(node)
+        examples = self.example_generator(node)
+        input_output_block = []
+
+        for in_example, output in examples:
+            input_output_block.extend(
+                [
+                    f"Input: {in_example}",
+                    f"Output: {output}",
+                ]
+            )
+
+        input_output_block.append(f"Input: {user_input}\nOutput:")
+        input_output_block = "\n".join(input_output_block)
+        return f"{instruction_segment}\n\n{input_output_block}"
+
+    def format_as_chat(
+        self, user_input: str, form: AbstractInput
+    ) -> list[dict[str, str]]:
+        """Format the template for a `chat` LLM model."""
+        instruction_segment = self.generate_instruction_segment(form)
+
+        messages = [
+            {
+                "role": "system",
+                "content": instruction_segment,
+            }
+        ]
+
+        for example_input, example_output in self.example_generator(form):
+            messages.extend(
+                [
+                    {"role": "user", "content": example_input},
+                    {
+                        "role": "assistant",
+                        "content": f"{example_output}",
+                    },
+                ]
+            )
+
+        messages.append({"role": "user", "content": user_input})
+        return messages
+
+
+STANDARD_EXTRACTION_TEMPLATE = ExtractionTemplate(
+    prefix=(
+        "Your goal is to extract structured information from the user's input that matches "
+        "the form described below. "
+        "When extracting information please make sure it matches the type information exactly. Do "
+        "not add any attributes that do not appear in the schema shown below."
+    ),
+    type_descriptor="TypeScript",
+    suffix=(
+        "For Union types the output must EXACTLY match one of the members "
+        "of the Union type.\n\n"
+        "Please enclose the extracted information in HTML style tags with the tag name "
+        "corresponding to the corresponding component ID. Use angle style brackets for the "
+        "tags ('>' and '<'). "
+        "Only output tags when you're confident about the information that was extracted "
+        "from the user's query. If you can extract several pieces of relevant information "
+        "from the query, then include all of them. If the type is an array, please "
+        "repeat the corresponding tag name multiple times once for each relevant extraction. "
+        "Do NOT output anything except for the extracted information. "
+        "Only output information inside the HTML style tags. Do not include any notes "
+        "or any clarifications. "
+    ),
+)
+
+
+BULLET_POINT_EXTRACTION_TEMPLATE = ExtractionTemplate(
+    prefix=(
+        "Your goal is to extract structured information from the user's input that matches "
+        "the form described below. "
+        "When extracting information please make sure it matches the type information exactly. "
+    ),
+    type_descriptor="BulletPoint",
+    suffix=(
+        "Your task is to parse the user input and determine to what values the user is attempting "
+        "to set each component of the form.\n"
+        "When the type of the input is a Selection, only output one of the options specified in "
+        "the square brackets as arguments to the Selection type of this input. "
+        "Please enclose the extracted information in HTML style tags with the tag name "
+        "corresponding to the corresponding component ID. Use angle style brackets for the "
+        "tags ('>' and '<'). "
+        "Only output tags when you're confident about the information that was extracted "
+        "from the user's query. If you can extract several pieces of relevant information "
+        "from the query include use a comma to separate the tags. "
+        "Do NOT output anything except for the extracted information. "
+        "Only output information inside the HTML style tags. Do not include any notes "
+        "or any clarifications. "
+    ),
+)
