466e030ec9af66a905b92181ea155252898bf727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:46 +0000

    Add package hiddifypanel/2.0.5/hiddifypanel-2.0.5-py3-none-any.whl

---
+++ packages/hiddifypanel/hiddifypanel-2.0.5-py3-none-any.whl/hiddifypanel/__init__.py
@@ -0,0 +1,7 @@
+from .base import create_app, create_app_wsgi
+from . import panel
+__all__ = ["create_app", "create_app_wsgi"]
+
+# application = create_app_wsgi()
+
+from .VERSION import __version__
