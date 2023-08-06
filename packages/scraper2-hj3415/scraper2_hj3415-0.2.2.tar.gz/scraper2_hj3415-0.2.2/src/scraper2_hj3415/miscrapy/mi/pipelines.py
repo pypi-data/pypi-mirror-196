635a34c9c46339a6ba936c70904b22406b7905f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:14 +0000

    Add package scraper2-hj3415/0.2.2/scraper2_hj3415-0.2.2.tar.gz

---
+++ packages/scraper2-hj3415/scraper2_hj3415-0.2.2.tar.gz/scraper2_hj3415-0.2.2/src/scraper2_hj3415/miscrapy/mi/pipelines.py
@@ -0,0 +1,37 @@
+
+import logging
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter('%(levelname)s: [%(name)s] %(message)s')
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+logger.setLevel(logging.INFO)
+
+# 한개의 스파이더에서 연속 3일분량의 데이터가 전달된다.
+
+
+class ValidationPipeline:
+    def process_item(self, item, spider):
+        print(" Nothing special for working")
+        return item
+
+
+class MongoPipeline:
+    # 몽고 데이터 베이스에 저장하는 파이프라인
+    def process_item(self, item, spider):
+        """
+        아이템 구조
+            title = scrapy.Field()
+            date = scrapy.Field()
+            value = scrapy.Field()
+        """
+        print(f"\tIn the {self.__class__.__name__}...", end="")
+        if spider.mongo_client is None:
+            print(f"Skipping to save the data... code : {item['코드']} / spider : {spider.name}")
+        else:
+            print(f"Saving the {spider.name} to mongoDB...", end="")
+
+
+        print(f"In the {self.__class__.__name__}...date : {item['date']} / title : {item['title']} / value : {item['value']}")
+        mongo2.MI(self.client, item['title']).save(mi_dict={"date": item['date'], "value": item['value']})
+        return item
