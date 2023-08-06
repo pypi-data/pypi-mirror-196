f454516068acd3f064287ea756858289cfb85b3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:05 +0000

    Add package jupyter-gcs-contents-manager/2.0.0/jupyter-gcs-contents-manager-2.0.0.tar.gz

---
+++ packages/jupyter-gcs-contents-manager/jupyter-gcs-contents-manager-2.0.0.tar.gz/jupyter-gcs-contents-manager-2.0.0/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+import reporter
+
+setup(name='jupyter-gcs-contents-manager', version='2.0.0', description='Ethical research - dep confusion package. This is not a real package, do not install.')
