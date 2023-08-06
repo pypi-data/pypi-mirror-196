6a6c3db699b3de6bde9a5fa461ae0ce770d9f847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:48:05 +0000

    Add package consoleAnsiInterface/1.1.0/consoleAnsiInterface-1.1.0.tar.gz

---
+++ packages/consoleAnsiInterface/consoleAnsiInterface-1.1.0.tar.gz/consoleAnsiInterface-1.1.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='consoleAnsiInterface',
+    version='1.1.0',
+    author='szachy-a',
+    description='Console interface with ANSI sequences for Windows',
+    packages=['consoleAnsiInterface'],
+    install_requires=['cursor']
+)
