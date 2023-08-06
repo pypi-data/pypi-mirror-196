cf49723f663d6f74dcf9ba9cfded0cf32360c125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:46 +0000

    Add package asfjzdemo/0.1/asfjzdemo-0.1.tar.gz

---
+++ packages/asfjzdemo/asfjzdemo-0.1.tar.gz/asfjzdemo-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name='asfjzdemo',
+	version='0.1',
+	description='A sample Python package',
+	author='Jiang Zhu',
+	author_email='jzhu4@alaska.edu.com',
+	packages=['asfjzdemo']
+)
+
