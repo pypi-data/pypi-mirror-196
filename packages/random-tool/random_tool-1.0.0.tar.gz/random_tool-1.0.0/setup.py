45bef33829b94514eec61de52f86611702609845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:53 +0000

    Add package random-tool/1.0.0/random_tool-1.0.0.tar.gz

---
+++ packages/random-tool/random_tool-1.0.0.tar.gz/random_tool-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(name='random_tool',  # 包名
+      version='1.0.0',  # 版本号
+      description='encrypt basic openssl',
+      long_description_content_type="text/markdown",
+      long_description=long_description,
+      author='zhang peng',
+      author_email='siburuxue@gmail.com',
+      url='https://github.com/siburuxue',
+      install_requires=[],
+      license='Apache-2.0 License',
+      packages=find_packages(),
+      platforms=["all"],
+      )
