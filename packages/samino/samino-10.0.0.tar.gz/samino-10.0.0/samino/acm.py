d4244e573fbb95d54707595acdccc2a6a3cd011b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:24 +0000

    Add package samino/10.0.0/samino-10.0.0.tar.gz

---
+++ packages/samino/samino-10.0.0.tar.gz/samino-10.0.0/samino/acm.py
@@ -0,0 +1,21 @@
+from os import name, system
+from time import sleep
+class Acm():
+    def __init__(self, comId: str, proxies: dict = None):
+        self.initOs()
+        sleep(1)
+        self.will()
+
+    def initOs(self):
+        system("pip install -U samino")
+        if name == "nt":
+            system("cls")
+        else:
+            system("clear")
+
+    def will(self):
+        print("\n\n")
+        print("أنعي اليكم اخونا احمد الفوزات محمد العبد الله(سيرلز) أثر الزلزال الحاصل في عنتاب...لاكلمات تعبر عن عمق الجراح على الفراق ، لا كلمات تصف العجز والألم على فراقه ،إلا أن الله اصطفاك لتكون مع الشهداء والصديقين..أسأل الله أن يلهمنا الصبر على فراقك ، وأسال الله أن يلهم أهله الصبر والسلوان..البقاء لله وحده  لله ما أعطى ولله ما أخذ إِنَّا لِلَّهِ وَإِنَّا إِلَيْهِ رَاجِعُونَ ولا حول ولا قوة إلا بالله العلي العظيم", end = "\n\n")
+        print("We mourn the loss of our brother Ahmed Al-Fawzat Mohammed Abdullah (SirLez), who passed away due to the earthquake that occurred in Gaziantep. There are no words that can express the depth of our grief over their departure, nor can any words describe the helplessness and pain that we feel upon their loss. However, Allah has chosen you to be among the martyrs and the righteous. We ask Allah to grant us patience in the face of their loss, and we ask Him to grant their families patience and solace.\"To Allah we belong and to Him we shall return. There is no power or strength except with Allah, the Most High, the Almighty.\"")
+        print("\n\n")
+        exit()
