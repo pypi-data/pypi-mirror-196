e7078aac50eaaab8ea716ea8b67986c9ab5dd6d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:40 +0000

    Add package oarepo-runtime/1.2.1/oarepo_runtime-1.2.1-py3-none-any.whl

---
+++ packages/oarepo-runtime/oarepo_runtime-1.2.1-py3-none-any.whl/oarepo_runtime/datastreams/transformers.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+from typing import List
+from .datastreams import StreamEntry
+
+
+class BaseTransformer(ABC):
+    """Base transformer."""
+
+    def __init__(self) -> None:
+        pass
+
+    @abstractmethod
+    def apply(self, stream_entry: StreamEntry, *args, **kwargs) -> StreamEntry:
+        """Applies the transformation to the entry.
+        :returns: A StreamEntry. The transformed entry.
+                  Raises TransformerError in case of errors.
+        """
+
+
+class BatchTransformer(BaseTransformer):
+    @abstractmethod
+    def apply_batch(
+        self, stream_entry: List[StreamEntry], *args, **kwargs
+    ) -> List[StreamEntry]:
+        """Applies the transformation to the entry.
+        :returns: A StreamEntry. The transformed entry.
+                  Raises TransformerError in case of errors.
+        """
