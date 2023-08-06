4da4c43a9c8846a3d2120fd5e30ae009b29b2d22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:46 +0000

    Add package documennt-detection/1.0.1/documennt_detection-1.0.1.tar.gz

---
+++ packages/documennt-detection/documennt_detection-1.0.1.tar.gz/documennt_detection-1.0.1/document_detection/utils.py
@@ -0,0 +1,130 @@
+import re
+import Levenshtein
+
+
+def _traverse_tree(root, li, current_path):
+    if root is None:
+        return
+    li.append(current_path)
+    for child in root.children:
+        cp = current_path + "/" + child.val
+        _traverse_tree(child, li, cp)
+
+
+def check_tree_diff(tree1, tree2):
+    all_catlog1 = []
+    all_catlog2 = []
+    _traverse_tree(tree1, all_catlog1, "")
+    _traverse_tree(tree2, all_catlog2, "")
+    mark1 = [True for i in range(len(all_catlog1))]
+    mark2 = [True for i in range(len(all_catlog2))]
+    for i, c1 in enumerate(all_catlog1):
+        for j, c2 in enumerate(all_catlog2):
+            if c2 == c1 and mark2[j]:
+                mark1[i] = False
+                mark2[j] = False
+                break
+    rest1, rest2 = [], []
+    for m1, c1 in zip(mark1, all_catlog1):
+        if m1:
+            rest1.append(c1)
+    for m2, c2 in zip(mark2, all_catlog2):
+        if m2:
+            rest2.append(c2)
+    return rest1, rest2
+
+
+def max_backward_match(word_list, vocab, max_k=10):
+    res = []
+    end = len(word_list)
+
+    while end > 0:
+        break_flag = False
+        for i in range(max_k):
+            start = end - max_k + i
+            start = start if start >= 0 else 0
+            temp = "".join(word_list[start:end])
+            if temp in vocab:
+                res.append([temp, start, end])
+                end = start
+                break_flag = True
+                break
+        if not break_flag:
+            end -= 1
+    res.reverse()
+    return res
+
+
+def detection_check(sentence, state_words):
+    pos = []
+    res = re.finditer("检查.*(确保|是否|有无)([^,，。！？!、/?]*)", sentence)
+    for elem in res:
+        elem = elem.group(2)
+        flag = True
+        for sw in state_words:
+            if sw in elem:
+                flag = False
+        if flag:
+            pos.append(elem.span())
+    return pos
+
+
+def similarity(s1, s2):
+    return Levenshtein.ratio(s1, s2)
+
+
+def read_words(filename):
+    words = set()
+    with open(filename, "r") as f:
+        for line in f:
+            words.add(line.strip())
+    return words
+
+
+def read_scope_words(filename):
+    di = dict()
+    words = read_words(filename)
+    for word in words:
+        parts = re.split("\s+", word.strip())
+        if len(parts) != 2:
+            continue
+        if parts[1] not in di:
+            di[parts[1]] = []
+        di[parts[1]].append(parts[0])
+    return di
+
+
+def read_device(filename):
+    di = dict()
+    words = read_words(filename)
+    for word in words:
+        parts = re.split("\s+", word.strip())
+        if len(parts) != 2:
+            continue
+        device_name = parts[0]
+        standard_name = parts[1]
+        di[device_name] = standard_name
+        di[standard_name] = standard_name
+    return di
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
