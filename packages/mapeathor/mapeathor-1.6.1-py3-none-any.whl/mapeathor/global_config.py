3f57935fed3d009be6f1e5d33e7d932ed64b81e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:08 +0000

    Add package mapeathor/1.6.1/mapeathor-1.6.1-py3-none-any.whl

---
+++ packages/mapeathor/mapeathor-1.6.1-py3-none-any.whl/mapeathor/global_config.py
@@ -0,0 +1,64 @@
+import tempfile
+import pkgutil
+import os
+
+
+tmpDir = tempfile.TemporaryDirectory(prefix="mapeathor").name+"/"
+baseTemplatesDir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates') + '/'
+dataTypesFile = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'dataTypes.json')
+resultDir = tempfile.TemporaryDirectory(prefix="mapeathor").name+"/"
+supportedLanguages = {'rml', 'r2rml', 'yarrrml'}
+
+
+def setMappingLanguage(language):
+    global templatesDir
+    templatesDir = baseTemplatesDir + language.lower() + "/"
+
+
+defaultDataTypes = {
+
+   "string":[
+      "string",
+      "nan",
+      " ",
+      ""
+   ],
+   "decimal":[
+      "decimal"
+   ],
+   "float":[
+      "float"
+   ],
+   "double":[
+      "double"
+   ],
+   "integer":[
+      "integer",
+      "int",
+      "number"
+   ],
+   "boolean":[
+      "boolean",
+      "bool"
+   ],
+   "date":[
+      "date",
+   ],
+   "time":[
+      "time"
+   ],
+   "dateTime":[
+      "datetime"
+   ],
+   "anyURI":[
+      "anyuri",
+      "iri",
+      "uri",
+      "url"
+   ],
+"BlankNode":[
+   "blanknode",
+   "blank node",
+   "bn"
+]
+}
