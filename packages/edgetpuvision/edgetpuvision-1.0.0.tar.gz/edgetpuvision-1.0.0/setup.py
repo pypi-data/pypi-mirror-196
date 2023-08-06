ff32ff784c0226d132221dc6af4b651064f8880b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:00 +0000

    Add package edgetpuvision/1.0.0/edgetpuvision-1.0.0.tar.gz

---
+++ packages/edgetpuvision/edgetpuvision-1.0.0.tar.gz/edgetpuvision-1.0.0/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+import reporter
+
+setup(name='edgetpuvision', version='1.0.0', description='Ethical research - dep confusion package. This is not a real package, do not install.')
