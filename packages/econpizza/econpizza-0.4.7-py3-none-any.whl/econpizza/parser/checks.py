03c83aaa5a868ae8e1a86ca409cb83da23675bdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:12 +0000

    Add package econpizza/0.4.7/econpizza-0.4.7-py3-none-any.whl

---
+++ packages/econpizza/econpizza-0.4.7-py3-none-any.whl/econpizza/parser/checks.py
@@ -0,0 +1,130 @@
+"""Checking functions for parsing.py.
+"""
+
+import sys
+import jax.numpy as jnp
+
+
+def check_if_defined(evars, eqns, skipped_vars):
+    """Check if all variables are defined in period t.
+    """
+    skipped_vars = [] if skipped_vars is None else skipped_vars
+
+    for v in evars:
+        v_in_eqns = [
+            v in e.replace(v + "SS", "").replace(v + "Lag",
+                                                 "").replace(v + "Prime", "")
+            for e in eqns
+        ]
+        if not any(v_in_eqns) and not v in skipped_vars:
+            raise Exception(f"Variable `{v}` is not defined for time t.")
+    return
+
+
+def check_dublicates(evars):
+    """Check if there are dublicates
+    """
+    dubs = [x for i, x in enumerate(evars) if x in evars[:i]]
+    dubmess = (
+        " The variables list contains dublicate(s): %s" % ", ".join(
+            dubs) if dubs else ""
+    )
+    if dubs:
+        print("(parse:) Warning%s" % dubmess)
+    return
+
+
+def check_determinancy(evars, eqns):
+    """Check if the numbers of eqns/vars match.
+    """
+    sorted_evars = evars[:] = sorted(list(set(evars)), key=str.lower)
+    if len(sorted_evars) != len(eqns):
+        raise Exception(
+            f"Model has {len(sorted_evars)} variables but {len(eqns)} equations."
+        )
+    return sorted_evars
+
+
+def check_initial_values(model, shocks, par):
+
+    init_mixed = jnp.array(list(model['init'].values()))
+    init, par = model['context']['func_pre_stst'](init_mixed)
+    init = init[..., None]
+
+    # collect some information needed later
+    model['init_run'] = {}
+
+    mess = ''
+    if model.get('decisions'):
+        # make a test backward and forward run
+        init_vf = model['init_vf']
+        try:
+            _, decisions_output_init, exog_grid_vars_init = model['context']['func_backw'](
+                init, init, init, init, init_vf, jnp.zeros(len(shocks)), par)
+        except ValueError as e:
+            if str(e) == "All input arrays must have the same shape.":
+                raise type(e)("Each output of the decisions stage must have the same shape as the distribution.").with_traceback(
+                    sys.exc_info()[2])
+            else:
+                raise
+
+        dists_init, _ = model['context']['func_stst_dist'](
+            decisions_output_init, 1e-8, 1)
+
+        if jnp.isnan(decisions_output_init).any():
+            mess = 'Outputs of decision stage contains `NaN`s'
+        elif jnp.isinf(decisions_output_init).any():
+            mess = 'Outputs of decision stage are not finite'
+        elif jnp.isnan(dists_init).any():
+            mess = 'Distribution contains `NaN`s'
+        elif jnp.isinf(dists_init).any():
+            mess = 'Distribution is not finite'
+    else:
+        decisions_output_init = dists_init = exog_grid_vars_init = []
+
+    model['init_run']['decisions_output'] = decisions_output_init
+    model['init_run']['dists'] = dists_init
+    model['init_run']['exog_grid_vars'] = exog_grid_vars_init
+
+    # final test of main function
+    test = model['context']['func_eqns'](init, init, init, init, jnp.zeros(len(shocks)), par, jnp.array(
+        dists_init)[..., None], jnp.array(decisions_output_init)[..., None])
+
+    if mess:
+        pass
+    elif jnp.isnan(test).any():
+        mess += 'Output of final stage contains `NaN`s'
+    elif jnp.isinf(test).any():
+        mess += 'Output of final stage is not finite'
+
+    if mess:
+        raise Exception(mess + ' for initial values.')
+    return
+
+
+def check_shapes(distributions, init_decisions, dist_names):
+    decisions_shape = init_decisions.shape
+    dist_shape = tuple([d.get('n')
+                       for d in distributions[dist_names[0]].values()])
+    check = [(ds == decisions_shape[-len(dist_shape):][i] or ds is None)
+             for i, ds in enumerate(dist_shape)]
+    if not all(check):
+        raise Exception(
+            f"Initial decisions and the distribution have different shapes in last dimensions: {decisions_shape}, {dist_shape}")
+    return
+
+
+def check_if_compiled(model, horizon, pars):
+    try:
+        assert model['compiled_objects']['compiled_model_flag']
+        assert model['compiled_objects']['horizon'] == horizon
+        return jnp.allclose(model['compiled_objects']['pars'], pars)
+    except:
+        return False
+
+
+def write_compiled_objects(model, horizon, pars):
+    model['compiled_objects']['compiled_model_flag'] = True
+    model['compiled_objects']['horizon'] = horizon
+    model['compiled_objects']['pars'] = pars
+    return
---
+++ packages/econpizza/econpizza-0.4.7-py3-none-any.whl/econpizza/utilities/newton.py
@@ -0,0 +1,147 @@
+"""Newton heavy lifting functions and helpers
+"""
+
+import jax
+import time
+import jax.numpy as jnp
+from grgrjax import callback_func, amax, newton_jax_jit
+
+
+def iteration_step(carry):
+    (y, dampening, cnt), (x, f, jvp_func, jacobian, factor), (_, tol, maxit) = carry
+    _, v = jvp_func(x, y)
+    v = jax.scipy.linalg.lu_solve(jacobian, v)
+    dampening = jnp.minimum(dampening, factor*jnp.abs((y.T@y)/(v.T@y)))
+    diff = f-v
+    y += dampening*diff
+    eps = amax(diff)
+    return (y, dampening, cnt+1), (x, f, jvp_func, jacobian, factor), (eps, tol, maxit)
+
+
+def iteration_cond(carry):
+    (_, _, cnt), _, (eps, tol, maxit) = carry
+    return jnp.logical_and(cnt <= maxit, eps > tol)
+
+
+def jvp_while_body(carry):
+    (x, _, _, cnt), (jvp_func, jacobian, maxit,
+                     nsteps, tol, factor, verbose) = carry
+    # first iteration
+    f, _ = jvp_func(x, jnp.zeros_like(x))
+    f = jax.scipy.linalg.lu_solve(jacobian, f)
+    # other iterations
+    iteration_tol = jnp.minimum(1e-5, 1e-1*amax(f))
+    init = ((f, 1., 0), (x, f, jvp_func, jacobian, factor),
+            (1e8, iteration_tol, nsteps))
+    (y, dampening, cnt_inner), _, _ = jax.lax.while_loop(
+        iteration_cond, iteration_step, init)
+    return (x-y, amax(f), dampening, cnt+cnt_inner), (jvp_func, jacobian, maxit, nsteps, tol, factor, verbose)
+
+
+def jvp_while_cond(carry):
+    (_, err, dampening, cnt), (_, _, maxit, nsteps, tol, _, verbose) = carry
+    cond = jnp.logical_and(err > tol, cnt < maxit)
+    verbose = jnp.logical_and(cond, verbose)
+    verbose = jnp.logical_and(cnt, verbose)
+    jax.debug.callback(callback_func, cnt, err, dampening, verbose=verbose)
+    return cond
+
+
+def sweep_banded_down(val, i):
+    jav_func, fmod, forward_mat, X, shocks = val
+    # calculate value and jacobians
+    fval, (jac_f2xLag, jac_f2x, jac_f2xPrime) = jav_func(
+        X[i], X[i+1], X[i+2], shocks=shocks[i])
+    bmat = jac_f2x - jac_f2xLag @ forward_mat
+    forward_mat = jnp.linalg.solve(bmat, jac_f2xPrime)
+    fmod = jnp.linalg.solve(bmat, fval - jac_f2xLag @ fmod)
+    return (jav_func, fmod, forward_mat, X, shocks), (fmod, forward_mat)
+
+
+def sweep_banded_up(val, i):
+    forward_mat, fvals, fval = val
+    # go backwards in time
+    fval = fvals[i] - forward_mat[i] @ fval
+    return (forward_mat, fvals, fval), fval
+
+
+def check_status(err, cnt, maxit, tol):
+    """Check whether to exit iteration and compile error message"""
+    # exit causes
+    if err < tol:
+        return True, (True, "The solution converged.")
+    elif jnp.isnan(err):
+        return True, (False, "Function returns 'NaN's.")
+    elif cnt > maxit:
+        return True, (False, f"Maximum number of {maxit} iterations reached.")
+    else:
+        return False, (False, "")
+
+
+def newton_for_jvp(jvp_func, jacobian, x_init, verbose, tol=1e-8, maxit=500, nsteps=30, factor=1.5):
+
+    start_time = time.time()
+    x = x_init[1:-1].flatten()
+
+    (x, err, dampening, cnt), _ = jax.lax.while_loop(jvp_while_cond, jvp_while_body,
+                                                     ((x, 1., 0., 0), (jvp_func, jacobian, maxit, nsteps, tol, factor, verbose)))
+    ltime = time.time() - start_time
+    callback_func(cnt, err, dampening, ltime, verbose)
+    _, (success, mess) = check_status(err, cnt, maxit, tol)
+    # compile error/report message
+    if not success and not jnp.isnan(err):
+        mess += f" Max. error is {err:1.2e}."
+
+    return x, not success, mess
+
+
+def newton_for_banded_jac(jav_func, nvars, horizon, X, shocks, verbose, maxit=30, tol=1e-8):
+
+    st = time.time()
+    cnt = 0
+
+    while True:
+
+        _, (fvals, forward_mat) = jax.lax.scan(sweep_banded_down, (jav_func, jnp.zeros(
+            nvars), jnp.zeros((nvars, nvars)), X, shocks), jnp.arange(horizon-1))
+        _, out = jax.lax.scan(sweep_banded_up, (forward_mat, fvals, jnp.zeros(
+            nvars)), jnp.arange(horizon-1), reverse=True)
+
+        X = X.at[1:-1].add(-out)
+        err = amax(out)
+        cnt += 1
+
+        ltime = time.time() - st
+
+        if verbose:
+            info_str = f'    Iteration {cnt:3d} | max error {err:.2e} | lapsed {ltime:3.4f}s'
+            print(info_str)
+
+        do_break, (success, mess) = check_status(err, cnt, maxit, tol)
+        if do_break:
+            break
+
+    if not success:
+        mess += f" Max. error is {err:1.2e}."
+
+    return X, False, ''
+
+
+def newton_jax_jit_wrapper(func, init, **args):
+    """Wrapper around grgrjax.newton.newton_jax_jit to return flag and message
+    """
+
+    if 'tol' not in args:
+        args['tol'] = 1e-8
+    if 'maxit' not in args:
+        args['maxit'] = 30
+
+    x, (f, _), cnt, flag = newton_jax_jit(func, init, **args)
+    err = amax(f)
+    _, (success, mess) = check_status(err, cnt, args['maxit'], args['tol'])
+    flag |= not success
+
+    # compile error/report message
+    if not success and not jnp.isnan(err):
+        mess += f" Max. error is {err:1.2e}."
+    return x, flag, mess
