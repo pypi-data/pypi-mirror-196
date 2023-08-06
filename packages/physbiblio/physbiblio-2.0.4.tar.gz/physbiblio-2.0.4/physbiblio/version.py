9cf9d63d808aa26a0ef83948d3809c963b7f86e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:42 +0000

    Add package physbiblio/2.0.4/physbiblio-2.0.4.tar.gz

---
+++ packages/physbiblio/physbiblio-2.0.4.tar.gz/physbiblio-2.0.4/physbiblio/version.py
@@ -0,0 +1,8 @@
+__version__ = "2.0.4"
+__version_date__ = "10/03/2023"
+
+__recent_changes__ = """<br>* deprecation warnings fixed in setup.cfg and setup.py<br>
+* code improvements thanks to refurb<br>
+* PySide6 6.4.0 enum system now compatible with matplotlib >3.6.2<br>
+* bug fixes<br>
+"""
