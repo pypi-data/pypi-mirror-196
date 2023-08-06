492e3a986d2105eb78eaad2f2b547e7b1bff1b68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:19 +0000

    Add package twitter-api-client/0.0.1/twitter-api-client-0.0.1.tar.gz

---
+++ packages/twitter-api-client/twitter-api-client-0.0.1.tar.gz/twitter-api-client-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="twitter-api-client",
+    version="0.0.1",
+    description="Twitter API",
+    author="Trevor Hobenshield",
+    author_email="trevorhobenshield@gmail.com",
+    url="https://github.com/trevorhobenshield/twitter-api",
+    install_requires=[
+        "aiohttp",
+        "requests",
+    ],
+    keywords="twitter search api async automation bot",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'src': ['*']}
+)
