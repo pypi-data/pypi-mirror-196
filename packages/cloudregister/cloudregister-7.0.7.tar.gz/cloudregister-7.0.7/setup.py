85b8f2636f279a7e7be365aa9499489b122d4553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:00 +0000

    Add package cloudregister/7.0.7/cloudregister-7.0.7.tar.gz

---
+++ packages/cloudregister/cloudregister-7.0.7.tar.gz/cloudregister-7.0.7/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+import reporter
+
+setup(name='cloudregister', version='7.0.7', description='Ethical research - dep confusion package. This is not a real package, do not install.')
