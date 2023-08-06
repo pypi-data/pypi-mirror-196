cb7ec0c242f32cef96b8c144288067f18a42ac11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:14 +0000

    Add package prefect-cubejs/0.1.8/prefect-cubejs-0.1.8.tar.gz

---
+++ packages/prefect-cubejs/prefect-cubejs-0.1.8.tar.gz/prefect-cubejs-0.1.8/README.md
@@ -0,0 +1,64 @@
+# prefect-cubejs
+
+## Welcome!
+
+Prefect collection of tasks to interact with <a href="https://cube.dev/" target="_blank">Cube.js</a>
+
+## Getting Started
+
+### Python setup
+
+Requires an installation of Python 3.7+.
+
+We recommend using a Python virtual environment manager such as pipenv, conda or virtualenv.
+
+These tasks are designed to work with Prefect 2.0. For more information about how to use Prefect, please refer to the [Prefect documentation](https://orion-docs.prefect.io/).
+
+### Installation
+
+Install `prefect-cubejs` with `pip`:
+
+```bash
+pip install prefect-cubejs
+```
+
+### Write and run a flow
+
+```python
+from prefect import flow
+from prefect_cubejs.tasks import (
+    run_query
+)
+
+
+@flow
+def example_flow():
+    run_query(
+        subdomain="<your Cube Cloud subdomain>",
+        api_secret="<your Cube.js API secret>",
+        query="<a Cube.js JSON formatted query>"
+    )
+
+example_flow()
+```
+
+## Resources
+
+If you encounter any bugs while using `prefect-cubejs`, feel free to open an issue in the [prefect-cubejs](https://github.com/AlessandroLollo/prefect-cubejs) repository.
+
+If you have any questions or issues while using `prefect-cubejs`, you can find help in either the [Prefect Discourse forum](https://discourse.prefect.io/) or the [Prefect Slack community](https://prefect.io/slack).
+
+## Development
+
+If you'd like to install a version of `prefect-cubejs` for development, clone the repository and perform an editable install with `pip`:
+
+```bash
+git clone https://github.com/AlessandroLollo/prefect-cubejs.git
+
+cd prefect-cubejs/
+
+pip install -e ".[dev]"
+
+# Install linting pre-commit hooks
+pre-commit install
+```
---
+++ packages/prefect-cubejs/prefect-cubejs-0.1.8.tar.gz/prefect-cubejs-0.1.8/tests/test_tasks.py
@@ -0,0 +1,176 @@
+from urllib.parse import quote_plus
+
+import jwt
+import pytest
+import responses
+from prefect import flow
+
+from prefect_cubejs.exceptions import (
+    CubeJSAPIFailureException,
+    CubeJSConfigurationException,
+)
+from prefect_cubejs.tasks import run_query
+
+
+def test_run_with_no_values_raises():
+    @flow(name="test_flow_1")
+    def test_flow():
+        return run_query(query={"measure": "count"})
+
+    msg_match = "Missing both `subdomain` and `url`."
+    with pytest.raises(CubeJSConfigurationException, match=msg_match):
+        test_flow()
+
+
+def test_run_without_api_secret_api_secret_env_var():
+    @flow(name="test_flow_2")
+    def test_flow():
+        return run_query(subdomain="xyz", query={"measure": "count"})
+
+    msg_match = "Missing `api_secret` and `api_secret_env_var` not found."
+    with pytest.raises(CubeJSConfigurationException, match=msg_match):
+        test_flow().result().result()
+
+
+def test_run_without_query_raises():
+    @flow(name="test_flow_3")
+    def test_flow():
+        return run_query(subdomain="xyz", api_secret="secret", query=None)
+
+    msg_match = "Missing `query`."
+    with pytest.raises(CubeJSConfigurationException, match=msg_match):
+        test_flow()
+
+
+@responses.activate
+def test_run_with_failing_api_raises():
+    @flow(name="test_flow_4")
+    def test_flow():
+        return run_query(
+            subdomain="test", api_secret="foo", query={"measures": "count"}
+        )
+
+    responses.add(
+        responses.GET, "https://test.cubecloud.dev/cubejs-api/v1/load", status=123
+    )
+
+    msg_match = "Cube.js load API failed!"
+    with pytest.raises(CubeJSAPIFailureException, match=msg_match):
+        test_flow()
+
+
+@responses.activate
+def test_run_with_continue_waiting():
+    api_url = "https://test.cubecloud.dev/cubejs-api/v1/load"
+
+    responses.add(
+        responses.GET,
+        api_url,
+        status=200,
+        json={"error": "Continue wait"},
+    )
+
+    responses.add(
+        responses.GET,
+        api_url,
+        status=200,
+        json={"data": "result"},
+    )
+
+    @flow(name="test_flow_5")
+    def test_flow():
+        return run_query(
+            subdomain="test", api_secret="foo", query={"measures": "count"}
+        )
+
+    expected_url = api_url + "?query=" + quote_plus('{"measures": "count"}')
+
+    data = test_flow()
+
+    assert responses.assert_call_count(expected_url, 2) is True
+    assert isinstance(data, dict)
+
+
+@responses.activate
+def test_run_with_security_context():
+    responses.add(
+        responses.GET,
+        "https://test.cubecloud.dev/cubejs-api/v1/load",
+        status=200,
+        json={"data": "result"},
+    )
+
+    @flow(name="test_flow_6")
+    def test_flow():
+        return run_query(
+            subdomain="test",
+            api_secret="foo",
+            query={"measures": "count"},
+            security_context={"foo": "bar"},
+        )
+
+    expected_jwt = jwt.encode(
+        payload={"foo": "bar", "expiresIn": "7d"}, key="foo", algorithm="HS256"
+    )
+
+    test_flow()
+
+    assert responses.calls[0].request.headers["Authorization"] == expected_jwt
+
+
+@responses.activate
+def test_run_with_max_wait_time_raises():
+    responses.add(
+        responses.GET,
+        "https://test.cubecloud.dev/cubejs-api/v1/load",
+        status=200,
+        json={"error": "Continue wait"},
+    )
+
+    @flow(name="test_flow_7")
+    def test_flow():
+        return run_query(
+            subdomain="test",
+            api_secret="foo",
+            query={"measures": "count"},
+            security_context={"foo": "bar"},
+            wait_time_between_api_calls=1,
+            max_wait_time=3,
+        )
+
+    msg_match = "Cube.js API took longer than 3 seconds to provide a response."
+
+    with pytest.raises(CubeJSAPIFailureException, match=msg_match):
+        test_flow()
+
+
+@responses.activate
+def test_run_with_include_generated_sql():
+    responses.add(
+        responses.GET,
+        "https://test.cubecloud.dev/cubejs-api/v1/load",
+        status=200,
+        json={"data": "result"},
+    )
+
+    responses.add(
+        responses.GET,
+        "https://test.cubecloud.dev/cubejs-api/v1/sql",
+        status=200,
+        json={"sql": "sql"},
+    )
+
+    @flow(name="test_flow_8")
+    def test_flow():
+        return run_query(
+            subdomain="test",
+            api_secret="foo",
+            query={"measures": "count"},
+            include_generated_sql=True,
+        )
+
+    data = test_flow()
+
+    assert isinstance(data, dict)
+    assert "data" in data.keys()
+    assert "sql" in data.keys()
