c3dca711e89d72244bbd53560fcc73c90c9e5be7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:58 +0000

    Add package pysmartmeter/0.4.1/pysmartmeter-0.4.1.tar.gz

---
+++ packages/pysmartmeter/pysmartmeter-0.4.1.tar.gz/pysmartmeter-0.4.1/pysmartmeter/__init__.py
@@ -0,0 +1,6 @@
+"""pysmartmeter - Collect data from Hichi Smartmeter and expose it via MQTT"""
+
+from importlib.metadata import version
+
+__version__ = version('pysmartmeter')
+__author__ = 'Jens Diemer <pysmartmeter@jensdiemer.de>'
