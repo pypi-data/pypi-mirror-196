e374d6d5cd093fe8035d827e28aedc3a8173bb6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:49 +0000

    Add package flet-route/0.1.2/flet_route-0.1.2.tar.gz

---
+++ packages/flet-route/flet_route-0.1.2.tar.gz/flet_route-0.1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name = "flet_route",
+    version = "0.1.2",
+    author="Saurabh Wadekar [ INDIA ]",
+    packages=["flet_route"],
+    license="MIT",
+    requires=["flet","repath"],
+    maintainer="Saurabh Wadekar",
+    maintainer_email="saurabhwadekar420@gmail.com",
+    keywords=["flet","routing","flet simple routing"],
+    description="This makes it easy to manage multiple views with dynamic routing.",
+    long_description="file: README.md",
+    url="https://github.com/saurabhwadekar/flet_route"   
+)
