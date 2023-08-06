5efba05630bc397705e33fd5a1917cf4bc3808e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:59 +0000

    Add package lineage-bwslib/0.0.1/lineage-bwslib-0.0.1.tar.gz

---
+++ packages/lineage-bwslib/lineage-bwslib-0.0.1.tar.gz/lineage-bwslib-0.0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="lineage-bwslib",
+    version="0.0.1",
+    description="BWS auth lib package",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
