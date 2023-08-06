d90e0dee07e0ead4eafd9f7dc8a5f6b92bfc4836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:04 +0000

    Add package JGVutils/1.0.5.0/JGVutils-1.0.5.0.tar.gz

---
+++ packages/JGVutils/JGVutils-1.0.5.0.tar.gz/JGVutils-1.0.5.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="JGVutils",
+    version="1.0.5.0",
+    author="JuanGV",
+    description="Utils for Python",
+    url="https://github.com/JuanGomezVilla/JGVutils",
+    license="MIT LICENSE",
+    packages=["JGVutils"],
+    install_requires=[
+        "pymysql"
+    ],
+)
