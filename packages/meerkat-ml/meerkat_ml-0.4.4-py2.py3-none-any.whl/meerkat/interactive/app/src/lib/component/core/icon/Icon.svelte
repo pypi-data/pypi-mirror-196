ef765029d5ce97466e1e7bc446712bb217067c16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:22 +0000

    Add package meerkat-ml/0.4.4/meerkat_ml-0.4.4-py2.py3-none-any.whl

---
+++ packages/meerkat-ml/meerkat_ml-0.4.4-py2.py3-none-any.whl/meerkat/interactive/app/src/lib/component/core/icon/Icon.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import {
+		CodeSquare,
+		CodeSlash,
+		Code,
+		Globe2,
+		Image,
+		FileEarmarkImage,
+		FileEarmarkFont,
+		FileEarmarkPdf,
+		Hash,
+		Magic,
+		BoxFill,
+		CheckSquare,
+		TrashFill
+	} from 'svelte-bootstrap-icons';
+
+	export let name: string = 'Globe2';
+	const icons: any = {
+		Globe2: Globe2,
+		CodeSlash: CodeSlash,
+		CodeSquare: CodeSquare,
+		Code: Code,
+		Image: Image,
+		FileEarmarkImage: FileEarmarkImage,
+		FileEarmarkFont: FileEarmarkFont,
+		Hash: Hash,
+		Magic: Magic,
+		FileEarmarkPdf: FileEarmarkPdf,
+		BoxFill: BoxFill,
+		CheckSquare: CheckSquare,
+		TrashFill: TrashFill
+	};
+</script>
+
+<svelte:component this={icons[name]} />
