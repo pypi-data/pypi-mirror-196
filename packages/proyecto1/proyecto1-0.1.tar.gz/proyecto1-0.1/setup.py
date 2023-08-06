b094eafb4e99cb8c96a2e7981df751880031f889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:42 +0000

    Add package proyecto1/0.1/proyecto1-0.1.tar.gz

---
+++ packages/proyecto1/proyecto1-0.1.tar.gz/proyecto1-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="proyecto1",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # lista de dependencias requeridas
+    ],
+    author="Lucho Delgado",
+    description="2do intento de crear modulo",
+    long_description="Una descripción más detallada del paquete",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
