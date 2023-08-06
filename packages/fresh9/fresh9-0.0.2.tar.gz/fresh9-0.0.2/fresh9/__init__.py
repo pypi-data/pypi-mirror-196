c89785443a6681587b8694e89a89c2d1a835d3bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:03 +0000

    Add package fresh9/0.0.2/fresh9-0.0.2.tar.gz

---
+++ packages/fresh9/fresh9-0.0.2.tar.gz/fresh9-0.0.2/fresh9/__init__.py
@@ -0,0 +1,13 @@
+"""
+:authors: Peopl3s
+:license: Apache License, Version 2.0, see LICENSE file
+
+:copyright: (c) 2021 Peopl3s
+"""
+
+from .clubhouse import (ClubHouse, ClubHouseApiMethod, Machine)
+
+
+__author__ = 'Peopl3s'
+__version__ = '0.9.9'
+__email__ = 'peoplesdreamer@gmail.com'
