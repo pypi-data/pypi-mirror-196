4574b89b887508209ff3b2742bc6834b9d7d6aee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:47 +0000

    Add package udacity-stat-dist/0.1/udacity-stat-dist-0.1.tar.gz

---
+++ packages/udacity-stat-dist/udacity-stat-dist-0.1.tar.gz/udacity-stat-dist-0.1/README.md
@@ -0,0 +1,3 @@
+Package for Gaussian and Binomial distribution classes for calculating and visualizing a Gaussian and Binomial distributions.
+
+
---
+++ packages/udacity-stat-dist/udacity-stat-dist-0.1.tar.gz/udacity-stat-dist-0.1/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(name='udacity-stat-dist',
+	version='0.1',
+	description='Gaussian distributions',
+	packages = ['udacity-stat-dist'],
+	zip_file=False)
