b5ef5e2a80553b48d11a8e40554fd61b3badfe87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:36 +0000

    Add package bioontologies/0.2.4/bioontologies-0.2.4.tar.gz

---
+++ packages/bioontologies/bioontologies-0.2.4.tar.gz/bioontologies-0.2.4/src/bioontologies/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Tools for biomedical ontologies."""
+
+from .robot import (
+    convert_to_obograph,
+    get_obograph_by_iri,
+    get_obograph_by_path,
+    get_obograph_by_prefix,
+)
+
+__all__ = [
+    "convert_to_obograph",
+    "get_obograph_by_prefix",
+    "get_obograph_by_iri",
+    "get_obograph_by_path",
+]
