a56fabd9a50de992576df04b407539ef8467249e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:39 +0000

    Add package arkitekt/0.4.51/arkitekt-0.4.51-py3-none-any.whl

---
+++ packages/arkitekt/arkitekt-0.4.51-py3-none-any.whl/arkitekt/apps/fakts.py
@@ -0,0 +1,16 @@
+from fakts import FaktsGrant
+from fakts.fakts import Fakts
+from fakts.grants.remote.base import RemoteGrant
+from fakts.grants.remote.device_code import DeviceCodeGrant
+from koil.composition.base import Composition
+from pydantic import Field
+
+
+class ArkitektFakts(Fakts):
+    pass
+
+
+class FaktsApp(Composition):
+    fakts: ArkitektFakts = Field(default_factory=ArkitektFakts)
+    """The fakts layer that is used for configuration
+    """
