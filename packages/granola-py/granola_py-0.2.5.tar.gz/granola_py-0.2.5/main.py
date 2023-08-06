3cc29bfe2c089ae55841fb6a24cf65f1c1941047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:18 +0000

    Add package granola-py/0.2.5/granola_py-0.2.5.tar.gz

---
+++ packages/granola-py/granola_py-0.2.5.tar.gz/granola_py-0.2.5/main.py
@@ -0,0 +1,14 @@
+from granola import serve 
+
+class Main:
+    def main(self):
+        return "data"
+
+class App:
+    def __granola__(self):
+        return "WELCOME TO SERVER"
+
+    def api(self, request):
+        return Main()
+
+serve(8685, App())
