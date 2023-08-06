fcabf57e2a661c275f66d1deda8f8550296c4c98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:53 +0000

    Add package nr-metadata/1.0.0/nr-metadata-1.0.0.tar.gz

---
+++ packages/nr-metadata/nr-metadata-1.0.0.tar.gz/nr-metadata-1.0.0/nr_metadata/theses/resources/records/resource.py
@@ -0,0 +1,8 @@
+from invenio_records_resources.resources import RecordResource
+
+
+class ThesesResource(RecordResource):
+    """ThesesRecord resource."""
+
+    # here you can for example redefine
+    # create_url_rules function to add your own rules
