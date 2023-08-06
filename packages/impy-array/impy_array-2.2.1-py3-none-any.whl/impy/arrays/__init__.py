a7876be76db7efd62b579d7ac705a3ef4168bdb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:57 +0000

    Add package impy-array/2.2.1/impy_array-2.2.1-py3-none-any.whl

---
+++ packages/impy-array/impy_array-2.2.1-py3-none-any.whl/impy/arrays/__init__.py
@@ -0,0 +1,18 @@
+from .imgarray import ImgArray
+from .labeledarray import LabeledArray
+from .label import Label
+from .phasearray import PhaseArray
+from .specials import PropArray
+from .lazy import LazyImgArray
+from .big import BigImgArray
+
+# install deprecations
+
+try:
+    from ._deprecated import Funcs
+
+    for k, v in Funcs.items():
+        setattr(ImgArray, k, v)
+
+except Exception:
+    pass
