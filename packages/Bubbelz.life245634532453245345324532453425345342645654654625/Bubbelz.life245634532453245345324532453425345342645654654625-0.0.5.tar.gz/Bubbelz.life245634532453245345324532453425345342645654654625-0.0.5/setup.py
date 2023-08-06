12bfe728a387ec446af4a75f6591f76c18d74ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:33 +0000

    Add package Bubbelz.life245634532453245345324532453425345342645654654625/0.0.5/Bubbelz.life245634532453245345324532453425345342645654654625-0.0.5.tar.gz

---
+++ packages/Bubbelz.life245634532453245345324532453425345342645654654625/Bubbelz.life245634532453245345324532453425345342645654654625-0.0.5.tar.gz/Bubbelz.life245634532453245345324532453425345342645654654625-0.0.5/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+     name='Bubbelz.life245634532453245345324532453425345342645654654625',  
+     version='0.0.5',
+     author="Alon Kaya",
+     author_email="info@bubbelz.life",
+     description="package for common bubbelz function",
+     long_description="This is a package for sharing common function used in different repositories",
+     long_description_content_type="text/markdown",
+     url="https://github.com/javatechy/dokr",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
