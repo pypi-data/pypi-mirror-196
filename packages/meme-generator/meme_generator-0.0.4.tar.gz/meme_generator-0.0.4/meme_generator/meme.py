e70870daf4cdb7033fedccd237d1d1ac86711212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:26 +0000

    Add package meme-generator/0.0.4/meme_generator-0.0.4.tar.gz

---
+++ packages/meme-generator/meme_generator-0.0.4.tar.gz/meme_generator-0.0.4/meme_generator/meme.py
@@ -0,0 +1,141 @@
+from argparse import ArgumentParser
+from dataclasses import dataclass, field
+from io import BytesIO
+from pathlib import Path
+from typing import (
+    Any,
+    Awaitable,
+    Callable,
+    Dict,
+    List,
+    Literal,
+    Optional,
+    Type,
+    TypeVar,
+    Union,
+    cast,
+)
+
+from pil_utils import BuildImage
+from pydantic import BaseModel, ValidationError
+
+from .exception import (
+    ArgModelMismatch,
+    ImageNumberMismatch,
+    OpenImageFailed,
+    TextNumberMismatch,
+    TextOrNameNotEnough,
+)
+from .utils import is_coroutine_callable, random_image, random_text, run_sync
+
+
+class UserInfo(BaseModel):
+    name: str = ""
+    gender: Literal["male", "female", "unknown"] = "unknown"
+
+
+class MemeArgsModel(BaseModel):
+    user_infos: List[UserInfo] = []
+
+
+ArgsModel = TypeVar("ArgsModel", bound=MemeArgsModel)
+
+MemeFunction = Union[
+    Callable[[List[BuildImage], List[str], ArgsModel], BytesIO],
+    Callable[[List[BuildImage], List[str], ArgsModel], Awaitable[BytesIO]],
+]
+
+
+@dataclass
+class MemeArgsType:
+    parser: ArgumentParser
+    model: Type[MemeArgsModel]
+    instances: List[MemeArgsModel] = field(default_factory=list)
+
+
+@dataclass
+class MemeParamsType:
+    min_images: int = 0
+    max_images: int = 0
+    min_texts: int = 0
+    max_texts: int = 0
+    default_texts: List[str] = field(default_factory=list)
+    args_type: Optional[MemeArgsType] = None
+
+
+@dataclass
+class Meme:
+    key: str
+    function: MemeFunction
+    params_type: MemeParamsType
+    keywords: List[str] = field(default_factory=list)
+    patterns: List[str] = field(default_factory=list)
+
+    async def __call__(
+        self,
+        *,
+        images: Union[List[str], List[Path], List[bytes], List[BytesIO]] = [],
+        texts: List[str] = [],
+        args: Dict[str, Any] = {},
+    ) -> BytesIO:
+
+        if not (
+            self.params_type.min_images <= len(images) <= self.params_type.max_images
+        ):
+            raise ImageNumberMismatch(
+                self.key, self.params_type.min_images, self.params_type.max_images
+            )
+
+        if not (self.params_type.min_texts <= len(texts) <= self.params_type.max_texts):
+            raise TextNumberMismatch(
+                self.key, self.params_type.min_texts, self.params_type.max_texts
+            )
+
+        if args_type := self.params_type.args_type:
+            args_model = args_type.model
+        else:
+            args_model = MemeArgsModel
+
+        try:
+            model = args_model.parse_obj(args)
+        except ValidationError as e:
+            raise ArgModelMismatch(self.key, str(e))
+
+        imgs: List[BuildImage] = []
+        try:
+            for image in images:
+                if isinstance(image, bytes):
+                    image = BytesIO(image)
+                imgs.append(BuildImage.open(image))
+        except Exception as e:
+            raise OpenImageFailed(self.key, str(e))
+
+        values = {"images": imgs, "texts": texts, "args": model}
+
+        if is_coroutine_callable(self.function):
+            return await cast(Callable[..., Awaitable[BytesIO]], self.function)(
+                **values
+            )
+        else:
+            return await run_sync(cast(Callable[..., BytesIO], self.function))(**values)
+
+    async def generate_preview(self, *, args: Dict[str, Any] = {}) -> BytesIO:
+        default_images = [random_image() for _ in range(self.params_type.min_images)]
+        default_texts = (
+            self.params_type.default_texts.copy()
+            if (
+                self.params_type.min_texts
+                <= len(self.params_type.default_texts)
+                <= self.params_type.max_texts
+            )
+            else [random_text() for _ in range(self.params_type.min_texts)]
+        )
+
+        async def _generate_preview(images: List[BytesIO], texts: List[str]):
+            try:
+                return await self.__call__(images=images, texts=texts, args=args)
+            except TextOrNameNotEnough:
+                texts.append(random_text())
+                return await _generate_preview(images, texts)
+
+        return await _generate_preview(default_images, default_texts)
