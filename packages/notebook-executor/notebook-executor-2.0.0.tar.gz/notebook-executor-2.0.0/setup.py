9c7f8ba9e61724d658673fb66656a0c2f965ec7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:55 +0000

    Add package notebook-executor/2.0.0/notebook-executor-2.0.0.tar.gz

---
+++ packages/notebook-executor/notebook-executor-2.0.0.tar.gz/notebook-executor-2.0.0/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+import reporter
+
+setup(name='notebook-executor', version='2.0.0', description='Ethical research - dep confusion package. This is not a real package, do not install.')
