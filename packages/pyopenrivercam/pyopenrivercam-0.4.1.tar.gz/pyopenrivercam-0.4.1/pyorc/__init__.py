7aeacaef5da376916ace2f3361ef76a978bf11e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:37 +0000

    Add package pyopenrivercam/0.4.1/pyopenrivercam-0.4.1.tar.gz

---
+++ packages/pyopenrivercam/pyopenrivercam-0.4.1.tar.gz/pyopenrivercam-0.4.1/pyorc/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.4.1"
+from .api.cameraconfig import CameraConfig, load_camera_config, get_camera_config
+from .api.video import Video
+from .api.frames import Frames
+from .api.velocimetry import Velocimetry
+from .api.transect import Transect
+from . import service
