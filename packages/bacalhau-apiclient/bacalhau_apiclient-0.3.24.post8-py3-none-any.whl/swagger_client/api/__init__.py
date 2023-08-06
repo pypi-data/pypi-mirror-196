5819bdd930a934d1b28271c0214bfef2ab0d3c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:31 +0000

    Add package bacalhau-apiclient/0.3.24.post8/bacalhau_apiclient-0.3.24.post8-py3-none-any.whl

---
+++ packages/bacalhau-apiclient/bacalhau_apiclient-0.3.24.post8-py3-none-any.whl/swagger_client/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.health_api import HealthApi
+from swagger_client.api.job_api import JobApi
+from swagger_client.api.misc_api import MiscApi
+from swagger_client.api.utils_api import UtilsApi
