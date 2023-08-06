1895c1c30e289766109de4220c7961fd1ee90a0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:01 +0000

    Add package aws-toolset/0.0.4/aws_toolset-0.0.4-py3-none-any.whl

---
+++ packages/aws-toolset/aws_toolset-0.0.4-py3-none-any.whl/aws_toolset/aws_toolset.py
@@ -0,0 +1,42 @@
+import argparse, importlib
+import importlib.util
+
+#Module Imports 
+from .modules import create_codestar_notifications
+
+def create_dict_modules():
+    modules = {}
+    modules["create_codestar_notifications"] = create_codestar_notifications
+    return modules
+
+def parameters_definitions(parser):
+    
+    modules = create_dict_modules()
+
+    for module in modules:
+        try:
+            modules[module].args_definitions(parser)
+        except AttributeError:
+            # print(f"There is no such attribute for {mod}")
+            pass
+        
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    subparser = parser.add_subparsers(dest="Module",title='Modules',description='List of Modules',help='List of modules', required = True)
+    # parser.add_argument("-v", "--verbose", default=False, action="store_true" ,help="Increase verbosity of program" )
+
+    parameters_definitions(subparser)
+
+    args = parser.parse_args()
+
+    modules = create_dict_modules()
+
+    try:
+        modules[args.Module].main(args)
+    except AttributeError:
+        print(f"There is no such attribute for {module}")
+
+if __name__ == "__main__":
+    main()
