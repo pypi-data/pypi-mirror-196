5588798f3ca415289a65c2d46aefc89ba5cae36d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:31 +0000

    Add package MomentumX/2.6.1/MomentumX-2.6.1.tar.gz

---
+++ packages/MomentumX/MomentumX-2.6.1.tar.gz/MomentumX-2.6.1/examples/sync_writer.py
@@ -0,0 +1,23 @@
+import momentumx as mx
+import threading
+import signal
+import time
+
+STREAM = "mx://incrementer"
+
+cancel = threading.Event()
+signal.signal(signal.SIGINT, (lambda _sig, _frm: cancel.set()))
+
+mx.set_log_level(mx.LogLevel.INFO)
+
+stream = mx.Producer(STREAM, 100, 10, True, cancel)
+while stream.subscriber_count == 0:
+    print("waiting for subscriber(s)")
+    if cancel.wait(0.5):
+        break
+
+for n in range(1, 500000):
+    if stream.subscriber_count == 0:
+        cancel.wait(0.5)
+    elif stream.send_string(str(n)):
+        print(f"Sent: {n}")
