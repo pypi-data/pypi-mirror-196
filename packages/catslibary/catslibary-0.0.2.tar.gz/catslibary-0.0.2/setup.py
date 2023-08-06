dd256670bf99bcd7c82efac1493f5042b7fcf306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:42 +0000

    Add package catslibary/0.0.2/catslibary-0.0.2.tar.gz

---
+++ packages/catslibary/catslibary-0.0.2.tar.gz/catslibary-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='catslibary',
+    version='0.0.2',
+    author='PythonProgrammer',
+    author_email='python.programmer.2012@gmail.com',
+    description='Koty miałczą!',
+    long_description='Koty zawładneły nad programowaniem!',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.11'
+    ],
+)
