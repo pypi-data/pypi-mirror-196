8ec605b843193ca3744fe4e497b79413f6f09959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:58 +0000

    Add package margo-parser/1.0.0/margo_parser-1.0.0-py3-none-any.whl

---
+++ packages/margo-parser/margo_parser-1.0.0-py3-none-any.whl/margo_parser/api/__init__.py
@@ -0,0 +1,22 @@
+"""The API for using margo-parser"""
+
+from .classes.MargoBlock import MargoBlock
+from .classes.MargoStatement import MargoStatement, MargoStatementTypes
+from .classes.MargoDirective import MargoDirective
+from .classes.MargoAssignment import MargoAssignment
+from .classes.MargoPythonCellPreambleBlock import MargoPythonCellPreambleBlock
+from .classes.MargoMarkdownCellPrambleBlock import (
+    MargoMarkdownCellPreambleBlock,
+)
+from ..exceptions import MargoParseException
+
+__all__ = [
+    MargoBlock,
+    MargoStatement,
+    MargoStatementTypes,
+    MargoDirective,
+    MargoAssignment,
+    MargoPythonCellPreambleBlock,
+    MargoMarkdownCellPreambleBlock,
+    MargoParseException,
+]
---
+++ packages/margo-parser/margo_parser-1.0.0-py3-none-any.whl/margo_parser/api/classes/MargoAssignment.py
@@ -0,0 +1,10 @@
+from .MargoStatement import MargoStatement
+
+
+class MargoAssignment(MargoStatement):
+    def __init__(self, name: str, value):
+        super().__init__("DECLARATION", name, value=value)
+
+    @property
+    def type(self) -> str:
+        return "DECLARATION"
