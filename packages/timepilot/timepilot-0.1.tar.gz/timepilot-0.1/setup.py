96f8081fa988cf9d3d9b8c8e84eafd3f95dbfcc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:13 +0000

    Add package timepilot/0.1/timepilot-0.1.tar.gz

---
+++ packages/timepilot/timepilot-0.1.tar.gz/timepilot-0.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='timepilot',
+    version='0.1',
+    description='A simple Python library for timezone formatting',
+    author='shuv',
+    author_email='',
+    url='',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
