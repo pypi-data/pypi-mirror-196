63cefa21800d5e4f9b2f99873b0887ad1bc02c36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:53 +0000

    Add package omspy/0.13.0/omspy-0.13.0.tar.gz

---
+++ packages/omspy/omspy-0.13.0.tar.gz/omspy-0.13.0/omspy/simulation/models.py
@@ -0,0 +1,243 @@
+"""
+This module contains all the models for running the simulation
+All the models start with **V** to indicate virtual models
+"""
+
+from pydantic import BaseModel
+from typing import Optional, Union, Any, Dict, List
+from enum import Enum
+import pendulum
+import omspy.utils as utils
+from omspy.models import OrderBook
+
+
+class Status(Enum):
+    COMPLETE = 1
+    REJECTED = 2
+    CANCELED = 3
+    PARTIAL_FILL = 4  # partially filled but completed order
+    OPEN = 5  # all quantity is pending to be filled
+    PENDING = 6  # partially filled, waiting to get complete
+
+
+class ResponseStatus(str, Enum):
+    SUCCESS = "success"
+    FAILURE = "failure"
+
+
+class Side(Enum):
+    BUY = 1
+    SELL = -1
+
+
+class OHLC(BaseModel):
+    open: float
+    high: float
+    low: float
+    close: float
+    last_price: float
+
+
+class OHLCV(OHLC):
+    volume: int
+
+
+class OHLCVI(OHLCV):
+    open_interest: int
+
+
+class VQuote(OHLCV):
+    orderbook: OrderBook
+
+
+class VTrade(BaseModel):
+    trade_id: str
+    order_id: str
+    symbol: str
+    quantity: int
+    price: float
+    side: Side
+    timestamp: Optional[pendulum.DateTime]
+
+    class Config:
+        validate_assignment = True
+
+    @property
+    def value(self) -> float:
+        return self.side.value * self.quantity * self.price
+
+
+class VOrder(BaseModel):
+    order_id: str
+    symbol: str
+    quantity: Union[int, float]
+    side: Side
+    price: Optional[float]
+    average_price: Optional[float]
+    trigger_price: Optional[float]
+    timestamp: Optional[pendulum.DateTime]
+    exchange_order_id: Optional[str]
+    exchange_timestamp: Optional[pendulum.DateTime]
+    status_message: Optional[str]
+    filled_quantity: Union[int, float] = 0
+    pending_quantity: Union[int, float] = 0
+    canceled_quantity: Union[int, float] = 0
+
+    class Config:
+        validate_assignment = True
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        if self.timestamp is None:
+            self.timestamp = pendulum.now(tz="local")
+        q = utils.update_quantity(
+            q=self.quantity,
+            f=self.filled_quantity,
+            p=self.pending_quantity,
+            c=self.canceled_quantity,
+        )
+        self.filled_quantity = q.f
+        self.pending_quantity = q.p
+        self.canceled_quantity = q.c
+        if self.average_price is None:
+            self.average_price = 0
+
+    @property
+    def status(self) -> Status:
+        if self.quantity == self.filled_quantity:
+            return Status.COMPLETE
+        elif self.quantity == self.canceled_quantity:
+            if self.status_message:
+                if str(self.status_message).upper().startswith("REJ"):
+                    return Status.REJECTED
+            else:
+                return Status.CANCELED
+        elif self.canceled_quantity > 0:
+            if (self.canceled_quantity + self.filled_quantity) == self.quantity:
+                return Status.PARTIAL_FILL
+            else:
+                return Status.PENDING
+        elif self.pending_quantity > 0:
+            if self.filled_quantity > 0:
+                return Status.PENDING
+            else:
+                return Status.OPEN
+        else:
+            return Status.OPEN
+
+    @property
+    def value(self) -> float:
+        """
+        returns the value of the order
+        negative means sell and positive means buy
+        """
+        return self.side.value * self.filled_quantity * self.average_price
+
+
+class VPosition(BaseModel):
+    symbol: str
+    buy_quantity: Optional[Union[int, float]]
+    sell_quantity: Optional[Union[int, float]]
+    buy_value: Optional[float]
+    sell_value: Optional[float]
+
+    class Config:
+        validate_assignment = True
+
+    @property
+    def average_buy_price(self) -> float:
+        """
+        Get the average buy price
+        returns 0 if there is no price or quantity
+        """
+        if self.buy_quantity and self.buy_value:
+            return self.buy_value / self.buy_quantity
+        else:
+            return 0.0
+
+    @property
+    def average_sell_price(self) -> float:
+        """
+        Get the average sell price
+        returns 0 if there is no price or quantity
+        """
+        if self.sell_quantity and self.sell_value:
+            return self.sell_value / self.sell_quantity
+        else:
+            return 0.0
+
+    @property
+    def net_quantity(self) -> float:
+        """
+        Get the net quantity for the position
+        negative indicates sell and positive indicates sell
+        """
+        buy_qty = self.buy_quantity if self.buy_quantity else 0
+        sell_qty = self.sell_quantity if self.sell_quantity else 0
+        return buy_qty - sell_qty
+
+    @property
+    def net_value(self) -> float:
+        """
+        Get the net value for the position
+        negative indicates a net sell value and positive indicates a net buy value
+        """
+        buy_value = self.buy_value if self.buy_value else 0
+        sell_value = self.sell_value if self.sell_value else 0
+        return buy_value - sell_value
+
+
+class Response(BaseModel):
+    status: ResponseStatus
+    timestamp: Optional[pendulum.DateTime]
+
+    class Config:
+        validate_assignment = True
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        if self.timestamp is None:
+            self.timestamp = pendulum.now(tz="local")
+
+
+class OrderResponse(Response):
+    error_msg: Optional[str]
+    data: Optional[VOrder]
+
+    class Config:
+        validate_assignment = True
+
+
+class AuthResponse(Response):
+    user_id: str
+    message: str = "Authentication successful"
+
+
+class GenericResponse(Response):
+    error_msg: Optional[str]
+    data: Optional[Any]
+
+
+class LTPResponse(Response):
+    error_msg: Optional[str]
+    data: Dict[str, Union[int, float]]
+
+
+class OHLCVResponse(Response):
+    error_msg: Optional[str]
+    data: Dict[str, OHLCV]
+
+
+class QuoteResponse(Response):
+    error_msg: Optional[str]
+    data: Dict[str, VQuote]
+
+
+class OrderBookResponse(Response):
+    error_msg: Optional[str]
+    data: Dict[str, OrderBook]
+
+
+class PositionResponse(Response):
+    error_msg: Optional[str]
+    data: List[VPosition]
---
+++ packages/omspy/omspy-0.13.0.tar.gz/omspy-0.13.0/omspy/simulation/server.py
@@ -0,0 +1,158 @@
+from typing import Optional
+from pydantic import BaseModel
+from fastapi import FastAPI
+from omspy.simulation.virtual import FakeBroker
+from omspy.simulation.models import (
+    OrderResponse,
+    Side,
+    Status,
+    AuthResponse,
+    LTPResponse,
+    OHLCVResponse,
+    QuoteResponse,
+    OrderBookResponse,
+    PositionResponse,
+)
+
+description = """
+Generate ready to use fake data to mimic
+stock market data for testing broker interface
+and in general
+
+### User
+Generates data with respect to individual user
+
+### Order
+Generates data for placing, modifying and canceling orders
+
+### Market Data
+Generates data regarding price, OHLC, orderbook for symbols
+"""
+app = FastAPI(title="Fake Data API for Stock Market", description=description)
+app.broker: FakeBroker = FakeBroker()
+app._type = type(app.broker)
+
+
+class OrderArgs(BaseModel):
+    symbol: Optional[str]
+    side: Optional[Side]
+    quantity: Optional[int]
+    price: Optional[float]
+    trigger_price: Optional[float]
+    s: Optional[Status]
+
+
+class CreateArgs(BaseModel):
+    symbol: str
+    side: Side
+    quantity: Optional[int]
+    price: Optional[float]
+    trigger_price: Optional[float]
+
+
+class ModifyArgs(BaseModel):
+    quantity: Optional[float]
+    price: Optional[float]
+    trigger_price: Optional[float]
+
+
+@app.get("/", summary="Fake Stock Data")
+def home():
+    return {"hello": "Welcome to Fake Stock Data"}
+
+
+@app.post(
+    "/auth/{user_id}",
+    summary="Authenticate the user",
+    description="Authenticate the user",
+    tags=["user"],
+)
+async def auth(user_id: str) -> AuthResponse:
+    return AuthResponse(status="success", user_id=user_id)
+
+
+@app.post(
+    "/order",
+    summary="Create an order",
+    description="Create an order with the given arguments",
+    tags=["order"],
+)
+async def create_order(order: OrderArgs) -> OrderResponse:
+    if app._type == FakeBroker:
+        response = app.broker.order_place(**order.dict(exclude_none=True))
+    return OrderResponse(status="success", data=response)
+
+
+@app.put(
+    "/order/{order_id}",
+    summary="Modify an order",
+    description="Modify a existing order with the given order_id",
+    tags=["order"],
+)
+async def modify_order(order_id: str, order: OrderArgs) -> OrderResponse:
+    if app._type == FakeBroker:
+        response = app.broker.order_modify(
+            order_id=order_id, **order.dict(exclude_none=True)
+        )
+    return OrderResponse(status="success", data=response)
+
+
+@app.delete(
+    "/order/{order_id}",
+    summary="Delete order",
+    description="Delete a existing order with the given order_id",
+    tags=["order"],
+)
+async def cancel_order(order_id: str, order: OrderArgs) -> OrderResponse:
+    if app._type == FakeBroker:
+        response = app.broker.order_cancel(
+            order_id=order_id, **order.dict(exclude_none=True)
+        )
+    return OrderResponse(status="success", data=response)
+
+
+@app.get(
+    "/ltp/{symbol}",
+    summary="Get the last price for the given symbol",
+    tags=["market data"],
+)
+async def ltp(symbol: str) -> LTPResponse:
+    response = app.broker.ltp(symbol)
+    return LTPResponse(status="success", data=response)
+
+
+@app.get(
+    "/ohlc/{symbol}",
+    summary="Get the ohlc prices and volume for the given symbol",
+    tags=["market data"],
+)
+async def ohlc(symbol: str) -> OHLCVResponse:
+    response = app.broker.ohlc(symbol)
+    return OHLCVResponse(status="success", data=response)
+
+
+@app.get(
+    "/quote/{symbol}",
+    summary="Get the detailed quote for the symbol",
+    description="Quote contains OHLC, volume, last_price and the orderbook",
+    tags=["market data"],
+)
+async def quote(symbol: str) -> QuoteResponse:
+    response = app.broker.quote(symbol)
+    return QuoteResponse(status="success", data=response)
+
+
+@app.get(
+    "/orderbook/{symbol}",
+    summary="Get the orderbook for the given symbol",
+    tags=["market data"],
+)
+async def orderbook(symbol: str) -> OrderBookResponse:
+    response = app.broker.orderbook(symbol)
+    return OrderBookResponse(status="success", data=response)
+
+
+@app.get("/positions", summary="Get random positions", tags=["user"])
+async def positions() -> PositionResponse:
+    response = app.broker.positions()
+    return PositionResponse(status="success", data=response)
