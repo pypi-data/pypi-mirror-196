f0229ed4dbccc5ff95c8f8f51596098adebc16d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:31 +0000

    Add package ampltools/0.5.1b11/ampltools-0.5.1b11-py3-none-any.whl

---
+++ packages/ampltools/ampltools-0.5.1b11-py3-none-any.whl/ampltools/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+__version__ = "0.5.1b11"
+from .notebooks import (
+    ampl_notebook,
+)
+
+from .licenses import (
+    activate_license,
+)
+
+from .utils import (
+    cloud_platform_name,
+    register_magics,
+    add_to_path,
+)
