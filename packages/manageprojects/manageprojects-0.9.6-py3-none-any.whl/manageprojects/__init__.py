ef2473a143c9984723e401eed94cb99c508f3e75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:37 +0000

    Add package manageprojects/0.9.6/manageprojects-0.9.6-py3-none-any.whl

---
+++ packages/manageprojects/manageprojects-0.9.6-py3-none-any.whl/manageprojects/__init__.py
@@ -0,0 +1,6 @@
+"""manageprojects - Manage Python / Django projects"""
+
+from importlib.metadata import version
+
+__version__ = version('manageprojects')
+__author__ = 'Jens Diemer <mamageprojects@jensdiemer.de>'
