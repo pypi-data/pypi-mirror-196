77c3d6ff3bcf2e8d3679212d4f0ed473d4fd433a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:49 +0000

    Add package osmosis/0.6.0/osmosis-0.6.0.tar.gz

---
+++ packages/osmosis/osmosis-0.6.0.tar.gz/osmosis-0.6.0/osmosis/backend/config.py
@@ -0,0 +1,15 @@
+import os
+
+
+def ensure_exists(path: str):
+    os.makedirs(path, exist_ok=True)
+    return path
+
+
+class Config:
+    DATA_DIR = ensure_exists(os.path.expanduser("~/.osmosis"))
+    OUTPUTS_DIR = ensure_exists(os.path.join(DATA_DIR, "outputs"))
+    ESRGAN_MODELS_DIR = ensure_exists(os.path.join(DATA_DIR, "esrgan"))
+    GFPGAN_MODELS_DIR = ensure_exists(os.path.join(DATA_DIR, "gfpgan"))
+
+    SHOW_STEP_LATENTS = False
