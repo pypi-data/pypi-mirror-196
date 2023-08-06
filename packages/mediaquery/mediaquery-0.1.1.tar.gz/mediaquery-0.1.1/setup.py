8ad51435f4a4b6cb331d1c2ab7f15c2fe70b2cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:45 +0000

    Add package mediaquery/0.1.1/mediaquery-0.1.1.tar.gz

---
+++ packages/mediaquery/mediaquery-0.1.1.tar.gz/mediaquery-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open('mediaquery/requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='mediaquery',
+    version='0.1.1',
+    author='Sibidharan',
+    author_email='sibi@selfmade.ninja',
+    description='MediaQuery is a wrapper for mediainfo tool',
+    packages=find_packages(),
+    url='https://git.selfmade.ninja/sibidharan/mediaquery-with-mediainfo',
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'mediaquery=mediaquery.mediaquery:main',
+        ],
+    },
+)
