e7bb3742165e6fc65e365fa8ea92b0a10cfca2c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:49 +0000

    Add package pywhlobf/23.1.0.dev1/pywhlobf-23.1.0.dev1-py3-none-any.whl

---
+++ packages/pywhlobf/pywhlobf-23.1.0.dev1-py3-none-any.whl/tests/opt.py
@@ -0,0 +1,95 @@
+import os
+import os.path
+import inspect
+
+import iolite as io
+from Cython.Compiler.Version import version as cython_version
+
+
+def get_data_folder(code_path: str):
+    proj_root = os.getenv('PYWHLOBF_ROOT')
+    data_root = os.getenv('PYWHLOBF_DATA')
+    assert proj_root and data_root
+
+    if os.path.isabs(code_path):
+        code_path = os.path.relpath(code_path, proj_root)
+
+    rel_folder = code_path.split('.')[0]
+    data_folder = os.path.join(data_root, rel_folder)
+
+    io.folder(data_folder, touch=True)
+
+    return data_folder
+
+
+def get_test_output_fd(frames_offset: int = 0):
+    if not os.getenv('PYWHLOBF_ROOT') or not os.getenv('PYWHLOBF_DATA'):
+        raise NotImplementedError()
+
+    frames = inspect.stack()
+    frames_offset += 1
+    module_path = frames[frames_offset].filename
+    function_name = frames[frames_offset].function
+    module_fd = io.folder(get_data_folder(module_path))
+    test_fd = io.folder(module_fd / function_name, touch=True)
+    return test_fd
+
+
+def get_test_output_path(rel_path: str, frames_offset: int = 0):
+    if not os.getenv('PYWHLOBF_ROOT') or not os.getenv('PYWHLOBF_DATA'):
+        raise NotImplementedError()
+
+    frames = inspect.stack()
+    frames_offset += 1
+    module_path = frames[frames_offset].filename
+    function_name = frames[frames_offset].function
+    module_fd = io.folder(get_data_folder(module_path))
+    test_fd = io.folder(module_fd / function_name, touch=True)
+    test_output_path = test_fd / rel_path
+    io.folder(test_output_path.parent, touch=True)
+    return test_output_path
+
+
+def get_test_py_file():
+    if cython_version[0] != '3':
+        return io.file(
+            '$PYWHLOBF_DATA/test-data/wheel-0.37.1/wheel/bdist_wheel.py',
+            expandvars=True,
+            exists=True,
+        )
+    else:
+        return io.file(
+            '$PYWHLOBF_DATA/test-data/wheel-0.38.4/wheel/bdist_wheel.py',
+            expandvars=True,
+            exists=True,
+        )
+
+
+def get_test_code_fd():
+    if cython_version[0] != '3':
+        return io.folder(
+            '$PYWHLOBF_DATA/test-data/wheel-0.37.1/wheel/',
+            expandvars=True,
+            exists=True,
+        )
+    else:
+        return io.folder(
+            '$PYWHLOBF_DATA/test-data/wheel-0.38.4/wheel/',
+            expandvars=True,
+            exists=True,
+        )
+
+
+def get_test_wheel_file():
+    if cython_version[0] != '3':
+        return io.file(
+            '$PYWHLOBF_DATA/test-data/wheel-0.37.1/wheel-0.37.1-py2.py3-none-any.whl',
+            expandvars=True,
+            exists=True,
+        )
+    else:
+        return io.file(
+            '$PYWHLOBF_DATA/test-data/wheel-0.38.4/wheel-0.38.4-py3-none-any.whl',
+            expandvars=True,
+            exists=True,
+        )
---
+++ packages/pywhlobf/pywhlobf-23.1.0.dev1-py3-none-any.whl/tests/test_package_folder_processor.py
@@ -0,0 +1,58 @@
+import os
+import sys
+import subprocess
+
+from pywhlobf.component.cpp_compiler import CppCompilerConfig
+from pywhlobf.code_file_processor import CodeFileProcessorConfig
+from pywhlobf.package_folder_processor import (
+    PackageFolderProcessorConfig,
+    PackageFolderProcessor,
+)
+from tests.opt import get_test_output_fd, get_test_code_fd
+
+
+def test_package_folder_processor():
+    test_output_fd = get_test_output_fd()
+    working_fd = test_output_fd / 'working'
+    input_fd = get_test_code_fd()
+    output_fd = test_output_fd / 'output' / input_fd.name
+
+    package_folder_processor = PackageFolderProcessor(
+        PackageFolderProcessorConfig(
+            code_file_processor_config=CodeFileProcessorConfig(
+                cpp_compiler_config=CppCompilerConfig(setup_build_ext_timeout=600),
+                verbose=True,
+            ),
+        )
+    )
+    output = package_folder_processor.run(
+        input_fd=input_fd,
+        output_fd=output_fd,
+        working_fd=working_fd,
+    )
+    if output.failed_outputs:
+        for failed_output in output.failed_outputs:
+            print('!!! DEBUG !!!')
+            print(failed_output.execution_context_collection.get_logging_message())
+        assert 0
+
+    env = os.environ.copy()
+    env['PYTHONPATH'] = str(test_output_fd / 'output')
+    process = subprocess.run(
+        [
+            sys.executable,
+            '-c',
+            f'import {input_fd.name}; print(wheel.__file__)',
+        ],
+        env=env,
+        capture_output=True,
+        text=True,
+    )
+    print('-' * 40)
+    print(process.stdout)
+    print('-' * 40)
+    print(process.stderr)
+    print('-' * 40)
+
+    assert not process.stderr
+    assert process.stdout and not process.stdout.strip().endswith('.py')
