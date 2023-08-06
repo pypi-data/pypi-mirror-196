6924d67d2fd68c8b67515495524a161d1b523f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:13 +0000

    Add package custom-nester/1.0.0/custom_nester-1.0.0.tar.gz

---
+++ packages/custom-nester/custom_nester-1.0.0.tar.gz/custom_nester-1.0.0/setup.py
@@ -0,0 +1,25 @@
+#This file contains metadata about the distribution
+
+from setuptools import find_packages, setup
+
+with open("./Readme.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="custom_nester",
+    version="1.0.0",
+    py_modules      =['custom_nester'],
+    description="A simple printer of nested lists",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arielden/",
+    author="arielden",
+    author_email="arieldenaro@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
