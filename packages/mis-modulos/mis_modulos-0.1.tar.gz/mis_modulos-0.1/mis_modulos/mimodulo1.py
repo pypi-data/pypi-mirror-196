03e6f8fee733b45e529319460db1639c0294f4f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:51 +0000

    Add package mis-modulos/0.1/mis_modulos-0.1.tar.gz

---
+++ packages/mis-modulos/mis_modulos-0.1.tar.gz/mis_modulos-0.1/mis_modulos/mimodulo1.py
@@ -0,0 +1,8 @@
+import os
+
+
+def saludar(nombre):
+    print("Hola,", nombre)
+
+
+print(os.getcwd())
---
+++ packages/mis-modulos/mis_modulos-0.1.tar.gz/mis_modulos-0.1/mis_modulos/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mis_modulos",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # lista de dependencias requeridas
+    ],
+    author="Luciano Delgado",
+    description="Una descripción breve del paquete",
+    long_description="Una descripción más detallada del paquete",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
