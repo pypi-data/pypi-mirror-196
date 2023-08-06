2503b89da1a03140cb9c001d25153dd43cce74fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:25 +0000

    Add package python-commentator/0.2.0/python_commentator-0.2.0-py3-none-any.whl

---
+++ packages/python-commentator/python_commentator-0.2.0-py3-none-any.whl/commentator/bar.py
@@ -0,0 +1,8 @@
+
+def absolutely(n):
+    # If the input is negative, then it's absolute value is simply the negation of the input.
+    if n < 0:
+        return -n
+    # If the input is already non-negative, then it's absolute value is simply itself.
+    else:
+        return n
