05d26fbb1550bff5342d403b5441f1b29479eab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:40:33 +0000

    Add package berconpy/2.0.0/berconpy-2.0.0.tar.gz

---
+++ packages/berconpy/berconpy-2.0.0.tar.gz/berconpy-2.0.0/src/berconpy/utils.py
@@ -0,0 +1,34 @@
+import inspect
+from typing import Callable, Coroutine, Iterable, TypeVar
+
+EMPTY = object()
+T = TypeVar("T")
+
+CoroFunc = Callable[..., Coroutine]
+MaybeCoroFunc = CoroFunc | Callable
+
+
+def find(iterable: Iterable[T], predicate: Callable[[T], bool]) -> T | None:
+    """Returns the first item in the iterable where the given predicate returns True."""
+    for item in iterable:
+        if predicate(item):
+            return item
+
+
+def get(iterable: Iterable[T], **attrs) -> T | None:
+    """Returns the first item in the iterable that matches the given attributes."""
+
+    def predicate(item: T):
+        for attr, expected in attrs.items():
+            value = getattr(item, attr)
+            if value != expected:
+                return False
+        return True
+
+    return find(iterable, predicate)
+
+
+async def maybe_coro(func, *args, **kwargs):
+    if inspect.iscoroutinefunction(func):
+        return await func(*args, **kwargs)
+    return func(*args, **kwargs)
