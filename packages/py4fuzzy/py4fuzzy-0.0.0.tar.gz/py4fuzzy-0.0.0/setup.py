eeced8677638be223e7e13786bbc88e9893430fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:25 +0000

    Add package py4fuzzy/0.0.0/py4fuzzy-0.0.0.tar.gz

---
+++ packages/py4fuzzy/py4fuzzy-0.0.0.tar.gz/py4fuzzy-0.0.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name = "py4fuzzy",
+    author = "Harsh Gupta",
+    version='0.0.0',
+    author_email = "harshnkgupta@gmail.com",
+    description = "Fuzzy Logic",
+    
+    install_requires=[]
+    )
