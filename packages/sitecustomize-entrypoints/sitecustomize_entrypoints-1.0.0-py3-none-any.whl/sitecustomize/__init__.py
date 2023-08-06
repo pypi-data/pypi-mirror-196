2aca8e24c7d86f47c133a7eaa0ea0851c50328f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:09 +0000

    Add package sitecustomize-entrypoints/1.0.0/sitecustomize_entrypoints-1.0.0-py3-none-any.whl

---
+++ packages/sitecustomize-entrypoints/sitecustomize_entrypoints-1.0.0-py3-none-any.whl/sitecustomize/__init__.py
@@ -0,0 +1,23 @@
+"""sitecustomize."""
+__version__ = "1.0.0"  # poetry-dynamic-versioning substitutes this
+
+import warnings
+
+from ._vendor.importlib_metadata import entry_points
+
+eps = entry_points(group="sitecustomize")
+
+
+for ep in eps:
+
+    try:
+        fn = ep.load()
+    except ModuleNotFoundError:
+        fn = None
+        warnings.warn(f"Registered entrypoint {ep.name}: Module not found")
+
+    if fn and callable(fn):
+        try:
+            fn()
+        except Exception as exc:
+            warnings.warn(f"Error executing registered entrypoint {ep.name}: {exc}")
