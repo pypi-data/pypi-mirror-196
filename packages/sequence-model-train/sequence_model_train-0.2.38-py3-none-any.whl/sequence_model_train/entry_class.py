554af1283e977598b5dfa3cbb54bbfe1f5deda58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:51 +0000

    Add package sequence-model-train/0.2.38/sequence_model_train-0.2.38-py3-none-any.whl

---
+++ packages/sequence-model-train/sequence_model_train-0.2.38-py3-none-any.whl/sequence_model_train/entry_class.py
@@ -0,0 +1,108 @@
+from .utils import data_preprocess
+from .utils.train_nn import TrainNNModel
+from .models.arima_model import ARIMAModel
+
+
+class Offline:
+    def __init__(self):
+        self.model = None
+        self.model_type = None
+        self.support_models = {
+            'nn': ['lstm', 'tcn'],
+            'arma': ['arima'],
+            'tbase': ['xgb']
+        }
+        self.data_prams = {
+            'train_data_path': None,
+            'train_start_date': '2021-01-01',
+            'train_end_date': '2023-01-01',
+            'valid_start_date': '2023-01-01',
+            'valid_end_date': '2023-03-01'
+        }
+        self.model_params = {
+            'n_in': 30,
+            'n_out': 7,
+            'batch_size': 32,
+            'hidden_size': 32,
+            'num_layers': 3,
+            'input_size': 10,
+            'num_epochs': 100
+        }
+        self.result = None
+
+    def update_params(self, **kwargs):
+        self.model = kwargs.get('model', self.model)
+        self.model_type = kwargs.get('model_type', self.model_type)
+        self.model_params['n_in'] = kwargs.get(
+            'n_in',
+            self.model_params['n_in']
+        )
+        self.model_params['n_out'] = kwargs.get(
+            'n_out',
+            self.model_params['n_out']
+        )
+        self.model_params['batch_size'] = kwargs.get(
+            'batch_size',
+            self.model_params['batch_size']
+        )
+        self.model_params['hidden_size'] = kwargs.get(
+            'hidden_size',
+            self.model_params['hidden_size']
+        )
+        self.model_params['num_layers'] = kwargs.get(
+            'num_layers',
+            self.model_params['num_layers']
+        )
+        self.model_params['num_epochs'] = kwargs.get(
+            'num_epochs',
+            self.model_params['num_epochs']
+        )
+        self.data_prams['train_data_path'] = kwargs.get(
+            'train_data_path',
+            self.data_prams['train_data_path']
+        )
+        self.data_prams['train_start_date'] = kwargs.get(
+            'train_start_date',
+            self.data_prams['train_start_date']
+        )
+        self.data_prams['train_end_date'] = kwargs.get(
+            'train_end_date',
+            self.data_prams['train_end_date']
+        )
+        self.data_prams['valid_start_date'] = kwargs.get(
+            'valid_start_date',
+            self.data_prams['valid_start_date']
+        )
+        self.data_prams['valid_end_date'] = kwargs.get(
+            'valid_end_date',
+            self.data_prams['valid_end_date']
+        )
+
+    def train_model(self):
+        if self.model_type in self.support_models['nn']:
+            t_, v_, n_ = data_preprocess.get_dataloader(
+                self.data_prams['train_data_path'],
+                self.model_params['n_in'],
+                self.model_params['n_out'],
+                self.data_prams['train_start_date'],
+                self.data_prams['train_end_date'],
+                self.data_prams['valid_start_date'],
+                self.data_prams['valid_end_date'],
+                self.model_params['batch_size'])
+            params = {
+                'input_size': n_,
+                'hidden_size':  self.model_params['hidden_size'],
+                'num_layers': self.model_params['num_layers'],
+                'output_size': self.model_params['n_out'],
+                'num_epochs': self.model_params['num_epochs'],
+            }
+            model = TrainNNModel(params, self.model_type)
+        if self.model_type in self.support_models['arma']:
+            t_, v_ = data_preprocess.get_uniq_data(
+                self.data_prams['train_data_path'],
+                self.model_params['n_out'])
+            print(self.model_params['n_out'])
+            model = ARIMAModel(self.model_params['n_out'])
+        model.fit(t_, v_)
+        self.result = model.get_base_line()
+        return self.result
