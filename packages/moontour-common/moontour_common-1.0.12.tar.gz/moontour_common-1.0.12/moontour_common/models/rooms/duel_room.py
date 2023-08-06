d69c262d63dd1dfba12f0f8d97e0ab7883e7c4ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:39 +0000

    Add package moontour-common/1.0.12/moontour_common-1.0.12.tar.gz

---
+++ packages/moontour-common/moontour_common-1.0.12.tar.gz/moontour_common-1.0.12/moontour_common/models/rooms/duel_room.py
@@ -0,0 +1,16 @@
+from moontour_common.models.rooms.health_room import HealthRoom, START_HEALTH
+from moontour_common.models.rooms.room import RoomMode
+from moontour_common.models.user import User
+
+
+class DuelPlayer(User):
+    health: int = START_HEALTH
+
+
+class DuelRoom(HealthRoom):
+    @staticmethod
+    def get_mode() -> RoomMode:
+        return RoomMode.duel
+
+    player_count: int = 2
+    players: list[DuelPlayer] = []
