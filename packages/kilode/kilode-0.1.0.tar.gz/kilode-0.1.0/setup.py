84b139b500d7c9a4ee2dc37928a6da531f37d0e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:38 +0000

    Add package kilode/0.1.0/kilode-0.1.0.tar.gz

---
+++ packages/kilode/kilode-0.1.0.tar.gz/kilode-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='kilode',
+    version='0.1.0',
+    packages=['kilode'],
+    author='hrety',
+    author_email='your@email.com',
+    description='A description of your package',
+    url='https://github.com/Digita1ocean/kilode.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
