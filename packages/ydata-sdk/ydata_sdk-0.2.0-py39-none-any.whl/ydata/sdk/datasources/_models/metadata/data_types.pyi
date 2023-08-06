22b300ff989d695fd16a797ccca5db1c3dc1cef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:34 +0000

    Add package ydata-sdk/0.2.0/ydata_sdk-0.2.0-py39-none-any.whl

---
+++ packages/ydata-sdk/ydata_sdk-0.2.0-py39-none-any.whl/ydata/sdk/datasources/_models/metadata/data_types.pyi
@@ -0,0 +1,15 @@
+from ydata.core.enum import StringEnum
+
+class DataType(StringEnum):
+    NUMERICAL: str
+    CATEGORICAL: str
+    DATE: str
+    LONGTEXT: str
+
+class VariableType(StringEnum):
+    INT: str
+    FLOAT: str
+    STR: str
+    BOOL: str
+    DATETIME: str
+    DATE: str
