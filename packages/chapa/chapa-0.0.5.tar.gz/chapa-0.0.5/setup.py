64f3e7bc33498dd4c84b2496b9399a791eb39bee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:17 +0000

    Add package chapa/0.0.5/chapa-0.0.5.tar.gz

---
+++ packages/chapa/chapa-0.0.5.tar.gz/chapa-0.0.5/setup.py
@@ -0,0 +1,37 @@
+from ensurepip import version
+import setuptools
+import os
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+version = os.environ.get('CHAPA_VERSION', '0.0.5')
+
+setuptools.setup(
+    name='chapa',
+    version=version,
+    author='Temkin Mengistu (Chapi)',
+    author_email='chapimenge3@gmail.com',
+    description='Python SDK for Chapa API https://developer.chapa.co',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/chapimenge3/chapa',
+    packages=['chapa'],
+    package_dir={'chapa': 'chapa'},
+    project_urls={
+        'Source': 'https://github.com/chapimenge3/chapa',
+        'Bug Tracker': 'https://github.com/chapimenge3/chapa/issues',
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'requests',
+    ],
+)
