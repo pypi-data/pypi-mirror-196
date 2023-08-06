f8db7831595c9d3c02c8e20bba23c7705755522f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:33 +0000

    Add package Criptografia/1.0.0/Criptografia-1.0.0.tar.gz

---
+++ packages/Criptografia/Criptografia-1.0.0.tar.gz/Criptografia-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="Criptografia",
+    version="1.0.0",
+    description="Um pacote para decifrar, cifrar e encriptar mensagens usando matrizes de permutação",
+    author="Fernando Santos e Erik Oliveira",
+    author_email="fervsantos3000@gmail.com",
+    url="https://github.com/eriksoaress/Criptografia",
+    packages=["criptografia"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+         "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",  
+        "Programming Language :: Python :: 3",    
+        "Programming Language :: Python :: 3.6",    
+        "Programming Language :: Python :: 3.7",    
+        "Programming Language :: Python :: 3.8",    
+        "Programming Language :: Python :: 3.9",    
+        "Programming Language :: Python :: 3.10",    
+        "Operating System :: OS Independent"
+    ],
+)
