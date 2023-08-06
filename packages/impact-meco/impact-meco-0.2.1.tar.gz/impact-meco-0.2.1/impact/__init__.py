bea80f0e2e111ea9541ad2b393592a1a1ea618be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:42 +0000

    Add package impact-meco/0.2.1/impact-meco-0.2.1.tar.gz

---
+++ packages/impact-meco/impact-meco-0.2.1.tar.gz/impact-meco-0.2.1/impact/__init__.py
@@ -0,0 +1,11 @@
+from .mpc import MPC, Model, DotDict
+from rockit import DirectCollocation, MultipleShooting, SingleShooting, matlab_path, external_method
+import os
+
+try:
+  matlab_path = matlab_path+os.pathsep+os.path.realpath(os.path.dirname(os.path.realpath(__file__)))
+except:
+  matlab_path = "not_found"
+
+build_dir_suffix = "_build_dir"
+build_dir_prefix = ""
