f05dc289faa3c321cc178560e7c11d2799ccb335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:46 +0000

    Add package datagen-validation-protocol/0.4.4/datagen_validation_protocol-0.4.4-py3-none-any.whl

---
+++ packages/datagen-validation-protocol/datagen_validation_protocol-0.4.4-py3-none-any.whl/datagen_protocol/validation/request.py
@@ -0,0 +1,13 @@
+from typing import List, Union
+
+from pydantic import Extra
+
+from datagen_protocol.validation.humans.human import HumanDatapoint as ValidationHumanDatapoint
+from datagen_protocol.schema import request as core_request_schema, DataSequence
+
+
+class DataRequest(core_request_schema.DataRequest):
+    datapoints: List[Union[DataSequence, ValidationHumanDatapoint]]
+
+
+core_request_schema.DataRequest = DataRequest
