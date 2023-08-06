d63005fd01c25ec97bcc4c0b4c3ae854e9ee3e9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:48:09 +0000

    Add package correct-hours/1.1.0/correct-hours-1.1.0.tar.gz

---
+++ packages/correct-hours/correct-hours-1.1.0.tar.gz/correct-hours-1.1.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['correct_hours',
+ 'correct_hours.report_processors',
+ 'correct_hours.report_processors.xero']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['openpyxl>=3.0.10,<4.0.0']
+
+setup_kwargs = {
+    'name': 'correct-hours',
+    'version': '1.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Jose Orozco',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
