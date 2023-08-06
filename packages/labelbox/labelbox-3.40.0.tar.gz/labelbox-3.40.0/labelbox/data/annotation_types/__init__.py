8faa717275bb1cf8b5447cdf297ead3f3c67ae3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:01 +0000

    Add package labelbox/3.40.0/labelbox-3.40.0.tar.gz

---
+++ packages/labelbox/labelbox-3.40.0.tar.gz/labelbox-3.40.0/labelbox/data/annotation_types/__init__.py
@@ -0,0 +1,41 @@
+from .geometry import Line
+from .geometry import Point
+from .geometry import Mask
+from .geometry import Polygon
+from .geometry import Rectangle
+from .geometry import Geometry
+
+from .annotation import ClassificationAnnotation
+from .annotation import VideoClassificationAnnotation
+from .annotation import ObjectAnnotation
+from .annotation import VideoObjectAnnotation
+
+from .ner import TextEntity
+
+from .classification import Checklist
+from .classification import ClassificationAnswer
+from .classification import Dropdown
+from .classification import Radio
+from .classification import Text
+
+from .data import ImageData
+from .data import MaskData
+from .data import TextData
+from .data import VideoData
+
+from .label import Label
+from .collection import LabelList
+from .collection import LabelGenerator
+
+from .metrics import ScalarMetric
+from .metrics import ScalarMetricAggregation
+from .metrics import ConfusionMatrixMetric
+from .metrics import ConfusionMatrixAggregation
+from .metrics import ScalarMetricValue
+from .metrics import ConfusionMatrixMetricValue
+
+from .data.tiled_image import EPSG
+from .data.tiled_image import EPSGTransformer
+from .data.tiled_image import TiledBounds
+from .data.tiled_image import TiledImageData
+from .data.tiled_image import TileLayer
