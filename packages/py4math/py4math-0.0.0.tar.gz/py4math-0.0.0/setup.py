3000c661532a3f9e9f9e1170e29202614611483c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:32 +0000

    Add package py4math/0.0.0/py4math-0.0.0.tar.gz

---
+++ packages/py4math/py4math-0.0.0.tar.gz/py4math-0.0.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name = "py4math",
+    author = "Harsh Gupta",
+    version='0.0.0',
+    author_email = "harshnkgupta@gmail.com",
+    description = "math",
+    
+    install_requires=[]
+    )
