67703bb242c544295aad65063b27fb646458fa43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:30 +0000

    Add package ilpy/0.2.1/ilpy-0.2.1.tar.gz

---
+++ packages/ilpy/ilpy-0.2.1.tar.gz/ilpy-0.2.1/ilpy/__init__.py
@@ -0,0 +1,26 @@
+from .wrapper import (
+    LinearConstraint,
+    LinearConstraints,
+    LinearObjective,
+    LinearSolver,
+    Preference,
+    QuadraticObjective,
+    Relation,
+    Sense,
+    Solution,
+    VariableType,
+)
+
+__version__ = "0.2.1"
+__all__ = [
+    "LinearConstraint",
+    "LinearConstraints",
+    "LinearObjective",
+    "LinearSolver",
+    "Preference",
+    "QuadraticObjective",
+    "Relation",
+    "Sense",
+    "Solution",
+    "VariableType",
+]
