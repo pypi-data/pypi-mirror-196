205c1fc008cf5e3a42740785c90322fe18775a15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:33 +0000

    Add package st-cytoscape/0.0.5/st_cytoscape-0.0.5-py3-none-any.whl

---
+++ packages/st-cytoscape/st_cytoscape-0.0.5-py3-none-any.whl/st_cytoscape/frontend/build/precache-manifest.5e2117b64c6bd0d320b69481c38405b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab12e510619f238c0d2265a55ea2359",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ab85bdd6537053f15e24",
+    "url": "./static/js/2.1c93493d.chunk.js"
+  },
+  {
+    "revision": "12ecc5f2f0d11f924dde9479673b3a5b",
+    "url": "./static/js/2.1c93493d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0319db4fb92df6845d4c",
+    "url": "./static/js/main.8125c52f.chunk.js"
+  },
+  {
+    "revision": "4f647358ff6c1f15178c",
+    "url": "./static/js/runtime-main.db64ee98.js"
+  }
+]);
---
+++ packages/st-cytoscape/st_cytoscape-0.0.5-py3-none-any.whl/st_cytoscape/frontend/build/precache-manifest.6566f95ef1b49eb1c661d972e2c3ce94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b682773d5149f169c79d9fa807ece697",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0174b1025930c257f366",
+    "url": "./static/js/2.b5e4971c.chunk.js"
+  },
+  {
+    "revision": "12ecc5f2f0d11f924dde9479673b3a5b",
+    "url": "./static/js/2.b5e4971c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4702a164978ca17d7517",
+    "url": "./static/js/main.25e00619.chunk.js"
+  },
+  {
+    "revision": "4f647358ff6c1f15178c",
+    "url": "./static/js/runtime-main.db64ee98.js"
+  }
+]);
