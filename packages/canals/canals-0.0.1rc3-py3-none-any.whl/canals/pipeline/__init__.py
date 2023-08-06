b8cf4f03ed2d20c0de43ca3847d65347c5c0d91a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:19 +0000

    Add package canals/0.0.1rc3/canals-0.0.1rc3-py3-none-any.whl

---
+++ packages/canals/canals-0.0.1rc3-py3-none-any.whl/canals/pipeline/__init__.py
@@ -0,0 +1,12 @@
+from canals.pipeline.pipeline import (
+    Pipeline,
+    PipelineError,
+    PipelineRuntimeError,
+    PipelineValidationError,
+)
+from canals.pipeline.save_load import (
+    save_pipelines,
+    load_pipelines,
+    marshal_pipelines,
+    unmarshal_pipelines,
+)
