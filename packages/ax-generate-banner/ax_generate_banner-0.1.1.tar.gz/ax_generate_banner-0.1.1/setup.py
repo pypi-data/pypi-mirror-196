196d8b872bdee9d00e6c7eae21499f8ab789d3bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:48 +0000

    Add package ax-generate-banner/0.1.1/ax_generate_banner-0.1.1.tar.gz

---
+++ packages/ax-generate-banner/ax_generate_banner-0.1.1.tar.gz/ax_generate_banner-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+with open('core/requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ax_generate_banner',
+    version='0.1.1',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Baxromov',
+    author_email='baxromov.shahzodbek@gmail.com',
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ]
+)
