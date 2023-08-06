b6efbc808eedac8870f7433bede0b2c503d6dd9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:46 +0000

    Add package gasify/1.3.1/gasify-1.3.1-py3-none-any.whl

---
+++ packages/gasify/gasify-1.3.1-py3-none-any.whl/gasify/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '1.3.1'
+
+__author__ = 'Chris Harrison'
+__credits__ = [__author__]
+
+__maintainer__ = 'Chris Harrison'
+__email__ = '629204+ravngr@users.noreply.github.com'
+
+__status__ = 'Development'
