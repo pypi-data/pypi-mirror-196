9db572a0e9bf41f03d840c9e471a57d625f28add
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:35 +0000

    Add package meshemy/1.0.1/meshemy-1.0.1-py3-none-any.whl

---
+++ packages/meshemy/meshemy-1.0.1-py3-none-any.whl/meshemy/utility/exception.py
@@ -0,0 +1,40 @@
+class BlenderNotInstalledError(ModuleNotFoundError):
+    pass
+
+
+class Open3DNotInstalledError(ModuleNotFoundError):
+    pass
+
+
+class PyMeshNotInstalledError(ModuleNotFoundError):
+    pass
+
+
+def blender_module_not_installed_error(e):
+    raise BlenderNotInstalledError(
+        """
+        Blender module, bpy, must be installed.
+        
+        Please run `pip install meshemy[blender]`
+        """
+    ) from e
+
+
+def open3d_module_not_installed_error(e):
+    raise Open3DNotInstalledError(
+        """
+        Open3D module must be installed.
+
+        Please run `pip install meshemy[open3d]`
+        """
+    ) from e
+
+
+def pymesh_module_not_installed_error(e):
+    raise PyMeshNotInstalledError(
+        """
+        PyMesh module must be installed.
+
+        Please run `pip install meshemy[pymesh]`
+        """
+    ) from e
