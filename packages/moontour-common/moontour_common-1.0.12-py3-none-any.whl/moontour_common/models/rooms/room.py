294b32416e0f65e7567277c1e7cf9cecbf9db59c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:39 +0000

    Add package moontour-common/1.0.12/moontour_common-1.0.12-py3-none-any.whl

---
+++ packages/moontour-common/moontour_common-1.0.12-py3-none-any.whl/moontour_common/models/rooms/room.py
@@ -0,0 +1,39 @@
+import uuid
+from abc import ABC, abstractmethod
+from datetime import datetime
+from enum import StrEnum
+
+from pydantic import BaseModel, Field
+
+
+class RoomMode(StrEnum):
+    streak = 'streak'
+    duel = 'duel'
+    teams = 'teams'
+
+
+class RoomStatus(StrEnum):
+    waiting = 'waiting'
+    waiting_to_start = 'waitingToStart'
+    running = 'running'
+
+
+class BaseRoom(BaseModel, ABC):
+    id: str = Field(alias='id_', default_factory=lambda: str(uuid.uuid4()))
+    map: str = 'world'
+    status: RoomStatus = RoomStatus.waiting
+    create_time: datetime = Field(default_factory=datetime.now)
+    start_time: datetime | None = None
+
+    def __init__(self, **data):
+        if 'mode' not in data:
+            data['mode'] = self.get_mode()
+        super().__init__(**data)
+
+    @staticmethod
+    @abstractmethod
+    def get_mode() -> RoomMode:
+        pass
+
+    class Config:
+        allow_population_by_field_name = True
