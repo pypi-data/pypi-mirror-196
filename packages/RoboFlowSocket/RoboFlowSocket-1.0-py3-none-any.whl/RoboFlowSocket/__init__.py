d61b0a3a721ae9fa000aee590920b382a9919bd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:43 +0000

    Add package RoboFlowSocket/1.0/RoboFlowSocket-1.0-py3-none-any.whl

---
+++ packages/RoboFlowSocket/RoboFlowSocket-1.0-py3-none-any.whl/RoboFlowSocket/__init__.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+
+from __future__ import absolute_import
+import datetime
+
+from RoboFlowSocket.RoboFlowSocketSDK import RoboFlowSocket
+
+__all__ = [
+    "RoboFlowSocket",
+]
+
+__version__ = "1.0"
+__author__ = "Elephantrobotics"
+__email__ = "weiquan.xu@elephantrobotics.com"
+__git_url__ = "https://github.com/elephantrobotics/pymycobot"
+__copyright__ = "CopyRight (c) 2023-{0} Shenzhen Elephantrobotics technology".format(
+    datetime.datetime.now().year
+)
