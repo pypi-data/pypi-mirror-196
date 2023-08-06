463f0cd83d16dbbc6eed03578754fe178c49b428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:23 +0000

    Add package basedmypy/1.7.0/basedmypy-1.7.0-cp38-cp38-musllinux_1_1_x86_64.whl

---
+++ packages/basedmypy/basedmypy-1.7.0-cp38-cp38-musllinux_1_1_x86_64.whl/mypy/version.py
@@ -0,0 +1,4 @@
+from mypy.versionutil import VersionInfo
+__version__ = "1.1.1"
+based_version_info = VersionInfo(major=1, minor=7, patch=0, release_level='final', iteration=0, mypy_version='1.1.1', mypy_release_level='final')
+__based_version__ = "1.7.0"
