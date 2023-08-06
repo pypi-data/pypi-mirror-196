781eded129c53d03a7644ec3da3c89fbaf368d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:38 +0000

    Add package iosense-connect/0.0.1/iosense-connect-0.0.1.tar.gz

---
+++ packages/iosense-connect/iosense-connect-0.0.1.tar.gz/iosense-connect-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name = "iosense-connect",
+    version = "0.0.1",
+    author = "Faclon-Labs",
+    description = "iosense connect library",
+    packages = ["iosense-connect"]
+)
