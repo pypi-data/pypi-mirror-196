604c63cdf74091f4d88fd7b51df0e285b8f6ee7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:54 +0000

    Add package fastdem/0.1.0/fastdem-0.1.0.tar.gz

---
+++ packages/fastdem/fastdem-0.1.0.tar.gz/fastdem-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fastdem']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['typer[all]>=0.7.0,<0.8.0']
+
+entry_points = \
+{'console_scripts': ['fastdem = fastdem.main:app']}
+
+setup_kwargs = {
+    'name': 'fastdem',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '',
+    'author': 'wanqiang.liu',
+    'author_email': 'wanqiang.liu@freetech.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
