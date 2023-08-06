93c98ec7bc5ae831642bda9cb9eb1792903cc80d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:37 +0000

    Add package imgyaso/2023.1.27.3/imgyaso-2023.1.27.3-py3-none-any.whl

---
+++ packages/imgyaso/imgyaso-2023.1.27.3-py3-none-any.whl/imgyaso/trunc.py
@@ -0,0 +1,39 @@
+import sys
+import cv2
+import numpy as np
+from .util import *
+
+def trunc_bts(img, l=4):
+    if not is_img_data(img): return img
+    img = conv2png(img)
+    img = np.frombuffer(img, np.uint8)
+    img = cv2.imdecode(img, cv2.IMREAD_GRAYSCALE)
+    if img is None: return None
+    img = trunc(img, l).astype(np.uint8)
+    img = cv2.imencode(
+        '.png', img, 
+        [cv2.IMWRITE_PNG_COMPRESSION, 9]
+    )[1]
+    return bytes(img)
+
+
+def trunc(img, l=4):
+    assert img.ndim == 2
+    
+    colors = np.linspace(0, 255, l).astype(int)
+    
+    img_3d = np.expand_dims(img, 2)
+    dist = np.abs(img_3d - colors)
+    idx = np.argmin(dist, axis=2)
+    img = colors[idx]
+    
+    return img
+
+def main():
+    fname = sys.argv[1]
+    img = open(fname, 'rb').read()
+    img = trunc_bts(img)
+    with open(fname, 'wb') as f:
+        f.write(img)
+    
+if __name__ == '__main__': main()
