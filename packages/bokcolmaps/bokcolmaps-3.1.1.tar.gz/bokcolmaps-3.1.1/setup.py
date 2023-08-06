cd45b50fc5a87f3b1e8ee61f86866cdd6e47e90d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:46 +0000

    Add package bokcolmaps/3.1.1/bokcolmaps-3.1.1.tar.gz

---
+++ packages/bokcolmaps/bokcolmaps-3.1.1.tar.gz/bokcolmaps-3.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(name='bokcolmaps',
+      version='3.1.1',
+      description='Colour map plots based on the Bokeh visualisation library',
+      author='Systems Engineering & Assessment Ltd.',
+      author_email='Marcus.Donnelly@sea.co.uk',
+      url='https://bitbucket.org/sea_dev/bokcolmaps',
+      license='MIT',
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Intended Audience :: Science/Research',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python :: 3',
+                   'Topic :: Scientific/Engineering'
+                   ],
+      keywords=['Bokeh',
+                '2D Plot',
+                '3D Plot'
+                ],
+      packages=find_packages(),
+      install_requires=['numpy >= 1.20',
+                        'bokeh >= 3'
+                        ],
+      include_package_data=True,
+      )
