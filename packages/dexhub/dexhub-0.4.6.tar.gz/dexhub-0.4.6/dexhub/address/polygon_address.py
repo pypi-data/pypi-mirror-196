3dcaf5c378583e1557942dbbb3f83521b06588e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:59 +0000

    Add package dexhub/0.4.6/dexhub-0.4.6.tar.gz

---
+++ packages/dexhub/dexhub-0.4.6.tar.gz/dexhub-0.4.6/dexhub/address/polygon_address.py
@@ -0,0 +1,6 @@
+from web3 import Web3
+
+class AddressUniswapV3Polygon:
+    route_v2=Web3.toChecksumAddress('0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45')
+    factory=Web3.toChecksumAddress('0x1F98431c8aD98523631AE4a59f267346ea31F984')
+    quoter_v2=Web3.toChecksumAddress('0x61fFE014bA17989E743c5F6cB21bF9697530B21e')
