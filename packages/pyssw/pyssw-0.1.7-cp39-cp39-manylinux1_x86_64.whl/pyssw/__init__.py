03b1365aabbed85f62b5f759f659e2c162cbb469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:07 +0000

    Add package pyssw/0.1.7/pyssw-0.1.7-cp39-cp39-manylinux1_x86_64.whl

---
+++ packages/pyssw/pyssw-0.1.7-cp39-cp39-manylinux1_x86_64.whl/pyssw/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+"""
+* The python wrapper 'ssw_wrapp" allow to perform high performance pairwise DNA alignment and return a simple python object describing the alignement.
+* C sources ssw.c and ssw.h were forked and modified from Mengyao's [Complete-Striped-Smith-Waterman-Library](https://github.com/mengyao/Complete-Striped-Smith-Waterman-Library)
+@copyright  [GNU General Public License v2](http://www.gnu.org/licenses/gpl-2.0.html)
+"""
+
+__version__ = "0.1.7"
+
