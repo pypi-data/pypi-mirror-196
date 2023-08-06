620de9872e44637b810b82d55b97468d48f68e73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:41 +0000

    Add package enso-nic/0.1.2/enso-nic-0.1.2.tar.gz

---
+++ packages/enso-nic/enso-nic-0.1.2.tar.gz/enso-nic-0.1.2/enso/consts.py
@@ -0,0 +1,15 @@
+DEFAULT_FPGA = "1-12"
+DEFAULT_DSC_BUF_SIZE = 16384
+DEFAULT_PKT_BUF_SIZE = 32768
+DEFAULT_NB_TX_CREDITS = 500
+DEFAULT_ETH_PORT = 1  # Can be 0 or 1.
+DEFAULT_NB_FALLBACK_QUEUES = 0
+DEFAULT_BATCH_SIZE = 64
+
+# The following paths are relative to the enso path.
+SETUP_SW_CMD = "setup.sh"
+ENSO_PKTGEN_CMD = "software/build/examples/pktgen"
+PCAPS_DIR = "frontend/pcaps/"
+PCAP_GEN_CMD = "hardware/input_gen/generate_synthetic_trace.py"
+
+FPGA_RATELIMIT_CLOCK = 200e6  # Hz
