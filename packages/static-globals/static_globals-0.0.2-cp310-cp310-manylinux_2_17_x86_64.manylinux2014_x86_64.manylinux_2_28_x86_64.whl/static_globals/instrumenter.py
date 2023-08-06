a4f98b13153ba928f2818327d797bd6c9bea53e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:19 +0000

    Add package static-globals/0.0.2/static_globals-0.0.2-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/static-globals/static_globals-0.0.2-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64.whl/static_globals/instrumenter.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+from dataclasses import replace
+from functools import cache
+from pathlib import Path
+
+from diopter.compiler import ClangTool, ClangToolMode, CompilerExe, ProgramType
+
+
+@cache
+def get_instrumenter(
+    instrumenter: ClangTool | None = None, clang: CompilerExe | None = None
+) -> ClangTool:
+    if not instrumenter:
+        if not clang:
+            # TODO: move this to diopter
+            try:
+                clang = CompilerExe.get_system_clang()
+            except:  # noqa: E722
+                pass
+            if not clang:
+                try:
+                    clang = CompilerExe.from_path(Path("clang-15"))
+                except:  # noqa: E722
+                    pass
+            if not clang:
+                clang = CompilerExe.from_path(Path("clang-14"))
+
+        instrumenter = ClangTool.init_with_paths_from_clang(
+            Path(__file__).parent / "make-globals-static", clang
+        )
+    return instrumenter
+
+
+def annotate_with_static(
+    program: ProgramType,
+    instrumenter: ClangTool | None = None,
+    clang: CompilerExe | None = None,
+) -> ProgramType:
+    """Turn all globals in the given file into static globals.
+
+    Args:
+        file (Path): Path to code file to be instrumented.
+        flags (list[str]): list of user provided clang flags
+        instrumenter (Path): Path to the instrumenter executable., if not
+                             provided will use what's specified in
+        clang (Path): Path to the clang executable.
+    Returns:
+        None:
+    """
+
+    instrumenter_resolved = get_instrumenter(instrumenter, clang)
+
+    result = instrumenter_resolved.run_on_program(
+        program, [], ClangToolMode.READ_MODIFIED_FILE
+    )
+    assert result.modified_source_code
+
+    return replace(program, code=result.modified_source_code)
