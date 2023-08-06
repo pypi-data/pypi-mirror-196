092f6ae4ff1f090e52b4f2f7417b6f4306519ac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:34 +0000

    Add package py-fumen-util/0.0.1/py_fumen_util-0.0.1.tar.gz

---
+++ packages/py-fumen-util/py_fumen_util-0.0.1.tar.gz/py_fumen_util-0.0.1/src/py_fumen_util/fumen_combiner.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+from py_fumen import encoder, decoder
+
+def fumen_combiner(fumen_codes):
+    combined_fumen = []
+    for code in fumen_codes:
+        combined_fumen += decoder.decode(code)
+    return encoder.encode(combined_fumen)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        print(fumen_combiner(' '.join(sys.argv[1:]).split(' ')))
