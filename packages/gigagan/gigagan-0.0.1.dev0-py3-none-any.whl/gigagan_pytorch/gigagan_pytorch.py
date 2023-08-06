e3f6a8e3dd4ce9910e86ca057f0578bcb3d69c2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:40:41 +0000

    Add package gigagan/0.0.1.dev0/gigagan-0.0.1.dev0-py3-none-any.whl

---
+++ packages/gigagan/gigagan-0.0.1.dev0-py3-none-any.whl/gigagan_pytorch/gigagan_pytorch.py
@@ -0,0 +1,188 @@
+import torch
+import torch.nn.functional as F
+from torch import nn, einsum
+
+from einops import rearrange, pack, unpack, repeat, reduce
+
+# helpers
+
+def exists(val):
+    return val is not None
+
+# activation functions
+
+def leaky_relu(neg_slope = 0.1):
+    return nn.LeakyReLU(neg_slope)
+
+# adaptive conv
+# the main novelty of the paper - they propose to learn a softmax weighted sum of N convolutional kernels, depending on the text embedding
+
+def get_same_padding(size, kernel, dilation, stride):
+    return ((size - 1) * (stride - 1) + dilation * (kernel - 1)) // 2
+
+class AdaptiveConv2DMod(nn.Module):
+    def __init__(
+        self,
+        dim,
+        dim_out,
+        dim_embed,
+        kernel,
+        demod = True,
+        stride = 1,
+        dilation = 1,
+        eps = 1e-8,
+        num_conv_kernels = 1 # set this to be greater than 1 for adaptive
+    ):
+        super().__init__()
+        self.eps = eps
+
+        self.dim_out = dim_out
+
+        self.kernel = kernel
+        self.stride = stride
+        self.dilation = dilation
+        self.adaptive = num_conv_kernels > 1
+
+        self.to_mod = nn.Linear(dim_embed, dim)
+        self.to_adaptive_weight = nn.Linear(dim_embed, num_conv_kernels) if self.adaptive else None
+
+        self.weights = nn.Parameter(torch.randn((num_conv_kernels, dim_out, dim, kernel, kernel)))
+
+        self.demod = demod
+
+        nn.init.kaiming_normal_(self.weights, a = 0, mode = 'fan_in', nonlinearity = 'leaky_relu')
+
+    def forward(self, fmap, embed):
+        """
+        notation
+
+        b - batch
+        n - convs
+        o - output
+        i - input
+        k - kernel
+        """
+
+        b, h = fmap.shape[0], fmap.shape[-2]
+
+        weights = self.weights
+
+        if self.adaptive:
+            weights = repeat(weights, '... -> b ...', b = b)
+
+            # determine an adaptive weight and 'select' the kernel to use with softmax
+
+            selections = self.to_adaptive_weight(embed).softmax(dim = -1)
+            selections = rearrange(selections, 'b n -> b n 1 1 1 1')
+
+            weights = reduce(weights * selections, 'b n ... -> b ...', 'sum')
+
+        # do the modulation, demodulation, as done in stylegan2
+
+        mod = self.to_mod(embed)
+
+        mod = rearrange(mod, 'b i -> b 1 i 1 1')
+
+        weights = weights * (mod + 1)
+
+        if self.demod:
+            inv_norm = reduce(weights ** 2, 'b o i k1 k2 -> b o 1 1 1', 'sum').clamp(min = self.eps).rsqrt()
+            weights = weights * inv_norm
+
+        fmap = rearrange(fmap, 'b c h w -> 1 (b c) h w')
+
+        weights = rearrange(weights, 'b o ... -> (b o) ...')
+
+        padding = get_same_padding(h, self.kernel, self.dilation, self.stride)
+        fmap = F.conv2d(fmap, weights, padding = padding, groups = b)
+
+        return rearrange(fmap, '1 (b o) ... -> b o ...', b = b)
+
+# attention
+# they use an attention with a better Lipchitz constant - l2 distance similarity instead of dot product - shown in vitgan to be more stable
+
+class Attention(nn.Module):
+    def __init__(
+        self,
+        dim,
+        dim_head = 64,
+        heads = 8
+    ):
+        super().__init__()
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+        dim_inner = dim_head * heads
+
+        self.to_qkv = nn.Conv2d(dim, dim_inner * 3, 1, bias = False)
+        self.to_out = nn.Conv2d(dim_inner, dim, 1, bias = False)
+
+    def forward(self, fmap):
+        """
+        einstein notation
+
+        b - batch
+        h - heads
+        x - height
+        y - width
+        d - dimension
+        i - source seq (attend from)
+        j - target seq (attend to)
+        """
+
+        x, y = fmap.shape[-2:]
+
+        h = self.heads
+
+        q, k, v = self.to_qkv(fmap).chunk(3, dim = 1)
+        q, k, v = map(lambda t: rearrange(t, 'b (h d) x y -> (b h) (x y) d', h = self.heads), (q, k, v))
+
+        sim = -torch.cdist(q, k, p = 2) * self.scale # l2 distance
+
+        attn = sim.softmax(dim = -1)
+
+        out = einsum('b i j, b j d -> b i d', attn, v)
+
+        out = rearrange(out, '(b h) (x y) d -> b (h d) x y', x = x, y = y, h = h)
+
+        return self.to_out(out)
+
+# style mapping network
+
+class StyleNetwork(nn.Module):
+    def __init__(
+        self,
+        dim,
+        depth,
+        dim_text_latent = 0,
+        frac_gradient = 0.1  # in the stylegan2 paper, they control the learning rate by multiplying the parameters by a constant, but we can use another trick here from attention literature
+    ):
+        super().__init__()
+
+        layers = []
+        for i in range(depth):
+            layers.extend([nn.Linear(dim + dim_text_latent, dim), leaky_relu()])
+
+        self.net = nn.Sequential(*layers)
+        self.frac_gradient = frac_gradient
+        self.dim_text_latent = dim_text_latent
+
+    def forward(self, x, text_latent = None):
+        grad_frac = self.frac_gradient
+
+        if self.dim_text_latent:
+            assert exists(text_latent)
+            x = torch.cat((x, text_latent), dim = -1)
+
+        x = F.normalize(x, dim = 1)
+        out = self.net(x)
+
+        return out * grad_frac + (1 - grad_frac) * out.detach()
+
+# gan
+
+class GigaGAN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return x
