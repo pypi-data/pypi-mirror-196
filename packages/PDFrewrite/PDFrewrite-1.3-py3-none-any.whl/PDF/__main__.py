840f93423244d9f3b06c2ce4ff100608b0284372
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:30 +0000

    Add package PDFrewrite/1.3/PDFrewrite-1.3-py3-none-any.whl

---
+++ packages/PDFrewrite/PDFrewrite-1.3-py3-none-any.whl/PDF/__main__.py
@@ -0,0 +1,23 @@
+from PyPDF4 import PdfFileReader, PdfFileWriter
+
+
+def pdfrewrite(path_in, path_out):
+    reader = PdfFileReader(open(path_in, 'rb'))
+    writer = PdfFileWriter()
+    pages = len(reader.pages)
+
+    for i in range(pages):
+        page = reader.pages[i]
+        # if "/XObject" in page["/Resources"].keys() or "/Font" in page["/Resources"].keys():
+        #     writer.addPage(page)
+        # /XObject图像，/Font文字
+        if "/XObject" in page["/Resources"].keys():
+            writer.addPage(page)
+
+    writer.write(open(path_out, 'wb'))
+
+
+if __name__ == '__main__':
+    path_in = input(r"input:")
+    path_out = input(r"output:")
+    pdfrewrite(path_in, path_out)
