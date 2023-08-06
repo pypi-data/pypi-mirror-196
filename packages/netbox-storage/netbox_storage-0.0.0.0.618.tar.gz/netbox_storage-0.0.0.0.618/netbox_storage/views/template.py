2621e15c258a2a3b5b4e200beb17c2d18edd9fa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:50 +0000

    Add package netbox-storage/0.0.0.0.618/netbox_storage-0.0.0.0.618.tar.gz

---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.618.tar.gz/netbox_storage-0.0.0.0.618/netbox_storage/views/template.py
@@ -0,0 +1,23 @@
+from netbox.views import generic
+
+from netbox_storage.forms.template import LVMTemplateForm, DriveTemplateForm, \
+    PartitionTemplateForm
+from netbox_storage.models import TemplateConfigurationDrive, Partition
+
+
+class LVMAddTemplateView(generic.ObjectEditView):
+    """View for editing a Drive instance."""
+
+    queryset = TemplateConfigurationDrive.objects.all()
+    form = LVMTemplateForm
+#    default_return_url = "plugins:netbox_storage:drive_list"
+
+
+class AddTemplateDriveView(generic.ObjectEditView):
+    queryset = TemplateConfigurationDrive.objects.all()
+    form = DriveTemplateForm
+
+
+class AddTemplatePartitionView(generic.ObjectEditView):
+    queryset = Partition.objects.all()
+    form = PartitionTemplateForm
