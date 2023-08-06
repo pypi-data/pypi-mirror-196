e62e026aa4c8a7ba9b1e14149d2ae5ed766e092d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:37 +0000

    Add package manageconfig/1.1.1/manageconfig-1.1.1.tar.gz

---
+++ packages/manageconfig/manageconfig-1.1.1.tar.gz/manageconfig-1.1.1/tests/test_yml.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+from manageconfig import Config
+
+YAML_FILE = Path(Path(__file__).parent, 'config.yml')
+
+
+def test_sanity():
+    conf = Config.load_from_yml(YAML_FILE)
+
+    assert conf.key == 'value'
+    assert conf.mysqldatabase.hostname == 'localhost'
+    assert type(conf.mysqldatabase.port) == int
+    assert not conf.booleanValue
