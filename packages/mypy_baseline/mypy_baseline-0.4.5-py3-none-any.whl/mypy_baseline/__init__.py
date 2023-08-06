0f22a1ee0436e80009c3d469b623a16bb769030a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:48 +0000

    Add package mypy_baseline/0.4.5/mypy_baseline-0.4.5-py3-none-any.whl

---
+++ packages/mypy_baseline/mypy_baseline-0.4.5-py3-none-any.whl/mypy_baseline/__init__.py
@@ -0,0 +1,8 @@
+"""Integrate mypy with existing codebase.
+"""
+
+from ._cli import entrypoint, main
+
+
+__version__ = '0.4.5'
+__all__ = ['entrypoint', 'main']
---
+++ packages/mypy_baseline/mypy_baseline-0.4.5-py3-none-any.whl/mypy_baseline/_error.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+import re
+from dataclasses import dataclass
+from functools import cached_property
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from ._config import Config
+
+COLOR_PATTERN = '(\x1b\\[\\d*m?|\x0f)*'
+REX_COLOR = re.compile(COLOR_PATTERN)
+REX_LINE = re.compile(rf"""
+    (?P<path>.+\.py):
+    (?P<lineno>[0-9]+):\s
+    {COLOR_PATTERN}(?P<severity>[a-z]+):{COLOR_PATTERN}\s
+    (?P<message>.+?)
+    (?:\s\s{COLOR_PATTERN}\[(?P<category>[a-z-]+)\]{COLOR_PATTERN})?
+    \s*
+""", re.VERBOSE | re.MULTILINE)
+REX_LINE_IN_MSG = re.compile(r'defined on line \d+')
+
+
+@dataclass
+class Error:
+    raw_line: str
+    _match: re.Match[str]
+
+    @classmethod
+    def new(self, line: str) -> Error | None:
+        match = REX_LINE.fullmatch(line)
+        if match is None:
+            return None
+        return Error(line, match)
+
+    @cached_property
+    def path(self) -> Path:
+        return Path(self._match.group('path'))
+
+    @cached_property
+    def line_number(self) -> int:
+        result = int(self._match.group('lineno'))
+        assert result >= 0
+        return result
+
+    @cached_property
+    def severity(self) -> str:
+        return self._match.group('severity')
+
+    @cached_property
+    def message(self) -> str:
+        return self._match.group('message')
+
+    @cached_property
+    def category(self) -> str:
+        return self._match.group('category') or 'note'
+
+    def get_clean_line(self, config: Config) -> str:
+        path = Path(*self.path.parts[:config.depth])
+        pos = self.line_number if config.preserve_position else 0
+        msg = REX_COLOR.sub('', self.message).strip()
+        msg = REX_LINE_IN_MSG.sub('defined on line 0', msg)
+        line = f'{path}:{pos}: {self.severity}: {msg}'
+        if self.category != 'note':
+            line += f'  [{self.category}]'
+        return line
