4a228da2b17babfbf3aaba9458d2d9321e494cd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:57 +0000

    Add package packageA/4.0.0/packageA-4.0.0.tar.gz

---
+++ packages/packageA/packageA-4.0.0.tar.gz/packageA-4.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='packageA',
+    version='4.0.0',
+    packages=['compiled_packages'],
+    install_requires=[],
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A short description of your package',
+    url='https://github.com/username/packageA',
+)
