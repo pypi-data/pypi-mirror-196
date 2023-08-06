cf0ab8e06d94d15728a53e646f0d2ecc6b09ad9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:58 +0000

    Add package devcontainer-contrib/0.0.27b12/devcontainer_contrib-0.0.27b12-py3-none-any.whl

---
+++ packages/devcontainer-contrib/devcontainer_contrib-0.0.27b12-py3-none-any.whl/devcontainer_contrib/cli/feature.py
@@ -0,0 +1,73 @@
+import json
+import logging
+import pathlib
+from typing import List, Optional
+
+import typer
+
+from devcontainer_contrib.cli.download.download_feature import download_feature
+from devcontainer_contrib.cli.inspect.extract_devcontainer_feature_dict import (
+    extract_devcontainer_feature_dict,
+)
+from devcontainer_contrib.cli.install.install_feature import install_feature
+
+logger = logging.getLogger(__name__)
+
+app = typer.Typer(pretty_exceptions_show_locals=False, pretty_exceptions_short=False)
+
+app.command()
+
+
+@app.command("generate")
+def generate_command(
+    feature_definition: pathlib.Path,
+    output_dir: pathlib.Path,
+) -> None:
+    try:
+        from devcontainer_contrib.cli.generate.generate_feature import generate
+    except ImportError as e:
+        logger.error(
+            "Some imports required for feature generation are missing.\nMake sure you have included the generate extras during installation.\n eg. 'pip install devcontainer-contrib[generate]'"
+        )
+        raise typer.Exit(code=1) from e
+
+    generate(feature_definition.as_posix(), output_dir.as_posix())
+
+
+def _validate_args(value: Optional[List[str]]):
+    if value is not None:
+        for arg in value:
+            splitted_arg = arg.split("=")
+            if len(splitted_arg) != 2 or len(splitted_arg[0]) == 0:
+                raise typer.BadParameter("Must be formatted as 'key=value'")
+    return value
+
+
+@app.command("download")
+def download_command(
+    feature: str,
+) -> None:
+    download_location = download_feature(feature)
+    print(f"feature downloaded to {download_location}")
+
+
+@app.command("inspect")
+def inspect_command(
+    feature: str,
+) -> None:
+    devcontainer_feature_dict = extract_devcontainer_feature_dict(feature)
+    print(json.dumps(devcontainer_feature_dict, indent=4))
+
+
+@app.command("install")
+def install_command(
+    feature: str,
+    option: Optional[List[str]] = typer.Option(None, callback=_validate_args),
+    verbose: bool = False
+) -> None:
+    if option is None:
+        option = []
+    options_dict = {
+        argument.split("=")[0]: argument.split("=")[1] for argument in option
+    }
+    install_feature(feature=feature, options=options_dict, verbose=verbose)
