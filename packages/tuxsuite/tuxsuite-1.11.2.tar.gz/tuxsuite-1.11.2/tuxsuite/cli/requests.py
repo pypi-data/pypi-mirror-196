99fa877f14c6819d235e63d38f26059161bc5f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:15 +0000

    Add package tuxsuite/1.11.2/tuxsuite-1.11.2.tar.gz

---
+++ packages/tuxsuite/tuxsuite-1.11.2.tar.gz/tuxsuite-1.11.2/tuxsuite/cli/requests.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import requests
+
+from tuxsuite.cli.version import __version__
+
+
+def headers(config):
+    return {
+        "User-Agent": f"tuxsuite.cli/{__version__}",
+        "Authorization": config.token,
+    }
+
+
+def apiurl(config, url):
+    return f"{config.tuxapi_url}{url}"
+
+
+def get(config, url, params=None):
+    # print(f"GET {config.tuxapi_url}{url} {params=}")
+    return requests.get(
+        f"{config.tuxapi_url}{url}", headers=headers(config), params=params
+    )
+
+
+def post(config, url, data):
+    # print(f"POST {config.tuxapi_url}{url}")
+    # print(f"POST {data}")
+    return requests.post(
+        f"{config.tuxapi_url}{url}", headers=headers(config), json=data
+    )
