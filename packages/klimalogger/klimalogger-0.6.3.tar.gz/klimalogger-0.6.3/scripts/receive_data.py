17f0e97e1ed25b2f58a5ec5069dcd67c921deba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:55 +0000

    Add package klimalogger/0.6.3/klimalogger-0.6.3.tar.gz

---
+++ packages/klimalogger/klimalogger-0.6.3.tar.gz/klimalogger-0.6.3/scripts/receive_data.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import secrets
+import sys
+
+from paho.mqtt import client as mqtt_client
+
+
+def connect_mqtt():
+    port = 1883
+    client_id = f'python-mqtt-{secrets.randbelow(1000)}'
+
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    # Set Connecting Client ID
+    client = mqtt_client.Client(client_id)
+    # client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+
+def subscribe(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+    client.subscribe(topic)
+    client.on_message = on_message
+
+
+if __name__ == "__main__":
+    topic = "klimalogger.py"
+
+    broker = sys.argv[1]
+    client = connect_mqtt()
+    subscribe(client)
+    client.loop_forever()
