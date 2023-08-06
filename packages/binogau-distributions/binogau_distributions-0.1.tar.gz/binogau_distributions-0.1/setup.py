b1413a4d79574a6ec970565a6c63d636207f98ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:09 +0000

    Add package binogau-distributions/0.1/binogau_distributions-0.1.tar.gz

---
+++ packages/binogau-distributions/binogau_distributions-0.1.tar.gz/binogau_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='binogau_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['binogau_distributions'],
+      author = "Laxmi Priya Kasi",
+     zip_safe = False)
