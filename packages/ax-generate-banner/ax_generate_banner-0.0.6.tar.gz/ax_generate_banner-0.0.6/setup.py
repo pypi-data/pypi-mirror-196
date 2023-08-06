9872c144c7ed0e6c0cda1192cc71d1ca5b45178c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:31 +0000

    Add package ax-generate-banner/0.0.6/ax_generate_banner-0.0.6.tar.gz

---
+++ packages/ax-generate-banner/ax_generate_banner-0.0.6.tar.gz/ax_generate_banner-0.0.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from pip._internal.req import parse_requirements
+requirements = parse_requirements('core/requirements.txt', session='hack')
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+requirements_list = [i.requirement for i in requirements]
+setup(
+    name='ax_generate_banner',
+    version='0.0.6',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Baxromov',
+    author_email='baxromov.shahzodbek@gmail.com',
+    packages=find_packages(),
+    install_requires=requirements_list,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ]
+)
