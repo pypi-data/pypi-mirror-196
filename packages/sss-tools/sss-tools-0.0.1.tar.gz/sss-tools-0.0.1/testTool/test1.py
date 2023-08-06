7615d3a9361df92f50687fb7cca74c89c72a213a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:40 +0000

    Add package sss-tools/0.0.1/sss-tools-0.0.1.tar.gz

---
+++ packages/sss-tools/sss-tools-0.0.1.tar.gz/sss-tools-0.0.1/testTool/test1.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     test1
+   Author :        sss
+   date：          2023-2-27
+-------------------------------------------------
+   Change Activity:
+                   2023-2-27:
+-------------------------------------------------
+"""
+__author__ = 'sss'
+import requests
+
+
+class tes1:
+    def __init__(self):
+        print('test1')
+        requests.get('www.baidu.com')
+
+
+if __name__ == '__main__':
+    pass
