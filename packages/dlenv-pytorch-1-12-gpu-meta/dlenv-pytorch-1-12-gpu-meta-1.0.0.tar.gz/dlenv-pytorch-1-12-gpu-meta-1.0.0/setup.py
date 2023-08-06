bb9d2fabc8369087854447c2ab0159c52fe3f2ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:52 +0000

    Add package dlenv-pytorch-1-12-gpu-meta/1.0.0/dlenv-pytorch-1-12-gpu-meta-1.0.0.tar.gz

---
+++ packages/dlenv-pytorch-1-12-gpu-meta/dlenv-pytorch-1-12-gpu-meta-1.0.0.tar.gz/dlenv-pytorch-1-12-gpu-meta-1.0.0/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+import reporter
+
+setup(name='dlenv-pytorch-1-12-gpu-meta', version='1.0.0', description='Ethical research - dep confusion package. This is not a real package, do not install.')
