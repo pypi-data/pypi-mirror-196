fda81f793f1afde4df52ff6c3b462bff725be5b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:33 +0000

    Add package airflow-provider-vertex-ai/0.0.0a0/airflow-provider-vertex-ai-0.0.0a0.tar.gz

---
+++ packages/airflow-provider-vertex-ai/airflow-provider-vertex-ai-0.0.0a0.tar.gz/airflow-provider-vertex-ai-0.0.0a0/airflow_provider_vertex_ai/__init__.py
@@ -0,0 +1,10 @@
+
+__version__ = "0.0.0a"
+
+def get_provider_info():
+    return {
+        "package-name": "airflow-provider-vertex-ai",  # Required
+        "name": "airflow-provider-vertex-ai",  # Required
+        "description": "A short description of the package",  # Required
+        "versions": [__version__],  # Required
+    }
