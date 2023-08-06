0d63daf03f0369ba0dd276737655b223a19d192e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:19 +0000

    Add package sweetbean/0.0.32/sweetbean-0.0.32.tar.gz

---
+++ packages/sweetbean/sweetbean-0.0.32.tar.gz/sweetbean-0.0.32/examples/text/backup/main_20230309_170720.js
@@ -0,0 +1,95 @@
+import jsPsychHtmlKeyboardResponse from '@jspsych/plugin-html-keyboard-response'
+import 'jspsych/css/jspsych.css'
+
+const jsPsychOptions = {}
+
+function buildTimeline(jsPsych) {
+    console.log(jsPsych.version())
+
+    const trials = [
+        {
+            timeline: [{
+                type: jsPsychHtmlKeyboardResponse, trial_duration: () => {
+                    let duration = "2000";
+                    return duration
+                }, stimulus: () => {
+                    let text = jsPsych.timelineVariable('word');
+                    let color = jsPsych.timelineVariable('color');
+                    return "<div style='color: " + color + "'>" + text + "</div>"
+                }, choices: () => {
+                    let choices = ['a', 'b'];
+                    return choices
+                }, on_finish: (data) => {
+                    data["bean_type"] = 'jsPsychHtmlKeyboardResponse';
+                    let duration = "2000";
+                    data["bean_duration"] = duration;
+                    let text = jsPsych.timelineVariable('word');
+                    data["bean_text"] = text;
+                    let color = jsPsych.timelineVariable('color');
+                    data["bean_color"] = color;
+                    let choices = ['a', 'b'];
+                    data["bean_choices"] = choices;
+                    let correct_key = "";
+                    data["bean_correct_key"] = correct_key;
+                    data["bean_correct"] = correct_key == data["response"]
+                }
+            }], timeline_variables: t_0
+        }, {
+            timeline: [{
+                type: jsPsychHtmlKeyboardResponse, trial_duration: () => {
+                    let duration = "2000";
+                    return duration
+                }, stimulus: () => {
+                    let text = jsPsych.timelineVariable('word');
+                    let color = jsPsych.timelineVariable('color');
+                    return "<div style='color: " + color + "'>" + text + "</div>"
+                }, choices: () => {
+                    let choices = ['a', 'b'];
+                    return choices
+                }, on_finish: (data) => {
+                    data["bean_type"] = 'jsPsychHtmlKeyboardResponse';
+                    let duration = "2000";
+                    data["bean_duration"] = duration;
+                    let text = jsPsych.timelineVariable('word');
+                    data["bean_text"] = text;
+                    let color = jsPsych.timelineVariable('color');
+                    data["bean_color"] = color;
+                    let choices = ['a', 'b'];
+                    data["bean_choices"] = choices;
+                    let correct_key = "";
+                    data["bean_correct_key"] = correct_key;
+                    data["bean_correct"] = correct_key == data["response"]
+                }
+            }], timeline_variables: t_1
+        }, {
+            timeline: [{
+                type: jsPsychHtmlKeyboardResponse, trial_duration: () => {
+                    let duration = "2000";
+                    return duration
+                }, stimulus: () => {
+                    let text = jsPsych.timelineVariable('word');
+                    let color = jsPsych.timelineVariable('color');
+                    return "<div style='color: " + color + "'>" + text + "</div>"
+                }, choices: () => {
+                    let choices = ['a', 'b'];
+                    return choices
+                }, on_finish: (data) => {
+                    data["bean_type"] = 'jsPsychHtmlKeyboardResponse';
+                    let duration = "2000";
+                    data["bean_duration"] = duration;
+                    let text = jsPsych.timelineVariable('word');
+                    data["bean_text"] = text;
+                    let color = jsPsych.timelineVariable('color');
+                    data["bean_color"] = color;
+                    let choices = ['a', 'b'];
+                    data["bean_choices"] = choices;
+                    let correct_key = "";
+                    data["bean_correct_key"] = correct_key;
+                    data["bean_correct"] = correct_key == data["response"]
+                }
+            }], timeline_variables: t_2
+        }]
+    return trials;
+}
+
+export {jsPsychOptions, buildTimeline}
