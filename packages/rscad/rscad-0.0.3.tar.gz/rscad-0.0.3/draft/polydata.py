bcaf1d228d87030e4b4fc9b9c344ac444edf970d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:39 +0000

    Add package rscad/0.0.3/rscad-0.0.3.tar.gz

---
+++ packages/rscad/rscad-0.0.3.tar.gz/rscad-0.0.3/draft/polydata.py
@@ -0,0 +1,41 @@
+# std
+from typing import Optional
+
+# thirdparty
+import numpy as np
+import pyvista as pv
+
+# local
+from . import core
+
+
+def overhangs(mesh: pv.PolyData, ref_normal, name="blocked"):
+    assert mesh.is_all_triangles, "overhangs expect triangulted mesh"
+    tris = mesh.faces.reshape((-1, 4))[:, 1:]
+    res = core.overhangs(mesh.points, tris, ref_normal)
+    return res
+
+
+def draft_angles(mesh, ref_normal, degrees=False, face=True):
+    if face:
+        normals = mesh.face_normals
+    else:
+        normals = mesh.point_normals
+
+    res = core.normals2angles(normals, ref_normal, degrees)
+    return res
+
+
+def draft_mask(
+    mesh, ref_normal, value, invert=False, name="Top", degrees=False, face=True
+):
+    if face:
+        normals = mesh.face_normals
+    else:
+        normals = mesh.point_normals
+
+    if degrees:
+        value = np.deg2rad(value)
+
+    res = core.draft_mask(normals, ref_normal, value, invert)
+    return np.array(res)
