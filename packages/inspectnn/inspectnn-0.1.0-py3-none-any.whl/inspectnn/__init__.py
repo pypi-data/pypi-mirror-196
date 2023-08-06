3493bf5eb6c31a64a41f54e67d03736bd2d5b6bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:40 +0000

    Add package inspectnn/0.1.0/inspectnn-0.1.0-py3-none-any.whl

---
+++ packages/inspectnn/inspectnn-0.1.0-py3-none-any.whl/inspectnn/__init__.py
@@ -0,0 +1,10 @@
+from .Conv.ConvLayer import ConvLayer
+from .Dense.DenseLayer import DenseLayer
+from .Pooling.PoolingLayer import PoolingLayer
+from .Flatten.FlattenLayer import FlattenLayer
+#from .OutputLayer import OutputLayer
+from .Model.NetworkModel import NetworkModel
+name = "inspectnn"
+__version__ = "0.1.0"
+__author__ = "Salvatore Barone, Filippo Ferrandino"
+__credits__ = "Department of Electrical Engineering and Information Technologies, University of Naples Federico II, Via Claudio 21, Naples, Italy"
