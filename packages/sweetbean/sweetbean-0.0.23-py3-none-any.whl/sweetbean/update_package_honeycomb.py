322825582df1e5aeb688d9e98e482ac5035be93c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:31 +0000

    Add package sweetbean/0.0.23/sweetbean-0.0.23-py3-none-any.whl

---
+++ packages/sweetbean/sweetbean-0.0.23-py3-none-any.whl/sweetbean/update_package_honeycomb.py
@@ -0,0 +1,13 @@
+import os
+import json
+
+def update_package(dependencies, path):
+    if os.path.exists(path):
+        with open(path, 'r') as f:
+            p_obj = json.load(f)
+            for key in dependencies:
+                p_obj['dependencies'][key] = dependencies[key]
+            json_object = json.dumps(p_obj, indent=2)
+            with open(path, 'w') as f_out:
+                f_out.write(json_object)
+
