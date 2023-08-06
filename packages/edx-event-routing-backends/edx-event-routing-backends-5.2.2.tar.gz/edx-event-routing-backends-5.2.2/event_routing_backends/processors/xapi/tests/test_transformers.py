6a6d1f399b551fd2b423e80b3a94359e8de231dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:13 +0000

    Add package edx-event-routing-backends/5.2.2/edx-event-routing-backends-5.2.2.tar.gz

---
+++ packages/edx-event-routing-backends/edx-event-routing-backends-5.2.2.tar.gz/edx-event-routing-backends-5.2.2/event_routing_backends/processors/xapi/tests/test_transformers.py
@@ -0,0 +1,43 @@
+"""
+Test the transformers for all of the currently supported events into xAPI format.
+"""
+import json
+import os
+
+from django.test import TestCase
+
+from event_routing_backends.processors.tests.transformers_test_mixin import TransformersTestMixin
+from event_routing_backends.processors.xapi import constants
+from event_routing_backends.processors.xapi.registry import XApiTransformersRegistry
+
+
+class TestXApiTransformers(TransformersTestMixin, TestCase):
+    """
+    Test that supported events are transformed into xAPI format correctly.
+    """
+    EXCEPTED_EVENTS_FIXTURES_PATH = '{}/fixtures/expected'.format(os.path.dirname(os.path.abspath(__file__)))
+    registry = XApiTransformersRegistry
+
+    def assert_correct_transformer_version(self, transformed_event, transformer_version):
+        self.assertEqual(
+            transformed_event.context.extensions[constants.XAPI_TRANSFORMER_VERSION_KEY],
+            transformer_version
+        )
+
+    def compare_events(self, transformed_event, expected_event):
+        """
+        Test that transformed_event and expected_event are identical.
+
+        Arguments:
+            transformed_event (dict)
+            expected_event (dict)
+
+        Raises:
+            AssertionError:     Raised if the two events are not same.
+        """
+        # id is a randomly generated UUID therefore not comparing that
+        transformed_event_json = json.loads(transformed_event.to_json())
+        self.assertIn('id', transformed_event_json)
+        expected_event.pop('id')
+        transformed_event_json.pop('id')
+        self.assertDictEqual(expected_event, transformed_event_json)
---
+++ packages/edx-event-routing-backends/edx-event-routing-backends-5.2.2.tar.gz/edx-event-routing-backends-5.2.2/event_routing_backends/tests/test_helpers.py
@@ -0,0 +1,34 @@
+"""
+Test the helper methods.
+"""
+from unittest.mock import patch
+
+from django.test import TestCase
+
+from event_routing_backends.helpers import get_anonymous_user_id, get_block_id_from_event_referrer
+from event_routing_backends.tests.factories import UserFactory
+
+
+class TestHelpers(TestCase):
+    """
+    Test the helper methods.
+    """
+    def setUp(self):
+        super().setUp()
+        UserFactory.create(username='edx')
+
+    def test_get_block_id_from_event_referrer_with_error(self):
+        sample_event = {
+            'context': {
+                'referer': None
+            }
+        }
+        self.assertEqual(get_block_id_from_event_referrer(sample_event['context']['referer']), None)
+
+    @patch('event_routing_backends.helpers.ExternalId')
+    def test_get_anonymous_user_id_with_error(self, mocked_external_id):
+        mocked_external_id.add_new_user_id.return_value = (None, False)
+        with self.assertRaises(ValueError):
+            get_anonymous_user_id('edx')
+
+        self.assertIsNotNone(get_anonymous_user_id('12345678'))
