dea1262ff654f34d4e77ed718453e96abdaab11f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:02 +0000

    Add package midigpt/0.0.1b5/midigpt-0.0.1b5-py3-none-any.whl

---
+++ packages/midigpt/midigpt-0.0.1b5-py3-none-any.whl/midigpt/__init__.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+from . import datasets
+from .config import ModelConfigure, TrainConfigure
+from .gpt import GPT
+from .trainer import Trainer
+
+__version__ = "0.0.1-beta.5"
