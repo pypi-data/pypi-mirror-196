c183ee08361577a917441dea35b15bb936d749f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:43 +0000

    Add package openrpc/6.3.9/openrpc-6.3.9.tar.gz

---
+++ packages/openrpc/openrpc-6.3.9.tar.gz/openrpc-6.3.9/openrpc/__init__.py
@@ -0,0 +1,48 @@
+"""Provides OpenRPC objects and the RPCServer class."""
+from openrpc._depends import Depends
+from openrpc._objects import (
+    ComponentsObject,
+    ContactObject,
+    ContentDescriptorObject,
+    ErrorObject,
+    ExampleObject,
+    ExamplePairingObject,
+    ExternalDocumentationObject,
+    InfoObject,
+    LicenseObject,
+    LinkObject,
+    MethodObject,
+    OpenRPCObject,
+    ParamStructure,
+    ReferenceObject,
+    SchemaObject,
+    ServerObject,
+    ServerVariableObject,
+    TagObject,
+)
+from openrpc._router import RPCRouter
+from openrpc._server import RPCServer
+
+__all__ = (
+    "ComponentsObject",
+    "ContactObject",
+    "ContentDescriptorObject",
+    "ErrorObject",
+    "ExampleObject",
+    "ExamplePairingObject",
+    "ExternalDocumentationObject",
+    "InfoObject",
+    "LicenseObject",
+    "LinkObject",
+    "MethodObject",
+    "OpenRPCObject",
+    "ParamStructure",
+    "RPCRouter",
+    "RPCServer",
+    "ReferenceObject",
+    "SchemaObject",
+    "ServerObject",
+    "ServerVariableObject",
+    "TagObject",
+    "Depends",
+)
