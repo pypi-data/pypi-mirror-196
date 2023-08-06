6641be8ed8bc3df605371429e326f9e538f7ad16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:01 +0000

    Add package jupyter-sdk/0.0.2/jupyter_sdk-0.0.2-py3-none-any.whl

---
+++ packages/jupyter-sdk/jupyter_sdk-0.0.2-py3-none-any.whl/jupyter_sdk/handlers.py
@@ -0,0 +1,25 @@
+import json
+
+import tornado
+
+from jupyter_server.base.handlers import APIHandler
+from jupyter_server.utils import url_path_join
+
+
+class JupyterSdkHandler(APIHandler):
+
+    # The following decorator should be present on all verb methods (head, get, post,
+    # patch, put, delete, options) to ensure only authorized user can request the Jupyter server
+    @tornado.web.authenticated
+    def get(self):
+        self.finish(json.dumps({
+            "data": "This is /juptyer_sdk/get_example endpoint!"
+        }))
+
+
+def setup_handlers(web_app):
+    host_pattern = ".*$"
+    base_url = web_app.settings["base_url"]
+    route_pattern = url_path_join(base_url, "jupyter_sdk", "get_example")
+    handlers = [(route_pattern, JupyterSdkHandler)]
+    web_app.add_handlers(host_pattern, handlers)
