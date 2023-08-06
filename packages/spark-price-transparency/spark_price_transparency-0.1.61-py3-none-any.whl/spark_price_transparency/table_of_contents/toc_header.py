91228bd175830df6222caa709ea766786a6130fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:34 +0000

    Add package spark-price-transparency/0.1.61/spark_price_transparency-0.1.61-py3-none-any.whl

---
+++ packages/spark-price-transparency/spark_price_transparency-0.1.61-py3-none-any.whl/spark_price_transparency/table_of_contents/toc_header.py
@@ -0,0 +1,10 @@
+from ..pt_ingest_table import IngestTable
+from pyspark.sql.types import StringType, LongType
+
+
+class Toc_header(IngestTable):
+
+    definition = [("file_name", StringType(), False, "File name of table of contents json"),
+                  ("batch_id", LongType(), True, "Streaming ingest batchId"),
+                  ("reporting_entity_name", StringType(), True, "Legal name of the entity publishing"),
+                  ("reporting_entity_type", StringType(), True, "Type of the legal entity")]
