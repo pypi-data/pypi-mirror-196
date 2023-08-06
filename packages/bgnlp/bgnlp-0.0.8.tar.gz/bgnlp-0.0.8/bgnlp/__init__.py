c401e05e833ebc9eed0d8df66ec615682492cea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:18 +0000

    Add package bgnlp/0.0.8/bgnlp-0.0.8.tar.gz

---
+++ packages/bgnlp/bgnlp-0.0.8.tar.gz/bgnlp-0.0.8/bgnlp/__init__.py
@@ -0,0 +1,5 @@
+from bgnlp.tools.pos import PosTagger
+from bgnlp.tools.configs import PosTaggerConfig
+
+
+__version__ = "0.0.8"
---
+++ packages/bgnlp/bgnlp-0.0.8.tar.gz/bgnlp-0.0.8/bgnlp/tools/configs.py
@@ -0,0 +1,29 @@
+import os
+import sys
+from typing import Dict
+from dataclasses import dataclass, field
+
+from bgnlp.tools.mappings import POS_ID2LABEL, POS_LABEL2ID
+
+
+PACKAGE_DIR = os.path.dirname(sys.modules["bgnlp"].__file__)
+
+
+@dataclass
+class ModelConfig:
+    base_model_id: str
+    model_path: str
+    device: str = "cpu"
+
+
+@dataclass
+class PosTaggerConfig(ModelConfig):
+    base_model_id: str = "rmihaylov/bert-base-bg"
+    label2id: Dict[str, int] = field(default_factory=lambda: POS_LABEL2ID)
+    id2label: Dict[int, str] = field(default_factory=lambda: POS_ID2LABEL)
+
+    device: str = "cpu"
+    model_url: str = "https://drive.google.com/uc?id=1MHADXPM5oYRMz4nyPqaKeW2zQSctOxBQ"
+    model_path: str = os.path.join(
+        PACKAGE_DIR, "serialized", "models", "lemma-bert-latest.pt"
+    )
