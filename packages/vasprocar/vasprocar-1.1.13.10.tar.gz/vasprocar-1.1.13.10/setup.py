d5a8e608f7001031e10e57693af52acb07ba49e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:37 +0000

    Add package vasprocar/1.1.13.10/vasprocar-1.1.13.10.tar.gz

---
+++ packages/vasprocar/vasprocar-1.1.13.10.tar.gz/vasprocar-1.1.13.10/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+from setuptools import setup, find_packages
+import json
+from pathlib import Path
+from typing import Optional
+
+setup(
+    name = "vasprocar",
+    version = "1.1.13.10",
+    entry_points={'console_scripts': ['vasprocar = vasprocar:main']},
+    description = "VASProcar is an open-source package written in the Python 3 programming language, which aims to provide an intuitive tool for the post-processing of the output files produced by the DFT VASP code, through an interactive user interface.",
+    author = "Augusto de Lelis Araujo and Renan da Paixao Maciel", 
+    author_email = "augusto-lelis@outlook.com, renan.maciel@physics.uu.se",
+    url = "https://doi.org/10.5281/zenodo.6343960",
+    download_url = "https://doi.org/10.5281/zenodo.6343960",
+    license = "GNU GPLv3",
+    package_data={"": ['*.dat', '*.png', '*.jpg']},
+)
+
+# python3 -m pip install --upgrade twine
+# python setup.py sdist
+# python3 -m twine upload dist/*
---
+++ packages/vasprocar/vasprocar-1.1.13.10.tar.gz/vasprocar-1.1.13.10/vasprocar/__main__.py
@@ -0,0 +1,40 @@
+# VASProcar Copyright (C) 2023
+# GNU GPL-3.0 license
+
+version = '1.1.14'
+VASProcar_name = 'VASProcar version ' + version
+url_1 = 'https://pypi.org/project/vasprocar'
+url_2 = 'https://doi.org/10.5281/zenodo.6343960'
+url_3 = 'https://github.com/Augusto-de-Lelis-Araujo/VASProcar-Python-tools-VASP'
+
+print(" ")
+print("!!!!!!!!!!! Access to VASProcar package temporarily closed !!!!!!!!!!!")
+print("!!!!!!!!!!! Access to VASProcar package temporarily closed !!!!!!!!!!!")
+print("!!!!!!!!!!! Access to VASProcar package temporarily closed !!!!!!!!!!!")
+print(" ")
+
+print("######################################################################")
+print(f'# {VASProcar_name} Copyright (C) 2023')
+print(f'# GNU GPL-3.0 license')
+print("# ====================================================================")
+print(f'# {url_1} ')
+print(f'# {url_2} ')
+print(f'# {url_3} ')
+print("######################################################################")
+print("# Authors:                                                            ")
+print("# ====================================================================")
+print("# Augusto de Lelis Araujo                                             ")
+print("# [2022-2023] CNPEM|Ilum|LNNano (Campinas-SP/Brazil)                  ")
+print("# [2007-2022] Federal University of Uberlandia (Uberlândia-MG/Brazil) ")
+print("# e-mail: augusto-lelis@outlook.com                                   ")
+print("# ====================================================================")
+print("# Renan da Paixao Maciel                                              ")
+print("# Uppsala University (Uppsala/Sweden)                                 ")
+print("# e-mail: renan.maciel@physics.uu.se                                  ")
+print("######################################################################")
+
+print(" ")
+print("!!!!!!!!!!! Access to VASProcar package temporarily closed !!!!!!!!!!!")
+print("!!!!!!!!!!! Access to VASProcar package temporarily closed !!!!!!!!!!!")
+print("!!!!!!!!!!! Access to VASProcar package temporarily closed !!!!!!!!!!!")
+print(" ")
