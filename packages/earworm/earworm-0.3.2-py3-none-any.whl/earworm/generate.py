912e5ecb621d2875a4fd841dd6147a2b1a890a95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:36 +0000

    Add package earworm/0.3.2/earworm-0.3.2-py3-none-any.whl

---
+++ packages/earworm/earworm-0.3.2-py3-none-any.whl/earworm/generate.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env python
+import argparse
+from dataclasses import fields
+import io
+import json
+import os
+import shutil
+from typing import List, Dict
+from urllib.parse import urljoin
+
+import jinja2
+from PIL import Image  # type: ignore
+import webassets  # type: ignore
+from webassets.ext.jinja2 import AssetsExtension  # type: ignore
+import yaml
+
+from .feed import generate_feed
+from .metadata import Config, create_or_update_metadata_csv, download_file, get_metadata, is_url
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_FILE = "template.html"
+
+
+def read_config(config_path: str) -> Config:
+    with open(config_path) as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    config_dir = os.path.dirname(config_path)
+    config["_config_dir"] = config_dir
+    config["_config_path"] = config_path
+
+    music_dir = config["music_dir"]
+    config["music_dir"] = (
+        None if music_dir is None else os.path.join(config_dir, os.path.expanduser(music_dir))
+    )
+
+    metadata_csv = config.get("metadata_csv")
+    if is_url(metadata_csv):
+        config["_metadata_url"] = metadata_csv
+        metadata_csv = download_file(metadata_csv, config_dir)
+    else:
+        metadata_csv = (
+            os.path.join(config_dir, os.path.expanduser(metadata_csv)) if metadata_csv else None
+        )
+    config["metadata_csv"] = metadata_csv
+
+    out_dir = os.path.join(config_dir, config.get("out_dir", "output"))
+    config["out_dir"] = out_dir
+
+    # Ensure base_url always ends with /
+    base_url = config.get("base_url", "")
+    if base_url and not base_url.endswith("/"):
+        config["base_url"] = f"{base_url}/"
+
+    return Config(**config)
+
+
+def generate_index(config: Config, songs: List[Dict]) -> str:
+    static_dir = os.path.join(HERE, "static")
+    output_dir = os.path.join(config.out_dir, "static")
+    assets_env = webassets.Environment(directory=output_dir, url="./static", load_path=[static_dir])
+    all_js = webassets.Bundle("bundle.js", output="bundle.js")
+    all_css = webassets.Bundle("main.css", output="bundle.css")
+    assets_env.register("all_js", all_js)
+    assets_env.register("all_css", all_css)
+
+    loader = jinja2.FileSystemLoader(searchpath=HERE)
+    env = jinja2.Environment(loader=loader, extensions=[AssetsExtension])
+    env.assets_environment = assets_env  # type: ignore
+
+    template = env.get_template(TEMPLATE_FILE)
+    output = template.render(
+        config=config,
+        songs=songs,
+        title=config.title,
+        base_url=config.base_url,
+        description=config.description,
+        feed_url=urljoin(config.base_url, "index.xml") if config.generate_feed else None,
+    )
+    with open(os.path.join(config.out_dir, "index.html"), "w") as f:
+        f.write(output)
+    return f.name
+
+
+def copy_media(config: Config, songs: List[Dict]) -> None:
+    music_dir = os.path.join(config.out_dir, config.media_dir)
+    os.makedirs(music_dir, exist_ok=True)
+    for song in songs:
+        shutil.copyfile(song["path"], os.path.join(config.out_dir, song["src"]))
+
+
+def create_covers(config: Config, songs: List[Dict]) -> List[str]:
+    """Create cover images for albums in the song list.
+
+    NOTE: When image is a binary blob of data in the song, we assume albums
+    have a single cover image. FIXME: This could potentially be fixed, later?
+
+    """
+    covers_dir = os.path.join(config.out_dir, "covers")
+    os.makedirs(covers_dir, exist_ok=True)
+    cover_images = []
+    for song in songs:
+        image = song.get("image")
+        if not image:
+            continue
+
+        if isinstance(image, bytes):
+            image_path = os.path.join(covers_dir, f'{song["album_slug"]}.jpg')
+            song["image"] = os.path.relpath(image_path, start=config.out_dir)
+            if os.path.exists(image_path):
+                continue
+            img = resize_image(image)
+            img.save(image_path, quality=95, optimize=True)
+
+        elif not is_url(image):
+            raise NotImplementedError
+
+        else:
+            image_path = image
+
+        cover_images.append(image_path)
+    return cover_images
+
+
+def resize_image(data: bytes, size: tuple = (300, 300)) -> Image:
+    img = Image.open(io.BytesIO(data))
+    w, h = img.size
+    l = max(w, h)
+    if l <= 300:
+        return img
+    square = Image.new(img.mode, (l, l), (0, 0, 0))
+    paste_coords = ((h - w) // 2, 0) if h > w else (0, (w - h) // 2)
+    square.paste(img, paste_coords)
+    return square.resize(size)
+
+
+def create_og_image(config: Config, path: str) -> None:
+    image_dir = os.path.dirname(path)
+    og_path = os.path.join(image_dir, "og-image.jpg")
+    shutil.copyfile(path, og_path)
+
+
+def create_favicon(config: Config, path: str) -> None:
+    favicon_path = os.path.join(config.out_dir, "favicon.ico")
+    with open(path, "rb") as f:
+        data = f.read()
+    img = resize_image(data, size=(96, 96))
+    img.save(favicon_path, quality=95, optimize=True)
+
+
+def generate_site(config: Config) -> None:
+    print(f"Generating site from {config.music_dir or config._config_path} ...")
+    songs = get_metadata(config)
+
+    os.makedirs(config.out_dir, exist_ok=True)
+
+    if config.music_dir:
+        copy_media(config, songs)
+    cover_images = create_covers(config, songs)
+    first_image = cover_images and cover_images[0]
+    if config.base_url and first_image and not is_url(first_image):
+        create_og_image(config, first_image)
+        create_favicon(config, first_image)
+    if config.generate_feed:
+        generate_feed(config, songs)
+    index_path = generate_index(config, songs)
+    print(f"Site generated in {index_path}!")
+
+
+def yaml_represent_config(dumper: yaml.dumper.Dumper, config: Config) -> yaml.nodes.MappingNode:
+    """Custom YAML dump representation for Config.
+
+    NOTE: We could've simply used dataclasses.asdict to convert the config
+    object to a dict, and dumped that data, but PyYAML seems to always sort the
+    dictionaries by the keys when serializing them. This custom implementation
+    overcomes that issue.
+
+    """
+
+    data = []
+    for field in fields(config):
+        name = field.name
+        if name.startswith("_"):
+            continue
+        key = dumper.represent_data(name)
+        value = dumper.represent_data(getattr(config, name))
+        data.append((key, value))
+    return yaml.nodes.MappingNode("tag:yaml.org,2002:map", data)
+
+
+def make_config_file(config: Config) -> None:
+    yaml.add_representer(Config, yaml_represent_config)
+    with open(config._config_path, "w") as f:
+        yaml.dump(config, f)
+    print(f"Created config file at {config._config_path}")
+    print("Please update the value of music_dir to your directory of files.")
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    # NOTE: Added here for running without any sub-command. But, the
+    # sub-commands themselves add this option, again to be able to pass this
+    # argument after the sub-command name
+    parser.add_argument("-c", "--config", action="store", default="config.yml")
+    parser.set_defaults(func=generate_site)
+
+    subparsers = parser.add_subparsers(title="sub-commands")
+    parser_update_csv = subparsers.add_parser(
+        "update-csv", help="Update CSV from files in the music dir"
+    )
+    parser_update_csv.add_argument("-c", "--config", action="store", default="config.yml")
+    parser_update_csv.set_defaults(func=create_or_update_metadata_csv)
+
+    parser_make_config = subparsers.add_parser("make-config", help="Make a sample config file")
+    parser_make_config.add_argument("-c", "--config", action="store", default="config.yml")
+    parser_make_config.set_defaults(func=make_config_file)
+
+    options = parser.parse_args()
+    config_path = os.path.abspath(options.config)
+    try:
+        if options.func.__name__ == "make_config_file":
+            config = Config(music_dir="/tmp", _config_path=config_path)
+        else:
+            config = read_config(config_path)
+    except FileNotFoundError:
+        print(f"Could not find the config file {options.config}.")
+        print("Run `earworm make-config' to create the config file")
+    else:
+        options.func(config)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/earworm/earworm-0.3.2-py3-none-any.whl/earworm/metadata.py
@@ -0,0 +1,264 @@
+import csv
+import glob
+import json
+import os
+import re
+import subprocess
+from dataclasses import dataclass, field, fields
+from typing import Dict, List, Optional, Union
+from urllib import parse
+
+import requests
+from tinytag.tinytag import TinyTag, TinyTagException  # type: ignore
+
+UNSUPPORTED_FORMATS = (".amr",)  # Not played by FF or Chrome. See issue #10
+
+
+@dataclass
+class Config:
+    music_dir: str
+    metadata_csv: str = ""
+    title_required: bool = False
+    album_required: bool = False
+    date_required: bool = False
+    date_regex: str = r"(?P<year>\d{4})_(?P<month>\d{2})_(?P<day>\d{2})"
+    ignored_dates: set = field(default_factory=set)
+    out_dir: str = "./public"
+    media_dir: str = "music"
+    title: str = "My Music"
+    song_description: str = "${song.album} (${song.date})"
+    description: str = "<small>Welcome to my music page.</small>"
+    base_url: str = ""
+    use_ffprobe: bool = True
+    generate_feed: bool = True
+    _config_path: str = ""
+    _config_dir: str = ""
+    _metadata_url: str = ""
+
+
+@dataclass
+class Row:
+    filename: str
+    title: str = ""
+    album: str = ""
+    artist: str = ""
+    composer: str = ""
+    lyricist: str = ""
+    instrument: str = ""
+    date: str = ""
+    duration: str = ""
+    image: str = ""
+    filesize: str = ""
+
+
+def is_url(text: str) -> bool:
+    return bool(parse.urlparse(text).scheme)
+
+
+def google_sheet_cell_link(url: str, row_num: int, column: str = "A") -> Optional[str]:
+    parsed_url = parse.urlparse(url)
+    if not (bool(parsed_url.scheme) and parsed_url.netloc == "docs.google.com"):
+        return None
+    qs = parse.parse_qs(parsed_url.query)
+    if not ("id" in qs and "gid" in qs):
+        return None
+
+    url = "https://docs.google.com/spreadsheets/d/{id[0]}/edit#gid={gid[0]}&range={column}{row_num}"
+    return url.format(row_num=row_num, column=column, **qs)
+
+
+def download_file(url: str, download_dir: str) -> str:
+    with requests.get(url, stream=True) as r:
+        header = r.headers.get("Content-Disposition", "")
+        match = re.search('filename="(.*)"', header)
+        name = match.group(1) if match else "metadata.csv"
+        filename = os.path.join(download_dir, name)
+        print(f"Downloading {name} ...")
+        r.raise_for_status()
+        with open(filename, "wb") as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+    return filename
+
+
+def get_metadata(config: Config) -> List[Dict]:
+    if config.metadata_csv:
+        songs = get_song_list_from_csv(config)
+    else:
+        songs = get_song_list_from_music_dir(config)
+
+    excluded_songs = {
+        song["path"] for song in songs if song["path"].endswith(UNSUPPORTED_FORMATS)
+    }
+    filtered_songs = [song for song in songs if song["path"] not in excluded_songs]
+    if excluded_songs:
+        print(
+            "\n\033[91mWARNING: The following files have unsupported formats:\n    ",
+            end="",
+        )
+        print("\n    ".join(excluded_songs))
+        print("\033[00m")
+    print(f"Publishing {len(filtered_songs)} songs ...")
+    return filtered_songs
+
+
+def read_metadata_csv(config: Config) -> List[Dict]:
+    with open(config.metadata_csv) as f:
+        reader = csv.DictReader(f)
+        reader.fieldnames = (
+            [f.lower() for f in reader.fieldnames] if reader.fieldnames else None
+        )
+        return [row for row in reader]
+
+
+def get_song_list_from_csv(config: Config) -> List[Dict]:
+    music_dir = config.music_dir
+    rows = read_metadata_csv(config)
+
+    def _path(row: Dict) -> str:
+        return (
+            row["filename"]
+            if config.music_dir is None
+            else os.path.join(config.music_dir, row["filename"])
+        )
+
+    metadata = {_path(row): Row(**row) for row in rows}
+    return metadata_to_song_list(config, metadata)
+
+
+def ffprobe_metadata(path: str) -> Dict:
+    command = [
+        "ffprobe",
+        "-v",
+        "quiet",
+        "-print_format",
+        "json",
+        "-show_format",
+        path,
+    ]
+    try:
+        output = json.loads(subprocess.check_output(command))
+    except FileNotFoundError as e:
+        fn = e.filename
+        print(f"Install {fn} to get duration, other metatdata for non-mp3 files.")
+        output = {"format": {"filename": os.path.basename(path)}}
+    except subprocess.CalledProcessError:
+        output = {}
+
+    data = output.get("format")
+    data["filesize"] = data.get("size", "")
+    return data
+
+
+def get_metadata_from_music_dir(
+    config: Config, song_list: bool = True
+) -> Dict[str, Union[Row, TinyTag]]:
+    music_dir = config.music_dir
+    paths = glob.glob(f"{music_dir}/**/*", recursive=True)
+    metadata = {}
+    row_keys = {f.name for f in fields(Row)}
+    for path in paths:
+        try:
+            metadata[path] = TinyTag.get(path, image=True)
+        except TinyTagException as e:
+            pass
+
+        if path not in metadata or not metadata[path].duration:
+            md = ffprobe_metadata(path) if config.use_ffprobe else {"filename": path}
+            if not md:
+                continue
+            md = {key: value for key, value in md.items() if key in row_keys}
+            metadata[path] = Row(**md)
+
+    date_re = re.compile(config.date_regex)
+    for path, tags in metadata.items():
+        match = date_re.search(path)
+        date = "{year}-{month}-{day}".format(**match.groupdict()) if match else ""
+        tags.date = date if date not in config.ignored_dates else None
+
+    return metadata
+
+
+def get_song_list_from_music_dir(config: Config) -> List[Dict]:
+    metadata = get_metadata_from_music_dir(config)
+    return metadata_to_song_list(config, metadata)
+
+
+def metadata_to_song_list(config: Config, metadata: Dict[str, Row]) -> List[Dict]:
+    songs = []
+
+    for num_row, (path, tags) in enumerate(metadata.items(), start=2):
+        src = os.path.basename(path)
+
+        if not ((config.music_dir is None and is_url(path)) or os.path.exists(path)):
+            continue
+        elif config.title_required and not tags.title:
+            continue
+        elif config.album_required and not tags.album:
+            continue
+        elif config.date_required and not tags.date:
+            continue
+
+        duration = int(float(tags.duration or 0))
+        album_slug = tags.album.lower().replace(" ", "-") if tags.album else ""
+        metadata_link = google_sheet_cell_link(config._metadata_url, num_row)
+        song = {
+            "path": path,
+            "filename": path if config.music_dir is None else src,
+            "src": path
+            if config.music_dir is None
+            else os.path.join(config.media_dir, src),
+            "title": tags.title or src,
+            "artist": tags.artist,
+            "album": tags.album,
+            "date": tags.date or "",
+            "duration": duration,
+            "image": tags.get_image() if isinstance(tags, TinyTag) else tags.image,
+            "album_slug": album_slug,
+            "metadata_link": metadata_link,
+            "filesize": str(tags.filesize),
+        }
+        songs.append(song)
+
+    n = len(songs)
+    print(f"Found {n} songs ...")
+    return sorted(songs, key=lambda s: s["date"], reverse=True)
+
+
+def create_or_update_metadata_csv(config: Config) -> None:
+    if not config.metadata_csv:
+        config.metadata_csv = os.path.join(config._config_dir, "metadata.csv")
+
+    if os.path.exists(config.metadata_csv):
+        rows = {r["filename"]: r for r in read_metadata_csv(config)}
+
+    else:
+        rows = {}
+
+    file_metadata = get_metadata_from_music_dir(config, song_list=False)
+    fieldnames = [f.name for f in fields(Row)]
+    for path, metadata in file_metadata.items():
+        filename = os.path.basename(path)
+        if filename in rows:
+            old_metadata = rows[filename]
+            for key in fieldnames:
+                old_value = old_metadata.get(key)
+                new_value = metadata.__dict__.get(key)
+                if not old_value and new_value:
+                    old_metadata[key] = new_value
+        else:
+            new_metadata = {
+                key: value
+                for key, value in metadata.__dict__.items()
+                if hasattr(Row, key)
+            }
+            new_metadata["filename"] = filename
+            rows[filename] = new_metadata
+
+    with open(config.metadata_csv, "w") as f:
+        writer = csv.DictWriter(f, fieldnames)
+        writer.writeheader()
+        for name, row in rows.items():
+            writer.writerow(row)
+
+    print(f"Wrote updated CSV to {f.name}")
