7c3b4c17bfa101095b5de7894be8dceacb878f41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:53 +0000

    Add package muffin-rest/4.4.3/muffin-rest-4.4.3.tar.gz

---
+++ packages/muffin-rest/muffin-rest-4.4.3.tar.gz/muffin-rest-4.4.3/tests/test_base.py
@@ -0,0 +1,305 @@
+import pytest
+import marshmallow as ma
+
+
+class FakeSchema(ma.Schema):
+
+    def dump(self, data, **kwargs):
+        return data
+
+    def load(self, data, **kwargs):
+        return data
+
+
+@pytest.fixture
+def api(app):
+    from muffin_rest import API
+
+    api = API(app, prefix='/api', title='API Title')
+
+    @api.authorization
+    async def fake_auth(request):
+        return not request.url.query.get('guest')
+
+    return api
+
+
+async def test_conf(client):
+    res = await client.get('/')
+    assert res.status_code == 200
+    assert await res.text() == 'OK'
+
+
+async def test_api(app, client):
+    from muffin_rest import API
+
+    api = API()
+    assert api
+
+    api.setup(app, prefix='/api')
+    assert api.app
+    assert api.prefix == '/api'
+
+    res = await client.get('/api')
+    assert res.status_code == 404
+
+
+async def test_api_methods(api, client):
+
+    @api.route('/simple')
+    async def simple_endpoint(request):
+        return {'data': 'simple'}
+
+    res = await client.get('/api/simple')
+    assert res.status_code == 200
+    assert await res.json() == {'data': 'simple'}
+
+
+async def test_handler(api, client):
+    from muffin_rest import RESTHandler
+    from muffin_rest.filters import Filters
+    from muffin_rest.sorting import Sorting
+
+    assert RESTHandler
+
+    @api.route('/simple')
+    class Simple(RESTHandler):
+
+        methods = 'get', 'put'
+
+        class Meta:
+            name = 'simple'
+            sorting = 'test',
+            schema_base = FakeSchema
+
+        async def prepare_collection(self, request):
+            return f'SIMPLE {request.method}'
+
+    assert Simple.meta
+    assert Simple.meta.name == 'simple'
+    assert Simple.meta.limit == 0
+    assert Simple.meta.filters is not None
+    assert isinstance(Simple.meta.filters, Filters)
+    assert Simple.meta.sorting is not None
+    assert isinstance(Simple.meta.sorting, Sorting)
+    assert Simple.methods == {'GET', 'PUT'}
+    assert api.router.routes()[2].methods == Simple.methods
+    assert Simple.meta.Schema
+    assert issubclass(Simple.meta.Schema, FakeSchema)
+    assert Simple.meta.Schema.opts
+    assert Simple.meta.Schema.opts.unknown == 'exclude'
+
+    res = await client.get('/api/simple')
+    assert res.status_code == 200
+    assert await res.body() == b'SIMPLE GET'
+
+    res = await client.put('/api/simple')
+    assert res.status_code == 404
+    assert await res.json() == {'error': True, 'message': 'Nothing matches the given URI'}
+
+    res = await client.post('/api/simple')
+    assert res.status_code == 405
+
+    res = await client.get('/api/simple?guest=1')
+    assert res.status_code == 401
+    assert await res.json() == {
+        'error': True, 'message': 'No permission -- see authorization schemes'}
+
+
+async def test_handler2(api, client):
+    from muffin_rest import RESTHandler, APIError
+
+    source = [1, 2, 3]
+
+    @api.route
+    class Source(RESTHandler):
+
+        class Meta:
+            name = 'source'
+            filters = 'val',
+            limit = 10
+            Schema = FakeSchema
+
+        async def prepare_collection(self, request):
+            return source
+
+        async def prepare_resource(self, request):
+            pk = request['path_params'].get(self.meta.name_id)
+            if not pk:
+                return
+
+            try:
+                return source[int(pk)]
+            except IndexError:
+                raise APIError.NOT_FOUND('Resource not found')
+
+        async def paginate(self, request, limit, offset):
+            return self.collection[offset: offset + limit], len(source)
+
+        async def load(self, request, resource=None):
+            data = await super().load(request, resource=resource)
+            if resource:
+                idx = source.index(resource)
+                source[idx] = data
+            return data
+
+        async def save(self, request, resource=None):
+            if resource not in source:
+                source.append(resource)
+            return resource
+
+        async def remove(self, request, resource=None):
+            source.remove(resource)
+
+        @RESTHandler.route('/source/custom', methods='get')
+        async def custom(self, request, resource=None):
+            return 'source: custom'
+
+    assert Source.meta.filters
+    assert Source.meta.filters.mutations
+
+    # Get collection
+    res = await client.get('/api/source')
+    assert res.status_code == 200
+    assert await res.json() == source
+
+    # Get a resource
+    res = await client.get('/api/source/1')
+    assert res.status_code == 200
+    assert await res.json() == 2
+
+    # Get an unknown resource
+    res = await client.get('/api/source/99')
+    assert res.status_code == 404
+    assert await res.json() == {'error': True, 'message': 'Resource not found'}
+
+    # Create a resource
+    res = await client.post('/api/source', json=42)
+    assert res.status_code == 200
+    assert await res.json() == 42
+
+    # Update a resource
+    res = await client.put('/api/source/3', json=99)
+    assert res.status_code == 200
+    assert await res.json() == 99
+    assert source == [1, 2, 3, 99]
+
+    # Delete a resource
+    res = await client.delete('/api/source/3')
+    assert res.status_code == 200
+    assert source == [1, 2, 3]
+
+    # Filter results
+    res = await client.get('/api/source?where={"val": 2}')
+    assert res.status_code == 200
+    assert await res.json() == [2]
+
+    # Filter results
+    res = await client.get('/api/source?where={"val": {">=": 2}}')
+    assert res.status_code == 200
+    assert await res.json() == [2, 3]
+
+    # Paginate results
+    res = await client.get('/api/source?limit=2')
+    assert res.status_code == 200
+    assert res.headers['x-total'] == str(len(source))
+    assert res.headers['x-limit'] == '2'
+    assert res.headers['x-offset'] == '0'
+    assert await res.json() == [1, 2]
+
+    res = await client.get('/api/source?limit=2&offset=1')
+    assert res.status_code == 200
+    assert await res.json() == [2, 3]
+
+    res = await client.get('/api/source/custom')
+    assert res.status_code == 200
+    assert await res.text() == 'source: custom'
+
+    res = await client.post('/api/source/custom')
+    assert res.status_code == 405
+
+
+async def test_bad_request(api, client):
+    from muffin_rest import RESTHandler
+
+    @api.route('/simple')
+    class Simple(RESTHandler):
+
+        methods = 'get', 'post'
+
+        class Meta:
+            name = 'simple'
+
+        async def prepare_collection(self, request):
+            return f'SIMPLE {request.method}'
+
+        async def save(self, request, resource=None):
+            pass
+
+    res = await client.post(
+        '/api/simple', data='invalid', headers={'content-type': 'application/json'})
+    assert res.status_code == 400
+
+
+async def test_handlers_with_schema(api, client):
+    from muffin_rest import RESTHandler
+
+    pets = []
+
+    @api.route('/pets', '/pets/{pet}')
+    class Pet(RESTHandler):
+
+        methods = 'get', 'post'
+
+        class Meta:
+            name = 'pet'
+
+            class Schema(ma.Schema):
+                name = ma.fields.String(required=True)
+
+        async def prepare_collection(self, request):
+            return pets
+
+        async def save(self, request, resource):
+            pets.append(resource)
+            return resource
+
+    res = await client.post('/api/pets', json={})
+    assert res.status_code == 400
+    json = await res.json()
+    assert json
+    assert json['errors']
+
+    res = await client.post('/api/pets', json={'name': 'muffin'})
+    assert res.status_code == 200
+    json = await res.json()
+    assert json == {'name': 'muffin'}
+
+    res = await client.get('/api/pets')
+    assert res.status_code == 200
+    json = await res.json()
+    assert json == [{'name': 'muffin'}]
+
+
+async def test_handler_with_path(api, client):
+    from muffin_rest import RESTHandler
+
+    @api.route
+    class Simple(RESTHandler):
+
+        methods = 'get', 'patch'
+
+        class Meta:
+            name = 'simple'
+
+        async def prepare_collection(self, request):
+            return [1, 2, 3, 4]
+
+        async def patch(self, request, **kwargs):
+            return True
+
+    res = await client.get('/api/simple')
+    assert res.status_code == 200
+
+    res = await client.patch('/api/simple')
+    assert res.status_code == 200
---
+++ packages/muffin-rest/muffin-rest-4.4.3.tar.gz/muffin-rest-4.4.3/tests/test_peewee.py
@@ -0,0 +1,340 @@
+import peewee as pw
+import pytest
+from muffin_peewee import JSONField
+from muffin_peewee import Plugin as Peewee
+
+
+@pytest.fixture(scope="module")
+def aiolib():
+    return "asyncio", {"use_uvloop": False}
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_logging():
+    import logging
+
+    logger = logging.getLogger("peewee")
+    logger.setLevel(logging.DEBUG)
+
+
+@pytest.fixture
+async def db(app):
+    db = Peewee(app, connection="sqlite:///:memory:", auto_connection=False)
+    async with db:
+        async with db.connection():
+            yield db
+
+
+class Resource(pw.Model):
+    active = pw.BooleanField(default=False)
+    name = pw.CharField(null=False)
+    count = pw.IntegerField(null=True)
+    config = JSONField(default={})
+
+
+@pytest.fixture(autouse=True)
+async def init(db):
+    db.manager.register(Resource)
+    assert Resource._manager
+
+    await db.manager.create_tables(Resource)
+    return Resource
+
+
+@pytest.fixture
+async def ResourceEndpoint(api):
+    from muffin_rest.peewee import PWRESTHandler
+
+    @api.route
+    class ResourceEndpoint(PWRESTHandler[Resource]):
+        class Meta:
+            filters = (
+                "active",
+                "name",
+                ("oid", {"field": "id"}),
+            )
+            limit = 10
+            model = Resource
+            sorting = ("id", {"default": "desc"}), "name", Resource.count
+            delete_recursive = True
+
+        @PWRESTHandler.route("/resource/action")
+        async def action(self, request, resource=None):
+            """Description for the action."""
+            resources = await self.meta.manager.fetchall(self.collection)
+            return await self.dump(request, resources, many=True)
+
+    return ResourceEndpoint
+
+
+@pytest.fixture
+async def resource(db):
+    return await db.manager.create(Resource, name="test")
+
+
+def test_imports():
+    from muffin_rest import PWFilter, PWFilters, PWRESTHandler, PWSort, PWSorting
+    from muffin_rest.peewee import PWRESTBase
+
+    assert PWRESTHandler
+    assert PWFilter
+    assert PWFilters
+    assert PWSort
+    assert PWSorting
+    assert PWRESTBase
+
+
+async def test_base(api, ResourceEndpoint):
+    assert ResourceEndpoint
+    assert ResourceEndpoint.meta.name == "resource"
+    assert ResourceEndpoint.meta.manager
+
+    # Schema
+    assert ResourceEndpoint.meta.Schema
+    assert ResourceEndpoint.meta.Schema._declared_fields
+    ff = ResourceEndpoint.meta.Schema._declared_fields["active"]
+    assert ff.load_default is False
+
+    # Sorting
+    assert ResourceEndpoint.meta.sorting
+    assert list(ResourceEndpoint.meta.sorting.mutations.keys()) == [
+        "id",
+        "name",
+        "count",
+    ]
+    assert ResourceEndpoint.meta.sorting.default == [Resource.id.desc()]
+
+    assert api.router.plain["/resource"]
+    assert api.router.dynamic[0].pattern.pattern == "^/resource/(?P<id>[^/]+)$"
+
+
+async def test_get(client, ResourceEndpoint, resource):
+    res = await client.get("/api/resource")
+    assert res.status_code == 200
+    json = await res.json()
+    assert json
+    assert json[0]["config"] == {}
+    assert json[0]["count"] is None
+    assert json[0]["id"] == "1"
+    assert json[0]["name"] == "test"
+
+    res = await client.get("/api/resource/1")
+    assert res.status_code == 200
+    assert await res.json() == {
+        "active": False,
+        "config": {},
+        "count": None,
+        "id": "1",
+        "name": "test",
+    }
+
+    res = await client.get("/api/resource/unknown")
+    assert res.status_code == 404
+    assert await res.json() == {"error": True, "message": "Resource not found"}
+
+    res = await client.get("/api/resource/action?custom=123")
+    assert res.status_code == 200
+    json = await res.json()
+    assert json
+
+
+async def test_create(client, ResourceEndpoint):
+    res = await client.post("/api/resource", json={"active": True})
+    assert res.status_code == 400
+    json = await res.json()
+    assert json["errors"]
+    assert "name" in json["errors"]
+
+    res = await client.post(
+        "/api/resource", data={"name": "test2", "active": True, "unknown": 22}
+    )
+    assert res.status_code == 200
+    json = await res.json()
+    assert json["id"] == "1"
+    assert json["name"] == "test2"
+    assert json["active"]
+
+
+async def test_edit(client, resource, ResourceEndpoint):
+    res = await client.put("/api/resource/1", data={"name": "new"})
+    assert res.status_code == 200
+    json = await res.json()
+    assert json["name"] == "new"
+    assert json["id"] == "1"
+
+
+async def test_delete(client, resource, ResourceEndpoint, db):
+    res = await client.delete("/api/resource/1")
+    assert res.status_code == 200
+    json = await res.json()
+    assert not json
+
+    assert not await db.manager.fetchone(Resource.select().where(Resource.id == 1))
+
+
+async def test_sort(client, ResourceEndpoint, db):
+    await db.manager.create(Resource, name="test2", count=2)
+    await db.manager.create(Resource, name="test3", count=3)
+    await db.manager.create(Resource, name="test4", count=1)
+
+    # Default sort
+    res = await client.get("/api/resource")
+    assert res.status_code == 200
+    json = await res.json()
+    assert json[0]["id"] == "3"
+    assert json[1]["id"] == "2"
+
+    res = await client.get("/api/resource?sort=-count")
+    assert res.status_code == 200
+    json = await res.json()
+    assert json[0]["id"] == "2"
+    assert json[1]["id"] == "1"
+
+
+async def test_filters(client, ResourceEndpoint, db):
+    await db.manager.create(Resource, name="test2", count=2)
+    await db.manager.create(Resource, name="test3", count=3)
+    await db.manager.create(Resource, name="test4", count=1)
+
+    res = await client.get('/api/resource?where={"name":"test"}')
+    assert res.status_code == 200
+    json = await res.json()
+    assert len(json) == 0
+
+    res = await client.get('/api/resource?where={"name": {"$in": ["test3", "test2"]}}')
+    assert res.status_code == 200
+    json = await res.json()
+    assert len(json) == 2
+
+    res = await client.get('/api/resource?where={"name": {"$starts": "test"}}')
+    assert res.status_code == 200
+    json = await res.json()
+    assert len(json) == 3
+
+    res = await client.get('/api/resource?where={"name": {"$ends": "3"}}')
+    assert res.status_code == 200
+    json = await res.json()
+    assert len(json) == 1
+
+    res = await client.get('/api/resource?where={"oid": {"$between": ["2", "3"]}}')
+    assert res.status_code == 200
+    json = await res.json()
+    assert len(json) == 2
+
+    res = await client.get('/api/resource?where={"oid": {"$gt": "2"}}')
+    assert res.status_code == 200
+    json = await res.json()
+    assert len(json) == 1
+
+
+async def test_paginate(client, ResourceEndpoint, db):
+    for n in range(12):
+        await db.manager.create(Resource, name=f"test{n}")
+
+    res = await client.get("/api/resource")
+    assert res.status_code == 200
+    json = await res.json()
+    assert len(json) == 10
+
+    res = await client.get("/api/resource?limit=5")
+    assert res.status_code == 200
+    assert res.headers["x-total"] == "12"
+    assert res.headers["x-limit"] == "5"
+    assert res.headers["x-offset"] == "0"
+    json = await res.json()
+    assert len(json) == 5
+
+    res = await client.get("/api/resource?limit=5&offset=9")
+    assert res.status_code == 200
+    assert res.headers["x-total"] == "12"
+    assert res.headers["x-limit"] == "5"
+    assert res.headers["x-offset"] == "9"
+    json = await res.json()
+    assert len(json) == 3
+
+
+async def test_batch_ops(client, ResourceEndpoint, db):
+    # Batch operations (only POST/DELETE are supported for now)
+    res = await client.post(
+        "/api/resource",
+        json=[
+            {"name": "test3", "active": True},
+            {"name": "test4", "active": True},
+            {"name": "test6", "active": True},
+        ],
+    )
+    assert res.status_code == 200
+    json = await res.json()
+    assert len(json) == 3
+    assert json[0]["id"] == "1"
+    assert json[1]["id"] == "2"
+    assert json[2]["id"] == "3"
+
+    res = await client.delete("/api/resource", json=["1", "2", "3"])
+    assert res.status_code == 200
+
+    assert not await db.manager.count(
+        Resource.select().where(Resource.id << ("11", "12", "13"))
+    )
+
+
+async def test_openapi(client, ResourceEndpoint):
+    res = await client.get("/api/openapi.json")
+    assert res.status_code == 200
+    json = await res.json()
+    assert json
+
+
+async def test_endpoint_inheritance():
+    from muffin_rest.peewee import PWRESTHandler
+
+    class ResourceEndpoint(PWRESTHandler):
+        class Meta:
+            model = Resource
+
+    assert ResourceEndpoint.meta.name == "resource"
+
+    class ChildEndpoint(ResourceEndpoint):
+        class Meta:
+            name = "child"
+
+    assert ChildEndpoint.meta.name == "child"
+
+
+async def test_aiomodels(client, db, api):
+
+    events = []
+
+    class TestModel(db.Model):
+
+        data = pw.CharField()
+
+        async def save(self, **kwargs):
+            events.append("custom-save")
+            return await super().save(**kwargs)
+
+        async def delete_instance(self, **kwargs):
+            events.append("custom-delete")
+            return await super().delete_instance(**kwargs)
+
+    await db.manager.create_tables(TestModel)
+
+    from muffin_rest.peewee import PWRESTHandler
+
+    @api.route
+    class Test(PWRESTHandler):
+        class Meta:
+            model = TestModel
+
+    res = await client.post("/api/testmodel", json={"data": "test"})
+    assert res.status_code == 200
+    json = await res.json()
+    assert json["id"]
+
+    assert events
+    assert "custom-save" in events
+
+    res = await client.delete(f"/api/testmodel/{json['id']}")
+    assert res.status_code == 200
+
+    assert "custom-delete" in events
