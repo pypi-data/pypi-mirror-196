32187640ff78a763fedeaeb10d51c7ec9411fe1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:19 +0000

    Add package TaskWorker/1.0.1/TaskWorker-1.0.1.tar.gz

---
+++ packages/TaskWorker/TaskWorker-1.0.1.tar.gz/TaskWorker-1.0.1/task_worker/defines.py
@@ -0,0 +1,10 @@
+import typing
+import multiprocessing
+
+class Task(typing.TypedDict):
+    task: typing.Callable
+    args: typing.Tuple | None
+    kwargs: typing.Dict[str, typing.Any] | None
+    callback: typing.Callable[[typing.Any], None]
+
+ProcessTaskQueue: multiprocessing.Queue = multiprocessing.Queue()
