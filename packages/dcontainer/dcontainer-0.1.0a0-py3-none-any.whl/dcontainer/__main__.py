a9edf5fd6f2142663a9eb179d50a1bbb14902122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:17 +0000

    Add package dcontainer/0.1.0a0/dcontainer-0.1.0a0-py3-none-any.whl

---
+++ packages/dcontainer/dcontainer-0.1.0a0-py3-none-any.whl/dcontainer/__main__.py
@@ -0,0 +1,14 @@
+import typer
+
+from dcontainer.cli.feature import app as feature_app
+
+app = typer.Typer(pretty_exceptions_show_locals=False, pretty_exceptions_short=False)
+app.add_typer(feature_app, name="feature")
+
+
+def main() -> None:
+    app()
+
+
+if __name__ == "__main__":
+    main()
