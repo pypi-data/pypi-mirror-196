d054452d4ba7380744a8cb1706f9b8c1c11fb9ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:34 +0000

    Add package filestostorage/0.0.2.4/filestostorage-0.0.2.4.tar.gz

---
+++ packages/filestostorage/filestostorage-0.0.2.4.tar.gz/filestostorage-0.0.2.4/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.2.4'
+DESCRIPTION = 'package for file storage'
+with open("README.md",'r') as fh:
+    LONG_DESCRIPTION = fh.read()
+# Setting up
+setup(
+    name="filestostorage",
+    version=VERSION,
+    author="ngit_kmit",
+    author_email="devikarubir@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[ 'rich>=10.12.0',
+        'gridfs>=1.10.1',
+        'certifi>=2021.5.30',
+        'pymongo>=3.12.0'],
+    keywords=['mongodb','store files', 'database', 'store', 'file', 'ngit','mongo','kmit'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Topic :: Database",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
