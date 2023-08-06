f8b188615e010d139f37794f5d709b1ce3e8bc3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:47 +0000

    Add package django-chatapp/1.2/django_chatapp-1.2-py3-none-any.whl

---
+++ packages/django-chatapp/django_chatapp-1.2-py3-none-any.whl/chatapp/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('django-chatapp/chat/supporter/', views.supporter_homepage),
+    path('django-chatapp/chat/supporter/unreads/', views.supporter_unreads),
+    path('django-chatapp/chat/supporter/read-all/', views.supporter_read_all),
+    path('django-chatapp/chat/supporter/ready-msg/get/', views.get_ready_msg),
+    path('django-chatapp/chat/supporter/ready-msg/del/', views.delete_ready_msg),
+    path('django-chatapp/chat/supporter/ready-msg/create/', views.create_ready_msg),
+    path('django-chatapp/chat/supporter/report/', views.report_user),
+
+    path('django-chatapp/auth/check/userid/', views.check_userid),
+    path('django-chatapp/auth/create/userid/', views.create_userid),
+    
+    path('django-chatapp/chat/setting/', views.setting_chat),
+]
