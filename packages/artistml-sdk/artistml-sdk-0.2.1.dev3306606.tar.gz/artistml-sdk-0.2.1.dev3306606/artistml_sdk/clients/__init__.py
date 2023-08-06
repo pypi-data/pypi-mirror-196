d6d8e7273d308ae6b54d96694c8be9f50f4e0711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:51 +0000

    Add package artistml-sdk/0.2.1.dev3306606/artistml-sdk-0.2.1.dev3306606.tar.gz

---
+++ packages/artistml-sdk/artistml-sdk-0.2.1.dev3306606.tar.gz/artistml-sdk-0.2.1.dev3306606/artistml_sdk/clients/__init__.py
@@ -0,0 +1,11 @@
+from .assoc import AssocClient
+from .echo import EchoClient
+from .muses import MusesClient
+from .tag import TagClient
+
+__all__ = [
+    "EchoClient",
+    "MusesClient",
+    "TagClient",
+    "AssocClient",
+]
