4043d00e5fcbf56b6b2a7ee342f155c5d8b8953e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:56 +0000

    Add package mypytoolkit/1.6.0/mypytoolkit-1.6.0.tar.gz

---
+++ packages/mypytoolkit/mypytoolkit-1.6.0.tar.gz/mypytoolkit-1.6.0/mypytoolkit/__init__.py
@@ -0,0 +1,17 @@
+# Files
+from mypytoolkit.files import *
+
+# Date and time
+from mypytoolkit.dates import *
+
+# Linear equation plotting
+from mypytoolkit.math import LinearEquation
+
+# General Python tools
+from mypytoolkit.pyfuncs import *
+
+# Finance
+from mypytoolkit import finance
+
+# Threads
+from mypytoolkit import threadtools
