62fe0e488faeea276b9a6c61564160db4ee99f84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:52 +0000

    Add package cloud-diffusion/0.1.0/cloud_diffusion-0.1.0.tar.gz

---
+++ packages/cloud-diffusion/cloud_diffusion-0.1.0.tar.gz/cloud_diffusion-0.1.0/cloud_diffusion/utils.py
@@ -0,0 +1,126 @@
+import random, argparse
+from types import SimpleNamespace
+from pathlib import Path
+from functools import partial
+import fastcore.all as fc
+
+import wandb
+import numpy as np
+import torch
+from torch.nn import init
+
+from fastprogress import progress_bar
+
+from diffusers.schedulers import DDIMScheduler
+
+def get_unet_params(model_name="unet_small", num_frames=4):
+    "Return the parameters for the diffusers UNet2d model"
+    if model_name == "unet_small":
+        return dict(
+            block_out_channels=(16, 32, 64, 128), # number of channels for each block
+            norm_num_groups=8, # number of groups for the normalization layer
+            in_channels=num_frames, # number of input channels
+            out_channels=1, # number of output channels
+            )
+    elif model_name == "unet_big":
+        return dict(
+            block_out_channels=(32, 64, 128, 256), # number of channels for each block
+            norm_num_groups=8, # number of groups for the normalization layer
+            in_channels=num_frames, # number of input channels
+            out_channels=1, # number of output channels
+            )
+    else:
+        raise(f"Model name not found: {model_name}, choose between 'unet_small' or 'unet_big'")
+
+def set_seed(s, reproducible=False):
+    "Set random seed for `random`, `torch`, and `numpy` (where available)"
+    try: torch.manual_seed(s)
+    except NameError: pass
+    try: torch.cuda.manual_seed_all(s)
+    except NameError: pass
+    try: np.random.seed(s%(2**32-1))
+    except NameError: pass
+    random.seed(s)
+    if reproducible:
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+def to_device(t, device="cpu"):
+    if isinstance(t, (tuple, list)):
+        return [_t.to(device) for _t in t]
+    elif isinstance(t, torch.Tensor):
+        return t.to(device)
+    else:
+        raise("Not a Tensor or list of Tensors")
+
+
+def ls(path: Path): 
+    "Return files on Path, sorted"
+    return sorted(list(path.iterdir()))
+
+def init_ddpm(model):
+    "From Jeremy's bag of tricks on fastai V2 2023"
+    for o in model.down_blocks:
+        for p in o.resnets:
+            p.conv2.weight.data.zero_()
+            for p in fc.L(o.downsamplers): init.orthogonal_(p.conv.weight)
+
+    for o in model.up_blocks:
+        for p in o.resnets: p.conv2.weight.data.zero_()
+
+    model.conv_out.weight.data.zero_()
+
+@torch.no_grad()
+def diff_sample(model, past_frames, sched, **kwargs):
+    "Using Diffusers built-in samplers"
+    model.eval()
+    device = next(model.parameters()).device
+    new_frame = torch.randn_like(past_frames[:,-1:], dtype=past_frames.dtype, device=device)
+    preds = []
+    for t in progress_bar(sched.timesteps, leave=False):
+        noise = model(torch.cat([past_frames, new_frame], dim=1), t).sample
+        new_frame = sched.step(noise, t, new_frame, **kwargs).prev_sample
+        preds.append(new_frame.float().cpu())
+    return preds
+
+def ddim_sampler(steps=350, eta=1.):
+    "DDIM sampler, faster and a bit better than the built-in sampler"
+    ddim_sched = DDIMScheduler()
+    ddim_sched.set_timesteps(steps)
+    return partial(diff_sample, sched=ddim_sched, eta=eta)
+
+
+## Wandb functions
+
+def to_wandb_image(img):
+    "Convert a tensor to a wandb.Image"
+    return wandb.Image(torch.cat(img.split(1), dim=-1).cpu().numpy())
+
+def log_images(xt, samples):
+    "Log sampled images to wandb"
+    predicted = samples[-1]
+    device = predicted.device
+    frames = torch.cat([xt[:, :-1,...].to(device), samples[-1]], dim=1)
+    wandb.log({"sampled_images": [to_wandb_image(img) for img in frames]})
+
+def save_model(model, model_name):
+    "Save the model to wandb"
+    model_name = f"{wandb.run.id}_{model_name}"
+    models_folder = Path("models")
+    if not models_folder.exists():
+        models_folder.mkdir()
+    torch.save(model.state_dict(), models_folder/f"{model_name}.pth")
+    at = wandb.Artifact(model_name, type="model")
+    at.add_file(f"models/{model_name}.pth")
+    wandb.log_artifact(at)
+
+def parse_args(config):
+    "A brute force way to parse arguments, it is probably not a good idea to use it"
+    parser = argparse.ArgumentParser(description='Run training baseline')
+    for k,v in config.__dict__.items():
+        parser.add_argument('--'+k, type=type(v), default=v)
+    args = vars(parser.parse_args())
+    
+    # update config with parsed args
+    for k, v in args.items():
+        setattr(config, k, v)
