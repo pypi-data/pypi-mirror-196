55593e15769e509b6fe35b9cbb2b63f98c120e2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:15 +0000

    Add package videoxt/0.1.3/videoxt-0.1.3.tar.gz

---
+++ packages/videoxt/videoxt-0.1.3.tar.gz/videoxt-0.1.3/videoxt/utils.py
@@ -0,0 +1,24 @@
+import datetime
+from typing import Union
+
+
+def timestamp_to_seconds(timestamp: str) -> int:
+    """Converts a timestamp string to seconds. Milliseconds are truncated."""
+    if "." in timestamp:
+        timestamp = timestamp.split(".")[0]
+
+    if ":" in timestamp:
+        seconds = 0
+        for value in timestamp.split(":"):
+            seconds = seconds * 60 + int(value)
+    else:
+        seconds = int(timestamp)
+
+    return seconds
+
+
+def seconds_to_timestamp(seconds: Union[float, int]) -> str:
+    """Converts seconds to a timestamp string. Milliseconds are truncated."""
+    if isinstance(seconds, float):
+        seconds = int(seconds)
+    return str(datetime.timedelta(seconds=seconds))
