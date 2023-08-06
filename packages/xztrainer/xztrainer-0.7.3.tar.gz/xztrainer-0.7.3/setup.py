ee463c8364573188eed6d6be8fbee65804149d63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:39 +0000

    Add package xztrainer/0.7.3/xztrainer-0.7.3.tar.gz

---
+++ packages/xztrainer/xztrainer-0.7.3.tar.gz/xztrainer-0.7.3/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['xztrainer', 'xztrainer.logger']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.22.1', 'tqdm>=4.62.3']
+
+extras_require = \
+{'deepspeed:sys_platform == "linux"': ['deepspeed>=0.5.10'],
+ 'tensorboard': ['tensorboard>=2.8.0'],
+ 'torch': ['torch>=1.10.0']}
+
+setup_kwargs = {
+    'name': 'xztrainer',
+    'version': '0.7.3',
+    'description': 'A customizable training pipeline for PyTorch',
+    'long_description': 'None',
+    'author': 'Maxim Afanasyev',
+    'author_email': 'mr.applexz@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.8',
+}
+
+
+setup(**setup_kwargs)
