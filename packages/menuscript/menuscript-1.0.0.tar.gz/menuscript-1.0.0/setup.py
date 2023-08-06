d34542180944bafa7f0a201731a34d30e9d3db48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:52 +0000

    Add package menuscript/1.0.0/menuscript-1.0.0.tar.gz

---
+++ packages/menuscript/menuscript-1.0.0.tar.gz/menuscript-1.0.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="menuscript",
+    version="1.0.0",
+    author="Your Name",
+    author_email="alertsontwitter@gmail.com",
+    description="A menu-based script generator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AIMadeScripts/createmenus",
+    py_modules=["menuscript"],
+    entry_points={
+        "console_scripts": [
+            "menuscript=menuscript:create_script"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
