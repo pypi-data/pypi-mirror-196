4fb8fb943c40edc9866c8bddd8e92654f6e1dcca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:05 +0000

    Add package python-circe/0.0.2/python-circe-0.0.2.tar.gz

---
+++ packages/python-circe/python-circe-0.0.2.tar.gz/python-circe-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_namespace_packages
+
+
+setup(
+    name='python-circe',
+    description='Python wrapper for Circe',
+    long_description='Python wrapper for Circe',
+    long_description_content_type='text/markdown',
+    version='0.0.2',
+    packages=find_namespace_packages(),
+    include_package_data=True,
+    package_data={
+        'circe': ['*.jar', '*.json'],
+    },
+    python_requires='>=3.9',
+    install_requires=[
+        'jpype1==1.4.1',
+    ]
+)
