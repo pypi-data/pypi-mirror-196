55ad7faa44542d96a59280c0817e20891bbb851c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:50 +0000

    Add package seldom/3.1.5/seldom-3.1.5-py3-none-any.whl

---
+++ packages/seldom/seldom-3.1.5-py3-none-any.whl/demo/test_dir/web_case/test_po_demo.py
@@ -0,0 +1,35 @@
+"""
+page object model
+Using the poium Library
+https://github.com/SeldomQA/poium
+```
+> pip install poium=1.2.0
+```
+"""
+import seldom
+from seldom import Seldom
+from poium import Page, Element
+
+
+class BaiduPage(Page):
+    """baidu page"""
+    input = Element(id_="kw", describe="搜索输入框")
+    button = Element(id_="su", describe="搜索按钮")
+
+
+class BaiduTest(seldom.TestCase):
+    """Baidu search test case"""
+
+    def test_case(self):
+        """
+        A simple test
+        """
+        page = BaiduPage(Seldom.driver, print_log=True)
+        page.open("https://www.baidu.com")
+        page.input.send_keys("seldom")
+        page.button.click()
+        self.assertElement(page.asser_ele)
+
+
+if __name__ == '__main__':
+    seldom.main(browser='chrome')
---
+++ packages/seldom/seldom-3.1.5-py3-none-any.whl/seldom/running/config.py
@@ -0,0 +1,28 @@
+"""
+Seldom configuration file
+"""
+
+
+class Seldom:
+    """
+    Seldom browser driver
+    """
+    driver = None
+    timeout = 10
+    debug = False
+    base_url = None
+    compare_url = None
+    app_server = None
+    app_info = None
+    env = None
+    api_data_url = None
+
+
+class BrowserConfig:
+    """
+    Define run browser config
+    """
+    NAME = None
+    REPORT_PATH = None
+    REPORT_TITLE = "Seldom Test Report"
+    LOG_PATH = None
