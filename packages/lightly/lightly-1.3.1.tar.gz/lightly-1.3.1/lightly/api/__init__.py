c540651bce871c275de185652f7d04e761a5eda6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:09 +0000

    Add package lightly/1.3.1/lightly-1.3.1.tar.gz

---
+++ packages/lightly/lightly-1.3.1.tar.gz/lightly-1.3.1/lightly/api/__init__.py
@@ -0,0 +1,12 @@
+""" The lightly.api module provides access to the Lightly web-app. """
+
+# Copyright (c) 2020. Lightly AG and its affiliates.
+# All Rights Reserved
+
+from lightly.api.api_workflow_artifacts import ArtifactNotExist
+from lightly.api.api_workflow_client import ApiWorkflowClient
+from lightly.api.patch_rest_client import patch_rest_client
+from lightly.openapi_generated.swagger_client.rest import RESTClientObject
+
+# Needed to handle list of arguments correctly
+patch_rest_client(RESTClientObject)
