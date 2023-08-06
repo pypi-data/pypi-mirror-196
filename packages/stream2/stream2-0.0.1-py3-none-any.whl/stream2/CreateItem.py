268680360919860e94e4dcb96580855bbfad15dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:58 +0000

    Add package stream2/0.0.1/stream2-0.0.1-py3-none-any.whl

---
+++ packages/stream2/stream2-0.0.1-py3-none-any.whl/stream2/CreateItem.py
@@ -0,0 +1,22 @@
+import threading
+
+from BranchNode import BranchNode
+
+
+class CreateItem:
+    _instance = None
+    _lock = threading.Lock()
+    __item_obj = None
+
+    @classmethod
+    def _get_instance(cls):
+        with cls._lock:
+            if not cls._instance:
+                cls._instance = cls()
+            return cls._instance
+
+    def item(self, _obj: object):
+        self.__item_obj = _obj
+        bn = BranchNode._get_instance()
+        bn._set_item_obj(self.__item_obj)
+        return bn
