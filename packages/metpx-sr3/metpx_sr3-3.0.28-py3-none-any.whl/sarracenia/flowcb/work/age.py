51d960b02635f9ea5d5021cd891b2dc49ce124bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:55 +0000

    Add package metpx-sr3/3.0.28/metpx_sr3-3.0.28-py3-none-any.whl

---
+++ packages/metpx-sr3/metpx_sr3-3.0.28-py3-none-any.whl/sarracenia/flowcb/work/age.py
@@ -0,0 +1,26 @@
+"""
+   print the age of files written (compare current time to mtime of message.)
+   usage:
+
+   flowcb work.age
+
+"""
+
+import os, stat, time
+import logging
+from sarracenia.flowcb import FlowCB
+from sarracenia import nowflt, timestr2flt
+
+logger = logging.getLogger(__name__)
+
+
+class Age(FlowCB):
+
+    def after_work(self, worklist):
+        for m in worklist.ok:
+            if not 'mtime' in m:
+                return None
+            completed = timestr2flt(m['timeCompleted'])
+            mtime = timestr2flt(m['mtime'])
+            age = completed - mtime
+            logger.info("file %s is %d seconds old" % (m['new_file'], age))
