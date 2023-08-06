4d6ac00761cc4b2c401e9a1cbe401a884bfe06e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:27 +0000

    Add package fmdpy/0.6.1/fmdpy-0.6.1.tar.gz

---
+++ packages/fmdpy/fmdpy-0.6.1.tar.gz/fmdpy-0.6.1/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+from fmdpy import VERSION, install_requires
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fmdpy",
+    version=VERSION,
+    author="Rohn Chatterjee",
+    author_email="rohn.ch@gmail.com",
+    description="Music Downloader",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Liupold/fmdpy",
+    license='GPLv3',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    entry_points = {
+        'console_scripts': ['fmdpy=fmdpy.__main__:fmdpy'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable"
+    ],
+    python_requires='>=3.7',
+)
