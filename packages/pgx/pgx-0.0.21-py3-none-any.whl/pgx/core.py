ed998874d3e44d11d4dc3916c75d292866a44c26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:02 +0000

    Add package pgx/0.0.21/pgx-0.0.21-py3-none-any.whl

---
+++ packages/pgx/pgx-0.0.21-py3-none-any.whl/pgx/core.py
@@ -0,0 +1,135 @@
+import abc
+from typing import Literal, Tuple
+
+import jax
+import jax.numpy as jnp
+
+from pgx.flax.struct import dataclass
+
+TRUE = jnp.bool_(True)
+
+
+EnvId = Literal[
+    "tic_tac_toe/v0",
+    "go-19x19/v0",
+    "shogi/v0",
+    "backgammon/v0",
+    "minatar/asterix/v0",
+]
+
+
+@dataclass
+class State:
+    curr_player: jnp.ndarray
+    observation: jnp.ndarray
+    reward: jnp.ndarray
+    terminated: jnp.ndarray
+    legal_action_mask: jnp.ndarray
+
+    def _repr_html_(self) -> str:
+        from pgx.visualizer import Visualizer
+
+        v = Visualizer()
+        return v._to_dwg_from_states(states=self).tostring()
+
+
+class Env(abc.ABC):
+    def __init__(self):
+        ...
+
+    def init(self, key: jax.random.KeyArray) -> State:
+        state = self._init(key)
+        observation = self.observe(state, state.curr_player)
+        return state.replace(observation=observation)  # type: ignore
+
+    def step(self, state: State, action: jnp.ndarray) -> State:
+        # TODO: legal_action_mask周りの挙動
+        #  - set legal_action_mask = all True
+        #    - Typical usage of legal_action_mask is normalizing action probability
+        #    - all zero mask will raise zero division error
+        #  - ends with negative reward if illegal action is taken
+
+        is_illegal = ~state.legal_action_mask[action]
+        curr_player = state.curr_player
+        # If the state is already terminated, environment does not take usual step, but
+        # return the same state with zero-rewards for all players
+        state = jax.lax.cond(
+            state.terminated,
+            lambda: self._step_if_terminated(state),
+            lambda: self._step(state, action),
+        )
+        # Taking illegal action leads to immediate game terminal with negative reward
+        state = jax.lax.cond(
+            is_illegal,
+            lambda: self._step_with_illegal_action(state, curr_player),
+            lambda: state,
+        )
+        # All legal_action_mask elements are TRUE (**not FALSE**) at terminal state
+        # This is to avoid zero-division error when normalizing action probability
+        # Taking any action at terminal state does not give any effect to the state
+        state = jax.lax.cond(
+            state.terminated,
+            lambda: state.replace(  # type: ignore
+                legal_action_mask=jnp.ones_like(state.legal_action_mask)
+            ),
+            lambda: state,
+        )
+        observation = self.observe(state, state.curr_player)
+        return state.replace(observation=observation)  # type: ignore
+
+    @abc.abstractmethod
+    def _init(self, key: jax.random.KeyArray) -> State:
+        """Implement game-specific init function here."""
+        ...
+
+    @abc.abstractmethod
+    def _step(self, state, action) -> State:
+        """Implement game-specific step function here."""
+        ...
+
+    @abc.abstractmethod
+    def observe(self, state: State, player_id: jnp.ndarray) -> jnp.ndarray:
+        """Implement game-specific observe function here."""
+        ...
+
+    @property
+    @abc.abstractmethod
+    def num_players(self) -> int:
+        ...
+
+    @property
+    @abc.abstractmethod
+    def reward_range(self) -> Tuple[float, float]:
+        """Note that min reward must be <= 0."""
+        ...
+
+    @property
+    def observation_shape(self) -> Tuple[int, ...]:
+        """Return the matrix shape of observation"""
+        state = self.init(jax.random.PRNGKey(0))
+        obs = self.observe(state, state.curr_player)
+        return obs.shape
+
+    @property
+    def action_shape(self) -> Tuple[int, ...]:
+        """Return the matrix shape of legal_action_mask"""
+        state = self.init(jax.random.PRNGKey(0))
+        return state.legal_action_mask.shape
+
+    @staticmethod
+    def _step_if_terminated(state: State) -> State:
+        return state.replace(  # type: ignore
+            reward=jnp.zeros_like(state.reward),
+        )
+
+    def _step_with_illegal_action(
+        self, state: State, loser: jnp.ndarray
+    ) -> State:
+        min_reward = self.reward_range[0]
+        reward = (
+            jnp.ones_like(state.reward)
+            * (-1 * min_reward)
+            * (self.num_players - 1)
+        )
+        reward = reward.at[loser].set(min_reward)
+        return state.replace(reward=reward, terminated=TRUE)  # type: ignore
---
+++ packages/pgx/pgx-0.0.21-py3-none-any.whl/pgx/tic_tac_toe.py
@@ -0,0 +1,109 @@
+from typing import Tuple
+
+import jax
+import jax.numpy as jnp
+
+import pgx.core as core
+from pgx.flax.struct import dataclass
+
+FALSE = jnp.bool_(False)
+TRUE = jnp.bool_(True)
+
+# fmt off
+IDX = jnp.int8(
+    [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+)
+# fmt on
+
+
+@dataclass
+class State(core.State):
+    curr_player: jnp.ndarray = jnp.int8(0)
+    observation: jnp.ndarray = jnp.zeros(27, dtype=jnp.bool_)
+    reward: jnp.ndarray = jnp.float32([0.0, 0.0])
+    terminated: jnp.ndarray = FALSE
+    legal_action_mask: jnp.ndarray = jnp.ones(9, dtype=jnp.bool_)
+    turn: jnp.ndarray = jnp.int8(0)
+    # 0 1 2
+    # 3 4 5
+    # 6 7 8
+    board: jnp.ndarray = -jnp.ones(9, jnp.int8)  # -1 (empty), 0, 1
+
+
+class TicTacToe(core.Env):
+    def __init__(self):
+        super().__init__()
+
+    def _init(self, key: jax.random.KeyArray) -> State:
+        return init(key)
+
+    def _step(self, state: core.State, action: jnp.ndarray) -> State:
+        assert isinstance(state, State)
+        return step(state, action)
+
+    def observe(
+        self, state: core.State, player_id: jnp.ndarray
+    ) -> jnp.ndarray:
+        assert isinstance(state, State)
+        return observe(state, player_id)
+
+    @property
+    def num_players(self) -> int:
+        return 2
+
+    @property
+    def reward_range(self) -> Tuple[float, float]:
+        return -1.0, 1.0
+
+
+def init(rng: jax.random.KeyArray) -> State:
+    rng, subkey = jax.random.split(rng)
+    curr_player = jnp.int8(jax.random.bernoulli(subkey))
+    return State(curr_player=curr_player)  # type:ignore
+
+
+def step(state: State, action: jnp.ndarray) -> State:
+    board = state.board.at[action].set(state.turn)
+    won = _win_check(board, state.turn)
+    reward = jax.lax.cond(
+        won,
+        lambda: jnp.float32([-1, -1]).at[state.curr_player].set(1),
+        lambda: jnp.zeros(2, jnp.float32),
+    )
+    terminated = won | jnp.all(board != -1)
+    curr_player = (state.curr_player + 1) % 2
+    legal_action_mask = board < 0
+    return State(
+        curr_player=curr_player,
+        legal_action_mask=legal_action_mask,
+        reward=reward,
+        terminated=terminated,
+        turn=(state.turn + 1) % 2,
+        board=board,
+    )  # type: ignore
+
+
+def _win_check(board, turn) -> jnp.ndarray:
+    return ((board[IDX] == turn).all(axis=1)).any()
+
+
+def observe(state: State, player_id: jnp.ndarray) -> jnp.ndarray:
+    empty_board = state.board == -1
+    my_board, opp_obard = jax.lax.cond(
+        state.curr_player == player_id,  # flip board if player_id is opposite
+        lambda: (state.turn == state.board, (1 - state.turn) == state.board),
+        lambda: ((1 - state.turn) == state.board, state.turn == state.board),
+    )
+    return jnp.concatenate(
+        [empty_board, my_board, opp_obard],
+        dtype=jnp.bool_,
+    )
