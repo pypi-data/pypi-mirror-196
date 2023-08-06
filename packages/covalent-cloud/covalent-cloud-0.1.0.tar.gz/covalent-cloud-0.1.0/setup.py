955e09508dbc5deff836549391543d1ab9620fad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:53 +0000

    Add package covalent-cloud/0.1.0/covalent-cloud-0.1.0.tar.gz

---
+++ packages/covalent-cloud/covalent-cloud-0.1.0.tar.gz/covalent-cloud-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='covalent-cloud',
+    version='0.1.0',
+    packages=find_packages(where='.'),
+    maintainer='Agnostiq',
+    author='Agnostiq',
+    install_requires=[],
+    url='https://github.com/AgnostiqHQ/covalent-cloud-sdk',
+    download_url='https://github.com/AgnostiqHQ/covalent-cloud-sdk/archive/v0.1.0.tar.gz',
+)
