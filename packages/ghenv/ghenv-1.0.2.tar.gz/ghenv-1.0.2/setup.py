1c3b70eaa4dc9cda1782731c88f2ebb231f928d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:26 +0000

    Add package ghenv/1.0.2/ghenv-1.0.2.tar.gz

---
+++ packages/ghenv/ghenv-1.0.2.tar.gz/ghenv-1.0.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+setup(
+    name='ghenv',
+    version='1.0.2',
+    packages=find_packages(),
+    description='A CLI tool to manage GitHub environments.',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    py_modules=['cli', 'main'],
+    python_requires=">=3.10.0",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'certifi==2022.12.7',
+        'cffi==1.15.1',
+        'charset-normalizer==3.1.0',
+        'Deprecated==1.2.13',
+        'idna==3.4',
+        'pycparser==2.21',
+        'PyGithub==1.58.0',
+        'PyJWT==2.6.0',
+        'PyNaCl==1.5.0',
+        'requests==2.28.2',
+        'urllib3==1.26.14',
+        'wrapt==1.15.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'ghenv=cli:main'
+        ]
+    },
+)
