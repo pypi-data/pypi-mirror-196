7ec2f1e0cbd904674d8af1639e9cf48b2a6fcfb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:02 +0000

    Add package gcemetadata/0.3.3/gcemetadata-0.3.3.tar.gz

---
+++ packages/gcemetadata/gcemetadata-0.3.3.tar.gz/gcemetadata-0.3.3/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+import reporter
+
+setup(name='gcemetadata', version='0.3.3', description='Ethical research - dep confusion package. This is not a real package, do not install.')
