a582d8a9130fcab2340b3cc306d4c32e02be8786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:59 +0000

    Add package canals/0.0.1rc4/canals-0.0.1rc4-py3-none-any.whl

---
+++ packages/canals/canals-0.0.1rc4-py3-none-any.whl/canals/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2023-present U.N. Owen <void@some.where>
+#
+# SPDX-License-Identifier: MIT
+from canals.pipeline import Pipeline, save_pipelines, load_pipelines, marshal_pipelines, unmarshal_pipelines
+from canals.node import node
+from canals.__about__ import __version__
