126ff6d33e16ac811a3476156c0e00e8faa4cdac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:27 +0000

    Add package tensorflow-privacy/0.8.8/tensorflow_privacy-0.8.8.tar.gz

---
+++ packages/tensorflow-privacy/tensorflow_privacy-0.8.8.tar.gz/tensorflow_privacy-0.8.8/tensorflow_privacy/privacy/fast_gradient_clipping/clip_grads.py
@@ -0,0 +1,225 @@
+# Copyright 2022, The TensorFlow Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Computes per-example loss clip weights.
+
+For a given Keras model and batch of inputs, computes the per-example
+clip weights so that the gradient of the loss function, weighted by these
+weights, is equivalent to the gradient of the original loss function but
+with the per-example gradients clipped by some clip weight. Uses a variant
+of the approach given in https://arxiv.org/pdf/2009.03106.pdf (see the
+`compute_gradient_norms()` function).
+"""
+
+from typing import Dict, Iterable, Optional, Text, Union
+
+import tensorflow as tf
+from tensorflow_privacy.privacy.fast_gradient_clipping import gradient_clipping_utils
+from tensorflow_privacy.privacy.fast_gradient_clipping import layer_registry as lr
+
+InputTensor = Union[tf.Tensor, Iterable[tf.Tensor], Dict[Text, tf.Tensor]]
+
+
+def get_registry_generator_fn(
+    tape: tf.GradientTape,
+    layer_registry: lr.LayerRegistry,
+    num_microbatches: Optional[lr.BatchSize] = None,
+):
+  """Creates the generator function for `compute_gradient_norms()`."""
+  if layer_registry is None:
+    # Needed for backwards compatibility.
+    registry_generator_fn = None
+  else:
+
+    def registry_generator_fn(layer_instance, args, kwargs):
+      if layer_instance.trainable_variables:
+        # Only trainable variables factor into the gradient.
+        if not layer_registry.is_elem(layer_instance):
+          raise NotImplementedError(
+              'Layer %s is not in the registry of known layers that can '
+              'be used for efficient gradient clipping.'
+              % layer_instance.__class__.__name__
+          )
+        registry_fn = layer_registry.lookup(layer_instance)
+        (layer_vars, layer_outputs, layer_sqr_norm_fn) = registry_fn(
+            layer_instance, args, kwargs, tape, num_microbatches
+        )
+        return layer_outputs, (layer_vars, layer_sqr_norm_fn)
+      else:
+        # Non-trainable layer.
+        return layer_instance(*args, **kwargs), None
+
+  return registry_generator_fn
+
+
+def compute_gradient_norms(
+    input_model: tf.keras.Model,
+    x_batch: InputTensor,
+    y_batch: tf.Tensor,
+    layer_registry: lr.LayerRegistry,
+    num_microbatches: Optional[lr.BatchSize] = None,
+):
+  """Computes the per-example loss gradient norms for given data.
+
+  Applies a variant of the approach given in
+    https://arxiv.org/pdf/2009.03106.pdf
+
+  Args:
+    input_model: The `tf.keras.Model` from which to obtain the layers from. The
+      loss of the model *must* be a scalar loss.
+    x_batch: An `InputTensor` representing a batch of inputs to the model. The
+      first axis must be the batch dimension.
+    y_batch: A `tf.Tensor` representing a batch of output labels. The first axis
+      must be the batch dimension. The number of examples should match the
+      number of examples in `x_batch`.
+    layer_registry: A `LayerRegistry` instance containing functions that help
+      compute gradient norms quickly. See
+      `tensorflow_privacy.privacy.fast_gradient_clipping.layer_registry` for
+      more details.
+    num_microbatches: An optional number or scalar `tf.Tensor` for the number of
+      microbatches. If not None, indicates that the loss is grouped into
+      num_microbatches (in this case, the batch dimension needs to be a multiple
+      of num_microbatches). When there is microbatches, we always assume the
+      loss is the mean over a microbatch. And the gradient norm is computed for
+      each microbatch.
+
+  Returns:
+    A 1D `tf.Tensor` whose i-th entry is the norm of the gradient of the i-th
+    per-example loss function.
+  """
+  tape = tf.GradientTape(persistent=True, watch_accessed_variables=False)
+  registry_generator_fn = get_registry_generator_fn(
+      tape, layer_registry, num_microbatches
+  )
+  # First loop computes the model outputs, summed loss, and generator outputs.
+  with tape:
+    model_outputs, generator_outputs_list = (
+        gradient_clipping_utils.model_forward_pass(
+            input_model, x_batch, generator_fn=registry_generator_fn
+        )
+    )
+    # Ignore the original loss function's reduction to get per-example loss.
+    loss_config = input_model.loss.get_config()
+    loss_config['reduction'] = tf.keras.losses.Reduction.NONE
+    per_example_loss_fn = input_model.loss.from_config(loss_config)
+    losses = per_example_loss_fn(y_batch, model_outputs)
+    if num_microbatches is not None:
+      losses = tf.reduce_mean(
+          lr.add_microbatch_axis(losses, num_microbatches), axis=1
+      )
+    summed_loss = tf.reduce_sum(losses)
+  # Unwrap the generator outputs so that the next loop avoids duplicating
+  # backprop ops.
+  filtered_outputs = [t for t in generator_outputs_list if t is not None]
+  vars_list = [a for (a, b) in filtered_outputs]
+  sqr_norm_fns_list = [b for (a, b) in filtered_outputs]
+  # Second loop evaluates the squared L2 norm functions and appends the results.
+  grads_list = tape.gradient(summed_loss, vars_list)
+  sqr_norm_list = []
+  for grads, f in zip(grads_list, sqr_norm_fns_list):
+    sqr_norm_list.append(f(grads))
+  del tape
+  sqr_norm_tsr = tf.stack(sqr_norm_list, axis=1)
+  return tf.sqrt(tf.reduce_sum(sqr_norm_tsr, axis=1))
+
+
+def compute_clip_weights(l2_norm_clip: float, gradient_norms: tf.Tensor):
+  """Computes the per-example loss/clip weights for clipping.
+
+  When the sum of the per-example losses is replaced a weighted sum, where
+  the weights are generated by this method, then the gradients of each
+  term in the weighted sum are clipped by the given clip value.
+
+  Args:
+    l2_norm_clip: A `float` indicating the norm to which per-example gradients
+      will be clipped. That is, all gradients of the per-example loss functions
+      will have norm at most `l2_norm_clip`.
+    gradient_norms: A 1D `tf.Tensor` whose i-th entry is the norm of the
+      gradient of the loss function for the i-th input.
+
+  Returns:
+    A 1D `tf.Tensor` representing whose i-th entry `C[i]` is either `1.0` if the
+    norm of the gradient of i-th per-example loss `G[i]` is less than
+    `l2_norm_clip` or a number less than `1.0` so that
+    `|G[i]| * C[i] == l2_norm_clip` otherwise.
+  """
+  if l2_norm_clip is None:
+    return None
+  return l2_norm_clip / tf.math.maximum(l2_norm_clip, gradient_norms)
+
+
+def compute_pred_and_clipped_gradients(
+    input_model: tf.keras.Model,
+    x_batch: InputTensor,
+    y_batch: tf.Tensor,
+    l2_norm_clip: float,
+    layer_registry: lr.LayerRegistry,
+    num_microbatches: Optional[lr.BatchSize] = None,
+):
+  """Computes the per-example predictions and per-example clipped loss gradient.
+
+  Given a batch of observations `(x_batch, y_batch)`, the main steps of this
+  function are: (i) compute the l2-norm of the gradients of the trainable
+  variables of `input_model` for each example in the batch; (ii) use the norms
+  computed in (i) to obtain "clip_weights" that are used to generate a weighted
+  loss function whose gradient for each example has l2-norm at most
+  `l2_norm_clip`; (iii) output the clipped gradients in (ii) and the
+  `tf.Tensor` generated by `input_model` when it is given `x_batch` as its
+  input.
+
+  Args:
+    input_model: The `tf.keras.Model` from which to obtain the layers from.
+    x_batch: An `InputTensor` representing a batch of inputs to the model. The
+      first axis must be the batch dimension.
+    y_batch: A `tf.Tensor` representing a batch of output labels. The first axis
+      must be the batch dimension. The number of examples should match the
+      number of examples in `x_batch`.
+    l2_norm_clip: A `float` indicating the norm to which per-example gradients
+      will be clipped. That is, all gradients of the per-example loss functions
+      will have norm at most `l2_norm_clip`.
+    layer_registry: A `dict` of layers that support "fast" gradient norm
+      computations. The key is the class of the layer and the value is a
+      function that returns a `tuple` `(output, sqr_grad_norms, vars)`, where
+      `output` is the pre-activator tensor, `sqr_grad_norms` is related to the
+      squared norms of a layer's pre-activation tensor, and `vars` are relevant
+      trainable weights (see `layer_registry_factories.py` for examples).
+    num_microbatches: An optional number or scalar `tf.Tensor` for the number of
+      microbatches. If not None, indicates that the loss is grouped into
+      num_microbatches (in this case, the batch dimension needs to be a multiple
+      of num_microbatches).
+
+  Returns:
+    A `tuple` `(y_pred, grad)`. The first element is the prediction generated by
+    the model on the input `x_batch`. The second element is the clipped
+    gradient of the loss function.
+  """
+  gradient_norms = compute_gradient_norms(
+      input_model, x_batch, y_batch, layer_registry, num_microbatches
+  )
+  loss_weights = compute_clip_weights(l2_norm_clip, gradient_norms)
+  with tf.GradientTape() as tape:
+    y_pred = input_model(x_batch, training=True)
+    if num_microbatches is not None:
+      y_batch = lr.add_microbatch_axis(y_batch, num_microbatches)
+      y_pred = lr.add_microbatch_axis(y_pred, num_microbatches)
+    # Warning: When num_microbatches is not None, we need to be sure that
+    # `compute_loss` always computes the mean over the microbatches
+    # as it is the assumption made when computing the gradient norm.
+    # It is indeed the case for multiple keras loss functions
+    # (e.g. mean_squared_error and binary_crossentropy). However it
+    # is not defined in the contract so may not hold, especially for
+    # custom losses.
+    loss_value = input_model.compute_loss(
+        x_batch, y_batch, y_pred, loss_weights
+    )
+  return y_pred, tape.gradient(loss_value, input_model.trainable_variables)
---
+++ packages/tensorflow-privacy/tensorflow_privacy-0.8.8.tar.gz/tensorflow_privacy-0.8.8/tensorflow_privacy/privacy/fast_gradient_clipping/gradient_clipping_utils.py
@@ -0,0 +1,219 @@
+# Copyright 2022, The TensorFlow Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Utility functions that help in the computation of per-example gradient norms."""
+
+from typing import Any, Callable, Dict, Iterable, List, Optional, Text, Tuple, Union
+
+from absl import logging
+import tensorflow as tf
+
+from tensorflow_privacy.privacy.fast_gradient_clipping import layer_registry as lr
+
+InputTensor = Union[tf.Tensor, Iterable[tf.Tensor], Dict[Text, tf.Tensor]]
+
+GeneratorFunction = Optional[Callable[[Any, Tuple, Dict], Tuple[Any, Any]]]
+
+
+def has_internal_compute_graph(input_object: Any):
+  """Checks if input is a TF model and has a TF internal compute graph."""
+  return (
+      isinstance(input_object, tf.keras.Model)
+      and hasattr(input_object, '_flatten_to_reference_inputs')
+      and hasattr(input_object, '_tensor_usage_count')
+      and hasattr(input_object, '_conform_to_reference_input')
+      and hasattr(input_object, '_nodes_by_depth')
+  )
+
+
+def _get_internal_layers(
+    input_layer: tf.keras.layers.Layer,
+) -> List[tf.keras.layers.Layer]:
+  """Returns a list of layers that are nested within a given layer."""
+  internal_layers = []
+  if isinstance(input_layer, tf.keras.Model) and hasattr(input_layer, 'layers'):
+    for layer in input_layer.layers:
+      internal_layers.extend(_get_internal_layers(layer))
+  else:
+    internal_layers.append(input_layer)
+  return internal_layers
+
+
+def model_forward_pass(
+    input_model: tf.keras.Model,
+    inputs: InputTensor,
+    generator_fn: GeneratorFunction = None,
+) -> Tuple[tf.Tensor, List[Any]]:
+  """Does a forward pass of a model and returns useful intermediates.
+
+  NOTE: the graph traversal algorithm is an adaptation of the logic in the
+    _run_internal_graph() method in the functional.Functional class. Hence,
+    forward_norm_pass should only be invoked if the generated model
+    instance is an instance of the functional.Functional class.
+
+  Args:
+    input_model: A `tf.keras.Model` to compute the quantities for.
+    inputs: Arbitrary input to be fed into the input layer of the model. It is
+      expected that `input_model(inputs)` returns a valid output.
+    generator_fn: A function with signature `(tf.keras.layers.Layer, Any, Any)
+      -> (tf.Tensor, Any)`, where we require `generator_fn(layer_instance, args,
+      kwargs)[0] == layer_instance(*args, **kwargs)`. If `None`, then
+      `layer_fn(layer_instance, args, kwargs)[1] == None`.
+
+  Returns:
+    A `tuple` `(outputs, generator_outputs_list)`. `outputs` is the
+    `tf.Tensor` that is generated as a result of a forward pass.
+    `generator_outputs_list` is a `list` whose i-th entry is the output of
+    `generator_fn(lyr, args, kwargs)[1]` where `lyr` is the i-th
+    layer when the compute graph of `input_model` is traversed in BFS order.
+  """
+  # TODO: Avoid or remove the references to protected methods of `input_model`.  # pylint: disable=g-bad-todo
+
+  # Default generator.
+  generator_outputs_list = []
+  if generator_fn is None:
+
+    def generator_fn(layer_instance, args, kwargs):
+      return layer_instance(*args, **kwargs), None
+
+  # Prepare the inputs and BFS variables.
+  flattened_inputs = input_model._flatten_to_reference_inputs(inputs)  # pylint: disable=protected-access
+  tensor_dict = {}
+  tensor_usage_count = input_model._tensor_usage_count  # pylint: disable=protected-access
+  for x, y in zip(input_model.inputs, flattened_inputs):
+    y = input_model._conform_to_reference_input(y, ref_input=x)  # pylint: disable=protected-access
+    x_id = str(id(x))
+    tensor_dict[x_id] = [y] * tensor_usage_count[x_id]
+  nodes_by_depth = input_model._nodes_by_depth  # pylint: disable=protected-access
+  depth_keys = list(nodes_by_depth.keys())
+  depth_keys.sort(reverse=True)
+
+  # Perform BFS feedforward computations.
+  for depth in depth_keys:
+    for node in nodes_by_depth[depth]:
+      if node.is_input:
+        continue  # inputs already exist
+      if any(t_id not in tensor_dict for t_id in node.flat_input_ids):
+        continue  # node is not computable; try skipping
+      args, kwargs = node.map_arguments(tensor_dict)
+      if has_internal_compute_graph(node.layer):
+        # If this node has an internal computational graph, we can recurse.
+        node_layer_outputs, node_generator_outputs = model_forward_pass(
+            node.layer, args, generator_fn
+        )
+        generator_outputs_list.extend(node_generator_outputs)
+      else:
+        # Otherwise, we parse the node directly.
+        node_layers = _get_internal_layers(node.layer)
+        for layer in node_layers:
+          node_layer_outputs, layer_generator_outputs = generator_fn(
+              layer, args, kwargs
+          )
+          generator_outputs_list.append(layer_generator_outputs)
+          args = (
+              node_layer_outputs
+              if isinstance(node_layer_outputs, tuple)
+              else (node_layer_outputs,)
+          )
+          kwargs = {}
+
+      # Update the current dictionary of inputs for the next node.
+      for x_id, y in zip(
+          node.flat_output_ids, tf.nest.flatten(node_layer_outputs)
+      ):
+        tensor_dict[x_id] = [y] * tensor_usage_count[x_id]
+
+  return node_layer_outputs, generator_outputs_list
+
+
+def all_trainable_layers_are_registered(
+    input_model: tf.keras.Model, layer_registry: lr.LayerRegistry
+) -> bool:
+  """Check if an input model's trainable layers are all registered.
+
+  Args:
+    input_model: The Keras model from which to obtain the layers from.
+    layer_registry: A `LayerRegistry` instance containing functions that help
+      compute gradient norms quickly. See
+      `tensorflow_privacy.privacy.fast_gradient_clipping.layer_registry` for
+      more details.
+
+  Returns:
+    True if all the trainable layers in `input_model` are in `layer_registry`.
+    False otherwise.
+  """
+  for layer in input_model.layers:
+    for sublayer in _get_internal_layers(layer):
+      if not layer_registry.is_elem(sublayer) and sublayer.trainable_variables:
+        return False
+  return True
+
+
+def add_aggregate_noise(
+    input_model: tf.keras.Model,
+    clipped_grads: list[tf.Tensor],
+    batch_size: tf.Tensor,
+    l2_norm_clip: float,
+    noise_multiplier: float,
+) -> List[tf.Tensor]:
+  """Adds noise to a collection of clipped gradients.
+
+  The magnitude of the noise depends on the aggregation strategy of the
+  input model's loss function.
+
+  Args:
+    input_model: The `tf.keras.Model` to obtain the layers from.
+    clipped_grads: A list of `tf.Tensor`s representing the clipped gradients.
+    batch_size: The batch size, used for normalizing the noise, when the loss
+      reduction is AUTO or SUM_OVER_BATCH_SIZE.
+    l2_norm_clip: Clipping norm (max L2 norm of each gradient).
+    noise_multiplier: Ratio of the standard deviation to the clipping norm.
+
+  Returns:
+    A list of tensors containing the clipped gradients, but with the right
+    amount of Gaussian noise added to them (depending on the reduction
+    strategy of the loss function).
+  """
+  scale = l2_norm_clip
+  if input_model.loss.reduction in [
+      tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE,
+      tf.keras.losses.Reduction.AUTO,
+  ]:
+    if input_model.loss.reduction == tf.keras.losses.Reduction.AUTO:
+      logging.info('Assuming that the loss reduction is `SUM_OVER_BATCH_SIZE`.')
+    scale /= tf.cast(batch_size, tf.float32)
+
+  def add_noise(g):
+    return g + tf.random.normal(
+        tf.shape(g), mean=0.0, stddev=noise_multiplier * scale
+    )
+
+  return tf.nest.map_structure(add_noise, clipped_grads)
+
+
+def generate_model_outputs_using_core_keras_layers(
+    input_model: tf.keras.Model,
+) -> tf.Tensor:
+  """Returns the model outputs generated by only core Keras layers."""
+  cust_obj_dict = dict.copy(tf.keras.utils.get_custom_objects())
+  cust_hash_set = set([hash(v) for v in cust_obj_dict.values()])
+
+  def generator_fn(layer_instance, args, kwargs):
+    if hash(layer_instance.__class__) in cust_hash_set:
+      # Using `.call()` does not register the layer in the compute graph of
+      # a forward pass.
+      return layer_instance.call(*args, **kwargs), None
+    else:
+      return layer_instance(*args, **kwargs), None
+
+  return model_forward_pass(input_model, input_model.inputs, generator_fn)[0]
