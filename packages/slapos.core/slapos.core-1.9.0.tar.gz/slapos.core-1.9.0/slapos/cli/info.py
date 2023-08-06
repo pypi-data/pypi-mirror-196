b638e57d819834836fff3dc83e5d263102d65ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:38 +0000

    Add package slapos.core/1.9.0/slapos.core-1.9.0.tar.gz

---
+++ packages/slapos.core/slapos.core-1.9.0.tar.gz/slapos.core-1.9.0/slapos/cli/info.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2010-2014 Vifib SARL and Contributors.
+# All Rights Reserved.
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsibility of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# guarantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation; either version 2.1
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+import json
+import pprint
+import sys
+
+from slapos.cli.command import resetLogger
+from slapos.cli.config import ClientConfigCommand
+from slapos.client import init, ClientConfig
+from slapos.slap import ResourceNotReady, NotFoundError
+
+from slapos.util import (
+  SoftwareReleaseSchema,
+  SoftwareReleaseSerialisation,
+  StrPrettyPrinter,
+  UndefinedSerializationError,
+  xml2dict,
+)
+
+
+class InfoCommand(ClientConfigCommand):
+    """get status, software_release and parameters of an instance"""
+
+    def get_parser(self, prog_name):
+        ap = super(InfoCommand, self).get_parser(prog_name)
+
+        ap.add_argument('reference',
+                        help='Your instance reference')
+
+        return ap
+
+    def take_action(self, args):
+        configp = self.fetch_config(args)
+        conf = ClientConfig(args, configp)
+
+        local = init(conf, self.app.log)
+        exit_code = do_info(self.app.log, conf, local)
+        if exit_code != 0:
+          exit(exit_code)
+
+
+def do_info(logger, conf, local):
+    resetLogger(logger)
+    try:
+        instance = local['slap'].registerOpenOrder().getInformation(
+            partition_reference=conf.reference,
+        )
+    except ResourceNotReady:
+        logger.warning('Instance does not exist or is not ready yet.')
+        return(2)
+    except NotFoundError:
+        logger.warning('Instance %s does not exist.', conf.reference)
+        return(2)
+
+    software_schema = SoftwareReleaseSchema(
+        instance._software_release_url,
+        getattr(instance, '_software_type', None))
+    if isinstance(instance._connection_dict, list):
+        # this is slapos master connection dict
+        connection_parameter_dict = {}
+        for param in instance._connection_dict:
+            connection_parameter_dict[param['connection_key']] = param['connection_value']
+    else:
+        # this is slapproxy connection dict
+        connection_parameter_dict = xml2dict(instance._connection_dict)
+    try:
+        software_serialisation = software_schema.getSerialisation()
+    except UndefinedSerializationError:
+        software_serialisation = SoftwareReleaseSerialisation.JsonInXml
+    if software_serialisation == SoftwareReleaseSerialisation.JsonInXml:
+        if '_' in connection_parameter_dict:
+            connection_parameter_dict = json.loads(connection_parameter_dict['_'])
+
+    logger.info(
+        json.dumps(
+            {
+              'software-url': instance._software_release_url,
+              'instance-state': instance._requested_state,
+              'instance-parameters': instance._parameter_dict,
+              'connection-parameters': connection_parameter_dict,
+            },
+            indent=2,
+        )
+    )
