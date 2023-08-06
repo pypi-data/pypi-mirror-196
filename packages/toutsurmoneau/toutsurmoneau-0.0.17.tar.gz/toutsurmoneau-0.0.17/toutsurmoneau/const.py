046770256fa0e3c067448c9f984d58951933cfb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:12 +0000

    Add package toutsurmoneau/0.0.17/toutsurmoneau-0.0.17.tar.gz

---
+++ packages/toutsurmoneau/toutsurmoneau-0.0.17.tar.gz/toutsurmoneau-0.0.17/toutsurmoneau/const.py
@@ -0,0 +1,5 @@
+# Known providers, give the base url, other can be supported
+KNOWN_PROVIDER_URLS = [
+    'https://www.toutsurmoneau.fr/mon-compte-en-ligne',
+    'https://www.eau-olivet.fr/mon-compte-en-ligne'
+]
