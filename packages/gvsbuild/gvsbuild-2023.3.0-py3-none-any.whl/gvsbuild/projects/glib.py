113c51c365c473acb77a35eea278f91a97ed3b4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:43 +0000

    Add package gvsbuild/2023.3.0/gvsbuild-2023.3.0-py3-none-any.whl

---
+++ packages/gvsbuild/gvsbuild-2023.3.0-py3-none-any.whl/gvsbuild/projects/glib.py
@@ -0,0 +1,85 @@
+#  Copyright (C) 2016 - Yevgen Muntyan
+#  Copyright (C) 2016 - Ignacio Casal Quinteiro
+#  Copyright (C) 2016 - Arnavion
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+from gvsbuild.utils.base_builders import Meson
+from gvsbuild.utils.base_expanders import NullExpander, Tarball
+from gvsbuild.utils.base_project import Project, project_add
+
+
+@project_add
+class GLib(Tarball, Meson):
+    def __init__(self):
+        Project.__init__(
+            self,
+            "glib",
+            version="2.74.5",
+            lastversion_even=True,
+            archive_url="https://download.gnome.org/sources/glib/{major}.{minor}/glib-{version}.tar.xz",
+            hash="ceba83a5999ceb31a4c4fc9921207cb9ffffd2ab1d6ec03c162d3f608a5c14c8",
+            dependencies=[
+                "ninja",
+                "meson",
+                "pkgconf",
+                "gettext",
+                "libffi",
+                "zlib",
+                "pcre2",
+            ],
+            patches=[
+                "glib-package-installation-directory.patch",
+            ],
+        )
+
+    def build(self):
+        Meson.build(self)
+        self.install(r".\LICENSES\* share\doc\glib")
+
+
+@project_add
+class GLibNetworking(Tarball, Meson):
+    def __init__(self):
+        Project.__init__(
+            self,
+            "glib-networking",
+            version="2.74.0",
+            lastversion_even=True,
+            repository="https://gitlab.gnome.org/GNOME/glib-networking",
+            archive_url="https://download.gnome.org/sources/glib-networking/{major}.{minor}/glib-networking-{version}.tar.xz",
+            hash="1f185aaef094123f8e25d8fa55661b3fd71020163a0174adb35a37685cda613b",
+            dependencies=["pkgconf", "ninja", "meson", "glib", "openssl"],
+        )
+
+    def build(self):
+        Meson.build(self, meson_params="-Dgnutls=disabled -Dopenssl=enabled")
+        self.install(r".\COPYING share\doc\glib-networking")
+        self.install(r".\LICENSE_EXCEPTION share\doc\glib-networking")
+
+
+@project_add
+class GLibPyWrapper(NullExpander, Meson):
+    def __init__(self):
+        Project.__init__(
+            self,
+            "glib-py-wrapper",
+            dependencies=["glib"],
+            version="0.1.0",
+            internal=True,
+            repository="https://gitlab.gnome.org/GNOME/glib-py-wrapper",
+        )
+
+    def build(self):
+        Meson.build(self)
