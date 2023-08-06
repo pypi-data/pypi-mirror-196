d58d3b81afe3a43577900b2e3577ad06617bf07a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:31 +0000

    Add package pyplanner/0.0.1/pyplanner-0.0.1-py3-none-any.whl

---
+++ packages/pyplanner/pyplanner-0.0.1-py3-none-any.whl/pyplanner/task_list.py
@@ -0,0 +1,26 @@
+from rich.console import Console
+from rich.table import Table
+from pyplanner.db_ops import TableSQL
+import os
+from pyplanner._global import *
+
+ops = TableSQL('set_task', attributes, datatypes)
+
+def print_tasks():
+    os.system("clear")
+    table = Table(title="\nYour Scheduled Tasks\n", expand=True, show_lines=True)
+
+    table.add_column("Sl.no", justify="center")
+    table.add_column("Task Name", justify="center")
+    table.add_column("Time", justify="center")
+
+    li = ops.show_tasks()
+    sl = 1
+
+    for i in li:
+        table.add_row(str(sl), i[0], i[1])
+        sl += 1
+
+    console = Console()
+    console.print(table)
+    print("\n")
