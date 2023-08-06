420fa3ee750df4083dbd98c8d0cc591e001a6355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:22 +0000

    Add package kinopoisk-dev/0.2.0/kinopoisk_dev-0.2.0.tar.gz

---
+++ packages/kinopoisk-dev/kinopoisk_dev-0.2.0.tar.gz/kinopoisk_dev-0.2.0/kinopoisk_dev/__init__.py
@@ -0,0 +1,16 @@
+from .fields import (  # noqa
+    ImageField,
+    MovieField,
+    PersonField,
+    PossValField,
+    ReviewField,
+    SeasonField,
+)
+from .kinopoisk_dev import KinopoiskDev  # noqa
+from .params import (  # noqa
+    ImageParams,
+    MovieParams,
+    PersonParams,
+    ReviewParams,
+    SeasonParams,
+)
---
+++ packages/kinopoisk-dev/kinopoisk_dev-0.2.0.tar.gz/kinopoisk_dev-0.2.0/kinopoisk_dev/params/season_params.py
@@ -0,0 +1,6 @@
+from ..fields import SeasonField
+from .base_params import BaseParams
+
+
+class SeasonParams(BaseParams[SeasonField]):
+    pass
