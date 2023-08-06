70cdd05d27d0e4c913284bbf9bff4d170cada695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:21 +0000

    Add package simple-system-tests/0.6.2/simple-system-tests-0.6.2.tar.gz

---
+++ packages/simple-system-tests/simple-system-tests-0.6.2.tar.gz/simple-system-tests-0.6.2/simple_system_tests/__init__.py
@@ -0,0 +1,2 @@
+from simple_system_tests.TestSuite import prepare_suite, teardown_suite, run_tests, add_cmd_option
+from simple_system_tests.TestSuite import get_env, set_env, logger, testcase, testcases
