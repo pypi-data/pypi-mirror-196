92929f8c8a9c5027541af3071008ade44e51013b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:10 +0000

    Add package rest-framework-simplejwt/0.0.1/rest_framework_simplejwt-0.0.1.tar.gz

---
+++ packages/rest-framework-simplejwt/rest_framework_simplejwt-0.0.1.tar.gz/rest_framework_simplejwt-0.0.1/src/__init__.py
@@ -0,0 +1,6 @@
+print("""
+This package was created to prevent package name-based dependency confusion in pipreqs.
+If you are reading this, and used pipreqs to generate requirements.txt file -- you are a 
+victim of a dependency confusion attack. Go check your requirements.txt to ensure that only
+required packages are listed inside.
+""")
