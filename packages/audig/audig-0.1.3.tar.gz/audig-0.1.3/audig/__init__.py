7a022d3d61913fb1380716fe9eba83729835ffef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:44 +0000

    Add package audig/0.1.3/audig-0.1.3.tar.gz

---
+++ packages/audig/audig-0.1.3.tar.gz/audig-0.1.3/audig/__init__.py
@@ -0,0 +1,5 @@
+from .paths import Caminhos
+from .mixer import TocarAudios
+from .main import Run
+
+__version__ = "0.1.1"
---
+++ packages/audig/audig-0.1.3.tar.gz/audig-0.1.3/audig/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env /home/bits/CursoIngles/Curso_de_Ingles_vm5_2022/programas_auxiliares/audIG/.venv/bin/python3.10
+from audig import Caminhos
+from audig import TocarAudios
+
+
+class Run:
+    def __init__(self):
+        self.run()
+
+    def run(self) -> None:
+        # Diretório raiz do curso.
+        path = "/home/bits/CursoIngles/Curso_de_Ingles_vm5_2022/01_Fundacao"
+        # Subdiretórios a serem listado os áudios
+        sub_dirs = ["Módulo 02", ]
+        # Instanciando um objeto Caminhos
+        caminhos = Caminhos(path=path, subdirs=sub_dirs)
+        # obtendo os caminhos absolutos dos arquivos de áudios.
+        paths_abs = caminhos.get_paths()
+
+        toca_audios = TocarAudios(paths_abs=paths_abs)
+        toca_audios.ouvir_lista()
+
+
+if __name__ == "__main__":
+    Run()
