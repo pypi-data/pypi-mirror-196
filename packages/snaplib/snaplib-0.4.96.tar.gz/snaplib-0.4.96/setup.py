0d70e0c5da56e3a125351242008a94bc3e5ca7ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:03 +0000

    Add package snaplib/0.4.96/snaplib-0.4.96.tar.gz

---
+++ packages/snaplib/snaplib-0.4.96.tar.gz/snaplib-0.4.96/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["pandas>=1", 
+                "numpy>=1", ]
+
+
+setup(
+    name="snaplib",
+    version="0.4.96",
+    author="Artsiom Kolas",
+    author_email="artyom.kolas@gmail.com",
+    description="Data preprocessing lib",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kolasdevpy/snaplib",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
+
