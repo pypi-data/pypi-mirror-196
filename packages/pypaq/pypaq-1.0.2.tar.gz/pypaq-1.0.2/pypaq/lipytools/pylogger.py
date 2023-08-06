ffbee118611100ddebabd73d409ffc612b12559a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:03 +0000

    Add package pypaq/1.0.2/pypaq-1.0.2.tar.gz

---
+++ packages/pypaq/pypaq-1.0.2.tar.gz/pypaq-1.0.2/pypaq/lipytools/pylogger.py
@@ -0,0 +1,51 @@
+import logging
+from typing import Optional
+
+from pypaq.lipytools.printout import stamp
+from pypaq.lipytools.files import prep_folder
+
+
+# returns formatted Logger
+def get_pylogger(
+        name: Optional[str]=    None,
+        add_stamp=              True,   # to prevent merging loggers from one-class-many-objects
+        folder: Optional[str]=  None,   # if given then writes logfile
+        level=                  logging.INFO,
+        format: str=            '%(asctime)s {%(filename)20s:%(lineno)3d} p%(process)s %(levelname)s: %(message)s',
+        to_stdout=              True):
+
+    if not name: name = 'logger'
+    if add_stamp: name += '_' + stamp()
+
+    formatter = logging.Formatter(format)
+
+    # manage file handler
+    fh = None
+    if folder:
+        prep_folder(folder)
+        fh = logging.FileHandler(f'{folder}/{name}.log')
+        fh.setFormatter(formatter)
+
+    # manage stream handler
+    sh = None
+    if to_stdout:
+        sh = logging.StreamHandler()
+        sh.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    if fh: logger.addHandler(fh)
+    if sh: logger.addHandler(sh)
+    return logger
+
+# returns child with higher level
+def get_child(
+        logger,
+        name: Optional[str]=    None,
+        change_level: int=      10):
+    if not name: name = '_child'
+    clogger = logger.getChild(name)
+    if change_level != 0:
+        lvl = clogger.getEffectiveLevel()
+        clogger.setLevel(lvl + change_level)
+    return clogger
