0d8ef63de284c2c58b013a3cf007533e97669a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:11 +0000

    Add package e2e-cli/0.9.3.3/e2e_cli-0.9.3.3.tar.gz

---
+++ packages/e2e-cli/e2e_cli-0.9.3.3.tar.gz/e2e_cli-0.9.3.3/install_man.py
@@ -0,0 +1,12 @@
+import os
+import subprocess
+
+
+class runcls:
+    def run(self):
+       
+        MAN_DIR = "/usr/local/share/man/man1"
+        man_file = os.path.join(os.path.dirname(__file__), "e2e_cli/docs/e2e_cli.1")
+        subprocess.call(["sudo", "cp", man_file, MAN_DIR])
+        # subprocess.call(["sudo", "gzip", os.path.join(MAN_DIR, "e2e_cli/docs/e2e_cli.1")])
+        
