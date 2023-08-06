d419520f0ab028972f669f9b37aed5fe8516ba8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:37 +0000

    Add package aunly-bbot/1.2.4b4/aunly-bbot-1.2.4b4.tar.gz

---
+++ packages/aunly-bbot/aunly-bbot-1.2.4b4.tar.gz/aunly-bbot-1.2.4b4/aunly_bbot/test.py
@@ -0,0 +1,3 @@
+from graia.saya import Channel
+
+channel = Channel.current()
---
+++ packages/aunly-bbot/aunly-bbot-1.2.4b4.tar.gz/aunly-bbot-1.2.4b4/aunly_bbot/utils/fonts_provider.py
@@ -0,0 +1,61 @@
+import httpx
+
+from yarl import URL
+from pathlib import Path
+from loguru import logger
+from playwright.async_api import Request, Route
+
+DEFUALT_DYNAMIC_FONT = (
+    "https://cdn.jsdelivr.net"
+    "/gh/irozhi/HarmonyOS-Sans/HarmonyOS_Sans_SC/HarmonyOS_Sans_SC_Medium.woff2"
+)
+
+
+font_path = Path("data", "font")
+font_mime_map = {
+    "collection": "font/collection",
+    "otf": "font/otf",
+    "sfnt": "font/sfnt",
+    "ttf": "font/ttf",
+    "woff": "font/woff",
+    "woff2": "font/woff2",
+}
+font_path.mkdir(parents=True, exist_ok=True)
+
+
+async def get_font(font: str = DEFUALT_DYNAMIC_FONT):
+    logger.debug(f"font: {font}")
+    url = URL(font)
+    if url.is_absolute():
+        if font_path.joinpath(url.name).exists():
+            logger.debug(f"Font {url.name} found in local")
+            return font_path.joinpath(url.name)
+        else:
+            logger.warning(f"字体 {font} 不存在，尝试从网络获取")
+            async with httpx.AsyncClient() as client:
+                resp = await client.get(font)
+                if resp.status_code != 200:
+                    raise ConnectionError(f"字体 {font} 获取失败")
+                font_path.joinpath(url.name).write_bytes(resp.content)
+                return font_path.joinpath(url.name)
+    else:
+        if not font_path.joinpath(font).exists():
+            raise FileNotFoundError(f"字体 {font} 不存在")
+        logger.debug(f"Font {font} found in local")
+        return font_path.joinpath(font)
+
+
+async def fill_font(route: Route, request: Request):
+    url = URL(request.url)
+    if not url.is_absolute():
+        raise ValueError("字体地址不合法")
+    try:
+        logger.debug(f"Font {url.name} requested")
+        await route.fulfill(
+            path=await get_font(url.query["name"]),
+            content_type=font_mime_map.get(url.suffix, None),
+        )
+        return
+    except Exception:
+        logger.debug(f"can't get font {url.name}")
+        await route.fallback()
