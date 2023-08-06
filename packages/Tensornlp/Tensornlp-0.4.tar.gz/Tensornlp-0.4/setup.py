5e119b79433351b64247e351aa8f8cc58133df3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:32 +0000

    Add package Tensornlp/0.4/Tensornlp-0.4.tar.gz

---
+++ packages/Tensornlp/Tensornlp-0.4.tar.gz/Tensornlp-0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Tensornlp',
+    version='0.4',
+    description='A tensornlp library for NLP tasks',
+    packages=find_packages(['client', 'grpcDir']),
+    author='Hamza Khan',
+    author_email='hamzamania@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'grpcio',
+        'grpcio-tools',
+        'protobuf',
+        'pymongo',
+        # Add any other dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'my_service=tensornlp.main:serve',
+        ],
+    },
+    python_requires='>=3.6',
+)
