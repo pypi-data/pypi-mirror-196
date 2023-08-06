c727b9f5936716978c242559e90a078d6e6b93b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:17 +0000

    Add package runhouse/0.0.4/runhouse-0.0.4-py3-none-any.whl

---
+++ packages/runhouse/runhouse-0.0.4-py3-none-any.whl/runhouse/rns/tables/ray_table.py
@@ -0,0 +1,46 @@
+import logging
+from typing import Optional
+
+from .. import OnDemandCluster
+from ..top_level_rns_fns import save
+
+from .table import Table
+
+logger = logging.getLogger(__name__)
+
+
+class RayTable(Table):
+    DEFAULT_FOLDER_PATH = "/runhouse/ray-tables"
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    @staticmethod
+    def from_config(config: dict, dryrun=True):
+        """Load config values into the object."""
+        if isinstance(config["system"], dict):
+            config["system"] = OnDemandCluster.from_config(
+                config["system"], dryrun=dryrun
+            )
+        return RayTable(**config, dryrun=dryrun)
+
+    def save(
+        self,
+        name: Optional[str] = None,
+        overwrite: bool = True,
+    ):
+        if self._cached_data is not None:
+            self.write_ray_dataset(self.data)
+            logger.info(f"Saved {str(self)} to: {self.fsspec_url}")
+
+        save(self, name=name, overwrite=overwrite)
+
+        return self
+
+    def fetch(self, **kwargs):
+        import ray
+
+        self._cached_data = ray.data.read_parquet(
+            self.fsspec_url, filesystem=self._folder.fsspec_fs
+        )
+        return self._cached_data
