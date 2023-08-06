3aded94ad57441b61b2a91ebf368047080394825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:40 +0000

    Add package sicaulc/1.0/sicaulc-1.0.tar.gz

---
+++ packages/sicaulc/sicaulc-1.0.tar.gz/sicaulc-1.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup(
+    name= 'sicaulc',
+    version= '1.0',
+    author= 'abderamane',
+    description= 'simple calc for math',
+    packages=setuptools.find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
---
+++ packages/sicaulc/sicaulc-1.0.tar.gz/sicaulc-1.0/src/__init__.py
@@ -0,0 +1,8 @@
+def plusT(a,b,c):
+	return a + b + c
+def xdT(a,b,c):
+	return a * b * c
+def maynT(a,b,c):
+	return a - b - c
+def diviT(a,b,c):
+	return a / b / c
