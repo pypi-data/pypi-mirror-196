5218bc1d3c820084c84085822a3e379b5096a1f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:54 +0000

    Add package flask-wtf-polyglot/0.4.0/flask_wtf_polyglot-0.4.0.tar.gz

---
+++ packages/flask-wtf-polyglot/flask_wtf_polyglot-0.4.0.tar.gz/flask_wtf_polyglot-0.4.0/flask_wtf_polyglot/__init__.py
@@ -0,0 +1,3 @@
+from wtf_polyglot import SubmitField  # noqa: F401
+
+from flask_wtf_polyglot.form import PolyglotForm  # noqa: F401
---
+++ packages/flask-wtf-polyglot/flask_wtf_polyglot-0.4.0.tar.gz/flask_wtf_polyglot-0.4.0/flask_wtf_polyglot/form.py
@@ -0,0 +1,6 @@
+from flask_wtf.form import FlaskForm
+from wtf_polyglot.meta import PolyglotMeta
+
+
+class PolyglotForm(FlaskForm):
+    Meta = PolyglotMeta
