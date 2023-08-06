6be79d51b84833329ae9a3be1f37824d62268343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:40 +0000

    Add package quorum-lightnode/0.0.6/quorum_lightnode-0.0.6-py2.py3-none-any.whl

---
+++ packages/quorum-lightnode/quorum_lightnode-0.0.6-py2.py3-none-any.whl/lightnode/content.py
@@ -0,0 +1,35 @@
+import base64
+import json
+from typing import Union, Dict
+
+from .utils import aes_encrypt
+
+
+def get_content_param(
+    aes_key: bytes,
+    group_id: str,
+    start_trx: Union[str, None] = None,
+    count: int = 20,
+    reverse: bool = False,
+) -> Dict[str, str]:
+    params = {
+        "group_id": group_id,
+        "reverse": "true" if reverse is True else "false",
+        "num": count,
+        "include_start_trx": "false",
+        "senders": [],
+    }
+    if start_trx:
+        params["start_trx"] = start_trx
+
+    get_group_ctn_item = {
+        "Req": params,
+    }
+
+    get_group_ctn_item_str = json.dumps(get_group_ctn_item)
+    encrypted = aes_encrypt(aes_key, get_group_ctn_item_str.encode())
+    send_param = {
+        "Req": base64.b64encode(encrypted).decode(),
+    }
+
+    return send_param
