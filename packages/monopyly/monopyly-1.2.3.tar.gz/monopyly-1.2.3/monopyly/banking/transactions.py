be43dfd80cae2d6bc3435a0a6a1d457cd079eae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:50 +0000

    Add package monopyly/1.2.3/monopyly-1.2.3.tar.gz

---
+++ packages/monopyly/monopyly-1.2.3.tar.gz/monopyly-1.2.3/monopyly/banking/transactions.py
@@ -0,0 +1,132 @@
+"""
+Tools for interacting with the bank transactions in the database.
+"""
+from ..common.forms.utils import execute_on_form_validation
+from ..common.transactions import TransactionHandler
+from ..core.internal_transactions import add_internal_transaction
+from ..database.handler import DatabaseViewHandler
+from ..database.models import (
+    BankAccountView,
+    BankSubtransaction,
+    BankTransaction,
+    BankTransactionView,
+)
+
+
+class BankTransactionHandler(
+    TransactionHandler, model=BankTransaction, model_view=BankTransactionView
+):
+    """
+    A database handler for accessing bank transactions.
+
+    Attributes
+    ----------
+    user_id : int
+        The ID of the user who is the subject of database access.
+    model : type
+        The type of database model that the handler is primarily
+        designed to manage.
+    table : str
+        The name of the database table that this handler manages.
+    """
+
+    @classmethod
+    @DatabaseViewHandler.view_query
+    def get_transactions(cls, account_ids=None, active=None, sort_order="DESC"):
+        """
+        Get bank transactions from the database.
+
+        Query the database to select bank transaction information.
+        Transaction information includes details specific to the
+        transaction and the corresponding bank account. Transactions can
+        be filtered by bank, and query results can be ordered
+        by either ascending or descending transaction date.
+
+        Parameters
+        ----------
+        account_ids : tuple of int, optional
+            A sequence of bank account IDs with which to filter
+            transactions (if `None`, all bank account IDs will be
+            shown).
+        active : bool, optional
+            A flag indicating whether to return transactions for active
+            accounts, inactive accounts, or both. The default is `None`,
+            where all transactions are returned regardless of the
+            account's active status.
+        sort_order : {'ASC', 'DESC'}
+            An indicator of whether the transactions should be ordered
+            in ascending (oldest at top) or descending (newest at top)
+            order. The default is descending order.
+
+        Returns
+        -------
+        transactions : sqlalchemy.engine.ScalarResult
+            Returns bank account transactions matching the criteria.
+        """
+        criteria = [
+            cls._filter_values(cls.model.account_id, account_ids),
+            cls._filter_value(BankAccountView.active, active),
+        ]
+        transactions = super().get_entries(*criteria, sort_order=sort_order)
+        return transactions
+
+    @staticmethod
+    def _prepare_subtransaction(transaction, subtransaction_data):
+        """Prepare a subtransaction for the given transaction."""
+        return BankSubtransaction(transaction_id=transaction.id, **subtransaction_data)
+
+
+@execute_on_form_validation
+def save_transaction(form, transaction_id=None):
+    """
+    Save a banking transaction.
+
+    Saves a transaction in the database. If a transaction ID is given,
+    then the transaction is updated with the form information. Otherwise
+    the form information is added as a new entry.
+
+    Parameters
+    ----------
+    form : BankTransactionForm
+        The form being used to provide the data being saved.
+    transaction_id : int
+        The ID of the transaction to be saved. If provided, the
+        named transaction will be updated in the database. Otherwise, if
+        the transaction ID is `None`, a new transaction will be added.
+
+    Returns
+    -------
+    transaction : database.models.BankTransactionView
+        The saved transaction.
+    """
+    transaction_data = form.transaction_data
+    transfer_data = form.transfer_data
+    if transaction_id:
+        transaction = BankTransactionHandler.get_entry(transaction_id)
+        # Update the database with the updated transaction
+        transaction_data.update(
+            internal_transaction_id=transaction.internal_transaction_id
+        )
+        transaction = BankTransactionHandler.update_entry(
+            transaction_id,
+            **transaction_data,
+        )
+        # The transfer is not updated automatically; update it independently
+    else:
+        # Insert the new transaction into the database
+        if transfer_data:
+            transfer = record_new_transfer(transfer_data)
+            transaction_data.update(
+                internal_transaction_id=transfer.internal_transaction_id
+            )
+        transaction = BankTransactionHandler.add_entry(**transaction_data)
+    return transaction
+
+
+def record_new_transfer(transfer_data):
+    """Record a new transfer given the data for populating the database."""
+    # Create a new internal transaction ID to assign to the transfer
+    transfer_data["internal_transaction_id"] = add_internal_transaction()
+    # Add the transfer transaction to the database
+    transfer = BankTransactionHandler.add_entry(**transfer_data)
+    return transfer
---
+++ packages/monopyly/monopyly-1.2.3.tar.gz/monopyly-1.2.3/monopyly/database/__init__.py
@@ -0,0 +1,121 @@
+"""
+Expose commonly used database functionality to the rest of the package.
+"""
+from functools import wraps
+from pathlib import Path
+
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+from sqlalchemy import MetaData, create_engine, event
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.schema import Table
+
+from .models import Model
+from .schema import DATABASE_SCHEMA
+
+DIALECT = "sqlite"
+DBAPI = "pysqlite"
+DB_NAME = "monopyly.sqlite"
+
+
+class SQLAlchemy:
+    """Store SQLAlchemy database objects."""
+
+    _base = Model
+
+    def __init__(self, db_path=None):
+        self.engine = None
+        self.metadata = None
+        self.scoped_session = None
+
+    @property
+    def tables(self):
+        return self.metadata.tables
+
+    @property
+    def session(self):
+        # Returns the current `Session` object
+        return self.scoped_session()
+
+    def setup_engine(self, db_path, echo_engine=False):
+        """Setup the database engine, a session factory, and metadata."""
+        # Create the engine using the custom database URL
+        db_url = f"{DIALECT}+{DBAPI}:///{db_path}"
+        self.engine = create_engine(db_url, echo=echo_engine)
+        # Use a session factory to generate sessions
+        session_factory = sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            future=True,
+        )
+        self.scoped_session = scoped_session(session_factory)
+        self._base.query = self.scoped_session.query_property()
+        # Add metadata
+        self.metadata = MetaData()
+
+    def access_tables(self):
+        for table_name in DATABASE_SCHEMA.keys():
+            Table(table_name, self.metadata, autoload_with=self.engine)
+
+
+db = SQLAlchemy()
+
+
+def db_transaction(func):
+    """A decorator denoting the wrapped function as a database transaction."""
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        with current_app.db.session.begin():
+            return func(*args, **kwargs)
+
+    return wrapper
+
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
+
+@click.command("init-db")
+@with_appcontext
+def init_db_command():
+    """Initialize the database from the command line (if it does not exist)."""
+    db_path = current_app.config["DATABASE"]
+    if not db_path.is_file():
+        preload_path = current_app.config.get("PRELOAD_DATA_PATH")
+        init_db(current_app.db, preload_path)
+        click.echo(f"Initialized the database ('{db_path}')")
+        if preload_path:
+            click.echo(f"Prepopulated the database using '{preload_path}'")
+    else:
+        click.echo(f"Database exists, using '{db_path}'")
+
+
+def init_db(db, auxiliary_preload_path=None):
+    """Execute the SQL schema to clear existing data and create new tables."""
+    # Establish a raw connection in order to execute the complete files
+    raw_conn = db.engine.raw_connection()
+    # Load the tables, table views, and preloaded data
+    sql_dir = Path(__file__).parent
+    sql_filepaths = [
+        sql_dir / path for path in ("schema.sql", "views.sql", "preloads.sql")
+    ]
+    if auxiliary_preload_path:
+        sql_filepaths.append(auxiliary_preload_path)
+    for sql_filepath in sql_filepaths:
+        with current_app.open_resource(sql_filepath) as sql_file:
+            raw_conn.executescript(sql_file.read().decode("utf8"))
+    raw_conn.close()
+    # Register tables with the SQLAlchemy metadata
+    db.metadata.create_all(bind=db.engine)
+
+
+def close_db(exception=None):
+    """Close the database if it is open."""
+    if current_app.db.scoped_session is not None:
+        current_app.db.scoped_session.remove()
