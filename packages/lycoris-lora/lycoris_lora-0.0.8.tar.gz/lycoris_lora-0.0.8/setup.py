fe3abb4c1620d4d5016a05520e3085c09b8b7cb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:21 +0000

    Add package lycoris-lora/0.0.8/lycoris_lora-0.0.8.tar.gz

---
+++ packages/lycoris-lora/lycoris_lora-0.0.8.tar.gz/lycoris_lora-0.0.8/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='lycoris_lora',
+    packages=['lycoris'],
+    version='0.0.8',
+    url='https://github.com/KohakuBlueleaf/LoCon',
+    description='Lora beYond Conventional methods, Other Rank adaptation Implementations for Stable diffusion',
+    author='Shih-Ying Yeh(KohakuBlueLeaf)',
+    author_email='apolloyeh0123@gmail.com',
+    zip_safe=False,
+    install_requires=[
+        'torch',
+        'safetensors',
+        'diffusers',
+        'transformers'
+    ],
+)
