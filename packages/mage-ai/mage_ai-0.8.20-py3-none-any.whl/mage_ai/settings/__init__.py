c00cc4a25b4f86e5932f7e186ae8588462808576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:22 +0000

    Add package mage-ai/0.8.20/mage_ai-0.8.20-py3-none-any.whl

---
+++ packages/mage-ai/mage_ai-0.8.20-py3-none-any.whl/mage_ai/settings/__init__.py
@@ -0,0 +1,16 @@
+import os
+
+
+DEBUG = os.getenv('DEBUG', False)
+QUERY_API_KEY = 'api_key'
+
+"""
+import secrets
+secrets.token_urlsafe()
+
+Make sure this value is the same in mage_ai/frontend/api/constants.ts
+"""
+OAUTH2_APPLICATION_CLIENT_ID = 'zkWlN0PkIKSN0C11CfUHUj84OT5XOJ6tDZ6bDRO2'
+DISABLE_NOTEBOOK_EDIT_ACCESS = os.getenv('DISABLE_NOTEBOOK_EDIT_ACCESS', False)
+
+REQUIRE_USER_AUTHENTICATION = os.getenv('REQUIRE_USER_AUTHENTICATION', False)
