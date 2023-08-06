39edd23f1b59af77e655ba831235e32198c23210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:46:14 +0000

    Add package bctestart/1.0/bctestart-1.0.tar.gz

---
+++ packages/bctestart/bctestart-1.0.tar.gz/bctestart-1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='bctestart',
+    version='1.0',
+    description='A simple Python package',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/mypackage',
+    packages=['bctestart'],
+    install_requires=[
+        # List any dependencies here
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
