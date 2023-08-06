f779821429ec78fe36b0cb0ae23423828efe28f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:38 +0000

    Add package fastapi-doc-http-response/0.0.3/fastapi_doc_http_response-0.0.3.tar.gz

---
+++ packages/fastapi-doc-http-response/fastapi_doc_http_response-0.0.3.tar.gz/fastapi_doc_http_response-0.0.3/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+
+setup(
+    name='fastapi_doc_http_response',
+    description="This is a Python package that simplifies the definition of standard HTTP responses for FastAPI APIs.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    version='0.0.3',
+    author='Tharles Amaro',
+    author_email='tharlesamaro@gmail.com',
+    url="https://github.com/tharlesamaro/fastapi-doc-http-response",
+    license='MIT',
+    keywords=["fastapi", "http", "response", "api", "documentation"],
+    packages=['fastapi_doc_http_response', 'tests'],
+    install_requires=[
+        'fastapi',
+    ],
+    python_requires='>=3.10',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        "Environment :: Web Environment",
+        'License :: OSI Approved :: MIT License',
+        "Topic :: Internet",
+        "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Code Generators",
+        "Topic :: Software Development",
+        "Typing :: Typed",
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        'Programming Language :: Python',
+    ],
+    
+)
