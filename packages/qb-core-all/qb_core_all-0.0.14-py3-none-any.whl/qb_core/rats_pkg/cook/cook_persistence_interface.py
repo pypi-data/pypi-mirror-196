fec39a2ebc0b730e670114ba6cbaac962ce76750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:14 +0000

    Add package qb-core-all/0.0.14/qb_core_all-0.0.14-py3-none-any.whl

---
+++ packages/qb-core-all/qb_core_all-0.0.14-py3-none-any.whl/qb_core/rats_pkg/cook/cook_persistence_interface.py
@@ -0,0 +1,19 @@
+
+class CookPersistenceInterface:
+    def __init__(self):
+        pass
+
+    def read_cook_count(self):
+        pass
+
+    def read_cook(self, cook_name):
+        pass
+
+    def save_cook(self, cook):
+        pass
+
+    def remove_cook(self, cook):
+        pass
+
+    def print_all(self):
+        pass
