9f6aa81fa95e9be34beb5588ffb64d996097db78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:58 +0000

    Add package nais/1.1/nais-1.1.tar.gz

---
+++ packages/nais/nais-1.1.tar.gz/nais-1.1/setup.py
@@ -0,0 +1,31 @@
+
+import setuptools
+from setuptools import find_packages
+
+with open("./README.md") as file:
+    read_me_description = file.read()
+
+req = []
+with open('./requirements.txt') as file:
+    for l in file.readlines():
+        req.append(l.strip())
+
+setuptools.setup(
+    name="nais",
+    version="1.1",
+    author="Erik B. Myklebust",
+    author_email="erik@norsar.no",
+    description="NORSAR AI System.",
+    #long_description=read_me_description,
+    #long_description_content_type="text/markdown",
+    url="https://github.com/NorwegianSeismicArray/nais",
+    download_url='https://github.com/NorwegianSeismicArray/nais/archive/refs/tags/1.0.tar.gz',
+    packages=find_packages(),
+    install_requires=req,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
