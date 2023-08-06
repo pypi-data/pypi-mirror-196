e0b745620b5f4be15a8c59a86871a0c9b232bd7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:21 +0000

    Add package wtforms-polyglot/0.4.0/wtforms_polyglot-0.4.0-py3-none-any.whl

---
+++ packages/wtforms-polyglot/wtforms_polyglot-0.4.0-py3-none-any.whl/wtf_polyglot/__init__.py
@@ -0,0 +1,2 @@
+from wtf_polyglot.fields import SubmitField  # noqa: F401
+from wtf_polyglot.form import PolyglotForm  # noqa: F401
---
+++ packages/wtforms-polyglot/wtforms_polyglot-0.4.0-py3-none-any.whl/wtf_polyglot/widgets.py
@@ -0,0 +1,37 @@
+from markupsafe import Markup
+from wtforms.widgets.core import html_params
+
+__all__ = ("SubmitButton",)
+
+
+class Button:
+    """
+    Render a basic ``<button>``.
+
+    The field's label is used as the text of the button instead of the data on
+    the field.
+    """
+
+    html_params = staticmethod(html_params)
+
+    def __init__(self, button_type=None):
+        if button_type is not None:
+            self.button_type = button_type
+
+    def __call__(self, field, **kwargs):
+        kwargs.setdefault("id", field.id)
+        kwargs.setdefault("type", self.button_type)
+        kwargs.setdefault("value", field.label.text)
+        return Markup(
+            "<button {}>{}</button>".format(
+                self.html_params(name=field.name, **kwargs), kwargs["value"]
+            )
+        )
+
+
+class SubmitButton(Button):
+    """
+    Render a submit button.
+    """
+
+    button_type = "submit"
