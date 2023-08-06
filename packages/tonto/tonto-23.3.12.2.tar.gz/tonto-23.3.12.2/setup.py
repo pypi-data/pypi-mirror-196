a12b3a203c5ca8b2168822b2ff05274bff0aa4e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:36 +0000

    Add package tonto/23.3.12.2/tonto-23.3.12.2.tar.gz

---
+++ packages/tonto/tonto-23.3.12.2.tar.gz/tonto-23.3.12.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+	name = 'tonto',
+	version = '23.3.12.2',
+	description = 'Tonto or nummy - Python advanced functionality module',
+	long_description = '''¡ Alpha version !
+	The necessary functions that are not available in pure (vanilla) Python
+	What's new:
+	— system functions
+	— some new tools for working with files''',
+	author = 'Igor_Shapovalov_Andrejovich',
+	packages = ['tonto'],
+	author_email = 'igor.shapovalov.andrejovich@gmail.com'
+	)
