1f6b56b395d388728032204cbb71d9510dda9312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:19 +0000

    Add package pylint-print/1.0.1/pylint_print-1.0.1-py3-none-any.whl

---
+++ packages/pylint-print/pylint_print-1.0.1-py3-none-any.whl/pylint_print/__version__.py
@@ -0,0 +1,10 @@
+"""pylint-print meta info."""
+
+__title__ = 'pylint-print'
+__description__ = 'Print function checker for PyLint'
+__url__ = 'https://gitlab.com/nwmitchell/pylint-print'
+__version__ = '1.0.1'
+__author__ = 'Nick Mitchell'
+__author_email__ = 'nmitchell@nvidia.com'
+__license__ = 'GPLv2'
+__copyright__ = 'Copyright 2021 Nick Mitchell'
