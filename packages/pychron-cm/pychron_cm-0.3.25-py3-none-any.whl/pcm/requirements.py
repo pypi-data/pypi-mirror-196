378018a5c73e6d59c8358cee9e8afefbd19c777f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:22 +0000

    Add package pychron-cm/0.3.25/pychron_cm-0.3.25-py3-none-any.whl

---
+++ packages/pychron-cm/pychron_cm-0.3.25-py3-none-any.whl/pcm/requirements.py
@@ -0,0 +1,93 @@
+# ===============================================================================
+# Copyright 2021 ross
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============================================================================
+
+EDM_REQUIREMENTS = [
+    "chaco",
+    "certifi",
+    "cython",
+    "envisage",
+    "future",
+    "gitpython",
+    "keyring",
+    "jinja2",
+    "lxml",
+    "numpy",
+    "pandas",
+    "patsy",
+    "pillow",
+    "pip",
+    "pyface",
+    "pyparsing",
+    "pyproj",
+    # "pymysql", #edm install 0.7.9 version 1.0.2 or greater required. used pip
+    "pyqt5",
+    "pytables",
+    "pyyaml",
+    "pygments",
+    "qt",
+    "Reportlab",
+    "requests",
+    "scipy",
+    "sqlalchemy",
+    "traits",
+    "xlrd",
+    "xlsxwriter",
+    "xlwt",
+    "statsmodels",
+    "cryptography",
+]
+
+CONDA_REQUIREMENTS = [
+    "chaco",
+    "certifi",
+    "cython",
+    "envisage",
+    "future",
+    "gitpython",
+    "keyring",
+    "jinja2",
+    "lxml",
+    "numpy",
+    "pandas",
+    "patsy",
+    "pillow",
+    "pip",
+    "pyface",
+    "pyparsing",
+    "pyproj",
+    # "pymysql", #edm install 0.7.9 version 1.0.2 or greater required. used pip
+    "pyqt5",
+    "pytables",
+    "pyyaml",
+    "pygments",
+    "qt",
+    "Reportlab",
+    "requests",
+    "scipy",
+    "sqlalchemy",
+    "traits",
+    "xlrd",
+    "xlsxwriter",
+    "xlwt",
+    "statsmodels",
+    "cryptography",
+]
+
+PIP_REQUIREMENTS = ["uncertainties", "qimage2ndarray", "pymysql"]
+VALVE_REQUIREMENTS = ["pyserial", "twisted"]
+PIP_EXTRAS = ["peakutils", "utm"]
+
+# ============= EOF =============================================
