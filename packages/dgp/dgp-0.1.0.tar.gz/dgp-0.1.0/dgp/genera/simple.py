750e3e428181dd592b5ddee214f4677c3e401090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:18 +0000

    Add package dgp/0.1.0/dgp-0.1.0.tar.gz

---
+++ packages/dgp/dgp-0.1.0.tar.gz/dgp-0.1.0/dgp/genera/simple.py
@@ -0,0 +1,36 @@
+from dataflows import Flow
+
+from ..core import BaseDataGenusProcessor
+from ..config.log import logger
+from .load import LoaderDGP, PostLoaderDGP, PreLoaderDGP
+from .transform import TransformDGP
+from .enrich import EnricherDGP
+
+
+class SimpleDGP(BaseDataGenusProcessor):
+
+    def init(self,
+             steps=[
+                PreLoaderDGP,
+                LoaderDGP,
+                PostLoaderDGP,
+                TransformDGP,
+                EnricherDGP,
+             ]):
+
+        self.steps = self.init_classes(steps)
+
+    def flow(self):
+        flows = []
+        for i, step in enumerate(self.steps):
+            logger.debug('Adding step %s to the fdddddlow', step.__class__.__name__)
+            flow = step.flow()
+            if flow:
+                flows.append(flow)
+            else:
+                logger.debug('Step %s caused flow building to stop', step.__class__.__name__)
+                break
+            flow = step.preflow()
+            if flow:
+                flows.insert(0, flow)
+        return Flow(*flows)
