ec885222c9ead3235e65dcc50f431b63d0604c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:56 +0000

    Add package vathos/1.0.2/vathos-1.0.2-py3-none-any.whl

---
+++ packages/vathos/vathos-1.0.2-py3-none-any.whl/vathos/services/depalletizing.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+# Copyright (c) 2019-2023, Vathos GmbH
+#
+# All rights reserved.
+#
+################################################################################
+
+import json
+import logging
+from time import perf_counter
+
+import requests
+
+from vathos import BASE_URL
+from vathos.products import get_product
+from vathos.configurations import get_configuration
+from vathos.files import upload_files
+
+
+def train_product(product_id, calibration_image_path, token, device_id=None):
+  """Starts a training.
+  
+  Args: 
+    product_id (str): id of the product to start the training for. A product
+      is created with the function `create_product()` from the module
+      `vathos.products`.
+    calibration_image_path (str): path of an image for extrinsic calibration.
+      The image must be a depth image of the plane the detected objects will
+      rest upon during inference. It is converted to millimeters, then cast to a
+      short integer array whose LSB and MSB are put into the red respectively
+      green channel of an 8-bit RGB image before storing it as a PNG-compressed
+      file.
+    token (str): API access token
+
+  Returns:
+    str: id of the training task
+    
+  """
+  # upload calib image id (synced to device, if available)
+  calib_image = upload_files([calibration_image_path], token,
+                             device=device_id)[0]
+
+  train_data = {
+      'workflow':
+          'votenet',
+      'product':
+          product_id,
+      'device':
+          device_id,
+      'tasks': [{
+          'service': 'extrinsic.calibration.vathos.net',
+          'parameters': {
+              'planeImage': calib_image['_id']
+          }
+      }, {
+          'service': 'rendering.votenet.detection.vathos.net'
+      }, {
+          'service': 'train.votenet.detection.vathos.net'
+      }, {
+          'service': 'conversion.trt.vathos.net'
+      }]
+  }
+
+  post_task_response = requests.post(
+      f'{BASE_URL}/compositetasks',
+      json=train_data,
+      headers={'Authorization': f'Bearer {token}'},
+      timeout=5)
+
+  return post_task_response.json()['_id']
+
+
+def run_inference(product_id,
+                  test_image_path,
+                  token,
+                  score_threshold=0.9999,
+                  refine_detections=True):
+  """Runs an inference request.
+  
+  Args: 
+    product_id (str): id of the product to start the training for. A product
+      is created with the function `create_product()` from the module
+      `vathos.products`.
+    test_image_path (str): path of image to run inference on. This image
+    must measure the depth of each pixe in millimeters, where each pixel is
+    stored as a short integer, whose LSB and MSB are packed into the red
+    respectively green channel of an 8-bit RGB image before storing it as a
+    PNG-compressed file.
+    token (str): API access token
+
+  Returns:
+    list: detected objects  
+  """
+  product = get_product(product_id, token)
+  configuration = get_configuration(product_id, 'votenet.workflows.vathos.net',
+                                    token)
+  # overwrite config values with function parameters
+  configuration['metric']['conf_thresh'] = score_threshold
+  configuration['refine_detections'] = refine_detections
+  configuration['icp']['window_size'] = 2000
+  configuration['icp']['num_points_icp'] = 50000
+
+  # get depth image for visualization purposes and convert to m
+  inference_url = f'{BASE_URL}/workflows/votenet'
+  files = {'files': open(test_image_path, 'rb')}
+  values = {
+      'product': json.dumps(product),
+      'configuration': json.dumps(configuration)
+  }
+
+  tic = perf_counter()
+  inference_response = requests.post(
+      inference_url,
+      files=files,
+      data=values,
+      headers={'Authorization': 'Bearer ' + token},
+      timeout=20)
+
+  toc = perf_counter()
+  logging.info('Inference took %f s', toc - tic)
+
+  response_json = inference_response.json()
+
+  return {"product_id": product_id, "detections": response_json['detections']}
---
+++ packages/vathos/vathos-1.0.2-py3-none-any.whl/vathos/visualization.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+# Copyright (c) 2019-2023, Vathos GmbH
+#
+# All rights reserved.
+#
+################################################################################
+"""Visualization."""
+
+import logging
+
+import numpy as np
+
+from vathos.products import get_product
+from vathos.files import get_file
+
+try:
+  from imageio import imread
+  import mayavi.mlab as mlab
+  import trimesh
+  VISUALIZATION_ENABLED = True
+except ImportError:
+  VISUALIZATION_ENABLED = False
+  logging.warning('Optional dependencies for 3d visualization not installed!')
+
+# conversion to meters
+UNIT_CONVERSION_FACTOR = {'mm': 0.001, 'cm': 0.01, 'dm': 0.1, 'm': 1.0}
+
+
+def unpack_short(rgb):
+  """Unpacks two byte channels into a single channel of type short."""
+  return rgb[:, :, 0] + 256 * rgb[:, :, 1]
+
+
+def backproject(depth, K):
+  """Backprojects a depth image into a point cloud."""
+  size = depth.shape[::-1]
+
+  u, v = np.meshgrid(np.arange(0, size[0]), np.arange(0, size[1]))
+
+  xyz_image = np.zeros(depth.shape + (3,))
+
+  xyz_image[:, :, 0] = depth / K[0, 0] * (u - K[0, 2])
+  xyz_image[:, :, 1] = depth / K[1, 1] * (v - K[1, 2])
+  xyz_image[:, :, 2] = depth
+
+  pcl = np.reshape(xyz_image, (xyz_image.shape[0] * xyz_image.shape[1], 3))
+
+  # filter out 0
+  pcl = pcl[np.where(pcl[:, 2] > 0)[0], :]
+
+  return pcl
+
+
+def visualize_detections(detections,
+                         test_image_path,
+                         token,
+                         fitness_threshold=0.7):
+  """Visualizes a point cloud and detections.
+  
+  This function is only executed if mayavi and trimesh are installed.
+
+  Args:
+    model_file_name (str): path to a CAD model file on disk. Currently, the only
+      supported format is Wavefront OBJ.
+    unit (str): unit in which the CAD model is meaured. Must be one of
+      `['m', 'dm', 'cm', 'mm']`.
+    test_image_path (str): path of the depth image used in inference
+    projection_matrix (numpy.ndarray): a $3\\times 3$ projection matrix of the 
+      used camera
+    detections (list): inferred object poses      
+  """
+  if not VISUALIZATION_ENABLED:
+    logging.warning('Visualization is disabled.')
+    return
+
+  product = get_product(detections['product_id'], token)
+  projection_matrix = np.reshape(np.array(product['camera']['intrinsics']),
+                                 (3, 3), 'F')
+
+  meshes = []
+  for obj_model in product['models']:
+    mesh = trimesh.load(get_file(obj_model, token),
+                        file_type='OBJ',
+                        color=(0.5, 0.5, 0.5))
+    mesh.apply_scale(UNIT_CONVERSION_FACTOR[product['unit']])
+    meshes.append(mesh)
+
+  depth_img_compressed = imread(test_image_path)
+  pcl = backproject(0.001 * unpack_short(depth_img_compressed),
+                    projection_matrix)
+
+  mlab.figure("Vathos: Cloud Inference",
+              bgcolor=(0.0, 0.0666, 0.1137),
+              size=(800, 600))
+  mlab.points3d(pcl[:, 0], pcl[:, 1], pcl[:, 2], mode='point')
+
+  for detection in detections['detections']:
+
+    # frame is missing from the object if ICP has failed
+    if 'frame' in detection and detection.get('fitness',
+                                              1.0) > fitness_threshold:
+      # might want to use a differrent color for low fitness detections later
+      mesh_color = (0.5, 0.6, 0.7)
+      # read class and center, depending on use of ICP
+      if 'detection' in detection:
+        detection_class = detection['detection']['class']
+        detection_center = detection['detection']['center']
+      else:
+        detection_class = detection['class']
+        detection_center = detection['center']
+      mesh = meshes[int(product['class2product'][str(detection_class)])]
+
+      # pose is in camera coordinates
+      pose = np.reshape(np.array(detection['frame'], dtype='f'), (4, 4), 'F')
+      vertices = mesh.vertices @ pose[0:3, 0:3].transpose() + np.ones(
+          mesh.vertices.shape) @ np.diag(pose[0:3, 3])
+
+      mlab.triangular_mesh(vertices[:, 0],
+                           vertices[:, 1],
+                           vertices[:, 2],
+                           mesh.faces,
+                           color=mesh_color,
+                           opacity=0.5)
+      if detection.get('fitness', False):
+        mlab.text3d(x=detection_center[0],
+                    y=detection_center[1],
+                    z=detection_center[2],
+                    text="{:.2f}".format(detection['fitness']),
+                    color=(0.95, 0.71, 0.25),
+                    scale=0.01)
+
+  mlab.show()
