375dd908fe31306a21ff5ea3148b1141ce4ce1bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:55 +0000

    Add package st4sd-runtime-core/2.0.0a9.dev1/st4sd-runtime-core-2.0.0a9.dev1.tar.gz

---
+++ packages/st4sd-runtime-core/st4sd-runtime-core-2.0.0a9.dev1.tar.gz/st4sd-runtime-core-2.0.0a9.dev1/python/experiment/cli/git.py
@@ -0,0 +1,45 @@
+# Copyright IBM Inc. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Author: Alessandro Pomponio
+import subprocess
+import sys
+from pathlib import Path
+
+import typer
+
+
+def get_git_toplevel_path(path: Path):
+    try:
+        toplevel_path = subprocess.check_output(["git", "rev-parse", "--show-toplevel"],
+                                                cwd=path.parent).decode().strip()
+    except subprocess.CalledProcessError as e:
+        typer.echo("Unable to retrieve top level path for the experiment.")
+        typer.echo("Are you sure the path belongs to a git repository?")
+        sys.exit(e.returncode)
+
+    return toplevel_path
+
+
+def get_git_origin_url(path: Path):
+    try:
+        origin_url = subprocess.check_output(["git", "remote", "get-url", "origin"],
+                                             cwd=path.parent).decode().strip()
+        if origin_url.endswith(".git"):
+            origin_url = origin_url[:-4]
+    except subprocess.CalledProcessError as e:
+        typer.echo("Unable to retrieve origin url for the experiment.")
+        typer.echo("Are you sure the experiment path belongs to a git repository?")
+        sys.exit(e.returncode)
+
+    return origin_url
+
+
+def get_git_head_commit(path: Path):
+    try:
+        head_commit = subprocess.check_output(["git", "rev-parse", "HEAD"], cwd=path.parent).decode().strip()
+    except subprocess.CalledProcessError as e:
+        typer.echo("Unable to retrieve the head commit for the experiment.")
+        typer.echo("Are you sure the experiment path belongs to a git repository?")
+        sys.exit(e.returncode)
+    return head_commit
