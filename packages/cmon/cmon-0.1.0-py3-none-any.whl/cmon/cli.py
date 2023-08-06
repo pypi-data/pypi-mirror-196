99655fb4d9d7ba17aec9e4654b33bfcc0e39f06a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:59 +0000

    Add package cmon/0.1.0/cmon-0.1.0-py3-none-any.whl

---
+++ packages/cmon/cmon-0.1.0-py3-none-any.whl/cmon/cli.py
@@ -0,0 +1,29 @@
+"""
+cmon
+"""
+
+import os
+import time
+import click
+
+@click.command()
+@click.option('--file', '-f', help='Name of the file', required=True)
+def main(file):
+    os.system('clear')
+    print('Start...')
+    s_i = os.path.getsize(file)
+
+    while True:
+        s_n = os.path.getsize(file)
+        if s_i != s_n:
+            # run prog
+            os.system('clear')
+            print(f'Running: {file} - {s_i} bytes.')
+            print(f'>>>\n')
+            os.system(f'python3 {file}')
+        s_i = os.path.getsize(file)
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
