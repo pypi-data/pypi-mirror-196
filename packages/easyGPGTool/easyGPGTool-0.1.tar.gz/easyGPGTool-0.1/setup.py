733b290a916c3a7eba585464fb0a978fde89e77a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:39 +0000

    Add package easyGPGTool/0.1/easyGPGTool-0.1.tar.gz

---
+++ packages/easyGPGTool/easyGPGTool-0.1.tar.gz/easyGPGTool-0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup,find_packages
+with open('README.md','r')as f:
+    long_description = f.read()
+setup(
+    name='easyGPGTool',
+    version='0.1',
+    packages=['easyGPGTool'],
+    include_package_data=True,
+    long_description_content_type = 'text/x-rst',
+    description='A GUI GPG tool application',
+    long_description = long_description,
+    author='Mehdi Ghazanfari',
+    author_email='mehdirexon@gmail.com',
+    url = 'https://github.com/MehdiREXON/easyGPGTool',
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.1",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Security :: Cryptography"
+    ],
+    install_requires=[
+        'plyer',
+        'colorama',
+        'python-gnupg',
+        'python-magic',
+        'python-dateutil',
+        'PySide6'
+    ],
+    python_requires = ">=3.10"
+)
