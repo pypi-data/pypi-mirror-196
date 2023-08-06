0e5164d9ace08ebedc40f0c600738f6219672624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:04 +0000

    Add package airbyte/0.3.3/airbyte-0.3.3.tar.gz

---
+++ packages/airbyte/airbyte-0.3.3.tar.gz/airbyte-0.3.3/src/airbyte/sdk.py
@@ -0,0 +1,62 @@
+import requests as requests_http
+from . import utils
+from .connections import Connections
+from .root import Root
+
+SERVERS = [
+	"https://api.airbyte.com/",
+]
+
+class Airbyte:
+    connections: Connections
+    root: Root
+    
+    _client: requests_http.Session
+    _security_client: requests_http.Session
+    
+    _server_url: str = SERVERS[0]
+    _language: str = "python"
+    _sdk_version: str = "0.3.3"
+    _gen_version: str = "1.8.6"
+
+    def __init__(self) -> None:
+        self._client = requests_http.Session()
+        self._security_client = requests_http.Session()
+        self._init_sdks()
+
+    def config_server_url(self, server_url: str, params: dict[str, str] = None):
+        if params is not None:
+            self._server_url = utils.template_url(server_url, params)
+        else:
+            self._server_url = server_url
+
+        self._init_sdks()
+    
+    
+
+    def config_client(self, client: requests_http.Session):
+        self._client = client
+        self._init_sdks()
+    
+    
+    
+    def _init_sdks(self):
+        self.connections = Connections(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+        self.root = Root(
+            self._client,
+            self._security_client,
+            self._server_url,
+            self._language,
+            self._sdk_version,
+            self._gen_version
+        )
+        
+    
