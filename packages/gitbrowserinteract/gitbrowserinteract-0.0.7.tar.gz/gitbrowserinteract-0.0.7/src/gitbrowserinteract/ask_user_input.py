2cc68e65c8f85df9d9bc58f5e9c440273d23191d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:38 +0000

    Add package gitbrowserinteract/0.0.7/gitbrowserinteract-0.0.7.tar.gz

---
+++ packages/gitbrowserinteract/gitbrowserinteract-0.0.7.tar.gz/gitbrowserinteract-0.0.7/src/gitbrowserinteract/ask_user_input.py
@@ -0,0 +1,24 @@
+"""Asks user input through CLI."""
+from typeguard import typechecked
+
+
+@typechecked
+def ask_two_factor_code():
+    """Asks for the 2fac.
+
+    TODO: make save, hide code instead of displaying on terminal.
+    """
+    two_fac_code = get_input(
+        text="Please enter the two factor authentication you just received:"
+    )
+    return two_fac_code
+
+
+@typechecked
+def get_input(*, text):
+    """
+
+    :param text:
+
+    """
+    return input(text)
