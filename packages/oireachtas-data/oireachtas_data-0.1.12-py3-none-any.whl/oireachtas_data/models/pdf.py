647512fe0fbb3163f05dff29388a03a9790d0aca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:43 +0000

    Add package oireachtas-data/0.1.12/oireachtas_data-0.1.12-py3-none-any.whl

---
+++ packages/oireachtas-data/oireachtas_data-0.1.12-py3-none-any.whl/oireachtas_data/models/pdf.py
@@ -0,0 +1,34 @@
+import os
+
+from oireachtas_data.models.new_pdf import PDF as NEW_PDF
+from oireachtas_data.models.old_pdf import PDF as OLD_PDF
+
+
+def to_text(fp):
+    text_file = fp + ".txt"
+
+    if not os.path.exists(text_file):
+        with open(fp, "rb") as f:
+            os.system(f"pdftotext '{fp}' '{text_file}'")
+
+    if not os.path.exists(text_file):
+        print("Could not convert pdf to text: %s" % (text_file))
+        return
+
+    f = open(text_file, "r")
+    data = f.read()
+    f.close()
+
+    return data
+
+
+def pdf_parser_get(pdf_location):
+    content = to_text(pdf_location)
+
+    if content is None:
+        print("Could not parse pdf type: %s" % (pdf_location))
+        return
+
+    if "��������������" in content:
+        return NEW_PDF
+    return OLD_PDF
---
+++ packages/oireachtas-data/oireachtas_data-0.1.12-py3-none-any.whl/oireachtas_data/models/speech.py
@@ -0,0 +1,58 @@
+from oireachtas_data.models.para import Para
+
+from oireachtas_data import members
+
+
+class Speech:
+    __slots__ = ("_by", "_as", "eid", "paras")
+
+    def __init__(self, by=None, _as=None, eid=None, paras=None):
+        """
+
+        :kwarg by: The name / identifier of the speaker
+        :kwarg _as: The title / position of the speaker
+        :kwarf eid: incrementing id of the speech
+        :kwarg paras: list of Para objects
+        """
+        self._by = by
+        self._as = _as
+        self.eid = eid
+
+        if paras is not None and paras != []:
+            types = list(set([type(p) for p in paras]))
+            assert len(types) == 1
+            assert types[0] == Para
+
+        self.paras = paras if paras else []
+
+    @property
+    def by(self):
+        if isinstance(self._by, str) and self._by.startswith("#"):
+            return self._by.replace("#", "")
+
+        return self._by
+
+    @staticmethod
+    def parse(data):
+        return Speech(
+            by=data["by"],
+            _as=data["as"],
+            eid=data["eid"],
+            paras=[Para.parse(p) for p in data["paras"]],
+        )
+
+    def serialize(self):
+        return {
+            "by": self.by,
+            "as": self._as,
+            "eid": self.eid,
+            "paras": [p.serialize() for p in self.paras],
+        }
+
+    @property
+    def content(self):
+        return "\n".join(["\n" + para.content for para in self.paras])
+
+    @property
+    def member_obj(self):
+        return members.get_member(self.by)
