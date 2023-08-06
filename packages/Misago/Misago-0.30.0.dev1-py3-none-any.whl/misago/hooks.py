26710860215a5526761c6103ebfc62af6a694363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:12 +0000

    Add package Misago/0.30.0.dev1/Misago-0.30.0.dev1-py3-none-any.whl

---
+++ packages/Misago/Misago-0.30.0.dev1-py3-none-any.whl/misago/hooks.py
@@ -0,0 +1,12 @@
+apipatterns = []
+urlpatterns = []
+context_processors = []
+
+new_registrations_validators = []
+oauth2_user_data_filters = []
+
+post_search_filters = []
+post_validators = []
+
+markdown_extensions = []
+parsing_result_processors = []
