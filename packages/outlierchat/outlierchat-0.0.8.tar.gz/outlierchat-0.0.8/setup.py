16b51abc7efcea56b586ccd170f0558175f7d93f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:46 +0000

    Add package outlierchat/0.0.8/outlierchat-0.0.8.tar.gz

---
+++ packages/outlierchat/outlierchat-0.0.8.tar.gz/outlierchat-0.0.8/setup.py
@@ -0,0 +1,29 @@
+'''
+Date: 2023-02-26 11:12:35
+LastEditors: ThetisEliza wxf199601@gmail.com
+LastEditTime: 2023-03-10 02:41:47
+FilePath: /outlier/setup.py
+'''
+import setuptools
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+    
+setuptools.setup(
+    name="outlierchat",
+    version="0.0.8",
+    author="Thetis",
+    author_email="736396627@qq.com",
+    description="A simple server to build a secret channel for chat.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages("src"),
+    classifers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>3.6'
+)    
