6aac4177a188bfe94dfb6c2ef4c63b6a69781ff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:11 +0000

    Add package promformat/1.0.1/promformat-1.0.1.tar.gz

---
+++ packages/promformat/promformat-1.0.1.tar.gz/promformat-1.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="promformat",
+    version="1.0.1",
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=["antlr4-python3-runtime>=4.10"],
+    tests_require=["pytest", "PyYAML"],
+    setup_requires=["flake8", "black"],
+    entry_points={
+        "console_scripts": [
+            "promformat = promformat.__main__:main",
+        ],
+    },
+)
