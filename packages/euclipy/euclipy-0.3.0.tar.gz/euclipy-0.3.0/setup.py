34388c2362d253d411377a8bd0da33808e041eac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:07 +0000

    Add package euclipy/0.3.0/euclipy-0.3.0.tar.gz

---
+++ packages/euclipy/euclipy-0.3.0.tar.gz/euclipy-0.3.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+  name='euclipy',
+  version='0.3.0',
+  description='A Euclidean Geometry based library',
+  long_description=open('README.md').read(),
+  long_description_content_type='text/markdown',
+  url='',
+  project_urls={
+            #"Bug Tracker": "",
+            "Documentation": "https://joshuavaron.github.io/euclipy/",
+            "Source Code": "https://github.com/joshuavaron/euclipy",
+        },
+  author='Joshua Varon',    
+  author_email='32440072+joshuavaron@users.noreply.github.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='geometry, math',
+  packages=find_packages(include=['euclipy']),
+  install_requires=['sympy'] 
+)
