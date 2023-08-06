eafc9ac30a0de2488f8c0331cc473900d91f0995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:07 +0000

    Add package pyMEGA/0.0.5/pyMEGA-0.0.5-py3-none-any.whl

---
+++ packages/pyMEGA/pyMEGA-0.0.5-py3-none-any.whl/MEGA/__init__.py
@@ -0,0 +1,16 @@
+"""
+MEGA initialization module.
+"""
+import logging
+
+from .log import create_logging_handler
+
+LOGGER = logging.getLogger(__name__)
+# Set logging level.
+LOGGING_DEBUG_OPT = False
+LOGGER.addHandler(create_logging_handler(LOGGING_DEBUG_OPT))
+LOGGER.setLevel(logging.DEBUG)
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
---
+++ packages/pyMEGA/pyMEGA-0.0.5-py3-none-any.whl/MEGA/lib/__init__.py
@@ -0,0 +1,10 @@
+"""
+Library initialization module.
+"""
+
+
+def greet(name: str) -> str:
+    """
+    Greet the user.
+    """
+    return f"pyMEGA Loaded. {name}"
