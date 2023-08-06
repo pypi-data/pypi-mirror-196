e4b7c61c65f5085566d08f661f0a24581d35c49a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:00 +0000

    Add package BookerPubTool/2023.2.9.6/BookerPubTool-2023.2.9.6.tar.gz

---
+++ packages/BookerPubTool/BookerPubTool-2023.2.9.6.tar.gz/BookerPubTool-2023.2.9.6/BookerPubTool/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""pypi-book-publisher
+https://github.com/apachecn/pypi-book-publisher"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "SATA"
+__version__ = "2023.2.9.6"
