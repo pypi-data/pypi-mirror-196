0a758dbad806f322e746db63c7b806917709d546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:55 +0000

    Add package vermils/0.0.3/vermils-0.0.3.tar.gz

---
+++ packages/vermils/vermils-0.0.3.tar.gz/vermils-0.0.3/vermils/__init__.py
@@ -0,0 +1,20 @@
+"""Vermils' Python Library"""
+
+from . import (
+    asynctools,
+    collections,
+    gadgets,
+    io,
+    react,
+    tensorflow,
+)
+
+__author__: str = "VermiIIi0n"
+__all__ = (
+    "asynctools",
+    "collections",
+    "gadgets",
+    "io",
+    "react",
+    "tensorflow",
+)
