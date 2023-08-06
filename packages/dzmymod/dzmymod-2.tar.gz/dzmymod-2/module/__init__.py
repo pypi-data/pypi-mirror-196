612f724ee3c08f6485d83372c147c97de1880d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:14 +0000

    Add package dzmymod/2/dzmymod-2.tar.gz

---
+++ packages/dzmymod/dzmymod-2.tar.gz/dzmymod-2/module/__init__.py
@@ -0,0 +1,1 @@
+from dzmymod import *
---
+++ packages/dzmymod/dzmymod-2.tar.gz/dzmymod-2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='dzmymod',
+    version='2',
+    description='kk',
+    author='Vadim Laukonin',
+    packages=find_packages(),
+    install_requires=[
+        # 0 requries
+    ],
+)
