4c9a97efc0d52423b8f010e3832e2483225b52c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:13 +0000

    Add package fresh9/0.0.5/fresh9-0.0.5.tar.gz

---
+++ packages/fresh9/fresh9-0.0.5.tar.gz/fresh9-0.0.5/fresh9/clubhouse.py
@@ -0,0 +1,21 @@
+"""
+:authors: Peopl3s
+:license: Apache License, Version 2.0, see LICENSE file
+
+:copyright: (c) 2021 Peopl3s
+"""
+
+import json
+import logging
+import re
+import time
+import json
+import aiohttp
+import asyncio
+
+
+class Machine():
+    def str1(self):
+        return "bruh1"
+    def str2(self):
+        return "bruh2"
