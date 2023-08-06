04827999972e4d0c78e327599008d7a17785a8c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:12 +0000

    Add package alphatools-jv/2.2.0/alphatools_jv-2.2.0.tar.gz

---
+++ packages/alphatools-jv/alphatools_jv-2.2.0.tar.gz/alphatools_jv-2.2.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+__version__ = "2.2.0"
+
+setuptools.setup(
+    name="alphatools_jv",
+    version=__version__,
+    author="Jaskirat Singh",
+    author_email="jaskiratsingh1208@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jaskirat1208/backtest-platform",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
